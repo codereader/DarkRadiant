1e9d6b4e4651cb84f22ac4f9ee5b8666992a09bb
Matthew Mott <orbweaver3d@gmail.com>
2006-05-14 19:35:52 +0000

    Linux home dir changed to .darkradiant

--- radiant/environment.cpp
+++ radiant/environment.cpp
@@ -138,7 +138,7 @@ void environment_init(int argc, char* argv[])
 
   {
     StringOutputStream home(256);
-    home << DirectoryCleaned(g_get_home_dir()) << ".radiant/";
+    home << DirectoryCleaned(g_get_home_dir()) << ".darkradiant/";
     Q_mkdir(home.c_str());
     home_path = home.c_str();
   }
