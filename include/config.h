550b5c727ceb6303d3b5253db73351db435a95ec
Matthew Mott <orbweaver3d@gmail.com>
2006-05-23 19:29:37 +0000

    Added config.h for compile-time configuration options

---
+++ include/config.h
@@ -0,0 +1,20 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+/* This file defines compile-time configuration options for building DarkRadiant
+ */
+
+/* This option specifies that the new Entity Inspector should be used instead of
+ * the legacy GtkRadiant dialog
+ */
+ 
+#define USE_NEW_ENTITY_INSPECTOR
+
+
+/* Enable this option to force the Entity Inspector to use Text property editors
+ * for all key types
+ */
+
+#define ALL_PROPERTY_EDITORS_ARE_TEXT
+
+#endif /*CONFIG_H_*/
