d40ba8df018e0b394fda6a6f94fab95cf8510707
Matthew Mott <orbweaver3d@gmail.com>
2006-06-06 19:08:49 +0000

    EntityInspector: added etched outline for TreeView

--- radiant/dialog/einspector/EntityInspector.cpp
+++ radiant/dialog/einspector/EntityInspector.cpp
@@ -169,6 +169,7 @@ GtkWidget* EntityInspector::createTreeViewPane() {
     // Embed the TreeView in a scrolled viewport
     GtkWidget* scrollWin = gtk_scrolled_window_new(NULL, NULL);
     gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrollWin), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+	gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(scrollWin), GTK_SHADOW_ETCHED_IN);
     gtk_widget_set_size_request(scrollWin, TREEVIEW_MIN_WIDTH, TREEVIEW_MIN_HEIGHT);
     gtk_container_add(GTK_CONTAINER(scrollWin), _treeView);    
 
