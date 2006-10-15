abdc3007a9875e923e5c58a42d41b271d722d19c
Matthew Mott <orbweaver3d@gmail.com>
2006-10-15 15:02:23 +0000

    MediaBrowser creates tree structure

--- radiant/ui/mediabrowser/MediaBrowser.cpp
+++ radiant/ui/mediabrowser/MediaBrowser.cpp
@@ -10,8 +10,10 @@
 #include <gtk/gtkscrolledwindow.h>
 
 #include <iostream>
+#include <ext/hash_map>
 
 #include <boost/algorithm/string/predicate.hpp>
+#include <boost/functional/hash/hash.hpp>
 
 namespace ui
 {
@@ -61,6 +63,45 @@ namespace {
 		ShaderNameFunctor(GtkTreeStore* store)
 		: _store(store) {}
 		
+		// Map between string directory names and their corresponding Iters
+		typedef __gnu_cxx::hash_map<std::string, GtkTreeIter*, boost::hash<std::string> > DirIterMap;
+		DirIterMap _dirIterMap;
+
+		// Recursive function to add a folder (e.g. "textures/common/something") to the
+		// tree, returning the GtkTreeIter* pointing to the newly-added folder. All 
+		// parent folders ("textures/common", "textures/") will be added automatically
+		// and their iters cached for fast lookup.
+		GtkTreeIter* addFolder(const std::string& pathName) {
+
+			// Lookup pathname in map, and return the GtkTreeIter* if it is
+			// found
+			DirIterMap::iterator iTemp = _dirIterMap.find(pathName);
+			if (iTemp != _dirIterMap.end()) { // found in map
+				return iTemp->second;
+			}
+			
+			// Split the path into "this directory" and the parent path
+			unsigned int slashPos = pathName.rfind("/");
+			const std::string parentPath = pathName.substr(0, slashPos);
+			const std::string thisDir = pathName.substr(slashPos + 1);
+
+			// Recursively add parent path
+			GtkTreeIter* parIter = NULL;
+			if (slashPos != std::string::npos)
+				parIter = addFolder(parentPath);
+
+			// Now add "this directory" as a child, saving the iter in the map
+			// and returning it.
+			GtkTreeIter iter;
+			gtk_tree_store_append(_store, &iter, parIter);
+			gtk_tree_store_set(_store, &iter, 0, thisDir.c_str(), -1);
+			GtkTreeIter* dynIter = gtk_tree_iter_copy(&iter); // get a heap-allocated iter
+			
+			// Cache the dynamic iter and return it
+			_dirIterMap[pathName] = dynIter;
+			return dynIter;
+		}
+		
 		// Functor operator
 		
 		void operator() (const char* name) {
@@ -68,9 +109,17 @@ namespace {
 			
 			// If the name starts with "textures/", add it to the treestore.
 			if (boost::algorithm::istarts_with(rawName, "textures/")) {
+				// Separate path into the directory path and texture name
+				unsigned int slashPos = rawName.rfind("/");
+				const std::string dirPath = rawName.substr(0, slashPos);
+				const std::string texName = rawName.substr(slashPos + 1);
+
+				// Recursively add the directory path
+				GtkTreeIter* parentIter = addFolder(dirPath);
+				
 				GtkTreeIter iter;
-				gtk_tree_store_append(_store, &iter, NULL);
-				gtk_tree_store_set(_store, &iter, 0, name, -1);
+				gtk_tree_store_append(_store, &iter, parentIter);
+				gtk_tree_store_set(_store, &iter, 0, texName.c_str(), -1);
 			}
 		}
 		
