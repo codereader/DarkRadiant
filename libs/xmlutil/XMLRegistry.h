ea2cbdddc84194762febf5bca07889e5806cf69f
codereader <greebo@angua.at>
2006-10-16 07:11:29 +0000

    XMLRegistry code cleanup and documentation, tagged internal functions as private.

--- libs/xmlutil/XMLRegistry.h
+++ libs/xmlutil/XMLRegistry.h
@@ -6,6 +6,23 @@
 #include "Document.h"
 #include "Node.h"
 
+/*	This is the central XMLRegistry structure providing easy methods to store all kinds of
+ *  information like globals, toolbar structures and anything that fits into an XML file. 
+ * 
+ * 	The base structure is automatically created on instantiation and the class in turn
+ *  is also created automatically on inclusion of the source file (.cpp)
+ *   
+ *  Example: store a global variable:
+ *  	xmlRegistry.setXmlRegistry("globals/ui/showalllightradii", "1");
+ * 
+ *  Example: retrieve a global variable 
+ *  (this returns "" if the key is not found and an error is written to globalOutputStream):
+ *  	std::string value = xmlRegistry.getXmlRegistry("globals/ui/showalllightradii");
+ * 
+ *  Example: import an XML file into the registry:
+ * 		xmlRegistry.addXmlFile(absolute_path_to_file); 
+ */
+
 namespace xml {
 
 // Constants used in this class
@@ -23,20 +40,25 @@ class XMLRegistry {
 	public:
 		XMLRegistry();
 		
-		//xml::Document getXmlRegistry();
+		// Sets a variable in the XMLRegistry or retrieves one
 		void 		setXmlRegistry(const std::string& rawKey, const std::string& value);
 		std::string	getXmlRegistry(const std::string& rawKey);
 		
+		// Checks whether a key exists in the registry
 		bool keyExists(const std::string&);
-		std::string XMLRegistry::preparePath(const std::string&);
-		void insertKey(const std::string&);
 		
+		// Adds a whole XML file to the registry
 		void addXmlFile(const std::string&);
 		
+		// Dumps the whole XML content to std::out for debugging purposes
 		void dumpXmlRegistry() const;
 		
 		// Destructor
-		~XMLRegistry(); 
+		~XMLRegistry();
+		 
+	private:
+		std::string XMLRegistry::preparePath(const std::string&);
+		void insertKey(const std::string&);
 };
 
 } // namespace xml
