10e86456e8dca9b23263417f738f46fd1a8ed4a1
Matthew Mott <orbweaver3d@gmail.com>
2006-10-23 20:02:24 +0000

    Removed references to missing filters.h

--- plugins/entity/generic.cpp
+++ plugins/entity/generic.cpp
@@ -42,7 +42,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "targetable.h"
 #include "origin.h"
 #include "angle.h"
-#include "filters.h"
 #include "namedentity.h"
 #include "keyobservers.h"
 #include "namekeys.h"
