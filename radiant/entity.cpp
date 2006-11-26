26a69f4a68814489260b859192a15eae88e9ec34
Matthew Mott <orbweaver3d@gmail.com>
2006-11-26 14:28:59 +0000

    Removed unused config.h and ClassnamePropertyEditor

--- radiant/entity.cpp
+++ radiant/entity.cpp
@@ -45,8 +45,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "qe3.h"
 #include "commands.h"
 
-#include "config.h"
-
 #include "ui/modelselector/ModelSelector.h"
 
 #include <iostream>
