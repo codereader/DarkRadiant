c3e906b881b94f88b9e0ab8623892e71400ec860
Matthew Mott <orbweaver3d@gmail.com>
2006-12-01 21:05:51 +0000

    Added browse button to SkinPropertyEditor

--- radiant/ui/einspector/SkinPropertyEditor.cpp
+++ radiant/ui/einspector/SkinPropertyEditor.cpp
@@ -1,9 +1,11 @@
 #include "SkinPropertyEditor.h"
 
 #include <gtk/gtkhbox.h>
+#include <gtk/gtkvbox.h>
 #include <gtk/gtkentry.h>
 #include <gtk/gtkscrolledwindow.h>
 #include <gtk/gtklabel.h>
+#include <gtk/gtkbutton.h>
 
 namespace ui
 {
@@ -25,9 +27,18 @@ SkinPropertyEditor::SkinPropertyEditor(Entity* entity,
 	_textEntry = gtk_entry_new();
 	gtk_box_pack_start(GTK_BOX(hbx), _textEntry, TRUE, TRUE, 0);
 	
-	// Pack box into edit frame
+	GtkWidget* browseButton = gtk_button_new_with_label("...");
+	g_signal_connect(G_OBJECT(browseButton), 
+					 "clicked", 
+					 G_CALLBACK(_onBrowseButton), 
+					 this);
+	gtk_box_pack_start(GTK_BOX(hbx), browseButton, FALSE, FALSE, 0);
+	
+	// Pack hbox into vbox (to limit vertical size), then edit frame
+	GtkWidget* vbx = gtk_vbox_new(FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(vbx), hbx, TRUE, FALSE, 0);
 	gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(getEditWindow()),
-										  hbx);
+										  vbx);
 	
 }
 
@@ -39,6 +50,14 @@ void SkinPropertyEditor::setValue(const std::string& val) {
 // Return the value in the widgets
 const std::string SkinPropertyEditor::getValue() {
 	return gtk_entry_get_text(GTK_ENTRY(_textEntry));
+}
+
+/* GTK CALLBACKS */
+
+void SkinPropertyEditor::_onBrowseButton(GtkWidget* w, 
+										 SkinPropertyEditor* self)
+{
+	
 }
 
 }
