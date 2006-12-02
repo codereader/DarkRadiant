1dd0f35f4eddd8f04993615233b5b6be5887ff9f
codereader <greebo@angua.at>
2006-12-02 20:25:39 +0000

    Moved part of user.xml into colours.xml and upgradepaths.xml.

--- radiant/mainframe.cpp
+++ radiant/mainframe.cpp
@@ -605,6 +605,8 @@ void Radiant_Initialise()
 	
 	// Load default values for darkradiant, located in the game directory
 	GlobalRegistry().importFromFile(std::string(AppPath_get()) + "user.xml", "");
+	GlobalRegistry().importFromFile(std::string(AppPath_get()) + "upgradepaths.xml", "user");
+	GlobalRegistry().importFromFile(std::string(AppPath_get()) + "colours.xml", "user/ui");
 	
 	// Traverse the game files stored in the GamesDialog class and load them into the registry
 	// The information stored in the game files is needed to successfully instantiate the other modules
