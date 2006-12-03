cb8b380445da242079af6be0db8af3e129074507
Matthew Mott <orbweaver3d@gmail.com>
2006-12-03 20:47:11 +0000

    Added Colour and Texture widgets to LightInspector

---
+++ libs/gtkutil/LeftAlignedLabel.h
@@ -0,0 +1,34 @@
+#ifndef LEFTALIGNEDLABEL_H_
+#define LEFTALIGNEDLABEL_H_
+
+namespace gtkutil
+{
+
+/** A GtkLabel that is left-aligned, rather than the default centered align-
+ * ment.
+ */
+
+class LeftAlignedLabel
+{
+	// The label
+	GtkWidget* _label;
+	
+public:
+
+	/** Construct a left-aligned label with the given text.
+	 */
+	LeftAlignedLabel(const std::string& text)
+	: _label(gtk_label_new(text.c_str()))
+	{ }
+	
+	/** Operator cast to GtkWidget*. Left-aligns then returns the GtkLabel.
+	 */
+	operator GtkWidget* () {
+		gtk_misc_set_alignment(GTK_MISC(_label), 0.0, 0.5);
+		return _label;
+	}
+};
+
+}
+
+#endif /*LEFTALIGNEDLABEL_H_*/
