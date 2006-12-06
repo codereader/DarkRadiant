37a8273938ef68f6edb680387e6347757f16d5d9
Matthew Mott <orbweaver3d@gmail.com>
2006-12-06 20:59:04 +0000

    LightInspector uses new LightTextureSelector for full texture browsing capability

--- radiant/ui/common/LightTextureSelector.cpp
+++ radiant/ui/common/LightTextureSelector.cpp
@@ -178,13 +178,17 @@ GtkWidget* LightTextureSelector::createTreeView() {
 					 G_CALLBACK(_onSelChange), 
 					 this);
 
-	// Pack into scrolled window
+	// Pack into scrolled window and frame
 	GtkWidget* scroll = gtk_scrolled_window_new(NULL, NULL);
 	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scroll), 
 									GTK_POLICY_AUTOMATIC, 
 										GTK_POLICY_AUTOMATIC);
 	gtk_container_add(GTK_CONTAINER(scroll), tree);
-	return scroll;
+	
+	GtkWidget* fr = gtk_frame_new(NULL);
+	gtk_container_add(GTK_CONTAINER(fr), scroll);
+	
+	return fr;
 }
 
 // Create the preview panel (GL widget and info table)
