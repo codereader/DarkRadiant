f498e2363b8a00a5375cd29abaa5184c7d876b2f
codereader <greebo@angua.at>
2007-01-06 17:31:05 +0000

    Connected PatchDialog and SurfaceDialog to EventManager.

--- radiant/patchdialog.cpp
+++ radiant/patchdialog.cpp
@@ -621,6 +621,7 @@ GtkWindow* PatchInspector::BuildDialog()
   m_position_tracker.connect(window);
 
   global_accel_connect_window(window);
+  GlobalEventManager().connectDialogWindow(window);
 
   window_connect_focus_in_clear_focus_widget(window);
   
