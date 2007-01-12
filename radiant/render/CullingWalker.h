892ae86da03fe9e34e32f5e65987df69eab2d251
Matthew Mott <orbweaver3d@gmail.com>
2007-01-12 16:44:30 +0000

    Minor cleanup of front-end render classes

--- radiant/render/CullingWalker.h
+++ radiant/render/CullingWalker.h
@@ -1,29 +1,42 @@
 #ifndef CULLINGWALKER_H_
 #define CULLINGWALKER_H_
 
+/**
+ * Walker wrapper class which invokes its child walker for instances which pass
+ * a Cullable::intersectVolume test.
+ */
 template<typename _Walker>
 class CullingWalker
 {
-  const VolumeTest& m_volume;
-  const _Walker& m_walker;
+	const VolumeTest& m_volume;
+	const _Walker& m_walker;
+
 public:
-  CullingWalker(const VolumeTest& volume, const _Walker& walker)
+	
+	// Constructor
+	CullingWalker(const VolumeTest& volume, const _Walker& walker)
     : m_volume(volume), m_walker(walker)
-  {
-  }
-  bool pre(const scene::Path& path, scene::Instance& instance, VolumeIntersectionValue parentVisible) const
-  {
-    VolumeIntersectionValue visible = Cullable_testVisible(instance, m_volume, parentVisible);
-    if(visible != c_volumeOutside)
-    {
-      return m_walker.pre(path, instance);
-    }
-    return true;
-  }
-  void post(const scene::Path& path, scene::Instance& instance, VolumeIntersectionValue parentVisible) const
-  {
-    return m_walker.post(path, instance);
-  }
+	{  }
+  
+	// Pre-descent function
+	bool pre(const scene::Path& path, 
+			 scene::Instance& instance, 
+			 VolumeIntersectionValue parentVisible) const 
+	{
+    	VolumeIntersectionValue visible = Cullable_testVisible(instance, m_volume, parentVisible);
+		if(visible != c_volumeOutside) {
+			return m_walker.pre(path, instance);
+		}
+		return true;
+	}
+  
+  	// Post-descent function
+	void post(const scene::Path& path, 
+			  scene::Instance& instance, 
+			  VolumeIntersectionValue parentVisible) const 
+	{
+    	return m_walker.post(path, instance);
+	}
 };
 
 #endif /*CULLINGWALKER_H_*/
