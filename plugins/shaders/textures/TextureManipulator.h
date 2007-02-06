8670c75483ac2d527d7b29bbd2c2fa63d1fe9d75
codereader <greebo@angua.at>
2007-02-06 13:32:07 +0000

    Hooked TextureManipulator::getProcessImage() method into load() routine. Moved TextureManipulator into namespace shaders.

--- plugins/shaders/textures/TextureManipulator.h
+++ plugins/shaders/textures/TextureManipulator.h
@@ -1,13 +1,15 @@
 #ifndef TEXTUREMANIPULATOR_H_
 #define TEXTUREMANIPULATOR_H_
 
+#include "iimage.h"
 typedef unsigned char byte;
+
+namespace shaders {
 
 class TextureManipulator
 {
 public:
-	TextureManipulator() 
-	{}
+	TextureManipulator();
 	
 	void resampleTexture(const void *indata, int inwidth, int inheight, 
 						 void *outdata, int outwidth, int outheight, int bytesperpixel);
@@ -16,11 +18,18 @@ public:
 				   int width, int height, 
 				   int destwidth, int destheight);
 
+	/* greebo: Returns the readily fabricated pixel data, that passed
+	 * a bunch of stages (gamma calculation, mip reduction, stretching) 
+	 */
+	Image* getProcessedImage(Image* input);
+
 private:
 	
 	void resampleTextureLerpLine(const byte *in, byte *out, 
 								 int inwidth, int outwidth, int bytesperpixel);
 
 }; // class TextureManipulator
+
+} // namespace shaders
 
 #endif /*TEXTUREMANIPULATOR_H_*/
