e0b0875467d1ca9d7981ffca91b3a4e8b1514ee1
codereader <greebo@angua.at>
2007-02-06 20:40:09 +0000

    Added gamma and texture render mode preference handling to TextureManipulator.

--- plugins/shaders/plugin.cpp
+++ plugins/shaders/plugin.cpp
@@ -26,6 +26,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "itextures.h"
 #include "iregistry.h"
 #include "qerplugin.h"
+#include "preferencesystem.h"
 
 #include "modulesystem/singletonmodule.h"
 
@@ -36,7 +37,8 @@ class ShadersDependencies :
 	public GlobalFileSystemModuleRef,
 	public GlobalTexturesModuleRef,
 	public GlobalRadiantModuleRef,
-	public GlobalRegistryModuleRef 
+	public GlobalRegistryModuleRef,
+	public GlobalPreferenceSystemModuleRef 
 {};
 
 class ShadersDoom3API
--- plugins/shaders/textures/TextureManipulator.h
+++ plugins/shaders/textures/TextureManipulator.h
@@ -3,19 +3,34 @@
 
 #include "iimage.h"
 #include "ishaders.h"
+#include "iregistry.h"
+#include "preferencesystem.h"
+#include "itextures.h"
 typedef unsigned char byte;
 
 namespace shaders {
 
-class TextureManipulator
+class TextureManipulator :
+ 	public RegistryKeyObserver,
+ 	public PreferenceConstructor
 {
 	// The gamma correction table
 	byte _gammaTable[256];
 	
+	// The currently active gamma value (0.0...1.0)
 	float _gamma;
+	
+	// The filtering mode (mipmap_linear and such)
+	ETexturesMode _textureMode;
 	
 public:
 	TextureManipulator();
+	
+	// RegistryKeyObserver implementation
+	void keyChanged();
+
+	// PreferenceConstructor implementation to construct the prefpage
+	void constructPreferencePage(PreferenceGroup& group);
 	
 	void resampleTexture(const void *indata, int inwidth, int inheight, 
 						 void *outdata, int outwidth, int outheight, int bytesperpixel);
@@ -43,6 +58,14 @@ private:
 	// Recalculates the gamma table according to the given gamma value
 	// This is called on first startup or if the user changes the value
 	void calculateGammaTable();
+	
+	/* greebo: Sends the openGL texturemode commands according to the internal
+	 * texture mode member variable. (e.g. MIPMAP_LINEAR)
+	 */
+	void setTextureParameters();
+	
+	// Converts the <int> to the ETexturesMode enumeration
+	ETexturesMode readTextureMode(const unsigned int& mode);
 	
 	void resampleTextureLerpLine(const byte *in, byte *out, 
 								 int inwidth, int outwidth, int bytesperpixel);
