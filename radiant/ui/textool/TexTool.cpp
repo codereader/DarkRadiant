eedca1a3476b62740035b1bc2ae82727041e96dd
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2007-02-18 21:18:19 +0000

    Added cleared GL widget to TexTool.

--- radiant/ui/textool/TexTool.cpp
+++ radiant/ui/textool/TexTool.cpp
@@ -2,8 +2,13 @@
 
 #include "ieventmanager.h"
 #include "iregistry.h"
+
+#include <gtk/gtk.h>
+#include <GL/glew.h>
+
 #include "gtkutil/TransientWindow.h"
 #include "gtkutil/glwidget.h"
+#include "gtkutil/GLWidgetSentry.h"
 #include "mainframe.h"
 
 namespace ui {
@@ -20,8 +25,6 @@ TexTool::TexTool()
 	// Be sure to pass FALSE to the TransientWindow to prevent it from self-destruction
 	_window = gtkutil::TransientWindow(WINDOW_TITLE, MainFrame_getWindow(), false);
 	
-	_glWidget = glwidget_new(TRUE);
-	
 	// Set the default border width in accordance to the HIG
 	gtk_container_set_border_width(GTK_CONTAINER(_window), 12);
 	gtk_window_set_type_hint(GTK_WINDOW(_window), GDK_WINDOW_TYPE_HINT_DIALOG);
@@ -29,7 +32,9 @@ TexTool::TexTool()
 	g_signal_connect(G_OBJECT(_window), "delete-event", G_CALLBACK(onDelete), this);
 	
 	// Register this dialog to the EventManager, so that shortcuts can propagate to the main window
-	GlobalEventManager().connectDialogWindow(GTK_WINDOW(_window));
+	GlobalEventManager().connect(GTK_OBJECT(_window));
+	
+	populateWindow();
 	
 	// Connect the window position tracker
 	xml::NodeList windowStateList = GlobalRegistry().findXPath(RKEY_WINDOW_STATE);
@@ -39,6 +44,22 @@ TexTool::TexTool()
 	}
 	
 	_windowPosition.connect(GTK_WINDOW(_window));
+}
+
+void TexTool::populateWindow() {
+	// Create the GL widget
+	_glWidget = glwidget_new(TRUE);
+	
+	// Connect the events
+	g_signal_connect(G_OBJECT(_glWidget), "expose-event", G_CALLBACK(onExpose), this);
+	
+	// Make the GL widget accept the global shortcuts
+	GlobalEventManager().connect(GTK_OBJECT(_glWidget));
+	
+	GtkWidget* vbox = gtk_vbox_new(true, 0);
+	gtk_box_pack_start(GTK_BOX(vbox), _glWidget, true, true, 0);
+	
+	gtk_container_add(GTK_CONTAINER(_window), vbox);
 }
 
 void TexTool::toggle() {
@@ -67,13 +88,24 @@ void TexTool::shutdown() {
 	// Tell the position tracker to save the information
 	_windowPosition.saveToNode(node);
 	
-	GlobalEventManager().disconnectDialogWindow(GTK_WINDOW(_window));
+	GlobalEventManager().disconnect(GTK_OBJECT(_glWidget));
+	GlobalEventManager().disconnect(GTK_OBJECT(_window));
 }
 
 TexTool& TexTool::Instance() {
 	static TexTool _instance;
 	
 	return _instance;
+}
+
+void TexTool::onExpose(GtkWidget* widget, GdkEventExpose* event, TexTool* self) {
+	// Make the GL widget active
+	gtkutil::GLWidgetSentry sentry(self->_glWidget);
+	
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glDisable(GL_DEPTH_TEST);
+	glDisable(GL_BLEND);
 }
 
 gboolean TexTool::onDelete(GtkWidget* widget, GdkEvent* event, TexTool* self) {
