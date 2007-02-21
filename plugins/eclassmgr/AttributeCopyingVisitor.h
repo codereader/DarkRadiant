ef59ebd047fd636f2e5ee43c6e575ec82d429ab1
Matthew Mott <orbweaver3d@gmail.com>
2007-02-21 11:04:56 +0000

    EntityClasses copy properties from their parent

---
+++ plugins/eclassmgr/AttributeCopyingVisitor.h
@@ -0,0 +1,34 @@
+#ifndef ATTRIBUTECOPYINGVISITOR_H_
+#define ATTRIBUTECOPYINGVISITOR_H_
+
+#include "ieclass.h"
+
+namespace eclass
+{
+
+/**
+ * Visitor class which copies all of the class attributes from one entity onto
+ * another. Used for inheritance resolution.
+ */
+class AttributeCopyingVisitor
+: public EntityClassAttributeVisitor
+{
+	// Target entity to copy values onto
+	IEntityClassPtr _target;
+	
+public:
+
+	// Constructor sets target
+	AttributeCopyingVisitor(IEntityClassPtr target)
+	: _target(target)
+	{ }
+
+	// Required visit function
+	void visit(const EntityClassAttribute& attr) {
+		_target->addAttribute(attr);
+	}
+};
+
+}
+
+#endif /*ATTRIBUTECOPYINGVISITOR_H_*/
