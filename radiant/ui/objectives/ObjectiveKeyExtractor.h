75fb43526f50d71f61ecc03b400c92718f75f9b1
Matthew Mott <orbweaver3d@gmail.com>
2007-02-25 16:57:43 +0000

    Use icons and user-friendly names in tree

--- radiant/ui/objectives/ObjectiveKeyExtractor.h
+++ radiant/ui/objectives/ObjectiveKeyExtractor.h
@@ -1,12 +1,20 @@
 #ifndef OBJECTIVEKEYEXTRACTOR_H_
 #define OBJECTIVEKEYEXTRACTOR_H_
 
-#include <boost/lexical_cast.hpp>
+#include "gtkutil/image.h"
 
+#include <boost/lexical_cast.hpp>
 #include <map>
 
 namespace ui
 {
+
+/* CONSTANTS */
+namespace {
+	
+	const char* OBJ_ICON = "objectives16.png";
+	
+}
 
 /**
  * Entity Visitor which extracts objective keyvalues (of the form "obj<n>_blah")
@@ -19,13 +27,18 @@ class ObjectiveKeyExtractor
 	GtkTreeStore* _store;
 
 	// Map of iters for parent objectives
-	typedef std::map<int, GtkTreeIter*> IterMap; 
+	typedef std::map<int, boost::shared_ptr<GtkTreeIter> > IterMap; 
 	IterMap _parentMap;
+	
+	// Map of display names from objective standard keys (e.g. 
+	// "desc" -> "Description").
+	typedef std::map<std::string, std::string> StringMap;
+	StringMap _displayNames;
 	
 private:
 
 	// Get the iter for a numbered objective, creating it if necessary
-	GtkTreeIter* getParent(int num) {
+	boost::shared_ptr<GtkTreeIter> getParent(int num) {
 		
 		// Locate iter if it exists
 		IterMap::iterator i = _parentMap.find(num);
@@ -33,16 +46,29 @@ private:
 			return i->second;
 			
 		// Otherwise create at the tree root
-		GtkTreeIter* gtkIter = new GtkTreeIter();
+		boost::shared_ptr<GtkTreeIter> gtkIter(new GtkTreeIter());
 		std::string objText = "Objective " 
 							  + boost::lexical_cast<std::string>(num);
 		
-		gtk_tree_store_append(_store, gtkIter, NULL);
-		gtk_tree_store_set(_store, gtkIter, 0, objText.c_str(), -1);
+		gtk_tree_store_append(_store, gtkIter.get(), NULL);
+		gtk_tree_store_set(_store, gtkIter.get(), 
+						   0, objText.c_str(), 
+						   1, "",
+						   2, gtkutil::getLocalPixbuf(OBJ_ICON),
+						   -1);
 				
 		// Add to cache and return
 		_parentMap.insert(IterMap::value_type(num, gtkIter));
 		return gtkIter;
+	}
+	
+	// Get a display name for the given objective standard key.
+	std::string getDisplayName(const std::string& key) {
+		StringMap::iterator i = _displayNames.find(key);
+		if (i != _displayNames.end())
+			return i->second;
+		else
+			return key;		
 	}
 	
 public:
@@ -52,7 +78,15 @@ public:
 	 */
 	ObjectiveKeyExtractor(GtkTreeStore* store)
 	: _store(store)
-	{ }
+	{ 
+		// Populate the display name map.
+		_displayNames["desc"] = "Description";
+		_displayNames["state"] = "State";
+		_displayNames["ongoing"] = "Ongoing";
+		_displayNames["mandatory"] = "Mandatory";
+		_displayNames["visible"] = "Visible";
+		_displayNames["irreversible"] = "Irreversible";
+	}
 	
 	/**
 	 * Required visit function.
@@ -70,12 +104,13 @@ public:
 		int num = boost::lexical_cast<int>(sObjNum);
 
 		// Chop of the "obj1_" prefix to get the standard parameter name
-		std::string objParm = key.substr(uPos + 1);
+		std::string objParm = getDisplayName(key.substr(uPos + 1));
 
 		GtkTreeIter iter;
-		gtk_tree_store_append(_store, &iter, getParent(num));
+		gtk_tree_store_append(_store, &iter, getParent(num).get());
 		gtk_tree_store_set(_store, &iter, 
-						   0, objParm.c_str(), 1, value.c_str(), -1);
+						   0, objParm.c_str(), 
+						   1, value.c_str(), -1);
 	}
 };
 
