e872e84b4a88e7ca4afed2a6b7acbb84d9ad2884
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2007-02-26 13:04:39 +0000

    Separated entity::Doom3GroupNode from doom3group.cpp.

---
+++ plugins/entity/doom3group/Doom3GroupNode.h
@@ -0,0 +1,82 @@
+#ifndef DOOM3GROUPNODE_H_
+#define DOOM3GROUPNODE_H_
+
+#include "Doom3Group.h"
+#include "instancelib.h"
+
+namespace entity {
+
+class Doom3GroupNode :
+	public scene::Node::Symbiot,
+	public scene::Instantiable,
+	public scene::Cloneable,
+	public scene::Traversable::Observer 
+{
+	class TypeCasts {
+		NodeTypeCastTable m_casts;
+	public:
+		TypeCasts() {
+			NodeStaticCast<Doom3GroupNode, scene::Instantiable>::install(m_casts);
+			NodeStaticCast<Doom3GroupNode, scene::Cloneable>::install(m_casts);
+			NodeContainedCast<Doom3GroupNode, scene::Traversable>::install(m_casts);
+			NodeContainedCast<Doom3GroupNode, Snappable>::install(m_casts);
+			NodeContainedCast<Doom3GroupNode, TransformNode>::install(m_casts);
+			NodeContainedCast<Doom3GroupNode, Entity>::install(m_casts);
+			NodeContainedCast<Doom3GroupNode, Nameable>::install(m_casts);
+			NodeContainedCast<Doom3GroupNode, Namespaced>::install(m_casts);
+			NodeContainedCast<Doom3GroupNode, ModelSkin>::install(m_casts);
+		}
+		NodeTypeCastTable& get() {
+			return m_casts;
+		}
+	};
+
+	scene::Node m_node;
+	InstanceSet m_instances;
+	Doom3Group m_contained;
+
+public:
+
+	typedef LazyStatic<TypeCasts> StaticTypeCasts;
+
+	Doom3GroupNode(IEntityClassPtr eclass);
+	Doom3GroupNode(const Doom3GroupNode& other);
+	
+	~Doom3GroupNode();
+
+	// Casts
+	scene::Traversable& get(NullType<scene::Traversable>);
+	Snappable& get(NullType<Snappable>);
+	TransformNode& get(NullType<TransformNode>);
+	Entity& get(NullType<Entity>);
+	Nameable& get(NullType<Nameable>);
+	Namespaced& get(NullType<Namespaced>);
+	ModelSkin& get(NullType<ModelSkin>);
+
+	void release();
+	
+	scene::Node& node();
+
+	scene::Node& clone() const;
+
+	void insert(scene::Node& child);
+	
+	void erase(scene::Node& child);
+
+	scene::Instance* create(const scene::Path& path, scene::Instance* parent);
+	
+	void forEachInstance(const scene::Instantiable::Visitor& visitor);
+	
+	void insert(scene::Instantiable::Observer* observer, const scene::Path& path, scene::Instance* instance);
+	
+	scene::Instance* erase(scene::Instantiable::Observer* observer, const scene::Path& path);
+
+private:
+	void construct();
+	void destroy();
+
+}; // class Doom3GroupNode
+
+} // namespace entity
+
+#endif /*DOOM3GROUPNODE_H_*/
