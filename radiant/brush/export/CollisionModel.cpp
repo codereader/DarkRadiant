c57c6c88183a411fd88b618fd486bf9e4d0075a5
codereader <greebo@angua.at>
2007-03-04 21:05:01 +0000

    Faces are added to the collision model (WIP) / added stream insertion operator.

--- radiant/brush/export/CollisionModel.cpp
+++ radiant/brush/export/CollisionModel.cpp
@@ -5,11 +5,34 @@
 #include "iselection.h"
 #include "selectionlib.h"
 #include "brush/Brush.h"
+#include "winding.h"
 
 namespace selection {
 
 void CollisionModel::addBrush(Brush& brush) {
+	BrushStruc b;
+
+	// The number of faces
+	b.numFaces = brush.size();
+		
+	// Populate the FaceList
+	for (Brush::const_iterator i = brush.begin(); i != brush.end(); i++) {
+		const Winding& winding = (*i)->getWinding();
+		
+		// Check for degenerate winding, just to make sure
+		if (winding.numpoints > 2) {
+			b.point1 = winding[0].vertex;
+			b.point2 = winding[winding.numpoints - 1].vertex;
+		}
+		else {
+			globalErrorStream() << "CollisionModel: Warning: degenerate winding found.\n"; 
+		}
+		
+		b.planes.push_back((*i)->plane3());
+	}
 	
+	// Store the BrushStruc into the list
+	_brushes.push_back(b);
 }
 
 bool CollisionModel::isValid() const {
@@ -31,10 +54,19 @@ void CollisionModel::createFromSelection() {
 			// Add the selected brush to the collision model
 			cm->addBrush(*brushes[i]);
 		}
+		
+		// Output the data of the cm (debug)
+		std::cout << *cm;
 	}
 	else {
 		globalErrorStream() << "Sorry, only brushes can be exported to an CM file.\n";
 	}
 }
-	
+
+// The friend stream insertion operator
+std::ostream& operator<<(std::ostream& st, const CollisionModel& cm) {
+	st << "test";
+	return st;
+}
+
 } // namespace selection
