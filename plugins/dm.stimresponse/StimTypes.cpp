c3f4c45f5f89e1c9108216ca94421ded1a6c269b
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2007-03-15 23:34:29 +0000

    Created a GtkListStore filled with possible stim types.

--- plugins/dm.stimresponse/StimTypes.cpp
+++ plugins/dm.stimresponse/StimTypes.cpp
@@ -2,14 +2,25 @@
 
 #include "iregistry.h"
 #include "string/string.h"
+#include <gtk/gtkliststore.h>
 
 #include <iostream>
 
 	namespace {
 		const std::string RKEY_STIM_DEFINITIONS = "game/stimResponseSystem//stim";
+		
+		enum {
+		  ID_COL,
+		  CAPTION_COL,
+		  NUM_COLS
+		};
 	}
 
 StimTypes::StimTypes() {
+	// Create a new liststore
+	_listStore = gtk_list_store_new(NUM_COLS, G_TYPE_INT, G_TYPE_STRING);
+	
+	// Find all the relevant nodes
 	xml::NodeList stimNodes = GlobalRegistry().findXPath(RKEY_STIM_DEFINITIONS);
 	
 	for (unsigned int i = 0; i < stimNodes.size(); i++) {
@@ -22,7 +33,19 @@ StimTypes::StimTypes() {
 		
 		// Add the stim to the map
 		_stims[id] = newStim;
+		
+		GtkTreeIter iter;
+		
+		gtk_list_store_append(_listStore, &iter);
+		gtk_list_store_set(_listStore, &iter, 
+							ID_COL, id,
+							CAPTION_COL, _stims[id].caption.c_str(),
+							-1);
 	}
+}
+
+StimTypes::operator GtkListStore* () {
+	return _listStore;
 }
 
 Stim StimTypes::get(int id) {
