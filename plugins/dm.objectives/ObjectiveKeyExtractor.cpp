e6361b34523e2b1fc2bff52840973637738a760f
Matthew Mott <orbweaver3d@gmail.com>
2007-03-18 13:50:36 +0000

    Implementation of ObjectiveKeyExtractor moved into separate .cpp file

---
+++ plugins/dm.objectives/ObjectiveKeyExtractor.cpp
@@ -0,0 +1,56 @@
+#include "ObjectiveKeyExtractor.h"
+
+#include <boost/lexical_cast.hpp>
+#include <boost/regex.hpp>
+
+namespace objectives {
+	
+// Required entity visit function
+void ObjectiveKeyExtractor::visit(const std::string& key, 
+								  const std::string& value)
+{
+	// Quick discard of any non-objective keys
+	if (key.substr(0, 3) != "obj")
+		return;
+		
+	// Extract the objective number
+	static const boost::regex reObjNum("obj(\\d+)_(.*)");
+	boost::smatch results;
+	int iNum;
+	
+	if (boost::regex_match(key, results, reObjNum)) {
+		// Get the objective number
+		iNum = boost::lexical_cast<int>(results[1]);			
+	}
+	else {
+		// No match, abort
+		return;
+	}
+
+	// We now have the objective number and the substring (everything after
+	// "obj<n>_" which applies to this objective.
+	std::string objSubString = results[2];
+	
+	// Switch on the substring
+	if (objSubString == "desc") {
+		_objMap[iNum].description = value;			
+	}
+	else if (objSubString == "state") {
+		_objMap[iNum].startActive = (value == "1");			
+	}
+	else if (objSubString == "ongoing") {
+		_objMap[iNum].ongoing = (value == "1");			
+	}
+	else if (objSubString == "mandatory") {
+		_objMap[iNum].mandatory = (value == "1");			
+	}
+	else if (objSubString == "visible") {
+		_objMap[iNum].visible = (value == "1");			
+	}
+	else if (objSubString == "irreversible") {
+		_objMap[iNum].irreversible = (value == "1");			
+	}
+
+}
+	
+} // namespace objectives
--- plugins/dm.objectives/ObjectiveKeyExtractor.h
+++ plugins/dm.objectives/ObjectiveKeyExtractor.h
@@ -3,21 +3,10 @@
 
 #include "Objective.h"
 
-#include "gtkutil/image.h"
-
-#include <boost/lexical_cast.hpp>
-#include <boost/regex.hpp>
-#include <map>
+#include "ientity.h"
 
 namespace objectives
 {
-
-/* CONSTANTS */
-namespace {
-	
-	const char* OBJ_ICON = "objectives16.png";
-	
-}
 
 /**
  * Entity Visitor which extracts objective keyvalues (of the form "obj<n>_blah")
@@ -43,50 +32,7 @@ public:
 	/**
 	 * Required visit function.
 	 */
-	void visit(const std::string& key, const std::string& value) {
-		
-		// Quick discard of any non-objective keys
-		if (key.substr(0, 3) != "obj")
-			return;
-			
-		// Extract the objective number
-		static const boost::regex reObjNum("obj(\\d+)_(.*)");
-		boost::smatch results;
-		int iNum;
-		
-		if (boost::regex_match(key, results, reObjNum)) {
-			// Get the objective number
-			iNum = boost::lexical_cast<int>(results[1]);			
-		}
-		else {
-			// No match, abort
-			return;
-		}
-
-		// We now have the objective number and the substring (everything after
-		// "obj<n>_" which applies to this objective.
-		std::string objSubString = results[2];
-		
-		// Switch on the substring
-		if (objSubString == "desc") {
-			_objMap[iNum].description = value;			
-		}
-		else if (objSubString == "state") {
-			_objMap[iNum].startActive = (value == "1");			
-		}
-		else if (objSubString == "ongoing") {
-			_objMap[iNum].ongoing = (value == "1");			
-		}
-		else if (objSubString == "mandatory") {
-			_objMap[iNum].mandatory = (value == "1");			
-		}
-		else if (objSubString == "visible") {
-			_objMap[iNum].visible = (value == "1");			
-		}
-		else if (objSubString == "irreversible") {
-			_objMap[iNum].irreversible = (value == "1");			
-		}
-	}
+	void visit(const std::string& key, const std::string& value);
 	
 };
 
