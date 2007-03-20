c1b43b7513c0b287f941d1974a7f0dc46bc7341e
codereader <greebo@angua.at>
2007-03-20 13:10:52 +0000

    Removed legacy GameDescription/GameDialog classes.

--- radiant/settings/GameFileLoader.cpp
+++ radiant/settings/GameFileLoader.cpp
@@ -1,48 +1,6 @@
 #include "GameFileLoader.h"
 
-#include <list>
-#include "xmlutil/Document.h"
-#include "itextstream.h"
-#include "iregistry.h"
-#include <iostream>
-
-#include "GameDescription.h"
 #include <boost/algorithm/string/predicate.hpp>
-
-// Constructor
-GameFileLoader::GameFileLoader(std::list<GameDescription*>& games, const char *path) :
-	mGames(games),
-	mPath(path) 
-{}
-
-// Main functor () function
-void GameFileLoader::operator() (const char *name) const {
-	// Only operate on .game files
-	if (!extension_equal(path_get_extension(name), "game")) {
-		return;
-	}
-
-	// Debug output
-	std::string strPath = mPath;
-	strPath += name;
-	globalOutputStream() << strPath.c_str() << '\n';
-
-	// Handle the XML file via libxml2
-	xmlDocPtr pDoc = xmlParseFile(strPath.c_str());
-
-	if (pDoc) {
-		// Import the game file into the registry
-		//GlobalRegistry().import(strPath, "", Registry::treeUser);
-		
-		// Parse success, add to list
-		std::string fileName = name;
-		mGames.push_front(new GameDescription(xml::Document(pDoc), fileName));
-	}
-	else {
-		// Error
-		globalErrorStream() << "XML parser failed on '" << strPath.c_str() << "'\n";
-	}
-}
 
 namespace game {
 
--- radiant/settings/GameFileLoader.h
+++ radiant/settings/GameFileLoader.h
@@ -1,30 +1,6 @@
 #ifndef GAMEFILELOADER_H_
 #define GAMEFILELOADER_H_
 
-#include <list>
-#include "os/path.h"
-class GameDescription;
-
-// Functor object responsible for parsing and loading an individual .game
-// file. (greebo: LEGACY, this is about to removed)
-
-class GameFileLoader 
-{
-	// Reference to external list of valid game descriptions. See the
-	// GameDescription decl for more info.
-	std::list<GameDescription*>& mGames;
-
-	// Path to the game file to be added
-	const char *mPath;
-
-public:
-	// Constructor
-	GameFileLoader(std::list<GameDescription*>& games, const char *path);
-
-	// Main functor () function, gets called with the file (without path)
-	void operator() (const char *name) const;
-};
-
 #include "GameManager.h"
 
 namespace game {
