21271e244a8c8752ca0426847dba819dc9c09e14
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2007-04-06 19:24:48 +0000

    S/R Editor: created new base class for the editors to hold all the shared methods and GTK callbacks.

---
+++ plugins/dm.stimresponse/ClassEditor.cpp
@@ -0,0 +1,11 @@
+#include "ClassEditor.h"
+
+#include <gtk/gtk.h>
+
+namespace ui {
+
+void ClassEditor::setEntity(SREntityPtr entity) {
+	_entity = entity; 
+}
+
+} // namespace ui
--- plugins/dm.stimresponse/StimEditor.cpp
+++ plugins/dm.stimresponse/StimEditor.cpp
@@ -1,8 +1,31 @@
 #include "StimEditor.h"
 
 #include <gtk/gtk.h>
+#include "gtkutil/LeftAlignment.h"
+#include "gtkutil/LeftAlignedLabel.h"
+#include "gtkutil/ScrolledFrame.h"
+#include "gtkutil/TreeModel.h"
+
+#include "SREntity.h"
 
 namespace ui {
+
+	namespace {
+		const std::string LABEL_STIM_LIST = "<b>Stims</b>";
+		const unsigned int TREE_VIEW_WIDTH = 220;
+		const unsigned int TREE_VIEW_HEIGHT = 200;
+		
+		static void textCellDataFunc(GtkTreeViewColumn* treeColumn,
+							 GtkCellRenderer* cell,
+							 GtkTreeModel* treeModel,
+							 GtkTreeIter* iter, 
+							 gpointer data)
+		{
+			bool inherited = gtkutil::TreeModel::getBoolean(treeModel, iter, INHERIT_COL);
+			
+			g_object_set(G_OBJECT(cell), "sensitive", !inherited, NULL);
+		}
+	}
 
 StimEditor::StimEditor() {
 	populatePage();
@@ -14,6 +37,97 @@ StimEditor::operator GtkWidget*() {
 
 void StimEditor::populatePage() {
 	_pageVBox = gtk_vbox_new(FALSE, 6);
+	
+	// Stim/Response list section
+    gtk_box_pack_start(GTK_BOX(_pageVBox), 
+    				   gtkutil::LeftAlignedLabel(LABEL_STIM_LIST), 
+    				   FALSE, FALSE, 0);
+	
+	GtkWidget* srHBox = gtk_hbox_new(FALSE, 0);
+	
+	// Pack it into an alignment so that it is indented
+	GtkWidget* srAlignment = gtkutil::LeftAlignment(GTK_WIDGET(srHBox), 18, 1.0); 
+	gtk_box_pack_start(GTK_BOX(_pageVBox), GTK_WIDGET(srAlignment), TRUE, TRUE, 0);
+	
+	_list = gtk_tree_view_new();
+	gtk_widget_set_size_request(_list, TREE_VIEW_WIDTH, TREE_VIEW_HEIGHT);
+	
+	_selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(_list));
+
+	// Connect the signals
+	/*g_signal_connect(G_OBJECT(_selection), "changed", 
+					 G_CALLBACK(onSRSelectionChange), this);
+	g_signal_connect(G_OBJECT(_list), "key-press-event", 
+					 G_CALLBACK(onTreeViewKeyPress), this);
+	g_signal_connect(G_OBJECT(_list), "button-release-event", 
+					 G_CALLBACK(onTreeViewButtonRelease), this);*/
+	
+	// ID number
+	GtkTreeViewColumn* numCol = gtk_tree_view_column_new();
+	gtk_tree_view_column_set_title(numCol, "#");
+	GtkCellRenderer* numRenderer = gtk_cell_renderer_text_new();
+	gtk_tree_view_column_pack_start(numCol, numRenderer, FALSE);
+	gtk_tree_view_column_set_attributes(numCol, numRenderer, 
+										"text", INDEX_COL,
+										NULL);
+	gtk_tree_view_column_set_cell_data_func(numCol, numRenderer,
+                                            textCellDataFunc,
+                                            NULL, NULL);
+	
+	gtk_tree_view_append_column(GTK_TREE_VIEW(_list), numCol);
+	
+	// The S/R icon
+	GtkTreeViewColumn* classCol = gtk_tree_view_column_new();
+	gtk_tree_view_column_set_title(classCol, "S/R");
+	GtkCellRenderer* pixbufRenderer = gtk_cell_renderer_pixbuf_new();
+	gtk_tree_view_column_pack_start(classCol, pixbufRenderer, FALSE);
+	gtk_tree_view_column_set_attributes(classCol, pixbufRenderer, 
+										"pixbuf", CLASS_COL,
+										NULL);
+	gtk_tree_view_column_set_cell_data_func(classCol, pixbufRenderer,
+                                            textCellDataFunc,
+                                            NULL, NULL);
+	
+	gtk_tree_view_append_column(GTK_TREE_VIEW(_list), classCol);
+	
+	// The Type
+	GtkTreeViewColumn* typeCol = gtk_tree_view_column_new();
+	gtk_tree_view_column_set_title(typeCol, "Type");
+	
+	GtkCellRenderer* typeIconRenderer = gtk_cell_renderer_pixbuf_new();
+	gtk_tree_view_column_pack_start(typeCol, typeIconRenderer, FALSE);
+	
+	GtkCellRenderer* typeTextRenderer = gtk_cell_renderer_text_new();
+	gtk_tree_view_column_pack_start(typeCol, typeTextRenderer, FALSE);
+	
+	gtk_tree_view_column_set_attributes(typeCol, typeTextRenderer, 
+										"text", CAPTION_COL,
+										NULL);
+	gtk_tree_view_column_set_cell_data_func(typeCol, typeTextRenderer,
+                                            textCellDataFunc,
+                                            NULL, NULL);
+	
+	gtk_tree_view_column_set_attributes(typeCol, typeIconRenderer, 
+										"pixbuf", ICON_COL,
+										NULL);
+	gtk_tree_view_column_set_cell_data_func(typeCol, typeIconRenderer,
+                                            textCellDataFunc,
+                                            NULL, NULL);
+	
+	gtk_tree_view_append_column(GTK_TREE_VIEW(_list), typeCol);
+	
+	gtk_box_pack_start(GTK_BOX(srHBox), 
+		gtkutil::ScrolledFrame(_list), FALSE, FALSE, 0);
+	
+	//gtk_box_pack_start(GTK_BOX(srHBox), createSRWidgets(), TRUE, TRUE, 6);
+	
+	// Response effects section
+    /*gtk_box_pack_start(GTK_BOX(_pageVBox),
+    				   gtkutil::LeftAlignedLabel(LABEL_RESPONSE_EFFECTS),
+    				   FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(_pageVBox), 
+					   gtkutil::LeftAlignment(createEffectWidgets(), 18, 1.0),
+					   TRUE, TRUE, 0);*/
 }
 
 } // namespace ui
