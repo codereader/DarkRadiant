8a271a5cc4643ef4f39af552d56fa1b6725571d2
codereader <greebo@angua.at>
2007-04-07 07:56:06 +0000

    S/R Editor: Added Response list and connected add/remove buttons (context menu to follow).

--- plugins/dm.stimresponse/ResponseEditor.cpp
+++ plugins/dm.stimresponse/ResponseEditor.cpp
@@ -1,6 +1,7 @@
 #include "ResponseEditor.h"
 
 #include <gtk/gtk.h>
+#include "gtkutil/ScrolledFrame.h"
 
 namespace ui {
 
@@ -14,6 +15,11 @@ void ResponseEditor::setEntity(SREntityPtr entity) {
 	// Pass the call to the base class
 	ClassEditor::setEntity(entity);
 	
+	if (entity != NULL) {
+		GtkListStore* listStore = _entity->getResponseStore();
+		gtk_tree_view_set_model(GTK_TREE_VIEW(_list), GTK_TREE_MODEL(listStore));
+		g_object_unref(listStore); // treeview owns reference now
+	}
 }
 
 void ResponseEditor::update() {
@@ -21,6 +27,17 @@ void ResponseEditor::update() {
 }
 
 void ResponseEditor::populatePage() {
+	GtkWidget* srHBox = gtk_hbox_new(FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(_pageVBox), GTK_WIDGET(srHBox), TRUE, TRUE, 0);
+	
+	// List and buttons below
+	GtkWidget* vbox = gtk_vbox_new(FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(vbox), 
+		gtkutil::ScrolledFrame(_list), TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(vbox), createListButtons(), FALSE, FALSE, 6);
+	
+	gtk_box_pack_start(GTK_BOX(srHBox),	vbox, FALSE, FALSE, 0);
+	
 	// Response effects section
     /*gtk_box_pack_start(GTK_BOX(_pageVBox),
     				   gtkutil::LeftAlignedLabel(LABEL_RESPONSE_EFFECTS),
@@ -30,16 +47,72 @@ void ResponseEditor::populatePage() {
 					   TRUE, TRUE, 0);*/
 }
 
+GtkWidget* ResponseEditor::createListButtons() {
+	GtkWidget* hbox = gtk_hbox_new(TRUE, 6);
+	
+	_listButtons.add = gtk_button_new_with_label("Add new Response");
+	gtk_button_set_image(
+		GTK_BUTTON(_listButtons.add), 
+		gtk_image_new_from_stock(GTK_STOCK_ADD, GTK_ICON_SIZE_BUTTON)
+	);
+	
+	_listButtons.remove = gtk_button_new_with_label("Remove Response");
+	gtk_button_set_image(
+		GTK_BUTTON(_listButtons.remove), 
+		gtk_image_new_from_stock(GTK_STOCK_DELETE, GTK_ICON_SIZE_BUTTON)
+	);
+	
+	gtk_box_pack_start(GTK_BOX(hbox), _listButtons.add, TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(hbox), _listButtons.remove, TRUE, TRUE, 0);
+	
+	g_signal_connect(G_OBJECT(_listButtons.add), "clicked", G_CALLBACK(onAddResponse), this);
+	g_signal_connect(G_OBJECT(_listButtons.remove), "clicked", G_CALLBACK(onRemoveResponse), this);
+	
+	return hbox; 
+}
+
 void ResponseEditor::openContextMenu(GtkTreeView* view) {
 	
 }
 
 void ResponseEditor::removeItem(GtkTreeView* view) {
-	
+	// Check the treeview this remove call is targeting
+	if (view == GTK_TREE_VIEW(_list)) {	
+		// Get the selected stim ID
+		int id = getIdFromSelection();
+		
+		if (id > 0) {
+			_entity->remove(id);
+		}
+	}
 }
 
 void ResponseEditor::selectionChanged() {
 	
+}
+
+void ResponseEditor::addResponse() {
+	if (_entity == NULL) return;
+
+	// Create a new StimResponse object
+	int id = _entity->add();
+	
+	// Get a reference to the newly allocated object
+	StimResponse& sr = _entity->get(id);
+	sr.set("class", "R");
+	sr.set("type", _stimTypes.getFirstName());
+
+	// Refresh the values in the liststore
+	_entity->updateListStores();
+}
+
+void ResponseEditor::onAddResponse(GtkWidget* button, ResponseEditor* self) {
+	self->addResponse();
+}
+
+void ResponseEditor::onRemoveResponse(GtkWidget* button, ResponseEditor* self) {
+	// Delete the selected stim from the list
+	self->removeItem(GTK_TREE_VIEW(self->_list));
 }
 
 } // namespace ui
