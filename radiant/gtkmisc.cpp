6bc03759dddd9b65c6ce0d8400f3e0f78f25fa7f
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2007-04-20 16:36:54 +0000

    Fixed the initial engine path dialog disappearing below other applications. Tweaked the About dialog to be more "stretchable".

--- radiant/gtkmisc.cpp
+++ radiant/gtkmisc.cpp
@@ -1,21 +1,21 @@
 /*
 Copyright (c) 2001, Loki software, inc.
 All rights reserved.
-
+ 
 Redistribution and use in source and binary forms, with or without modification, 
 are permitted provided that the following conditions are met:
-
+ 
 Redistributions of source code must retain the above copyright notice, this list 
 of conditions and the following disclaimer.
-
+ 
 Redistributions in binary form must reproduce the above copyright notice, this
 list of conditions and the following disclaimer in the documentation and/or
 other materials provided with the distribution.
-
+ 
 Neither the name of Loki software nor the names of its contributors may be used 
 to endorse or promote products derived from this software without specific prior 
 written permission. 
-
+ 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' 
 AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
 IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
@@ -45,28 +45,32 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // =============================================================================
 // File dialog
 
-void button_clicked_entry_browse_file(GtkWidget* widget, GtkEntry* entry)
-{
-  std::string filename = file_dialog(gtk_widget_get_toplevel(widget), TRUE, "Choose File", gtk_entry_get_text(entry));
-  
-  if(!filename.empty())
-  {
-    gtk_entry_set_text(entry, filename.c_str());
-  }
+void button_clicked_entry_browse_file(GtkWidget* widget, GtkEntry* entry) {
+	std::string filename = file_dialog(gtk_widget_get_toplevel(widget), TRUE, "Choose File", gtk_entry_get_text(entry));
+
+	if (GTK_IS_WINDOW(gtk_widget_get_toplevel(widget))) {
+		gtk_window_present(GTK_WINDOW(gtk_widget_get_toplevel(widget)));
+	}
+
+	if(!filename.empty()) {
+		gtk_entry_set_text(entry, filename.c_str());
+	}
 }
 
-void button_clicked_entry_browse_directory(GtkWidget* widget, GtkEntry* entry)
-{
-  const char* text = gtk_entry_get_text(entry);
-  char *dir = dir_dialog(gtk_widget_get_toplevel(widget), "Choose Directory", path_is_absolute(text) ? text : "" );
-  
-  if(dir != 0)
-  {
-    gchar* converted = g_filename_to_utf8(dir, -1, 0, 0, 0);
-    gtk_entry_set_text(entry, converted);
-    g_free(dir);
-    g_free(converted);
-  }
+void button_clicked_entry_browse_directory(GtkWidget* widget, GtkEntry* entry) {
+	const char* text = gtk_entry_get_text(entry);
+	char *dir = dir_dialog(gtk_widget_get_toplevel(widget), "Choose Directory", path_is_absolute(text) ? text : "" );
+
+	if (GTK_IS_WINDOW(gtk_widget_get_toplevel(widget))) {
+		gtk_window_present(GTK_WINDOW(gtk_widget_get_toplevel(widget)));
+	}
+
+	if(dir != 0) {
+		gchar* converted = g_filename_to_utf8(dir, -1, 0, 0, 0);
+		gtk_entry_set_text(entry, converted);
+		g_free(dir);
+		g_free(converted);
+	}
 }
 
 
