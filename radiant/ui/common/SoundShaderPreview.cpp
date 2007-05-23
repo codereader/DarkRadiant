c7aacb8051a43196c0977ceb9192f363998d93a1
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2007-05-23 14:39:27 +0000

    SoundShader preview class gets notified about sound shader selection changes.

--- radiant/ui/common/SoundShaderPreview.cpp
+++ radiant/ui/common/SoundShaderPreview.cpp
@@ -2,6 +2,7 @@
 
 #include "gtkutil/ScrolledFrame.h"
 #include <gtk/gtk.h>
+#include <iostream>
 
 namespace ui {
 
@@ -18,17 +19,21 @@ SoundShaderPreview::SoundShaderPreview() :
 	
 	gtk_box_pack_start(GTK_BOX(_widget), gtkutil::ScrolledFrame(_treeView), TRUE, TRUE, 0);
 	gtk_box_pack_start(GTK_BOX(_widget), gtk_label_new("Test"), FALSE, FALSE, 0);
+	
+	// Trigger the initial update of the widgets
+	update();
 }
 
 void SoundShaderPreview::setSoundShader(const std::string& soundShader) {
 	_soundShader = soundShader;
-	
 	update();
 }
 
 void SoundShaderPreview::update() {
 	// Clear the current treeview model
 	gtk_tree_view_set_model(GTK_TREE_VIEW(_treeView), NULL);
+	
+	gtk_widget_set_sensitive(_widget, !_soundShader.empty()); 
 }
 
 SoundShaderPreview::operator GtkWidget*() {
