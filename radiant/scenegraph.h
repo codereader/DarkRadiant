a8267f44ea2780ed1842513440bd660b7a5b5e09
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2007-05-24 14:56:10 +0000

    Separated CompiledGraph declaration from its definition.

--- radiant/scenegraph.h
+++ radiant/scenegraph.h
@@ -22,7 +22,77 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #if !defined(INCLUDEDE_SCENEGRAPH_H)
 #define INCLUDED_SCENEGRAPH_H
 
+#include <map>
+#include <list>
+#include "signal/signal.h"
+#include "scenelib.h"
+
 struct GraphTreeModel;
 GraphTreeModel* scene_graph_get_tree_model();
+
+/** greebo: This is the actual implementation of the scene::Graph
+ * 			defined in iscenegraph.h. This keeps track of all
+ * 			the instances.
+ */
+class CompiledGraph : 
+	public scene::Graph, 
+	public scene::Instantiable::Observer
+{
+	typedef std::map<PathConstReference, scene::Instance*> InstanceMap;
+	InstanceMap m_instances;
+
+	typedef std::list<scene::Graph::Observer*> ObserverList;
+	ObserverList _sceneObservers;
+
+	// This is the associated graph tree model (used for the EntityList)
+	GraphTreeModel* _treeModel;
+	
+	Signal0 m_boundsChanged;
+	scene::Path m_rootpath;
+
+public:
+	// Constructor
+	CompiledGraph();
+	
+	// Destructor
+	~CompiledGraph();
+	
+	GraphTreeModel* getTreeModel();
+  
+	/** greebo: Adds/removes an observer from the scenegraph,
+	 * 			to get notified upon insertions/deletions
+	 */
+	void addSceneObserver(scene::Graph::Observer* observer);
+	void removeSceneObserver(scene::Graph::Observer* observer);
+
+	// Triggers a call to all the connected Scene::Graph::Observers
+	void sceneChanged();
+
+	// Root node accessor methods
+	scene::Node& root();
+	void insert_root(scene::Node& root);
+	void erase_root();
+
+	// greebo: Emits the "bounds changed" signal to all connected observers
+	// Note: these are the WorkZone and the SelectionSystem, AFAIK
+	void boundsChanged();
+
+	void traverse(const Walker& walker);
+	void traverse_subgraph(const Walker& walker, const scene::Path& start);
+
+	scene::Instance* find(const scene::Path& path);
+
+	void insert(scene::Instance* instance);
+	void erase(scene::Instance* instance);
+
+	SignalHandlerId addBoundsChangedCallback(const SignalHandler& boundsChanged);
+	void removeBoundsChangedCallback(SignalHandlerId id);
+
+private:
+	bool pre(const Walker& walker, const InstanceMap::iterator& i);
+	void post(const Walker& walker, const InstanceMap::iterator& i);
+
+	void traverse_subgraph(const Walker& walker, InstanceMap::iterator i);
+}; // class CompiledGraph
 
 #endif
