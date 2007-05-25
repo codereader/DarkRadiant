2bfe6f9616f27377d6831391755c05d7a98947c1
codereader <greebo@angua.at>
2007-05-25 10:06:00 +0000

    Removed unused accessor methods and cleaned up the code a bit.

--- radiant/namespace/NamespaceModule.cpp
+++ radiant/namespace/NamespaceModule.cpp
@@ -4,29 +4,14 @@
 #include "modulesystem/moduleregistry.h"
 
 NamespaceAPI::NamespaceAPI() :
-	_defaultNamespace(new Namespace())
+	_namespace(new Namespace())
 {}
 
 INamespace* NamespaceAPI::getTable() {
 	// Return the pointer contained in the shared_ptr object
-	return _defaultNamespace.get();
+	return _namespace.get();
 }
 
 typedef SingletonModule<NamespaceAPI> NamespaceModule;
 typedef Static<NamespaceModule> StaticNamespaceModule;
 StaticRegisterModule staticRegisterDefaultNamespace(StaticNamespaceModule::instance());
-
-Namespace& getDefaultNamespace() {
-	// Retrieve the contained object from the static module
-	INamespace* base = static_cast<INamespace*>(StaticNamespaceModule::instance().getTable());
-	// Downcast it to Namespace 
-	Namespace* ns = dynamic_cast<Namespace*>(base);
-	// This must succeed
-	assert(ns);
-	return *ns;
-}
-
-Namespace& getClonedNamespace() {
-	static Namespace _clonedNamespace;
-	return _clonedNamespace;
-}
