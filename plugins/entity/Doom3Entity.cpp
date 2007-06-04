f32bdb812229ae21ce4609cad03236304a9aeb70
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2007-06-04 09:57:31 +0000

    Migrated Doom3Entity storage from UnsortedSet and PooledString to std::vector<KeyValuePair>, this time it works.

--- plugins/entity/Doom3Entity.cpp
+++ plugins/entity/Doom3Entity.cpp
@@ -22,8 +22,11 @@ Doom3Entity::Doom3Entity(const Doom3Entity& other) :
 	m_observerMutex(false),
 	m_isContainer(other.m_isContainer)
 {
-	for(KeyValues::const_iterator i = other.m_keyValues.begin(); i != other.m_keyValues.end(); ++i) {
-		insert((*i).first.c_str(), (*i).second->c_str());
+	for (KeyValues::const_iterator i = other.m_keyValues.begin(); 
+		 i != other.m_keyValues.end(); 
+		 ++i)
+	{
+		insert(i->first, i->second->get());
 	}
 }
 
@@ -46,8 +49,8 @@ void Doom3Entity::importState(const KeyValues& keyValues) {
 		erase(i++);
 	}
 
-	for(KeyValues::const_iterator i = keyValues.begin(); i != keyValues.end(); ++i) {
-		insert((*i).first.c_str(), (*i).second);
+	for (KeyValues::const_iterator i = keyValues.begin(); i != keyValues.end(); ++i) {
+		insert(i->first, i->second);
 	}
 
 	m_entityKeyValueChanged();
@@ -57,7 +60,7 @@ void Doom3Entity::attach(Observer& observer) {
 	ASSERT_MESSAGE(!m_observerMutex, "observer cannot be attached during iteration");
 	m_observers.insert(&observer);
 	for(KeyValues::const_iterator i = m_keyValues.begin(); i != m_keyValues.end(); ++i) {
-		observer.insert((*i).first.c_str(), *(*i).second);
+		observer.insert(i->first.c_str(), *i->second);
 	}
 }
 
@@ -65,24 +68,23 @@ void Doom3Entity::detach(Observer& observer) {
 	ASSERT_MESSAGE(!m_observerMutex, "observer cannot be detached during iteration");
 	m_observers.erase(&observer);
 	for(KeyValues::const_iterator i = m_keyValues.begin(); i != m_keyValues.end(); ++i) {
-		observer.erase((*i).first.c_str(), *(*i).second);
+		observer.erase(i->first.c_str(), *i->second);
 	}
 }
 
 void Doom3Entity::forEachKeyValue_instanceAttach(MapFile* map) {
 	for(KeyValues::const_iterator i = m_keyValues.begin(); i != m_keyValues.end(); ++i) {
-		(*i).second->instanceAttach(map);
+		i->second->instanceAttach(map);
 	}
 }
 
 void Doom3Entity::forEachKeyValue_instanceDetach(MapFile* map) {
 	for(KeyValues::const_iterator i = m_keyValues.begin(); i != m_keyValues.end(); ++i) {
-		(*i).second->instanceDetach(map);
+		i->second->instanceDetach(map);
 	}
 }
 
 void Doom3Entity::instanceAttach(MapFile* map) {
-	
 	GlobalRadiant().getCounter(counterEntities).increment();
 	
 	m_instanced = true;
@@ -106,7 +108,7 @@ IEntityClassConstPtr Doom3Entity::getEntityClass() const {
 
 void Doom3Entity::forEachKeyValue(Visitor& visitor) const {
 	for(KeyValues::const_iterator i = m_keyValues.begin(); i != m_keyValues.end(); ++i) {
-		visitor.visit((*i).first.c_str(), (*i).second->c_str());
+		visitor.visit(i->first, i->second->get());
 	}
 }
 
@@ -114,11 +116,14 @@ void Doom3Entity::forEachKeyValue(Visitor& visitor) const {
  */
 void Doom3Entity::setKeyValue(const std::string& key, const std::string& value) {
 	if (value.empty()) {
-		erase(key.c_str());
+		// Empty value means: delete the key
+		erase(key);
 	}
 	else {
-		insert(key.c_str(), value.c_str());
+		// Non-empty value, "insert" it (will overwrite existing keys - no duplicates)
+		insert(key, value);
 	}
+	// Notify the global observer (usually the EntityInspector)
 	m_entityKeyValueChanged();
 }
 
@@ -127,7 +132,7 @@ void Doom3Entity::setKeyValue(const std::string& key, const std::string& value) 
 std::string Doom3Entity::getKeyValue(const std::string& key) const {
 
 	// Lookup the key in the map
-	KeyValues::const_iterator i = m_keyValues.find(key.c_str());
+	KeyValues::const_iterator i = find(key);
 
 	// If key is found, return it, otherwise lookup the default value on
 	// the entity class
@@ -147,71 +152,115 @@ void Doom3Entity::setIsContainer(bool isContainer) {
 	m_isContainer = isContainer;
 }
 
-void Doom3Entity::notifyInsert(const char* key, KeyValue& value) {
+void Doom3Entity::notifyInsert(const std::string& key, KeyValue& value) {
 	m_observerMutex = true;
-	for(Observers::iterator i = m_observers.begin(); i != m_observers.end(); ++i) {
-		(*i)->insert(key, value);
+	for (Observers::iterator i = m_observers.begin(); i != m_observers.end(); ++i) {
+		(*i)->insert(key.c_str(), value);
 	}
 	m_observerMutex = false;
 }
 
-void Doom3Entity::notifyErase(const char* key, KeyValue& value) {
+void Doom3Entity::notifyErase(const std::string& key, KeyValue& value) {
 	m_observerMutex = true;
 	for(Observers::iterator i = m_observers.begin(); i != m_observers.end(); ++i) {
-		(*i)->erase(key, value);
+		(*i)->erase(key.c_str(), value);
 	}
 	m_observerMutex = false;
 }
 
 void Doom3Entity::forEachKeyValue_notifyInsert() {
 	for(KeyValues::const_iterator i = m_keyValues.begin(); i != m_keyValues.end(); ++i) {
-		notifyInsert((*i).first.c_str(), *(*i).second);
+		notifyInsert(i->first, *i->second);
 	}
 }
 
 void Doom3Entity::forEachKeyValue_notifyErase() {
 	for(KeyValues::const_iterator i = m_keyValues.begin(); i != m_keyValues.end(); ++i) {
-		notifyErase((*i).first.c_str(), *(*i).second);
+		notifyErase(i->first, *i->second);
 	}
 }
 
-void Doom3Entity::insert(const char* key, const KeyValuePtr& keyValue) {
-	KeyValues::iterator i = m_keyValues.insert(KeyValues::value_type(key, keyValue));
-	notifyInsert(key, *(*i).second);
+void Doom3Entity::insert(const std::string& key, const KeyValuePtr& keyValue) {
+	// Insert the new key at the end of the list
+	KeyValues::iterator i = m_keyValues.insert(
+		m_keyValues.end(), 
+		KeyValuePair(key, keyValue)
+	);
+	// Dereference the iterator to get a KeyValue& reference and notify the observers
+	notifyInsert(key, *i->second);
 
-	if(m_instanced) {
-		(*i).second->instanceAttach(m_undo.map());
+	if (m_instanced) {
+		i->second->instanceAttach(m_undo.map());
 	}
 }
 
-void Doom3Entity::insert(const char* key, const char* value) {
-	KeyValues::iterator i = m_keyValues.find(key);
-	if(i != m_keyValues.end()) {
-		(*i).second->assign(value);
+void Doom3Entity::insert(const std::string& key, const std::string& value) {
+	// Try to lookup the key in the map
+	KeyValues::iterator i = find(key);
+	
+	if (i != m_keyValues.end()) {
+		// Key has been found
+		i->second->assign(value);
 	}
 	else {
+		// No key with that name found, create a new one
 		m_undo.save();
+		// Allocate a new KeyValue object and insert it into the map
 		insert(key, KeyValuePtr(new KeyValue(value, m_eclass->getValueForKey(key))));
 	}
 }
 
 void Doom3Entity::erase(KeyValues::iterator i) {
-	if(m_instanced) {
-		(*i).second->instanceDetach(m_undo.map());
+	if (m_instanced) {
+		i->second->instanceDetach(m_undo.map());
 	}
 
-	Key key((*i).first);
-	KeyValuePtr value((*i).second);
+	// Retrieve the key and value from the vector before deletion
+	std::string key(i->first);
+	KeyValuePtr value(i->second);
+	// Actually delete the object from the list
 	m_keyValues.erase(i);
-	notifyErase(key.c_str(), *value);
+	
+	// Notify about the deletion
+	notifyErase(key, *value);
+	// Scope ends here, the KeyValue object will be deleted automatically
+	// as the boost::shared_ptr useCount will reach zero.
 }
 
-void Doom3Entity::erase(const char* key) {
-	KeyValues::iterator i = m_keyValues.find(key);
-	if(i != m_keyValues.end()) {
+void Doom3Entity::erase(const std::string& key) {
+	// Try to lookup the key
+	KeyValues::iterator i = find(key);
+	
+	if (i != m_keyValues.end()) {
 		m_undo.save();
 		erase(i);
 	}
+}
+
+Doom3Entity::KeyValues::const_iterator Doom3Entity::find(const std::string& key) const {
+	for (KeyValues::const_iterator i = m_keyValues.begin(); 
+		 i != m_keyValues.end(); 
+		 i++)
+	{
+		if (i->first == key) {
+			return i;
+		}
+	}
+	// Not found
+	return m_keyValues.end();
+}
+
+Doom3Entity::KeyValues::iterator Doom3Entity::find(const std::string& key) {
+	for (KeyValues::iterator i = m_keyValues.begin(); 
+		 i != m_keyValues.end(); 
+		 i++)
+	{
+		if (i->first == key) {
+			return i;
+		}
+	}
+	// Not found
+	return m_keyValues.end();
 }
 
 } // namespace entity
