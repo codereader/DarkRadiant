1047233c65d241963446c3b03a992cf036a5c91c
Matthew Mott <orbweaver3d@gmail.com>
2007-06-17 13:31:33 +0000

    MD5Models use Display Lists for improved rendering performance

--- plugins/md5model/MD5Surface.cpp
+++ plugins/md5model/MD5Surface.cpp
@@ -1,5 +1,7 @@
 #include "MD5Surface.h"
 
+#include "GLProgramAttributes.h"
+
 namespace md5
 {
 
@@ -8,46 +10,108 @@ inline VertexPointer vertexpointer_arbitrarymeshvertex(const ArbitraryMeshVertex
   return VertexPointer(VertexPointer::pointer(&array->vertex), sizeof(ArbitraryMeshVertex));
 }
 
-// Refresh AABB
-void MD5Surface::updateAABB() {
+// Constructor
+MD5Surface::MD5Surface()
+: _shaderName(""),
+  _normalList(0),
+  _lightingList(0)
+{ }
+
+// Destructor
+MD5Surface::~MD5Surface() {
+	
+	// Release GL display lists
+	glDeleteLists(_normalList, 1);
+	glDeleteLists(_lightingList, 1);	
+}
+
+// Update geometry
+void MD5Surface::updateGeometry() {
 	m_aabb_local = AABB();
-	for(vertices_t::iterator i = m_vertices.begin(); i != m_vertices.end(); ++i)
+	for (vertices_t::iterator i = _vertices.begin(); 
+		 i != _vertices.end(); 
+		 ++i)
 	  m_aabb_local.includePoint(reinterpret_cast<const Vector3&>(i->vertex));
 	
-	for(MD5Surface::indices_t::iterator i = m_indices.begin(); i != m_indices.end(); i += 3)
+	for (MD5Surface::indices_t::iterator i = _indices.begin(); 
+		 i != _indices.end(); 
+		 i += 3)
 	{
-			ArbitraryMeshVertex& a = m_vertices[*(i + 0)];
-			ArbitraryMeshVertex& b = m_vertices[*(i + 1)];
-			ArbitraryMeshVertex& c = m_vertices[*(i + 2)];
+			ArbitraryMeshVertex& a = _vertices[*(i + 0)];
+			ArbitraryMeshVertex& b = _vertices[*(i + 1)];
+			ArbitraryMeshVertex& c = _vertices[*(i + 2)];
 	
 	  ArbitraryMeshTriangle_sumTangents(a, b, c);
 	}
 	
-	for(MD5Surface::vertices_t::iterator i = m_vertices.begin(); i != m_vertices.end(); ++i)
+	for (MD5Surface::vertices_t::iterator i = _vertices.begin(); 
+		 i != _vertices.end(); 
+		 ++i)
 	{
 	  vector3_normalise(reinterpret_cast<Vector3&>((*i).tangent));
 	  vector3_normalise(reinterpret_cast<Vector3&>((*i).bitangent));
 	}
+	
+	// Build the display lists
+	createDisplayLists();
 }
 
 // Back-end render
-void MD5Surface::render(RenderStateFlags state) const
-{
-  if((state & RENDER_BUMP) != 0)
-  {
-      glVertexAttribPointerARB(11, 3, GL_DOUBLE, 0, sizeof(ArbitraryMeshVertex), &m_vertices.data()->normal);
-      glVertexAttribPointerARB(8, 2, GL_DOUBLE, 0, sizeof(ArbitraryMeshVertex), &m_vertices.data()->texcoord);
-      glVertexAttribPointerARB(9, 3, GL_DOUBLE, 0, sizeof(ArbitraryMeshVertex), &m_vertices.data()->tangent);
-      glVertexAttribPointerARB(10, 3, GL_DOUBLE, 0, sizeof(ArbitraryMeshVertex), &m_vertices.data()->bitangent);
-  }
-  else
-  {
-    glNormalPointer(GL_DOUBLE, sizeof(ArbitraryMeshVertex), &m_vertices.data()->normal);
-    glTexCoordPointer(2, GL_DOUBLE, sizeof(ArbitraryMeshVertex), &m_vertices.data()->texcoord);
-  }
-  glVertexPointer(3, GL_DOUBLE, sizeof(ArbitraryMeshVertex), &m_vertices.data()->vertex);
-  glDrawElements(GL_TRIANGLES, GLsizei(m_indices.size()), RenderIndexTypeID, m_indices.data());
+void MD5Surface::render(RenderStateFlags state) const {
+	if (state & RENDER_BUMP) {
+		glCallList(_lightingList);
+	}
+	else {
+		glCallList(_normalList);
+	}
+}
 
+// Construct the display lists
+void MD5Surface::createDisplayLists()
+{
+	// Create the list for lighting mode
+	_lightingList = glGenLists(1);
+	glNewList(_lightingList, GL_COMPILE);
+	
+	glBegin(GL_TRIANGLES);
+	for (indices_t::const_iterator i = _indices.begin();
+		 i != _indices.end();
+		 ++i)
+	{
+		// Get the vertex for this index
+		ArbitraryMeshVertex& v = _vertices[*i];
+
+		// Submit the vertex attributes and coordinate
+		glVertexAttrib2dvARB(ATTR_TEXCOORD, v.texcoord);
+		glVertexAttrib3dvARB(ATTR_TANGENT, v.tangent);
+		glVertexAttrib3dvARB(ATTR_BITANGENT, v.bitangent);
+		glVertexAttrib3dvARB(ATTR_NORMAL, v.normal);		
+		glVertex3dv(v.vertex);	
+	}
+	glEnd();
+	
+	glEndList();
+	
+	// Generate the list for flat-shaded (unlit) mode
+	_normalList = glGenLists(1);
+	glNewList(_normalList, GL_COMPILE);
+	
+	glBegin(GL_TRIANGLES);
+	for (indices_t::const_iterator i = _indices.begin();
+		 i != _indices.end();
+		 ++i)
+	{
+		// Get the vertex for this index
+		ArbitraryMeshVertex& v = _vertices[*i];
+
+		// Submit attributes
+		glNormal3dv(v.normal);
+		glTexCoord2dv(v.texcoord);
+		glVertex3dv(v.vertex);	
+	}
+	glEnd();
+	
+	glEndList();
 }
 
 // Selection test
@@ -59,8 +123,8 @@ void MD5Surface::testSelect(Selector& selector,
 	
 	SelectionIntersection best;
 	test.TestTriangles(
-	  vertexpointer_arbitrarymeshvertex(m_vertices.data()),
-	  IndexPointer(m_indices.data(), IndexPointer::index_type(m_indices.size())),
+	  vertexpointer_arbitrarymeshvertex(_vertices.data()),
+	  IndexPointer(_indices.data(), IndexPointer::index_type(_indices.size())),
 	  best
 	);
 
