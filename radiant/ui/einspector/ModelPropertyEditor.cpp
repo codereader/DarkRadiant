63b81b679b494b5a2d411d672701933440b160d9
Matthew Mott <orbweaver3d@gmail.com>
2007-07-22 19:46:58 +0000

    Added "Choose particle" button to ModelPropertyEditor

--- radiant/ui/einspector/ModelPropertyEditor.cpp
+++ radiant/ui/einspector/ModelPropertyEditor.cpp
@@ -2,7 +2,9 @@
 #include "PropertyEditorFactory.h"
 
 #include "ui/modelselector/ModelSelector.h"
+
 #include "ientity.h"
+#include "iradiant.h"
 
 #include <gtk/gtk.h>
 
@@ -21,7 +23,7 @@ ModelPropertyEditor::ModelPropertyEditor(Entity* entity,
 	GtkWidget* hbx = gtk_hbox_new(FALSE, 3);
 	gtk_container_set_border_width(GTK_CONTAINER(hbx), 3);
 	
-	// Browse button
+	// Browse button for models
 	GtkWidget* browseButton = gtk_button_new_with_label("Choose model...");
 	gtk_button_set_image(
 		GTK_BUTTON(browseButton),
@@ -29,12 +31,22 @@ ModelPropertyEditor::ModelPropertyEditor(Entity* entity,
 			PropertyEditorFactory::getPixbufFor("model")
 		)
 	);
-			
 	g_signal_connect(G_OBJECT(browseButton), 
 					 "clicked", 
 					 G_CALLBACK(_onBrowseButton), 
 					 this);
 	gtk_box_pack_start(GTK_BOX(hbx), browseButton, TRUE, FALSE, 0);
+	
+			
+	// Browse button for particles
+	GtkWidget* particleButton = gtk_button_new_with_label("Choose particle...");
+	gtk_button_set_image(
+		GTK_BUTTON(particleButton),
+		gtk_image_new_from_pixbuf(
+			GlobalRadiant().getLocalPixbuf("particle16.png")
+		)
+	);
+	gtk_box_pack_start(GTK_BOX(hbx), particleButton, TRUE, FALSE, 0);
 	
 	// Pack hbox into vbox (to limit vertical size), then edit frame
 	GtkWidget* vbx = gtk_vbox_new(FALSE, 0);
