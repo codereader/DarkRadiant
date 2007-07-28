9b937558111cc9562b13d003a62435c5665eb1b9
Matthew Mott <orbweaver3d@gmail.com>
2007-07-28 08:42:12 +0000

    Set correct window properties, added main list view

--- radiant/ui/particles/ParticlesChooser.cpp
+++ radiant/ui/particles/ParticlesChooser.cpp
@@ -1,5 +1,10 @@
 #include "ParticlesChooser.h"
 
+#include "mainframe.h"
+#include "gtkutil/TextColumn.h"
+#include "gtkutil/ScrolledFrame.h"
+
+
 #include <gtk/gtk.h>
 
 namespace ui
@@ -8,10 +13,46 @@ namespace ui
 // Create GTK widgets
 ParticlesChooser::ParticlesChooser()
 : _widget(gtk_window_new(GTK_WINDOW_TOPLEVEL)),
+  _particlesList(gtk_list_store_new(1, G_TYPE_STRING)),
   _selectedParticle("")
 {
+	// Set up main window
+	gtk_window_set_transient_for(GTK_WINDOW(_widget), MainFrame_getWindow());
+	gtk_window_set_modal(GTK_WINDOW(_widget), TRUE);
+	gtk_window_set_title(GTK_WINDOW(_widget), "Choose particles");
+    gtk_window_set_position(GTK_WINDOW(_widget), GTK_WIN_POS_CENTER_ON_PARENT);
 	g_signal_connect(G_OBJECT(_widget), "delete-event", 
 					 G_CALLBACK(_onDestroy), this);
+	
+	// Set the default size of the window
+	GdkScreen* scr = gtk_window_get_screen(GTK_WINDOW(_widget));
+	gint w = gdk_screen_get_width(scr);
+	gint h = gdk_screen_get_height(scr);
+	
+	gtk_window_set_default_size(GTK_WINDOW(_widget), w / 3, h / 2);
+	
+	// Main dialog vbox
+	GtkWidget* vbx = gtk_vbox_new(FALSE, 12);
+	gtk_box_pack_start(GTK_BOX(vbx), createTreeView(), TRUE, TRUE, 0);
+	
+	// Add main vbox to dialog
+	gtk_container_set_border_width(GTK_CONTAINER(_widget), 12);
+	gtk_container_add(GTK_CONTAINER(_widget), vbx);	
+}
+
+// Create the tree view
+GtkWidget* ParticlesChooser::createTreeView() {
+	
+	GtkWidget* tv = 
+		gtk_tree_view_new_with_model(GTK_TREE_MODEL(_particlesList));
+	
+	// Single text column
+	gtk_tree_view_append_column(GTK_TREE_VIEW(tv),
+								gtkutil::TextColumn("Particle", 0));
+	
+	// Pack into scrolled window and return
+	return gtkutil::ScrolledFrame(tv);
+	
 }
 
 // Static instance owner
