13be6205372d7739ae3d345eb41c551c7f289377
Matthew Mott <orbweaver3d@gmail.com>
2007-11-23 11:58:51 +0000

    Image types now loaded from standard registry XPath rather than space-separated attribute on the <game> node.

--- plugins/shaders/textures/DefaultConstructor.cpp
+++ plugins/shaders/textures/DefaultConstructor.cpp
@@ -2,31 +2,68 @@
 
 #include "ifilesystem.h"
 #include "iarchive.h"
+#include "iregistry.h"
+
+#include <boost/algorithm/string/case_conv.hpp>
 
 namespace shaders {
 
+namespace {
+
+	// Registry key holding texture types
+	const char* RKEY_IMAGE_TYPES = "game/filetypes/texture//extension";
+
+}
+
 // The default case uses the file extensions specified in the .game file
 DefaultConstructor::DefaultConstructor(const std::string& filename) :
-	_imageLoaders(
-		ImageLoaderManager::getLoaders(
-			GlobalRadiant().getRequiredGameDescriptionKeyValue("texturetypes")
-		)
-	),
 	_filename(filename)
-{}
+{ }
+
+// Static accessor for the list of .game-defined ImageLoaders
+const ImageLoaderList& DefaultConstructor::getImageLoaders() {
+
+	static ImageLoaderList _imageLoaders;
+	
+	if (_imageLoaders.empty()) 
+	{
+		// Load the texture types from the .game file
+		xml::NodeList texTypes = GlobalRegistry().findXPath(RKEY_IMAGE_TYPES);
+		for (xml::NodeList::const_iterator i = texTypes.begin();
+			 i != texTypes.end();
+			 ++i)
+		{
+			// Get the file extension
+			std::string extension = i->getContent();
+			boost::algorithm::to_upper(extension);
+			
+			// Attempt to obtain an ImageLoader for this extension
+			ImageLoaderPtr loader = GlobalImageLoader(extension);
+			if (loader) {
+				_imageLoaders.push_back(loader);
+			}
+		}
+	}
+	
+	return _imageLoaders;
+}
 
 // Load the actual image on demand
 ImagePtr DefaultConstructor::construct() {
 
 	ImagePtr returnValue;
 
-	for (unsigned int i = 0; i < _imageLoaders.size(); i++) {
-		ImageLoader& loader = *_imageLoaders[i];
+	const ImageLoaderList& loaders = getImageLoaders();
+	for (ImageLoaderList::const_iterator i = loaders.begin();
+		 i != loaders.end();
+		 ++i) 
+	{
+		ImageLoaderPtr ldr = *i;
 		
 		// Construct the full name of the image to load, including the 
 		// prefix (e.g. "dds/") and the file extension.
-		std::string fullName = loader.getPrefix() + _filename + "." 
-							   + loader.getExtension();
+		std::string fullName = ldr->getPrefix() + _filename + "." 
+							   + ldr->getExtension();
 		
 		// Try to open the file (will fail if the extension does not fit)
 		ArchiveFile* file = GlobalFileSystem().openFile(fullName.c_str());
@@ -35,7 +72,7 @@ ImagePtr DefaultConstructor::construct() {
 		if (file != NULL) {
 			// Try to invoke the imageloader with a reference to the 
 			// ArchiveFile
-			returnValue = loader.load(*file);
+			returnValue = ldr->load(*file);
 			
 			// Release the loaded file
 			file->release();
