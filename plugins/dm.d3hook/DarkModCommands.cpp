f02a1c1914cfce9e4da54c319b5b758ca4fcf7ed
codereader <greebo@angua.at>
2007-11-23 11:04:19 +0000

    Added RunAAS command, refactored all commands into static class members.

--- plugins/dm.d3hook/DarkModCommands.cpp
+++ plugins/dm.d3hook/DarkModCommands.cpp
@@ -9,23 +9,41 @@
 
 #include <boost/algorithm/string/predicate.hpp>
 
-namespace darkmodcommands {
-
-// Compiles the map (including or excluding AAS)
-void CompileMap(bool noAAS) {
+std::string DarkModCommands::getMapName() {
 	// Get the map name
 	// Note: this is a temporary way to retrieve the map name 
 	// until a proper IMap interface is in place
 	NameablePtr rootNameable = 
 		boost::dynamic_pointer_cast<Nameable>(GlobalSceneGraph().root());
-
+	
+	std::string fullName("");
+	
 	if (rootNameable != NULL && rootNameable->name().size() > 0) {
-		std::string fullName(rootNameable->name());
-
+		fullName = rootNameable->name();
+	
+		// Substract the maps/ prefix
 		if (boost::algorithm::istarts_with(fullName, "maps/")) {
 			fullName = fullName.substr(5, fullName.size());
 		}
-		
+	}
+	
+	return fullName;
+}
+
+// Compiles the map (including or excluding AAS)
+void DarkModCommands::compileMap(bool noAAS) {
+	// Prevent re-entering this method
+	static bool mutex(false);
+	
+	if (mutex) {
+		return;
+	}
+	
+	mutex = true;
+	
+	std::string mapName = getMapName();
+	
+	if (!mapName.empty()) {
 		// Show the console during compile
 		IEventPtr toggleConsoleEvent = GlobalEventManager().findEvent("ToggleConsole");
 		toggleConsoleEvent->keyDown();
@@ -36,13 +54,54 @@ void CompileMap(bool noAAS) {
 		// Assemble the command
 		std::string command = "dmap ";
 		command += noAAS ? "noaas " : "";
-		command += fullName;
+		command += mapName;
 		
 		client.executeCommand(command);
 	}
 	else {
 		gtkutil::errorDialog("Cannot compile empty or unnamed map.", GlobalRadiant().getMainWindow());
 	}
+	
+	mutex = false;
 }
 
-} // namespace darkmodcommands
+void DarkModCommands::compileMap() {
+	// Compile including AAS
+	compileMap(false);
+}
+
+void DarkModCommands::compileMapNoAAS() {
+	// Compile excluding AAS
+	compileMap(true);
+}
+
+void DarkModCommands::runAAS() {
+	// Prevent re-entering this method
+	static bool mutex(false);
+	
+	if (mutex) {
+		return;
+	}
+	
+	mutex = true;
+	
+	std::string mapName = getMapName();
+	
+	if (!mapName.empty()) {
+		// Show the console during compile
+		IEventPtr toggleConsoleEvent = GlobalEventManager().findEvent("ToggleConsole");
+		toggleConsoleEvent->keyDown();
+
+		// Instantiate a client and issue the command
+		DarkModRCFClient client;
+		
+		// Assemble the command
+		std::string command = "runaas " + mapName;
+		client.executeCommand(command);
+	}
+	else {
+		gtkutil::errorDialog("Cannot compile AAS of empty or unnamed map.", GlobalRadiant().getMainWindow());
+	}
+	
+	mutex = false;
+}
