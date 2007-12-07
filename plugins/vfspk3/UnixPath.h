c00b90a15658424377ba08b12fdd602fe167e34b
codereader <greebo@angua.at>
2007-12-07 23:58:18 +0000

    Optimised UnixPath container a bit.

--- plugins/vfspk3/UnixPath.h
+++ plugins/vfspk3/UnixPath.h
@@ -22,23 +22,22 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #if !defined(INCLUDED_FS_PATH_H)
 #define INCLUDED_FS_PATH_H
 
-#include <boost/algorithm/string/predicate.hpp>
-
 /// \brief A unix-style path string which can be modified at runtime.
 ///
 /// - Maintains a path ending in a path-separator.
 /// - Provides a limited STL-style interface to push and pop file or directory names at the end of the path.
-
-// greebo: TODO: Perform a profile run with a lot of VFS traversals to check the performance of this class
 class UnixPath
 {
 	// The actual string representation of the path
 	std::string _string;
+	std::size_t _size; // cached size
 
-	void check_separator() {
+	void update() {
 		// If the string is non-empty, but hasn't a trailing slash, add it
-		if (!_string.empty() && !boost::algorithm::ends_with(_string, "/")) {
+		_size = _string.size();
+		if (_size > 0 && _string[_size-1] != '/') {
 			_string.push_back('/');
+			_size++;
 		}
 	}
 
@@ -47,11 +46,11 @@ public:
 	UnixPath(const std::string& root) :
 		_string(root)
 	{
-		check_separator();
+		update();
 	}
 
 	bool empty() const {
-		return _string.empty();
+		return (_size == 0);
 	}
 
 	const char* c_str() const {
@@ -61,33 +60,42 @@ public:
 	/// \brief Appends the directory \p name.
 	void push(const char* name) {
 		_string += name;
-		check_separator();
+		update();
 	}
+
 	/// \brief Appends the directory [\p first, \p last).
 	void push(const char* first, const char* last) {
 		for (const char* i = first; i != last; i++) {
 			_string.push_back(*i);
 		}
-		check_separator();
+		update();
 	}
 	
 	/// \brief Appends the filename \p name.
 	void push_filename(const char* name) {
 		_string += name;
+		_size = _string.size();
 	}
 	
 	/// \brief Removes the last directory or filename appended.
-	// greebo: TODO: This looks slow
 	void pop() {
 		// Remove the trailing slash
-		if (boost::algorithm::ends_with(_string, "/")) {
-			_string.erase(_string.length()-1, 1);
+		if (_size > 0 && _string[_size-1] == '/') {
+			_string.erase(_size-1, 1);
+		}
+		// _size is considered invalid after this point
+		
+		std::size_t slashPos = _string.rfind("/");
+		if (slashPos != std::string::npos) {
+			// Erase everything after the slashpos
+			_string.erase(slashPos+1);
+		}
+		else {
+			// No slash at all, clear everything
+			_string.clear();
 		}
 		
-		// Remove all characters, back to front, till we hit a slash
-		while (!_string.empty() && !boost::algorithm::ends_with(_string, "/")) {
-			_string.erase(_string.length()-1, 1);
-		}
+		_size = _string.size();
 	}
 };
 
