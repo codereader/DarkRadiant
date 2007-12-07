d64a38e4e2a5097c5c440c4b2e59eae4957f993c
codereader <greebo@angua.at>
2007-12-07 17:31:47 +0000

    Moved and renamed Archives to SortedFilenames.h.

--- plugins/vfspk3/vfs.cpp
+++ plugins/vfspk3/vfs.cpp
@@ -62,58 +62,13 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <boost/algorithm/string/case_conv.hpp>
 #include <boost/algorithm/string/predicate.hpp>
 
+#include "SortedFilenames.h"
+
 #if defined(WIN32) && !defined(PATH_MAX)
 #define PATH_MAX 260
 #endif
 
 Archive* OpenArchive(const char* name);
-
-// Arnout: note - sort pakfiles in reverse order. This ensures that
-// later pakfiles override earlier ones. This because the vfs module
-// returns a filehandle to the first file it can find (while it should
-// return the filehandle to the file in the most overriding pakfile, the
-// last one in the list that is).
-class PakLess
-{
-public:
-	inline int ascii_to_upper(int c) const {
-		if (c >= 'a' && c <= 'z') {
-			return c - ('a' - 'A');
-		}
-		return c;
-	}
-	
-	/*!
-		This behaves identically to stricmp(a,b), except that ASCII chars
-		[\]^`_ come AFTER alphabet chars instead of before. This is because
-		it converts all alphabet chars to uppercase before comparison,
-		while stricmp converts them to lowercase.
-	*/
-	bool operator()(const std::string& self, const std::string& other) const {
-		const char* a = self.c_str();
-		const char* b = other.c_str();
-		
-		for (;;) {
-			int c1 = ascii_to_upper(*a++);
-			int c2 = ascii_to_upper(*b++);
-
-			if (c1 < c2) {
-				return false; // a < b
-			}
-
-			if (c1 > c2) {
-				return true; // a > b
-			}
-
-			if (c1 == 0) {
-				// greebo: End of first string reached, strings are equal
-				return false; // a == b && a == 0
-			}
-		}
-	}
-};
-
-typedef std::set<std::string, PakLess> Archives;
 
 Quake3FileSystem::Quake3FileSystem() :
 	_moduleObservers(getName()),
@@ -153,8 +108,8 @@ void Quake3FileSystem::initDirectory(const std::string& inputPath) {
 		ignore_prefix = "mp_";
 		override_prefix = "sp_";
 
-		Archives archives;
-		Archives archivesOverride;
+		SortedFilenames archives;
+		SortedFilenames archivesOverride;
 		for (;;) {
 			const char* name= g_dir_read_name(dir);
 			if (name == 0)
@@ -182,17 +137,19 @@ void Quake3FileSystem::initDirectory(const std::string& inputPath) {
 		ArchiveLoader& archiveModule = GlobalArchive("PK4");  
 			
 		// add the entries to the vfs
-		for (Archives::iterator i = archivesOverride.begin(); i != archivesOverride.end(); ++i) {
+		for (SortedFilenames::iterator i = archivesOverride.begin(); i != archivesOverride.end(); ++i) {
 			char filename[PATH_MAX];
 			strcpy(filename, path);
 			strcat(filename, i->c_str());
+			std::cout << "Filename: " << filename << "\n";
 			initPakFile(archiveModule, filename);
 		}
 		
-		for (Archives::iterator i = archives.begin(); i != archives.end(); ++i) {
+		for (SortedFilenames::iterator i = archives.begin(); i != archives.end(); ++i) {
 			char filename[PATH_MAX];
 			strcpy(filename, path);
 			strcat(filename, (*i).c_str());
+			std::cout << "archives Filename: " << filename << "\n";
 			initPakFile(archiveModule, filename);
 		}
 	}
