9328f3d47c4702629f88b25d5e70a9ec2625c08b
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2007-12-14 21:22:29 +0000

    Some more refactoring in winding-related code.

--- radiant/brush/FixedWinding.h
+++ radiant/brush/FixedWinding.h
@@ -2,15 +2,22 @@
 #define FIXEDWINDING_H_
 
 #include "math/Vector3.h"
+#include "math/Plane3.h"
 
 class Winding;
-class Plane3;
 
 #define MAX_POINTS_ON_WINDING 64
 
-struct DoubleLine {
+class DoubleLine {
+public:
 	Vector3 origin;
 	Vector3 direction;
+	
+	/// \brief Returns the point at which \p line intersects \p plane, 
+	// or an undefined value if there is no intersection.
+	inline Vector3 intersectPlane(const Plane3& plane) const {
+		return origin + direction * (-plane.distanceToPoint(origin) / direction.dot(plane.normal()));
+	}
 };
 
 class FixedWindingVertex {
@@ -43,6 +50,12 @@ public:
 	
 	/// \brief Keep the value of \p infinity as small as possible to improve precision in Winding_Clip.
 	void createInfinite(const Plane3& plane, double infinity);
+	
+	/// \brief Clip this winding which lies on \p plane by \p clipPlane, resulting in \p clipped.
+	/// If \p winding is completely in front of the plane, \p clipped will be identical to \p winding.  
+	/// If \p winding is completely in back of the plane, \p clipped will be empty.  
+	/// If \p winding intersects the plane, the edge of \p clipped which lies on \p clipPlane will store the value of \p adjacent.
+	void clip(const Plane3& plane, const Plane3& clipPlane, std::size_t adjacent, FixedWinding& clipped);
 };
 
 #endif /*FIXEDWINDING_H_*/
