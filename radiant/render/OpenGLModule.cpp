ccabe40f1f4c86a158be65b7e0f26751c87c97f4
codereader <greebo@angua.at>
2007-12-15 22:27:06 +0000

    Moved OpenGLBinding module implementation from qgl.* to render/OpenGLModule.*.

---
+++ radiant/render/OpenGLModule.cpp
@@ -0,0 +1,106 @@
+#include "OpenGLModule.h"
+
+#include "irender.h"
+#include "debugging/debugging.h"
+#include "modulesystem/StaticModule.h"
+
+OpenGLModule::OpenGLModule() :
+	_unknownError("Unknown error."),
+	_font(0, 0)
+{
+	// Populate the error list
+	_errorList[GL_NO_ERROR] = "GL_NO_ERROR - no error";
+	_errorList[GL_INVALID_ENUM] = "GL_INVALID_ENUM - An unacceptable value is specified for an enumerated argument.";
+	_errorList[GL_INVALID_VALUE] = "GL_INVALID_VALUE - A numeric argument is out of range.";
+	_errorList[GL_INVALID_OPERATION] = "GL_INVALID_OPERATION - The specified operation is not allowed in the current state.";
+	_errorList[GL_STACK_OVERFLOW] = "GL_STACK_OVERFLOW - Function would cause a stack overflow.";
+	_errorList[GL_STACK_UNDERFLOW] = "GL_STACK_UNDERFLOW - Function would cause a stack underflow.";
+	_errorList[GL_OUT_OF_MEMORY] = "GL_OUT_OF_MEMORY - There is not enough memory left to execute the function.";
+}
+
+void OpenGLModule::assertNoErrors() {
+#ifdef _DEBUG
+	GLenum error = glGetError();
+	while (error != GL_NO_ERROR) {
+		const std::string& errorString = getGLErrorString(error);
+		
+		if (error == GL_OUT_OF_MEMORY) {
+			ERROR_MESSAGE("OpenGL out of memory error: " << errorString.c_str());
+		}
+		else {
+			ERROR_MESSAGE("OpenGL error: " << errorString.c_str());
+		}
+
+		error = glGetError();
+	}
+#endif
+}
+
+void OpenGLModule::sharedContextCreated() {
+	// report OpenGL information
+	globalOutputStream() << "GL_VENDOR: "
+		<< reinterpret_cast<const char*>(glGetString(GL_VENDOR)) << "\n";
+	globalOutputStream() << "GL_RENDERER: "
+		<< reinterpret_cast<const char*>(glGetString(GL_RENDERER)) << "\n";
+	globalOutputStream() << "GL_VERSION: "
+		<< reinterpret_cast<const char*>(glGetString(GL_VERSION)) << "\n";
+	globalOutputStream() << "GL_EXTENSIONS: "
+		<< reinterpret_cast<const char*>(glGetString(GL_EXTENSIONS)) << "\n";
+
+	GLenum err = glewInit();
+	if (err != GLEW_OK)	{
+		// glewInit failed
+		globalErrorStream() << "GLEW error: " << 
+			reinterpret_cast<const char*>(glewGetErrorString(err));
+	}
+
+	GlobalShaderCache().extensionsInitialised();
+	GlobalShaderCache().realise();
+
+	_font = glfont_create("courier 8");
+	m_font = _font.getDisplayList();
+	m_fontHeight = _font.getPixelHeight();
+}
+	
+void OpenGLModule::sharedContextDestroyed() {
+	GlobalShaderCache().unrealise();
+}
+
+void OpenGLModule::drawString(const char* string) const {
+	glListBase(m_font);
+	glCallLists(GLsizei(strlen(string)), GL_UNSIGNED_BYTE, reinterpret_cast<const GLubyte*>(string));
+}
+
+void OpenGLModule::drawChar(char character) const {
+	glListBase(m_font);
+	glCallLists(1, GL_UNSIGNED_BYTE, reinterpret_cast<const GLubyte*>(&character));
+}
+
+// RegisterableModule implementation
+const std::string& OpenGLModule::getName() const {
+	static std::string _name(MODULE_OPENGL);
+	return _name;
+}
+
+const StringSet& OpenGLModule::getDependencies() const {
+	static StringSet _dependencies; // no dependencies
+	return _dependencies;
+}
+
+void OpenGLModule::initialiseModule(const ApplicationContext& ctx) {
+	globalOutputStream() << "OpenGL::initialiseModule called.\n";
+}
+
+const std::string& OpenGLModule::getGLErrorString(GLenum errorCode) const {
+	GLErrorList::const_iterator found = _errorList.find(errorCode);
+	
+	if (found != _errorList.end()) {
+		return found->second;
+	}
+	
+	// Not found
+	return _unknownError;
+}
+
+// Define the static OpenGLModule module
+module::StaticModule<OpenGLModule> openGLModule;
---
+++ radiant/render/OpenGLModule.h
@@ -0,0 +1,39 @@
+#ifndef OPENGLMODULE_H_
+#define OPENGLMODULE_H_
+
+#include "igl.h"
+#include <map>
+#include <string>
+#include "gtkutil/glfont.h"
+
+class OpenGLModule :
+	public OpenGLBinding
+{
+	typedef std::map<GLenum, std::string> GLErrorList;
+	GLErrorList _errorList;
+	
+	const std::string _unknownError;
+		
+	GLFont _font;
+	
+public:
+	OpenGLModule();
+	
+	virtual void assertNoErrors();
+	
+	virtual void sharedContextCreated();
+	virtual void sharedContextDestroyed();
+	
+	virtual void drawString(const char* string) const;
+	virtual void drawChar(char character) const;
+	
+	// RegisterableModule implementation
+	virtual const std::string& getName() const;
+	virtual const StringSet& getDependencies() const;
+	virtual void initialiseModule(const ApplicationContext& ctx);
+
+private:
+	const std::string& getGLErrorString(GLenum errorCode) const;
+};
+
+#endif /*OPENGLMODULE_H_*/
