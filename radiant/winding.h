11a4b5d020d0a2915ca5f0e161b09d0bedcbffaf
codereader <greebo@angua.at>
2007-12-15 11:48:29 +0000

    Moved a bunch of stuff into winding.cpp source file.

--- radiant/winding.h
+++ radiant/winding.h
@@ -28,31 +28,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "iclipper.h"
 #include "irender.h"
-#include "igl.h"
 #include "selectable.h"
 
 #include "math/Vector2.h"
 #include "math/Vector3.h"
-#include "texturelib.h"
-
-struct indexremap_t
-{
-  indexremap_t(std::size_t _x, std::size_t _y, std::size_t _z)
-    : x(_x), y(_y), z(_z)
-  {
-  }
-  std::size_t x, y, z;
-};
-
-inline indexremap_t indexremap_for_projectionaxis(const ProjectionAxis axis)
-{
-  switch(axis)
-  {
-  case eProjectionAxisX: return indexremap_t(1, 2, 0);
-  case eProjectionAxisY: return indexremap_t(2, 0, 1);
-  default: return indexremap_t(0, 1, 2);
-  }
-}
 
 const std::size_t c_brush_maxFaces = 1024;
 
@@ -68,18 +47,6 @@ public:
 };
 
 const double ON_EPSILON	= 1.0 / (1 << 8);
-
-inline PlaneClassification Winding_classifyDistance(const double distance, const double epsilon) {
-	if (distance > epsilon) {
-		return ePlaneFront;
-	}
-	
-	if (distance < -epsilon) {
-		return ePlaneBack;
-	}
-	
-	return ePlaneOn;
-}
 
 class Winding
 {
@@ -140,15 +107,7 @@ public:
   
 	/** greebo: Calculates the AABB of this winding
 	 */
-	AABB aabb() const {
-		AABB returnValue;
-		
-		for (const_iterator i = begin(); i != end(); i++) {
-			returnValue.includePoint(i->vertex);
-		}
-		
-		return returnValue;
-	}
+	AABB aabb() const;
 	
 	void testSelect(SelectionTest& test, SelectionIntersection& best) {
 		test.TestPolygon(VertexPointer(reinterpret_cast<VertexPointer::pointer>(&points.front().vertex), sizeof(WindingVertex)), numpoints, best);
@@ -156,40 +115,10 @@ public:
 	
 	// greebo: Updates the array containing the normal vectors of this winding
 	// The normal is the same for each vertex, so this just copies the values 
-	void updateNormals(const Vector3& normal) {
-		// Copy all normals into the winding vertices
-		for (iterator i = begin(); i != end(); i++) {
-			i->normal = normal;
-		}
-	}
+	void updateNormals(const Vector3& normal);
 	
 	// Submits this winding to OpenGL
-	inline void draw(RenderStateFlags state) const {
-		// A shortcut pointer to the first array element to avoid
-		// massive calls to std::vector<>::begin()
-		const WindingVertex& firstElement = points.front();
-			
-		// Set the vertex pointer first
-		glVertexPointer(3, GL_DOUBLE, sizeof(WindingVertex), &firstElement.vertex);
-
-		if ((state & RENDER_BUMP) != 0) {
-			glVertexAttribPointerARB(11, 3, GL_DOUBLE, 0, sizeof(WindingVertex), &firstElement.normal);
-			glVertexAttribPointerARB(8, 2, GL_DOUBLE, 0, sizeof(WindingVertex), &firstElement.texcoord);
-			glVertexAttribPointerARB(9, 3, GL_DOUBLE, 0, sizeof(WindingVertex), &firstElement.tangent);
-			glVertexAttribPointerARB(10, 3, GL_DOUBLE, 0, sizeof(WindingVertex), &firstElement.bitangent);
-		} 
-		else {
-			if (state & RENDER_LIGHTING) {
-				glNormalPointer(GL_DOUBLE, sizeof(WindingVertex), &firstElement.normal);
-			}
-
-			if (state & RENDER_TEXTURE) {
-				glTexCoordPointer(2, GL_DOUBLE, sizeof(WindingVertex), &firstElement.texcoord);
-			}
-		}
-		
-		glDrawArrays(GL_POLYGON, 0, GLsizei(numpoints));
-	}
+	void draw(RenderStateFlags state) const;
 	
 	// Wraps the given index around if it's larger than the size of this winding
 	inline std::size_t wrap(std::size_t i) const {
@@ -207,15 +136,9 @@ public:
 	std::size_t opposite(std::size_t index) const;
 	
 	// Returns the classification for the given plane
-	BrushSplitType classifyPlane(const Plane3& plane) const {
-		BrushSplitType split;
-		
-		for (const_iterator i = begin(); i != end(); ++i) {
-			++split.counts[Winding_classifyDistance(plane.distanceToPoint(i->vertex), ON_EPSILON)];
-		}
-		
-		return split;
-	}
+	BrushSplitType classifyPlane(const Plane3& plane) const;
+	
+	static PlaneClassification classifyDistance(const double distance, const double epsilon);
 	
 	/// \brief Returns true if
 	/// !flipped && winding is completely BACK or ON
