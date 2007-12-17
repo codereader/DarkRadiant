56e9bf0d21a3734dc8d06a8c863cfe077e3dca1b
codereader <greebo@angua.at>
2007-12-17 08:35:21 +0000

    Added barebone interface to IModelCache class declaration.

--- radiant/modelcache/ModelCache.h
+++ radiant/modelcache/ModelCache.h
@@ -9,7 +9,17 @@ class ModelCache :
 	public IModelCache
 {
 public:
+	/**
+	 * greebo: Looks up the model with the given path in the cache.
+	 * 
+	 * @returns: The singleton model NodePtr or NULL if not found.
+	 */
+	virtual scene::INodePtr find(const std::string& path);
 	
+	/**
+	 * greebo: Inserts the given node into the modelcache. 
+	 */
+	virtual void insert(const scene::INodePtr& modelNode);
 	
 	// RegisterableModule implementation
 	virtual const std::string& getName() const;
