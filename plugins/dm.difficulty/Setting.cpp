27115510f0b856a28cb1b7d9f534fd6dd48449f0
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2008-01-29 18:41:50 +0000

    Changed parsing approach to use IEntityClass::getAttributeList() instead of using a visitor class.

--- plugins/dm.difficulty/Setting.cpp
+++ plugins/dm.difficulty/Setting.cpp
@@ -1,13 +1,41 @@
 #include "Setting.h"
 
+#include "iregistry.h"
+
 namespace difficulty {
 
-Setting::Setting() :
-	isValid(false)
-{}
+	namespace {
+		const std::string RKEY_APPTYPE_IGNORE("game/difficulty/appTypeIgnore");
+	}
 
-void Setting::parseFromEClass(const IEntityClassPtr& eclass, int level, int index) {
-	
+void Setting::parseAppType() {
+	if (!argument.empty())
+	{
+		// Check for ignore argument
+		if (argument == GlobalRegistry().get(RKEY_APPTYPE_IGNORE))
+		{
+			appType = EIgnore;
+			argument.clear(); // clear the argument
+		}
+		// Check for special modifiers
+		else if (argument[0] == '+')
+		{
+			appType = EAdd;
+			// Remove the first character
+			argument = argument.substr(1);
+		}
+		else if (argument[0] == '*')
+		{
+			appType = EMultiply;
+			// Remove the first character
+			argument = argument.substr(1);
+		}
+		else if (argument[0] == '-')
+		{
+			appType = EAdd;
+			// Leave the "-" sign, it will be the sign of the parsed int
+		}
+	}
 }
 
 } // namespace difficulty
