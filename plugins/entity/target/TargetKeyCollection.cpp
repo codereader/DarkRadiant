c20c0708f88dfd958f00ece194063e141f422f76
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2008-02-14 10:16:24 +0000

    Moved TargetKeys to TargetKeyCollection file pair. Removed empty targetable.cpp.

---
+++ plugins/entity/target/TargetKeyCollection.cpp
@@ -0,0 +1,69 @@
+#include "TargetKeyCollection.h"
+
+#include <boost/algorithm/string/predicate.hpp>
+
+namespace entity {
+
+void TargetKeyCollection::forEachTarget(Visitor& visitor) const {
+	for (TargetKeyMap::const_iterator i = _targetKeys.begin(); 
+		 i != _targetKeys.end(); i++)
+	{
+		visitor.visit(i->second.getTarget());
+	}
+}
+
+bool TargetKeyCollection::empty() const {
+	return _targetKeys.empty();
+}
+
+bool TargetKeyCollection::isTargetKey(const std::string& key) {
+	// A key is a target key if it starts with "target" (any case)
+	return (boost::algorithm::istarts_with(key, "target"));
+}
+
+void TargetKeyCollection::setTargetsChanged(const Callback& targetsChanged) {
+	_targetsChanged = targetsChanged;
+}
+
+void TargetKeyCollection::targetsChanged() {
+	_targetsChanged();
+}
+
+// Entity::Observer implementation, gets called on key insert
+void TargetKeyCollection::onKeyInsert(const std::string& key, EntityKeyValue& value) {
+	// ignore non-target keys
+	if (!isTargetKey(key)) {
+		return; 
+	}
+
+	TargetKeyMap::iterator i = _targetKeys.insert(
+		TargetKeyMap::value_type(key, entity::TargetKey())
+	).first;
+	
+	i->second.attachToKeyValue(value);
+	//value.attach(TargetingEntity::TargetChangedCaller(i->second));
+	targetsChanged();
+}
+
+// Entity::Observer implementation, gets called on key erase
+void TargetKeyCollection::onKeyErase(const std::string& key, EntityKeyValue& value) {
+	// ignore non-target keys
+	if (!isTargetKey(key)) {
+		return; 
+	}
+
+	TargetKeyMap::iterator i = _targetKeys.find(key);
+	
+	// This must be found
+	assert(i != _targetKeys.end());
+
+	i->second.detachFromKeyValue(value);
+	//value.detach(TargetingEntity::TargetChangedCaller(i->second));
+
+	// Remove the found element
+	_targetKeys.erase(i);
+
+	targetsChanged();
+}
+
+} // namespace entity
---
+++ plugins/entity/target/TargetKeyCollection.h
@@ -0,0 +1,52 @@
+#ifndef _ENTITY_TARGETKEYCOLLECTION_H_
+#define _ENTITY_TARGETKEYCOLLECTION_H_
+
+#include <map>
+#include "TargetKey.h"
+
+namespace entity {
+
+class TargetKeyCollection : 
+	public Entity::Observer
+{
+public:
+	class Visitor {
+	public:
+		// Gets called with each Target contained in the TargetKeys object
+		virtual void visit(const TargetPtr& target) = 0;
+	};
+
+private:
+	// greebo: A container mapping "targetN" keys to TargetKey objects
+	typedef std::map<std::string, TargetKey> TargetKeyMap;
+	TargetKeyMap _targetKeys;
+
+	Callback _targetsChanged;
+
+public:
+	void setTargetsChanged(const Callback& targetsChanged);
+
+	// Entity::Observer implementation, gets called on key insert/erase
+	void onKeyInsert(const std::string& key, EntityKeyValue& value);
+	void onKeyErase(const std::string& key, EntityKeyValue& value);
+
+	/**
+	 * greebo: Walker function, calls visit() for each target
+	 *         contained in this structure.
+	 */
+	void forEachTarget(Visitor& visitor) const;
+
+	// Returns TRUE if there are no "target" keys observed
+	bool empty() const;
+
+	// Triggers a callback that the targets have been changed
+	void targetsChanged();
+
+private:
+	// Returns TRUE if the given key matches the pattern for target keys
+	bool isTargetKey(const std::string& key);
+};
+
+} // namespace entity
+
+#endif /* _ENTITY_TARGETKEYCOLLECTION_H_ */
