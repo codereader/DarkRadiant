9d3054fae1fbb3ffc55e66003fd50df41b7fe546
codereader <greebo@angua.at>
2008-02-14 09:38:44 +0000

    Moved Target class to separate header file.

--- plugins/entity/targetable.h
+++ plugins/entity/targetable.h
@@ -36,6 +36,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "stringio.h"
 #include "Doom3Entity.h"
 
+#include "target/Target.h"
+
 class Targetable
 {
 public:
@@ -43,52 +45,6 @@ public:
 };
 
 namespace entity {
-
-/**
- * greebo: This is an abstract representation of a target.
- *         In Doom3 maps, a Target can be any map entity, that's
- *         why this object encapsulates a reference to an actual 
- *         scene::Instance. 
- *
- * Note: Such a Target object can be empty. That's the case for 
- *       entities referring to non-existing entities in their 
- *       "target" spawnarg.
- *
- * All Targets are owned by the TargetManager class.
- *
- * A Target can be referenced by one ore more TargetingEntity objects.
- */
-class Target
-{
-	// The actual instance this Target refers to (can be NULL)
-	scene::Instance* _instance;
-public:
-	scene::Instance* getInstance() const {
-		return _instance;
-	}
-
-	void setInstance(scene::Instance* instance) {
-		_instance = instance;
-	}
-
-	bool isEmpty() const {
-		return _instance == NULL;
-	}
-
-	void clear() {
-		_instance = NULL;
-	}
-
-	// greebo: Returns the position of this target or <0,0,0> if empty
-	Vector3 getPosition() const {
-		if (_instance == NULL) {
-			return Vector3(0,0,0);
-		}
-
-		return _instance->worldAABB().getOrigin();
-	}
-};
-typedef boost::shared_ptr<Target> TargetPtr;
 
 class TargetManager
 {
