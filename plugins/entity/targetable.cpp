5c6b0b7bd66421e8690dc57f1507419a3e37de02
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2008-02-14 09:29:35 +0000

    Reorganised the Target code (WIP).

--- plugins/entity/targetable.cpp
+++ plugins/entity/targetable.cpp
@@ -22,18 +22,124 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "targetable.h"
 #include <boost/algorithm/string/predicate.hpp>
 
-typedef std::map<std::string, targetables_t> targetnames_t;
+namespace entity {
 
-targetnames_t g_targetnames;
-
-targetables_t* getTargetables(const std::string& targetname)
+TargetManager::TargetManager() :
+	_emptyTarget(new Target)
 {
-  if (targetname.empty())
-    return 0;
-  return &g_targetnames[targetname];
+	// Just to be sure, clear the target
+	_emptyTarget->clear();
 }
 
-ShaderPtr RenderableTargetingEntity::m_state;
+// Static accessor method
+TargetManager& TargetManager::Instance() {
+	static TargetManager _instance;
+	return _instance;
+}
+
+TargetPtr TargetManager::getTarget(const std::string name) {
+	if (name.empty()) {
+		return _emptyTarget;
+	}
+
+	TargetList::iterator found = _targets.find(name);
+
+	if (found != _targets.end()) {
+		return found->second;
+	}
+
+	// Doesn't exist yet, create this one
+	TargetPtr target(new Target);
+	target->clear();
+
+	// Insert into the local map and return
+	_targets.insert(TargetList::value_type(name, target));
+
+	return target;
+}
+
+void TargetManager::associateTarget(const std::string& name, scene::Instance* instance) {
+	if (name.empty()) {
+		return; // don't associate empty names
+	}
+
+	TargetList::iterator found = _targets.find(name);
+
+	if (found != _targets.end()) {
+		if (found->second->isEmpty()) {
+			// Already registered, but empty => associate it
+			found->second->setInstance(instance);
+		}
+		else {
+			// Non-empty target!
+			globalErrorStream() << "TargetManager: Target " << name.c_str() << " already associated!\n";
+		}
+
+		return;
+	}
+
+	// Doesn't exist yet, create a new target
+	TargetPtr target(new Target);
+
+	// Associate the target
+	target->setInstance(instance);
+
+	// Insert into the local map and return
+	_targets.insert(TargetList::value_type(name, target));
+}
+
+void TargetManager::clearTarget(const std::string& name) {
+	// Locate and clear the named target
+	TargetList::iterator found = _targets.find(name);
+
+	if (found != _targets.end()) {
+		// Found, clear it
+		found->second->clear();
+	}
+}
+
+// --------------------------------------------------------------------
+
+const TargetPtr& TargetKey::getTarget() const {
+	return _target;
+}
+
+// Observes the given keyvalue
+void TargetKey::attachToKeyValue(EntityKeyValue& value) {
+	// Observe this entity keyvalue
+	value.attach(TargetChangedCaller(*this));
+}
+
+// Stops observing the given keyvalue
+void TargetKey::detachFromKeyValue(EntityKeyValue& value) {
+	value.detach(TargetChangedCaller(*this));
+}
+
+// This gets called as soon as the "target" key in the spawnargs changes
+void TargetKey::targetChanged(const std::string& target) {
+	// Acquire the Target object (will be created if nonexistent)
+	_target = TargetManager::Instance().getTarget(target);
+}
+
+} // namespace entity
+
+// This maps names to Targetable pointers
+typedef std::map<std::string, Targetable*> TargetableMap;
+TargetableMap g_targetables;
+
+Targetable* getTargetable(const std::string& targetname) {
+	if (targetname.empty()) {
+		return NULL;
+	}
+
+	TargetableMap::iterator i = g_targetables.find(targetname);
+	if (i == g_targetables.end()) {
+		return NULL;
+	}
+	return i->second;
+}
+
+//ShaderPtr RenderableTargetLines::m_state;
 
 // TargetKeys implementation
 bool TargetKeys::isTargetKey(const std::string& key) {
@@ -56,11 +162,12 @@ void TargetKeys::onKeyInsert(const std::string& key, EntityKeyValue& value) {
 		return; 
 	}
 
-	TargetingEntities::iterator i = _targetingEntities.insert(
-		TargetingEntities::value_type(key, TargetingEntity())
+	TargetKeyMap::iterator i = _targetKeys.insert(
+		TargetKeyMap::value_type(key, entity::TargetKey())
 	).first;
 	
-	value.attach(TargetingEntity::TargetChangedCaller(i->second));
+	i->second.attachToKeyValue(value);
+	//value.attach(TargetingEntity::TargetChangedCaller(i->second));
 	targetsChanged();
 }
 
@@ -71,19 +178,20 @@ void TargetKeys::onKeyErase(const std::string& key, EntityKeyValue& value) {
 		return; 
 	}
 
-	TargetingEntities::iterator i = _targetingEntities.find(key);
+	TargetKeyMap::iterator i = _targetKeys.find(key);
 	
 	// This must be found
-	assert(i != _targetingEntities.end());
+	assert(i != _targetKeys.end());
 
-	value.detach(TargetingEntity::TargetChangedCaller(i->second));
+	i->second.detachFromKeyValue(value);
+	//value.detach(TargetingEntity::TargetChangedCaller(i->second));
 
 	// Remove the found element
-	_targetingEntities.erase(i);
+	_targetKeys.erase(i);
 
 	targetsChanged();
 }
 
-const TargetKeys::TargetingEntities& TargetKeys::get() const {
+/*const TargetKeys::TargetingEntities& TargetKeys::get() const {
 	return _targetingEntities;
-}
+}*/
