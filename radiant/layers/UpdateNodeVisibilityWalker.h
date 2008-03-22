12a56f3ea023a3f05e08664d2cc09a44f734be93
codereader <greebo@angua.at>
2008-03-22 21:46:31 +0000

    Overhauled UpdateNodeVisibilityWalker - Nodes with visible children must not be hidden, otherwise the root node will override the visibility of all child nodes in the map for example.

--- radiant/layers/UpdateNodeVisibilityWalker.h
+++ radiant/layers/UpdateNodeVisibilityWalker.h
@@ -1,6 +1,7 @@
 #ifndef UPDATENODEVISIBILITYWALKER_H_
 #define UPDATENODEVISIBILITYWALKER_H_
 
+#include <stack>
 #include "ilayer.h"
 #include "iscenegraph.h"
 #include "scenelib.h"
@@ -10,14 +11,44 @@ namespace scene {
 class UpdateNodeVisibilityWalker :
 	public scene::Graph::Walker
 {
+	mutable std::stack<bool> _visibilityStack;
 public:
 	bool pre(const Path& path, const INodePtr& node) const {
-		// Update the visibility and check if the node is visible now
-		if (!GlobalLayerSystem().updateNodeVisibility(node)) {
-			// Node is hidden after update, de-select
+		// Update the node visibility and store the result
+		bool nodeIsVisible = GlobalLayerSystem().updateNodeVisibility(node);
+
+		// Add a new element for this level
+		_visibilityStack.push(nodeIsVisible);
+
+		return true;
+	}
+
+	void post(const Path& path, const INodePtr& node) const {
+		// Is this child visible?
+		bool childIsVisible = _visibilityStack.top();
+
+		_visibilityStack.pop();
+
+		if (childIsVisible) {
+			// Show the node, regardless whether it was hidden before
+			// otherwise the parent would hide the visible children as well
+			node->disable(Node::eLayered);
+		}
+
+		if (_visibilityStack.empty()) {
+			// We've passed the root node the second time, we're done
+			return;
+		}
+
+		if (childIsVisible) {
+			// The child was visible, set this to true
+			_visibilityStack.top() = true;
+		}
+		
+		if (!_visibilityStack.top()) {
+			// Node is hidden after update (and no children are visible), de-select
 			Node_setSelected(node, false);
 		}
-		return true;
 	}
 };
 
