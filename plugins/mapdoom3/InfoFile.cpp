bb5749a7f945b1a059e7c4c2c85defa7e23dc05b
codereader <greebo@angua.at>
2008-03-23 21:24:36 +0000

    Added InfoFile class which will take care of the data found in the information file (WIP).

---
+++ plugins/mapdoom3/InfoFile.cpp
@@ -0,0 +1,56 @@
+#include "InfoFile.h"
+
+#include "stream/textstream.h"
+
+#include "Doom3MapFormat.h"
+#include <boost/lexical_cast.hpp>
+
+namespace map {
+
+// Pass the input stream to the constructor
+InfoFile::InfoFile(std::istream& infoStream) :
+	_tok(infoStream),
+	_isValid(false)
+{}
+
+const InfoFile::LayerNameList& InfoFile::getLayerNames() const {
+	return _layerNames;
+}
+
+void InfoFile::parse() {
+	// parse the header
+	try {
+		_tok.assertNextToken("DarkRadiant");
+		_tok.assertNextToken("Map");
+		_tok.assertNextToken("Information");
+		_tok.assertNextToken("File");
+		_tok.assertNextToken("Version");
+		float version = boost::lexical_cast<float>(_tok.nextToken());
+
+		if (version != MAP_INFO_VERSION) {
+			throw parser::ParseException("Map version invalid");
+		}
+	}
+	catch (parser::ParseException e) {
+        globalErrorStream() 
+            << "[mapdoom3] Unable to parse info file header: " 
+            << e.what() << "\n";
+        return;
+    }
+    catch (boost::bad_lexical_cast e) {
+        globalErrorStream() 
+            << "[mapdoom3] Unable to parse info file version: " 
+            << e.what() << "\n";
+        return;
+    }
+
+	_tok.assertNextToken("{");
+	
+	_isValid = true;
+}
+
+void InfoFile::parseLayerNames() {
+	
+}
+
+} // namespace map
---
+++ plugins/mapdoom3/InfoFile.h
@@ -0,0 +1,38 @@
+#ifndef INFO_FILE_H_
+#define INFO_FILE_H_
+
+#include "parser/DefTokeniser.h"
+
+namespace map {
+
+class InfoFile {
+public:
+	typedef std::vector<std::string> LayerNameList;
+
+private:
+	// The actual DefTokeniser to split the infoStream into pieces
+	parser::BasicDefTokeniser<std::istream> _tok;
+
+	// The list of layernames
+	LayerNameList _layerNames;
+
+	// TRUE if the map info fail was found to be valid
+	bool _isValid;
+
+public:
+	// Pass the input stream to the constructor
+	InfoFile(std::istream& infoStream);
+
+	// Parse the entire file
+	void parse();
+
+	const LayerNameList& getLayerNames() const;
+
+private:
+	// Parses the Layers section
+	void parseLayerNames();
+};
+
+} // namespace map
+
+#endif /* INFO_FILE_H_ */
