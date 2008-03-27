33bca372970fcb9b9995fbae58f0a58b8da77dda
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2008-03-27 13:03:28 +0000

    Moved CSG algorithm for hollow and makeRoom to brush/csg and tightened the code a bit.

---
+++ radiant/brush/csg/CSG.cpp
@@ -0,0 +1,127 @@
+#include "CSG.h"
+
+#include "iundo.h"
+#include "igrid.h"
+#include "scenelib.h"
+
+#include "brush/Face.h"
+#include "brush/Brush.h"
+#include "brush/BrushNode.h"
+#include "brush/BrushVisit.h"
+#include "selection/algorithm/Primitives.h"
+
+namespace algorithm {
+namespace csg {
+
+void Face_makeBrush(Face& face, const Brush& brush, BrushVector& out, float offset, bool makeRoom) {
+	if (!face.contributes()) {
+		return;
+	}
+
+	out.push_back(new Brush(brush));
+
+	FacePtr newFace = out.back()->addFace(face);
+
+	if (newFace != 0) {
+		newFace->flipWinding();
+		newFace->getPlane().offset(offset);
+		newFace->planeChanged();
+  
+		if (makeRoom) {
+			// Retrieve the normal vector of the "source" face
+			out.back()->transform(
+				Matrix4::getTranslation(face.getPlane().plane3().normal()*offset)
+			);
+			out.back()->freezeTransform();
+		}
+	}
+}
+
+class FaceMakeBrush
+{
+	const Brush& _brush;
+	BrushVector& _out;
+	float _offset;
+	bool _makeRoom;
+public:
+	FaceMakeBrush(const Brush& brush, BrushVector& out, float offset, bool makeRoom = false) : 
+		_brush(brush), 
+		_out(out), 
+		_offset(offset), 
+		_makeRoom(makeRoom)
+	{}
+
+	void operator()(Face& face) const {
+		Face_makeBrush(face, _brush, _out, _offset, _makeRoom);
+	}
+};
+
+void hollowBrush(const BrushNodePtr& sourceBrush, bool makeRoom) {
+	// Create the brushes
+	BrushVector out;
+
+	// Hollow the brush using the current grid size
+	Brush_forEachFace(
+		sourceBrush->getBrush(), 
+		FaceMakeBrush(sourceBrush->getBrush(), out, GlobalGrid().getGridSize(), makeRoom)
+	);
+
+	scene::INodePtr parent = sourceBrush->getParent();
+
+	for (BrushVector::const_iterator i = out.begin(); i != out.end(); ++i) {
+		(*i)->removeEmptyFaces();
+
+		scene::INodePtr newNode = GlobalBrushCreator().createBrush();
+		BrushNodePtr brushNode = boost::dynamic_pointer_cast<BrushNode>(newNode);
+		assert(brushNode != NULL);
+
+		// Move the child brushes to the same layer as their source
+		scene::assignNodeToLayers(brushNode, sourceBrush->getLayers());
+
+		brushNode->getBrush().copy(*(*i));
+
+		delete (*i);
+		
+		// Add the child to the same parent as the source brush
+		parent->addChildNode(brushNode);
+
+		Node_setSelected(brushNode, true);
+	}
+
+	// Now unselect and remove the source brush from the scene
+	Node_setSelected(sourceBrush, false);
+	parent->removeChildNode(sourceBrush);
+}
+
+void hollowSelectedBrushes() {
+	UndoableCommand undo("hollowSelectedBrushes");
+
+	// Find all brushes
+	BrushPtrVector brushes = selection::algorithm::getSelectedBrushes();
+
+	// Cycle through the brushes and hollow them
+	// We assume that all these selected brushes are visible as well.
+	for (std::size_t i = 0; i < brushes.size(); i++) {
+		hollowBrush(brushes[i], false);
+	}
+
+	SceneChangeNotify();
+}
+
+void makeRoomForSelectedBrushes() {
+	UndoableCommand undo("brushMakeRoom");
+
+	// Find all brushes
+	BrushPtrVector brushes = selection::algorithm::getSelectedBrushes();
+
+	// Cycle through the brushes and hollow them
+	// We assume that all these selected brushes are visible as well.
+	for (std::size_t i = 0; i < brushes.size(); i++) {
+		hollowBrush(brushes[i], true);
+	}
+
+	SceneChangeNotify();
+}
+
+} // namespace csg
+} // namespace algorithm
---
+++ radiant/brush/csg/CSG.h
@@ -0,0 +1,36 @@
+#ifndef BRUSH_CSG_H_
+#define BRUSH_CSG_H_
+
+#include <boost/shared_ptr.hpp>
+
+// Contains the routines for brush subtract, merge and hollow
+
+class BrushNode;
+typedef boost::shared_ptr<BrushNode> BrushNodePtr;
+
+namespace algorithm {
+namespace csg {
+
+/** 
+ * greebo: Hollows the given brush. Note that this selects the 
+ *         resulting brushes and removes the source brush from the scene.
+ *
+ * @makeRoom: Set this to TRUE to move the wall brushes outside a bit 
+ *            (see makeRoomForSelectedBrushes() routine).
+ */
+void hollowBrush(const BrushNodePtr& sourceBrush, bool makeRoom);
+
+/**
+ * greebo: Hollows all currently selected brushes.
+ */
+void hollowSelectedBrushes();
+
+/** greebo: This tries to move the newly created brushes towards the outside
+ * 			so that the corners don't overlap (works only for rectangular prisms). 
+ */
+void makeRoomForSelectedBrushes();
+
+} // namespace csg
+} // namespace algorihtm
+
+#endif /* BRUSH_CSG_H_ */
