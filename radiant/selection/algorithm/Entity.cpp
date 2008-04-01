7f4ff56dd5fc25bbd94ffe94288eb010b205e6b2
codereader <greebo@angua.at>
2008-04-01 16:21:24 +0000

    Fixed a crash when changing the classname of entities.

--- radiant/selection/algorithm/Entity.cpp
+++ radiant/selection/algorithm/Entity.cpp
@@ -16,17 +16,32 @@ class EntitySetClassnameSelected :
 	public SelectionSystem::Visitor
 {
 	std::string _classname;
+
+	// Entites are getting accumulated in this list and processed at destruction time
+	mutable std::set<scene::INodePtr> _entities;
 public:
 	EntitySetClassnameSelected(const std::string& classname) :
 		_classname(classname)
 	{}
+
+	~EntitySetClassnameSelected() {
+		for (std::set<scene::INodePtr>::iterator i = _entities.begin();
+			 i != _entities.end(); i++)
+		{
+			// "Rename" the entity, this deletes the old node and creates a new one
+			scene::INodePtr newNode = changeEntityClassname(*i, _classname);
+
+			// Select the new entity node
+			Node_setSelected(newNode, true);
+		}
+	}
 
 	virtual void visit(const scene::INodePtr& node) const {
 		// Check if we have an entity
 		Entity* entity = Node_getEntity(node);
 
 		if (entity != NULL && !_classname.empty() && Node_isSelected(node)) { 
-			changeEntityClassname(node, _classname);
+			_entities.insert(node);
 		}
 	}
 };
@@ -35,6 +50,8 @@ void setEntityClassname(const std::string& classname) {
 	// greebo: instantiate a walker and traverse the current selection
 	EntitySetClassnameSelected classnameSetter(classname);
 	GlobalSelectionSystem().foreachSelected(classnameSetter);
+
+	// The destructor of the classNameSetter will rename the entities
 }
 
 	} // namespace algorithm
