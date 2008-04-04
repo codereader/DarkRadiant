83127ae4a7f02c3c46bace429a391b6f152f9c01
codereader <greebo@angua.at>
2008-04-04 17:35:06 +0000

    Improved InstanceUpdateWalker so that filtered items get deselected as well. This still doesn't resolve problems with items staying selected even if all their shaders are filtered.

--- plugins/filters/InstanceUpdateWalker.h
+++ plugins/filters/InstanceUpdateWalker.h
@@ -8,55 +8,63 @@
 
 #include "scenelib.h"
 
-namespace filters
+namespace filters {
+
+class Deselector :
+	public scene::NodeVisitor
 {
+public:
+	bool pre(const scene::INodePtr& node) {
+		Node_setSelected(node, false);
+		return true;
+	}
+};
 
 /**
  * Scenegraph walker to update filtered status of Instances based on the
  * status of their parent entity class.
  */
-class InstanceUpdateWalker
-: public scene::Graph::Walker
+class InstanceUpdateWalker : 
+	public scene::Graph::Walker
 {
 public:
-
 	// Pre-descent walker function
 	bool pre(const scene::Path& path, const scene::INodePtr& node) const { 
 
 		// Retrieve the parent entity and check its entity class.
-		Entity* entity = Node_getEntity(path.top());
-		if (entity) {
+		Entity* entity = Node_getEntity(node);
+		if (entity != NULL) {
 			IEntityClassConstPtr eclass = entity->getEntityClass();
-			std::string name = eclass->getName();
 			node->setFiltered(
-				!GlobalFilterSystem().isVisible("entityclass", name)
+				!GlobalFilterSystem().isVisible("entityclass", eclass->getName())
 			);
 		}
 		
 		// greebo: Update visibility of PatchInstances
-		if (Node_isPatch(path.top())) {
+		if (Node_isPatch(node)) {
 			node->setFiltered(
 				!GlobalFilterSystem().isVisible("object", "patch")
 			);
 		}
 		
 		// greebo: Update visibility of BrushInstances
-		if (Node_isBrush(path.top())) {
+		if (Node_isBrush(node)) {
 			node->setFiltered(
 				!GlobalFilterSystem().isVisible("object", "brush")
 			);
 		}
-		
+
+		if (!node->visible()) {
+			// de-select this node and all children
+			Deselector deselector;
+			Node_traverseSubgraph(node, deselector);
+		}
+
 		// Continue the traversal
 		return true;
 	}
-
-	// Post descent function
-	void post(const scene::Path& path, const scene::INodePtr& node) const {
-		
-	}
 };
 
-}
+} // namespace filters
 
 #endif /*INSTANCEUPDATEWALKER_H_*/
