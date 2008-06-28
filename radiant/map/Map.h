1529c2ac1e0a24feb35726ba4697288db01777a8
codereader <greebo@angua.at>
2008-06-28 10:04:05 +0000

    Fixed a compilation issue in Linux.

--- radiant/map/Map.h
+++ radiant/map/Map.h
@@ -40,9 +40,9 @@ public:
 	virtual IMapRootNodePtr getRoot();
 
 	// RegisterableModule implementation
-	virtual const std::string& Map::getName() const;
-	virtual const StringSet& Map::getDependencies() const;
-	virtual void Map::initialiseModule(const ApplicationContext& ctx);
+	virtual const std::string& getName() const;
+	virtual const StringSet& getDependencies() const;
+	virtual void initialiseModule(const ApplicationContext& ctx);
 
 	void realiseResource();
 	void unrealiseResource();
