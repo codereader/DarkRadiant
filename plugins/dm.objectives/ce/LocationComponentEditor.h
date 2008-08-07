dcae07dc2fd10870043a0f4490415e8cd62477a5
codereader <greebo@angua.at>
2008-08-07 18:00:06 +0000

    Added LocationComponentEditor.

---
+++ plugins/dm.objectives/ce/LocationComponentEditor.h
@@ -0,0 +1,82 @@
+#ifndef LOCATION_COMPONENT_EDITOR_H_
+#define LOCATION_COMPONENT_EDITOR_H_
+
+#include "ComponentEditor.h"
+#include "ComponentEditorFactory.h"
+#include "SpecifierEditCombo.h"
+#include "../ComponentType.h"
+
+namespace objectives {
+
+namespace ce {
+
+/**
+ * ComponentEditor subclass for COMP_LOCATION component type.
+ * 
+ * An COMP_LOCATION component usually uses a two specifiers.
+ */
+class LocationComponentEditor : 
+	public ComponentEditor
+{
+	// Registration class
+	static struct RegHelper 
+	{
+		RegHelper() {
+			ComponentEditorFactory::registerType(
+				objectives::ComponentType::COMP_LOCATION().getName(), 
+				ComponentEditorPtr(new LocationComponentEditor())
+			);
+		}
+	} regHelper;
+	
+	// Main widget
+	GtkWidget* _widget;
+	
+	// Component to edit
+	Component* _component;	
+	
+	// SpecifierEditCombo for the entity in question
+	SpecifierEditCombo _entSpec;
+
+	// SpecifierEditCombo for the location the entity should be in (or not)
+	SpecifierEditCombo _locationSpec;
+	
+public:
+
+	/**
+	 * Construct a default LocationComponentEditor.
+	 */
+	LocationComponentEditor() : 
+		_component(NULL)
+	{}
+	
+	/**
+	 * Construct a LocationComponentEditor with a Component object to display and
+	 * edit.
+	 * 
+	 * @param component
+	 * The Component to edit.
+	 */
+	LocationComponentEditor(Component& component);
+	
+	/**
+	 * Destructor
+	 */
+	~LocationComponentEditor();
+	
+	/* ComponentEditor implementation */
+	
+	ComponentEditorPtr clone(Component& component) const {
+		return ComponentEditorPtr(new LocationComponentEditor(component));
+	}
+	
+	GtkWidget* getWidget() const; 
+
+    void writeToComponent() const;
+};
+
+} // namespace ce
+
+} // namespace objectives
+
+#endif /* LOCATION_COMPONENT_EDITOR_H_ */
--- plugins/dm.objectives/ce/PickpocketComponentEditor.h
+++ plugins/dm.objectives/ce/PickpocketComponentEditor.h
@@ -48,7 +48,7 @@ public:
 	{}
 	
 	/**
-	 * Construct a ItemComponentEditor with a Component object to display and
+	 * Construct a PickpocketComponentEditor with a Component object to display and
 	 * edit.
 	 * 
 	 * @param component
