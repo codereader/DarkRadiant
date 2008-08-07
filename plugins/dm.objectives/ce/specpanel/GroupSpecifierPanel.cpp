98c6ab3e245419c78e4abdd372a1a02c02631f74
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2008-08-07 14:02:24 +0000

    Added SpecifierPanel for SPEC_GROUP.

---
+++ plugins/dm.objectives/ce/specpanel/GroupSpecifierPanel.cpp
@@ -0,0 +1,12 @@
+#include "GroupSpecifierPanel.h"
+
+namespace objectives {
+
+namespace ce {
+
+// Reg helper
+GroupSpecifierPanel::RegHelper GroupSpecifierPanel::_regHelper;
+
+} // namespace ce
+
+} // namespace objectives
