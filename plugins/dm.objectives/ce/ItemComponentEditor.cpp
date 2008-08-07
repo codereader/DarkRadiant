35569b08e40cf21edd4bb60103a3da657621da41
codereader <greebo@angua.at>
2008-08-07 11:52:43 +0000

    Added ItemComponentEditor class (is currently just a copy of the KillComponentEditor).

---
+++ plugins/dm.objectives/ce/ItemComponentEditor.cpp
@@ -0,0 +1,62 @@
+#include "ItemComponentEditor.h"
+#include "../SpecifierType.h"
+#include "../Component.h"
+
+#include "gtkutil/LeftAlignment.h"
+#include "gtkutil/LeftAlignedLabel.h"
+
+#include <gtk/gtk.h>
+
+namespace objectives {
+
+namespace ce {
+
+// Registration helper, will register this editor in the factory
+ItemComponentEditor::RegHelper ItemComponentEditor::regHelper;
+
+// Constructor
+ItemComponentEditor::ItemComponentEditor(Component& component) :
+	_component(&component),
+	_targetCombo(SpecifierType::SET_STANDARD_AI())
+{
+	// Main vbox
+	_widget = gtk_vbox_new(FALSE, 6);
+
+    gtk_box_pack_start(
+        GTK_BOX(_widget), 
+        gtkutil::LeftAlignedLabel("<b>Item target:</b>"),
+        FALSE, FALSE, 0
+    );
+	gtk_box_pack_start(
+		GTK_BOX(_widget), _targetCombo.getWidget(), FALSE, FALSE, 0
+	);
+
+    // Populate the SpecifierEditCombo with the first specifier
+    _targetCombo.setSpecifier(
+        component.getSpecifier(Specifier::FIRST_SPECIFIER)
+    );
+}
+
+// Destructor
+ItemComponentEditor::~ItemComponentEditor() {
+	if (GTK_IS_WIDGET(_widget)) {
+		gtk_widget_destroy(_widget);
+	}
+}
+
+// Get the main widget
+GtkWidget* ItemComponentEditor::getWidget() const {
+	return _widget;
+}
+
+// Write to component
+void ItemComponentEditor::writeToComponent() const {
+    assert(_component);
+    _component->setSpecifier(
+        Specifier::FIRST_SPECIFIER, _targetCombo.getSpecifier()
+    );
+}
+
+} // namespace ce
+
+} // namespace objectives
---
+++ plugins/dm.objectives/ce/ItemComponentEditor.h
@@ -0,0 +1,79 @@
+#ifndef _ITEM_COMPONENT_EDITOR_H_
+#define _ITEM_COMPONENT_EDITOR_H_
+
+#include "ComponentEditor.h"
+#include "ComponentEditorFactory.h"
+#include "SpecifierEditCombo.h"
+#include "../ComponentType.h"
+
+namespace objectives {
+
+namespace ce {
+
+/**
+ * ComponentEditor subclass for COMP_ITEM component type.
+ * 
+ * An ITEM component requires that the player acquires or loses an item.
+ */
+class ItemComponentEditor : 
+	public ComponentEditor
+{
+	// Registration class
+	static struct RegHelper 
+	{
+		RegHelper() {
+			ComponentEditorFactory::registerType(
+				objectives::ComponentType::COMP_ITEM().getName(), 
+				ComponentEditorPtr(new ItemComponentEditor())
+			);
+		}
+	} regHelper;
+	
+	// Main widget
+	GtkWidget* _widget;
+	
+	// Component to edit
+	Component* _component;	
+	
+	// SpecifierEditCombo for the item name
+	SpecifierEditCombo _targetCombo;
+	
+public:
+
+	/**
+	 * Construct a default KillComponentEditor.
+	 */
+	ItemComponentEditor() : 
+		_component(NULL)
+	{}
+	
+	/**
+	 * Construct a ItemComponentEditor with a Component object to display and
+	 * edit.
+	 * 
+	 * @param component
+	 * The Component to edit.
+	 */
+	ItemComponentEditor(Component& component);
+	
+	/**
+	 * Destructor
+	 */
+	~ItemComponentEditor();
+	
+	/* ComponentEditor implementation */
+	
+	ComponentEditorPtr clone(Component& component) const {
+		return ComponentEditorPtr(new ItemComponentEditor(component));
+	}
+	
+	GtkWidget* getWidget() const; 
+
+    void writeToComponent() const;
+};
+
+} // namespace ce
+
+} // namespace objectives
+
+#endif /* _ITEM_COMPONENT_EDITOR_H_ */
