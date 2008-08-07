96eab02b7b018ef4bda03bf24f21ee1bd8896620
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2008-08-07 18:48:10 +0000

    Added CustomComponentEditor.

---
+++ plugins/dm.objectives/ce/CustomComponentEditor.h
@@ -0,0 +1,77 @@
+#ifndef CUSTOM_COMPONENT_EDITOR_H_
+#define CUSTOM_COMPONENT_EDITOR_H_
+
+#include "ComponentEditor.h"
+#include "ComponentEditorFactory.h"
+#include "SpecifierEditCombo.h"
+#include "../ComponentType.h"
+
+namespace objectives {
+
+namespace ce {
+
+/**
+ * ComponentEditor subclass for COMP_CUSTOM_ASYNC component type.
+ * 
+ * An COMP_CUSTOM_ASYNC component requires no specifiers, the state of these
+ * components are controlled manually (by script or trigger).
+ */
+class CustomComponentEditor : 
+	public ComponentEditor
+{
+	// Registration class
+	static struct RegHelper 
+	{
+		RegHelper() {
+			ComponentEditorFactory::registerType(
+				objectives::ComponentType::COMP_CUSTOM_ASYNC().getName(), 
+				ComponentEditorPtr(new CustomComponentEditor())
+			);
+		}
+	} regHelper;
+	
+	// Main widget
+	GtkWidget* _widget;
+	
+	// Component to edit
+	Component* _component;
+	
+public:
+
+	/**
+	 * Construct a default CustomComponentEditor.
+	 */
+	CustomComponentEditor() : 
+		_component(NULL)
+	{}
+	
+	/**
+	 * Construct an CustomComponentEditor with a Component object to display and
+	 * edit.
+	 * 
+	 * @param component
+	 * The Component to edit.
+	 */
+	CustomComponentEditor(Component& component);
+	
+	/**
+	 * Destructor
+	 */
+	~CustomComponentEditor();
+	
+	/* ComponentEditor implementation */
+	
+	ComponentEditorPtr clone(Component& component) const {
+		return ComponentEditorPtr(new CustomComponentEditor(component));
+	}
+	
+	GtkWidget* getWidget() const; 
+
+    void writeToComponent() const;
+};
+
+} // namespace ce
+
+} // namespace objectives
+
+#endif /* CUSTOM_COMPONENT_EDITOR_H_ */
