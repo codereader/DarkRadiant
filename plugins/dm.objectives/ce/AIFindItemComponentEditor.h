ddcd60253e1823309297a4bc56e773da9ff8e5bf
codereader <greebo@angua.at>
2008-08-08 05:41:22 +0000

    Added AIFindItemComponentEditor.

---
+++ plugins/dm.objectives/ce/AIFindItemComponentEditor.h
@@ -0,0 +1,75 @@
+#ifndef AI_FIND_ITEM_COMPONENT_EDITOR_H_
+#define AI_FIND_ITEM_COMPONENT_EDITOR_H_
+#include "ComponentEditor.h"
+#include "ComponentEditorFactory.h"
+#include "SpecifierEditCombo.h"
+#include "../ComponentType.h"
+
+namespace objectives {
+
+namespace ce {
+
+/**
+ * ComponentEditor subclass for COMP_AI_FIND_ITEM component type.
+ * 
+ * An COMP_AI_FIND_ITEM component doesn't use specifiers.
+ */
+class AIFindItemComponentEditor : 
+	public ComponentEditor
+{
+	// Registration class
+	static struct RegHelper 
+	{
+		RegHelper() {
+			ComponentEditorFactory::registerType(
+				objectives::ComponentType::COMP_AI_FIND_ITEM().getName(), 
+				ComponentEditorPtr(new AIFindItemComponentEditor())
+			);
+		}
+	} regHelper;
+	
+	// Main widget
+	GtkWidget* _widget;
+	
+	// Component to edit
+	Component* _component;	
+	
+public:
+
+	/**
+	 * Construct a default AIFindItemComponentEditor.
+	 */
+	AIFindItemComponentEditor() : 
+		_component(NULL)
+	{}
+	
+	/**
+	 * Construct a AIFindItemComponentEditor with a Component object to display and
+	 * edit.
+	 * 
+	 * @param component
+	 * The Component to edit.
+	 */
+	AIFindItemComponentEditor(Component& component);
+	
+	/**
+	 * Destructor
+	 */
+	~AIFindItemComponentEditor();
+	
+	/* ComponentEditor implementation */
+	
+	ComponentEditorPtr clone(Component& component) const {
+		return ComponentEditorPtr(new AIFindItemComponentEditor(component));
+	}
+	
+	GtkWidget* getWidget() const; 
+
+    void writeToComponent() const;
+};
+
+} // namespace ce
+
+} // namespace objectives
+
+#endif /* AI_FIND_ITEM_COMPONENT_EDITOR_H_ */
