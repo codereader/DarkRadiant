8c15e2c386cc7a2e94c599882230aa6df0da559b
codereader <greebo@angua.at>
2008-08-08 15:31:03 +0000

    Added ClassnameSpecifierPanel.

---
+++ plugins/dm.objectives/ce/specpanel/ClassnameSpecifierPanel.cpp
@@ -0,0 +1,12 @@
+#include "ClassnameSpecifierPanel.h"
+
+namespace objectives {
+
+namespace ce {
+
+// Reg helper
+ClassnameSpecifierPanel::RegHelper ClassnameSpecifierPanel::_regHelper;
+
+} // namespace ce
+
+} // namespace objectives
