efdd945cf61834db92e4073836e2957889606fd8
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2008-08-08 15:33:31 +0000

    Added SpawnClassSpecifierPanel.

---
+++ plugins/dm.objectives/ce/specpanel/SpawnclassSpecifierPanel.cpp
@@ -0,0 +1,12 @@
+#include "SpawnClassSpecifierPanel.h"
+
+namespace objectives {
+
+namespace ce {
+
+// Reg helper
+SpawnClassSpecifierPanel::RegHelper SpawnClassSpecifierPanel::_regHelper;
+
+} // namespace ce
+
+} // namespace objectives
---
+++ plugins/dm.objectives/ce/specpanel/SpawnclassSpecifierPanel.h
@@ -0,0 +1,33 @@
+#ifndef SPAWNCLASS_SPECIFIER_PANEL_H_
+#define SPAWNCLASS_SPECIFIER_PANEL_H_
+
+#include "SpecifierPanel.h"
+#include "TextSpecifierPanel.h"
+
+namespace objectives {
+
+namespace ce {
+
+/**
+ * SpecifierPanel subclass for the SPEC_SPAWNCLASS (name of C++ class) specifier
+ * type.
+ */
+class SpawnClassSpecifierPanel :
+	public TextSpecifierPanel
+{
+	// Map registration
+	static struct RegHelper {
+		RegHelper() { 
+			SpecifierPanelFactory::registerType(
+				SpecifierType::SPEC_SPAWNCLASS().getName(),
+				SpecifierPanelPtr(new SpawnClassSpecifierPanel())
+			);
+		}
+	} _regHelper;
+};
+
+} // namespace ce
+
+} // namespace objectives
+
+#endif /* SPAWNCLASS_SPECIFIER_PANEL_H_ */
