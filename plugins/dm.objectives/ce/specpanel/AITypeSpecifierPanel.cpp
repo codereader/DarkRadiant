9b4cde036ebe1d1b6b7c1f3127f469455cf348bf
codereader <greebo@angua.at>
2008-08-08 15:37:51 +0000

    Added AITypeSpecifierPanel.

---
+++ plugins/dm.objectives/ce/specpanel/AITypeSpecifierPanel.cpp
@@ -0,0 +1,12 @@
+#include "AITypeSpecifierPanel.h"
+
+namespace objectives {
+
+namespace ce {
+
+// Reg helper
+AITypeSpecifierPanel::RegHelper AITypeSpecifierPanel::_regHelper;
+
+} // namespace ce
+
+} // namespace objectives
---
+++ plugins/dm.objectives/ce/specpanel/AITypeSpecifierPanel.h
@@ -0,0 +1,33 @@
+#ifndef AI_TYPE_SPECIFIERPANEL_H_
+#define AI_TYPE_SPECIFIERPANEL_H_
+
+#include "SpecifierPanel.h"
+#include "TextSpecifierPanel.h"
+
+namespace objectives {
+
+namespace ce {
+
+/**
+ * SpecifierPanel subclass for the SPEC_AI_TYPE (guards, civilians(?)) specifier
+ * type.
+ */
+class AITypeSpecifierPanel : 
+	public TextSpecifierPanel
+{
+	// Map registration
+	static struct RegHelper {
+		RegHelper() { 
+			SpecifierPanelFactory::registerType(
+				SpecifierType::SPEC_AI_TYPE().getName(),
+				SpecifierPanelPtr(new AITypeSpecifierPanel())
+			);
+		}
+	} _regHelper;
+};
+
+} // namespace ce
+
+} // namespace objectives
+
+#endif /* AI_TYPE_SPECIFIERPANEL_H_ */
