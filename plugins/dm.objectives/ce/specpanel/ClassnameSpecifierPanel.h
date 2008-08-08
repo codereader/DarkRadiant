ded7f752abbac4209caaf4882ff9f071b818916c
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2008-08-08 15:31:03 +0000

    Added ClassnameSpecifierPanel.

---
+++ plugins/dm.objectives/ce/specpanel/ClassnameSpecifierPanel.h
@@ -0,0 +1,33 @@
+#ifndef CLASSNAME_SPECIFIER_PANEL_H_
+#define CLASSNAME_SPECIFIER_PANEL_H_
+
+#include "SpecifierPanel.h"
+#include "TextSpecifierPanel.h"
+
+namespace objectives {
+
+namespace ce {
+
+/**
+ * SpecifierPanel subclass for the SPEC_CLASSNAME (name of entityDef) specifier
+ * type.
+ */
+class ClassnameSpecifierPanel :
+	public TextSpecifierPanel
+{
+	// Map registration
+	static struct RegHelper {
+		RegHelper() { 
+			SpecifierPanelFactory::registerType(
+				SpecifierType::SPEC_CLASSNAME().getName(),
+				SpecifierPanelPtr(new ClassnameSpecifierPanel())
+			);
+		}
+	} _regHelper;
+};
+
+} // namespace ce
+
+} // namespace objectives
+
+#endif /* CLASSNAME_SPECIFIER_PANEL_H_ */
