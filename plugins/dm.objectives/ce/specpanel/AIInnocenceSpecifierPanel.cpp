98ecce315cead99255eacd5f09d8b251dba76141
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2008-08-08 17:43:15 +0000

    Added AIInnocenceSpecifierPanel.

---
+++ plugins/dm.objectives/ce/specpanel/AIInnocenceSpecifierPanel.cpp
@@ -0,0 +1,12 @@
+#include "AIInnocenceSpecifierPanel.h"
+
+namespace objectives {
+
+namespace ce {
+
+// Reg helper
+AIInnocenceSpecifierPanel::RegHelper AIInnocenceSpecifierPanel::_regHelper;
+
+} // namespace ce
+
+} // namespace objectives
---
+++ plugins/dm.objectives/ce/specpanel/AIInnocenceSpecifierPanel.h
@@ -0,0 +1,32 @@
+#ifndef AI_INNOCENCE_SPECIFIERPANEL_H_
+#define AI_INNOCENCE_SPECIFIERPANEL_H_
+
+#include "SpecifierPanel.h"
+#include "TextSpecifierPanel.h"
+
+namespace objectives {
+
+namespace ce {
+
+/**
+ * SpecifierPanel subclass for the SPEC_AI_INNOCENCE specifier type.
+ */
+class AIInnocenceSpecifierPanel : 
+	public TextSpecifierPanel
+{
+	// Map registration
+	static struct RegHelper {
+		RegHelper() { 
+			SpecifierPanelFactory::registerType(
+				SpecifierType::SPEC_AI_INNOCENCE().getName(),
+				SpecifierPanelPtr(new AIInnocenceSpecifierPanel())
+			);
+		}
+	} _regHelper;
+};
+
+} // namespace ce
+
+} // namespace objectives
+
+#endif /* AI_INNOCENCE_SPECIFIERPANEL_H_ */
