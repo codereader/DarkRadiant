b7b3d0e3cfb8e45fb397727cb399ef8601a7b809
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2008-08-09 18:09:33 +0000

    PickpocketComponentEditor is complete.

--- plugins/dm.objectives/ce/PickpocketComponentEditor.cpp
+++ plugins/dm.objectives/ce/PickpocketComponentEditor.cpp
@@ -4,6 +4,7 @@
 
 #include "gtkutil/LeftAlignment.h"
 #include "gtkutil/LeftAlignedLabel.h"
+#include "string/string.h"
 
 #include <gtk/gtk.h>
 
@@ -17,7 +18,8 @@ PickpocketComponentEditor::RegHelper PickpocketComponentEditor::regHelper;
 // Constructor
 PickpocketComponentEditor::PickpocketComponentEditor(Component& component) :
 	_component(&component),
-	_itemSpec(SpecifierType::SET_ITEM())
+	_itemSpec(SpecifierType::SET_ITEM()),
+	_amount(gtk_spin_button_new_with_range(0, 65535, 1))
 {
 	// Main vbox
 	_widget = gtk_vbox_new(FALSE, 6);
@@ -27,14 +29,26 @@ PickpocketComponentEditor::PickpocketComponentEditor(Component& component) :
         gtkutil::LeftAlignedLabel("<b>Item:</b>"),
         FALSE, FALSE, 0
     );
-	gtk_box_pack_start(
-		GTK_BOX(_widget), _itemSpec.getWidget(), FALSE, FALSE, 0
-	);
+	// Create a new hbox for the targetcombo and the amount spin button
+	GtkWidget* hbox = gtk_hbox_new(FALSE, 6);
+	gtk_box_pack_start(GTK_BOX(_widget), hbox, FALSE, FALSE, 0);
+
+	gtk_spin_button_set_digits(GTK_SPIN_BUTTON(_amount), 0);
+
+	gtk_box_pack_start(GTK_BOX(hbox), _itemSpec.getWidget(), TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(hbox), gtkutil::LeftAlignedLabel("Amount:"), FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(hbox), _amount, FALSE, FALSE, 0);
 
     // Populate the SpecifierEditCombo with the first specifier
     _itemSpec.setSpecifier(
         component.getSpecifier(Specifier::FIRST_SPECIFIER)
     );
+
+	// Initialise the spin button with the value from the first component argument
+	gtk_spin_button_set_value(
+		GTK_SPIN_BUTTON(_amount), 
+		strToDouble(component.getArgument(0))
+	);
 }
 
 // Destructor
@@ -55,6 +69,9 @@ void PickpocketComponentEditor::writeToComponent() const {
     _component->setSpecifier(
         Specifier::FIRST_SPECIFIER, _itemSpec.getSpecifier()
     );
+
+	_component->setArgument(0, 
+		doubleToStr(gtk_spin_button_get_value(GTK_SPIN_BUTTON(_amount))));
 }
 
 } // namespace ce
