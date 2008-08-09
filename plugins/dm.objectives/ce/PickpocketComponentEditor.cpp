4a2d19ea96594c177485aaf99accffd8f6c0d588
codereader <greebo@angua.at>
2008-08-09 18:51:31 +0000

    Changed layout of PickpocketComponentEditor to display the amount below the item.

--- plugins/dm.objectives/ce/PickpocketComponentEditor.cpp
+++ plugins/dm.objectives/ce/PickpocketComponentEditor.cpp
@@ -21,23 +21,16 @@ PickpocketComponentEditor::PickpocketComponentEditor(Component& component) :
 	_itemSpec(SpecifierType::SET_ITEM()),
 	_amount(gtk_spin_button_new_with_range(0, 65535, 1))
 {
+	gtk_spin_button_set_digits(GTK_SPIN_BUTTON(_amount), 0);
+
 	// Main vbox
 	_widget = gtk_vbox_new(FALSE, 6);
 
-    gtk_box_pack_start(
-        GTK_BOX(_widget), 
-        gtkutil::LeftAlignedLabel("<b>Item:</b>"),
-        FALSE, FALSE, 0
-    );
-	// Create a new hbox for the targetcombo and the amount spin button
-	GtkWidget* hbox = gtk_hbox_new(FALSE, 6);
-	gtk_box_pack_start(GTK_BOX(_widget), hbox, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(_widget), gtkutil::LeftAlignedLabel("<b>Item:</b>"), FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(_widget), _itemSpec.getWidget(), FALSE, FALSE, 0);
 
-	gtk_spin_button_set_digits(GTK_SPIN_BUTTON(_amount), 0);
-
-	gtk_box_pack_start(GTK_BOX(hbox), _itemSpec.getWidget(), TRUE, TRUE, 0);
-	gtk_box_pack_start(GTK_BOX(hbox), gtkutil::LeftAlignedLabel("Amount:"), FALSE, FALSE, 0);
-	gtk_box_pack_start(GTK_BOX(hbox), _amount, FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(_widget), gtkutil::LeftAlignedLabel("<b>Amount:</b>"), FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(_widget), gtkutil::LeftAlignment(_amount), FALSE, FALSE, 0);
 
     // Populate the SpecifierEditCombo with the first specifier
     _itemSpec.setSpecifier(
