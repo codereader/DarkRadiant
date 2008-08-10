54297e2671f06bca09ecae9fbf2d0c8ce40963f2
codereader <greebo@angua.at>
2008-08-10 18:24:51 +0000

    LogicEditors are properly placed in the MissionLogicDialog, but not yet functional.

--- plugins/dm.objectives/MissionLogicDialog.cpp
+++ plugins/dm.objectives/MissionLogicDialog.cpp
@@ -4,6 +4,7 @@
 #include "gtkutil/LeftAlignedLabel.h"
 #include "gtkutil/LeftAlignment.h"
 #include "gtkutil/RightAlignment.h"
+#include "string/string.h"
 
 #include <gtk/gtk.h>
 
@@ -12,6 +13,13 @@ namespace objectives {
 namespace {
 
 	const char* const DIALOG_TITLE = "Edit Mission Logic";
+
+	const std::string STANDARD_LOGIC_DESCR = 
+		"This is the standard logic for all difficulty levels";
+
+	const std::string DIFFICULTY_LOGIC_DESCR =
+		"These logics override the standard logic for the given difficulty level\n" \
+		"if the logic string is non-empty.";
 
 	// Widget enum
 	enum {
@@ -25,29 +33,41 @@ MissionLogicDialog::MissionLogicDialog(GtkWindow* parent, ObjectiveEntity& objec
 	gtkutil::BlockingTransientWindow(DIALOG_TITLE, parent),
 	_objectiveEnt(objectiveEnt)
 {
-	// Dialog contains list view, edit panel and buttons
+	// Create one logic editor for each difficulty level plus the default one
+	createLogicEditors();
+
+	// Overall VBox for labels and alignments
 	GtkWidget* vbx = gtk_vbox_new(FALSE, 12);
 	
 	gtk_box_pack_start(GTK_BOX(vbx), gtkutil::LeftAlignedLabel("<b>Default Logic</b>"), FALSE, FALSE, 0);
 
 	// Default Logic
 	GtkWidget* defaultVBox = gtk_vbox_new(FALSE, 6);
+	gtk_box_pack_start(GTK_BOX(defaultVBox), gtkutil::LeftAlignedLabel(STANDARD_LOGIC_DESCR), FALSE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(defaultVBox), _logicEditors[-1]->getWidget(), TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(vbx), gtkutil::LeftAlignment(defaultVBox, 12, 1.0f) , TRUE, TRUE, 0);
+	
+	// Now add all difficulty-specific editors
+	gtk_box_pack_start(GTK_BOX(vbx), gtkutil::LeftAlignedLabel("<b>Difficulty-specific Logic</b>"), FALSE, FALSE, 0);
 
-	gtk_box_pack_start(GTK_BOX(defaultVBox), 
-		gtkutil::LeftAlignedLabel("Standard logic for all difficulty levels"), FALSE, FALSE, 0);
+	GtkWidget* diffVBox = gtk_vbox_new(FALSE, 6);
+	gtk_box_pack_start(GTK_BOX(diffVBox), gtkutil::LeftAlignedLabel(DIFFICULTY_LOGIC_DESCR), FALSE, FALSE, 0);
 
-	// Create the default logic editor
-	_logicEditors[-1] = LogicEditorPtr(new LogicEditor);
+	// Iterate over all editors for levels that are greater or equal 0
+	for (LogicEditorMap::iterator i = _logicEditors.lower_bound(0);
+		 i != _logicEditors.end(); i++)
+	{
+		gtk_box_pack_start(GTK_BOX(diffVBox), 
+			gtkutil::LeftAlignedLabel("<b>Logic for Difficulty Level " + intToStr(i->first) + "</b>"), 
+			FALSE, FALSE, 0
+		);
+		gtk_box_pack_start(GTK_BOX(diffVBox), i->second->getWidget(), TRUE, TRUE, 0);
+	}
 
-	GtkWidget* compvbox = gtk_vbox_new(FALSE, 6);
-	//gtk_box_pack_start(GTK_BOX(compvbox), createListView(), TRUE, TRUE, 0);
-	//gtk_box_pack_start(GTK_BOX(compvbox), createEditPanel(), FALSE, FALSE, 0);
-	//gtk_box_pack_start(GTK_BOX(compvbox), createComponentEditorPanel(), TRUE, TRUE, 0);
-
-	gtk_box_pack_start(GTK_BOX(vbx), gtkutil::LeftAlignment(compvbox, 12, 1.0f) , TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(vbx), gtkutil::LeftAlignment(diffVBox, 12, 1.0f) , TRUE, TRUE, 0);
 
 	gtk_box_pack_start(GTK_BOX(vbx), gtk_hseparator_new(), FALSE, FALSE, 0);
-	//gtk_box_pack_end(GTK_BOX(vbx), createButtons(), FALSE, FALSE, 0);
+	gtk_box_pack_end(GTK_BOX(vbx), createButtons(), FALSE, FALSE, 0);
 	
 	// Populate the list of components
 	//populateObjectiveEditPanel();
@@ -56,6 +76,17 @@ MissionLogicDialog::MissionLogicDialog(GtkWindow* parent, ObjectiveEntity& objec
 	// Add contents to main window
 	gtk_container_set_border_width(GTK_CONTAINER(getWindow()), 12);
 	gtk_container_add(GTK_CONTAINER(getWindow()), vbx);
+}
+
+void MissionLogicDialog::createLogicEditors() {
+	// Create the default logic editor
+	_logicEditors[-1] = LogicEditorPtr(new LogicEditor);
+	
+	// TODO: Connect this plugin to the difficulty plugin (which can be optional)
+	// to find out how many difficulty levels there are (and what their names are)
+	_logicEditors[0] = LogicEditorPtr(new LogicEditor);
+	_logicEditors[1] = LogicEditorPtr(new LogicEditor);
+	_logicEditors[2] = LogicEditorPtr(new LogicEditor);
 }
 
 // Create buttons
