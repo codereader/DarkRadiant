3805aff4eb8991dc71603ffce4cf83f731eb3f57
codereader <greebo@angua.at>
2008-12-03 21:23:51 +0000

    Removed old ireference.h file.

--- radiant/referencecache.cpp
+++ radiant/referencecache.cpp
@@ -32,7 +32,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "ifilesystem.h"
 #include "iarchive.h"
 #include "ifiletypes.h"
-#include "ireference.h"
 #include "ientity.h"
 #include "iradiant.h"
 #include "imodule.h"
