7f1e34a90b887c4c6cd583814bbf9bd1ad49b418
codereader <greebo@angua.at>
2008-12-04 18:49:10 +0000

    Removed old Select_GetMid() method.

--- radiant/select.h
+++ radiant/select.h
@@ -25,7 +25,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "math/Vector3.h"
 
 void Select_GetBounds(Vector3& mins, Vector3& maxs);
-void Select_GetMid(Vector3& mid);
 
 void Selection_Flipx();
 void Selection_Flipy();
