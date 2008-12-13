46351685fe794d5b500195d837559291c53a972a
codereader <greebo@angua.at>
2008-12-13 08:58:31 +0000

    Adjusted and cleaned post build event script for win64 target.

--- tools/vcprojects/post_build_event_x64.bat
+++ tools/vcprojects/post_build_event_x64.bat
@@ -3,22 +3,16 @@ copy ..\..\w32deps\libxml2\lib\libxml2.dll ..\..\install
 copy ..\..\w64deps\openal\bin\OpenAL32.dll ..\..\install
 copy ..\..\w64deps\openal\bin\wrap_oal.dll ..\..\install
 
-copy ..\..\w32deps\libpng\bin\libpng13.dll ..\..\install
-copy ..\..\w32deps\libiconv\lib\libiconv2.dll ..\..\install
-
+rem Copy everything from w32deps etc folder
 md ..\..\install\etc
 xcopy ..\..\w32deps\gtk2\bin\etc\* ..\..\install\etc\. /S /Y
+
+rem The LIB folder containing the engines should come from the w64deps folder
 md ..\..\install\lib
-xcopy ..\..\w32deps\gtk2\bin\lib\* ..\..\install\lib\. /S /Y
-copy ..\..\w32deps\gtk2\bin\iconv.dll ..\..\install
-copy ..\..\w32deps\gtk2\bin\intl.dll ..\..\install
-copy ..\..\w32deps\gtk2\bin\jpeg62.dll ..\..\install
+xcopy ..\..\w64deps\gtk2\bin\lib\* ..\..\install\lib\. /S /Y
 
 copy ..\..\w64deps\gtk2\bin\libatk-1.0-0.dll ..\..\install
 copy ..\..\w64deps\gtk2\bin\libcairo-2.dll ..\..\install
-
-copy ..\..\w32deps\gtk2\bin\libfontconfig-1.dll ..\..\install
-
 copy ..\..\w64deps\gtk2\bin\libjpeg-62.dll ..\..\install
 copy ..\..\w64deps\gtk2\bin\libpng12-0.dll ..\..\install
 copy ..\..\w64deps\gtk2\bin\libtiff.dll ..\..\install
@@ -33,9 +27,5 @@ copy ..\..\w64deps\gtk2\bin\libpango-1.0-0.dll ..\..\install
 copy ..\..\w64deps\gtk2\bin\zlib1.dll ..\..\install
 copy ..\..\w64deps\gtk2\bin\libpangocairo-1.0-0.dll ..\..\install
 copy ..\..\w64deps\gtk2\bin\libpangowin32-1.0-0.dll ..\..\install
-
-copy ..\..\w32deps\gtk2\bin\libpangoft2-1.0-0.dll ..\..\install
-copy ..\..\w32deps\gtk2\bin\libgdkglext-win32-1.0-0.dll ..\..\install
-copy ..\..\w32deps\gtk2\bin\libgtkglext-win32-1.0-0.dll ..\..\install
 
 copy ..\..\w64deps\glew\lib\glew32.dll ..\..\install
