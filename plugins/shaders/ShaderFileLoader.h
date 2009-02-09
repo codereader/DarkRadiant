ead3f71017705eda9ea4f73e1d31e4d11ee07a04
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2009-02-09 17:58:21 +0000

    Removed some old stuff from ShaderFileLoader.

--- plugins/shaders/ShaderFileLoader.h
+++ plugins/shaders/ShaderFileLoader.h
@@ -20,18 +20,8 @@ class ShaderFileLoader
 	
 private:
 
-	/*Parses the contents of a material definition. The shader name and opening
- 	 * brace "{" will already have been removed when this function is called. */
-	void parseShaderDecl(parser::DefTokeniser& tokeniser, 
-					  ShaderTemplatePtr shaderTemplate, 
-					  const std::string& filename); 
-
 	// Parse a shader file with the given contents and filename
 	void parseShaderFile(std::istream& inStr, const std::string& filename);
-
-	// Parse the name of a shader and return a ShaderTemplate ready to be
-	// filled in with parsed data
-	ShaderTemplatePtr parseShaderName(std::string& name); 
 
 	// Parse a "table" definition in a shader file
 	void parseShaderTable(parser::DefTokeniser& tokeniser);
