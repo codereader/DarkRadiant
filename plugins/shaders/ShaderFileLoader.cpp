a9cbbb0d812ad3dbc490494f6377f8f8b3321ace
codereader <greebo@angua.at>
2009-02-09 17:58:21 +0000

    Removed some old stuff from ShaderFileLoader.

--- plugins/shaders/ShaderFileLoader.cpp
+++ plugins/shaders/ShaderFileLoader.cpp
@@ -15,48 +15,12 @@
 
 namespace shaders {
 
-/**
- * Parses the contents of a material definition. The shader name and opening
- * brace "{" will already have been removed when this function is called.
- * 
- * @param tokeniser
- * DefTokeniser to retrieve tokens from.
- * 
- * @param shaderTemplate
- * An empty ShaderTemplate which will parse the token stream and populate
- * itself.
- * 
- * @param filename
- * The name of the shader file we are parsing.
- */
-void ShaderFileLoader::parseShaderDecl(parser::DefTokeniser& tokeniser, 
-					  ShaderTemplatePtr shaderTemplate, 
-					  const std::string& filename) 
-{
-	/*// Get the ShaderTemplate to populate itself by parsing tokens from the
-	// DefTokeniser. This may throw exceptions.	
-	shaderTemplate->parseDoom3(tokeniser);
-	
-	// Construct the ShaderDefinition wrapper class
-	ShaderDefinition def(shaderTemplate, filename);
-	
-	// Get the parsed shader name
-	std::string name = shaderTemplate->getName();
-	
-	// Insert into the definitions map, if not already present
-    if (!GetShaderLibrary().addDefinition(name, def)) {
-    	std::cout << "[shaders] " << filename << ": shader " << name
-    			  << " already defined." << std::endl;
-    }*/
-}
-
 /* Parses through the shader file and processes the tokens delivered by 
  * DefTokeniser. 
  */ 
 void ShaderFileLoader::parseShaderFile(std::istream& inStr, 
 									   const std::string& filename)
 {
-#if 1
 	// Parse the file with a blocktokeniser, the actual block contents
 	// will be parsed separately.
 	parser::BasicDefBlockTokeniser<std::istream> tokeniser(inStr);
@@ -83,71 +47,7 @@ void ShaderFileLoader::parseShaderFile(std::istream& inStr,
 				<< ": shader " << block.name << " already defined." << std::endl;
 		}
 	}
-#else
-	// Create the tokeniser
-	parser::BasicDefTokeniser<std::istream> tokeniser(inStr, 
-													  " \t\n\v\r", 
-													  "{}(),");
-	
-	while (tokeniser.hasMoreTokens()) {
-		// Load the first token, it should be a name
-		std::string token = tokeniser.nextToken();		
-
-		if (token == "table") {
-			parseShaderTable(tokeniser);		 		
-		} 
-		else if (token[0] == '{' || token[0] == '}') {
-			// This is not supposed to happen, as the shaderName is still 
-			// undefined
-			std::cerr << "[shaders] Missing shadername in " 
-					  << filename << std::endl;
-			return; 
-		} 
-		else if (token == "material") {
-			// Discard optional "material" keyword
-			token = tokeniser.nextToken();
-		}
-		else {			
-			// We are still outside of any braces, so this must be a shader name			
-			ShaderTemplatePtr shaderTemplate = parseShaderName(token);
-
-			// Try to parse the rest of the decl, catching and printing any
-			// exception
-			try {
-				tokeniser.assertNextToken("{");
-				parseShaderDecl(tokeniser, shaderTemplate, filename);
-			}
-			catch (parser::ParseException e) {
-				std::cerr << "[shaders] " << filename << ": Failed to parse \"" 
-						  << shaderTemplate->getName() << "\" (" << e.what() 
-						  << ")" << std::endl;
-				return;
-			}
-		}
-	}
-#endif
 }
-
-/* Normalises a given (raw) shadername (slash conversion, extension removal, ...) 
- * and inserts a new shader 
- */
-ShaderTemplatePtr ShaderFileLoader::parseShaderName(std::string& rawName) 
-{
-	/*boost::algorithm::replace_all(rawName, "\\", "/"); // use forward slashes
-	// greebo: Don't use lowercase as this might screw the shader lookup
-	//boost::algorithm::to_lower(rawName); // use lowercase
-
-	// Remove the extension if present
-	size_t dotPos = rawName.rfind(".");
-	if (dotPos != std::string::npos) {
-		rawName = rawName.substr(0, dotPos);
-	}
-
-	// Construct and return the ShaderTemplate pointer	
-	ShaderTemplatePtr shaderTemplate(new ShaderTemplate(rawName));
-	return shaderTemplate;*/
-	return ShaderTemplatePtr();
-} 
 
 /* Parses through a table definition within a material file.
  * It just skips over the whole content 
