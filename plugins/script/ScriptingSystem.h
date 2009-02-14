ef85d80785bc0dd814e099940475371fd6acbc04
codereader <greebo@angua.at>
2009-02-14 13:08:12 +0000

    Some refactoring.

--- plugins/script/ScriptingSystem.h
+++ plugins/script/ScriptingSystem.h
@@ -2,13 +2,18 @@
 #define _SCRIPTING_SYSTEM_H_
 
 #include "imodule.h"
+#include "PythonConsoleWriter.h"
 
 namespace script {
 
 class ScriptingSystem :
 	public RegisterableModule
 {
+	PythonConsoleWriter _outputWriter;
+	PythonConsoleWriter _errorWriter;
 public:
+	ScriptingSystem();
+
 	// RegisterableModule implementation
 	virtual const std::string& getName() const;
 	virtual const StringSet& getDependencies() const;
