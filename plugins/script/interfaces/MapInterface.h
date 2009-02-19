30e28e83da5614400de4225ede8ac8541108abd5
codereader <greebo@angua.at>
2009-02-19 21:23:47 +0000

    Added MapInterface.

---
+++ plugins/script/interfaces/MapInterface.h
@@ -0,0 +1,28 @@
+#ifndef _MAP_INTERFACE_H_
+#define _MAP_INTERFACE_H_
+
+#include <boost/python.hpp>
+
+#include "iscript.h"
+
+#include "SceneGraphInterface.h"
+
+namespace script {
+
+/**
+ * greebo: This class provides the script interface for the GlobalEntityClassManager module.
+ */
+class MapInterface :
+	public IScriptInterface
+{
+public:
+	ScriptSceneNode getWorldSpawn();
+
+	// IScriptInterface implementation
+	void registerInterface(boost::python::object& nspace);
+};
+typedef boost::shared_ptr<MapInterface> MapInterfacePtr;
+
+} // namespace script
+
+#endif /* _MAP_INTERFACE_H_ */
