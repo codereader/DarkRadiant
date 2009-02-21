485dc489ca639e1d00f9ffedc5f31d7fdf40839c
codereader <greebo@angua.at>
2009-02-21 16:19:55 +0000

    WIP CommandSystem code.

--- plugins/commandsystem/CommandSystem.cpp
+++ plugins/commandsystem/CommandSystem.cpp
@@ -21,12 +21,58 @@ void CommandSystem::initialiseModule(const ApplicationContext& ctx) {
 
 void CommandSystem::shutdownModule() {
 	globalOutputStream() << "CommandSystem: shutting down.\n";
+
+	// Free all commands
+	_commands.clear();
 }
 
 void CommandSystem::addCommand(const std::string& name, 
 					CommandFunction func, 
 					const CommandSignature& signature)
 {
+	// Create a new command
+	CommandPtr cmd(new Command(func, signature));
+
+	std::pair<CommandMap::iterator, bool> result = _commands.insert(
+		CommandMap::value_type(name, cmd)
+	);
+
+	if (!result.second) {
+		globalErrorStream() << "Cannot register command " << name 
+			<< ", this command is already registered." << std::endl;
+	}
+}
+
+void CommandSystem::executeCommand(const std::string& name, const Argument& arg1) {
+	ArgumentList args(1);
+	args[0] = arg1;
+
+	executeCommand(name, args);
+}
+
+void CommandSystem::executeCommand(const std::string& name, const Argument& arg1, 
+	const Argument& arg2)
+{
+	ArgumentList args(2);
+	args[0] = arg1;
+	args[1] = arg2;
+
+	executeCommand(name, args);
+}
+
+void CommandSystem::executeCommand(const std::string& name, 
+	const Argument& arg1, const Argument& arg2, 
+	const Argument& arg3)
+{
+	ArgumentList args(2);
+	args[0] = arg1;
+	args[1] = arg2;
+	args[2] = arg3;
+
+	executeCommand(name, args);
+}
+
+void CommandSystem::executeCommand(const std::string& name, const ArgumentList& args) {
 	// TODO
 }
 
--- plugins/commandsystem/CommandSystem.h
+++ plugins/commandsystem/CommandSystem.h
@@ -2,16 +2,43 @@
 #define _COMMANDSYSTEM_H_
 
 #include "icommandsystem.h"
+#include <map>
 
 namespace cmd {
 
 class CommandSystem :
 	public ICommandSystem
 {
+	struct Command 
+	{
+		// The actual function to call
+		CommandFunction function;
+
+		// The number and types of arguments to use
+		CommandSignature signature;
+
+		Command(const CommandFunction& _func, const CommandSignature& _sign) :
+			function(_func),
+			signature(_sign)
+		{}
+	};
+	typedef boost::shared_ptr<Command> CommandPtr;
+
+	// The named commands
+	typedef std::map<std::string, CommandPtr> CommandMap;
+	CommandMap _commands;
+
 public:
 	void addCommand(const std::string& name, 
 					CommandFunction func, 
 					const CommandSignature& signature);
+
+	void executeCommand(const std::string& name, const Argument& arg1);
+	void executeCommand(const std::string& name, const Argument& arg1, const Argument& arg2);
+	void executeCommand(const std::string& name, const Argument& arg1, const Argument& arg2, const Argument& arg3);
+
+	// For more than 3 arguments, use this method to pass a vector of arguments
+	void executeCommand(const std::string& name, const ArgumentList& args);
 
 	// RegisterableModule implementation
 	const std::string& getName() const;
