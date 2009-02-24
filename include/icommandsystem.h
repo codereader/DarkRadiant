0dd4bb6547d79c640bf127af2c200de8a89e2fb5
codereader <greebo@angua.at>
2009-02-24 17:38:30 +0000

    Added type safety checks to commands.

--- include/icommandsystem.h
+++ include/icommandsystem.h
@@ -12,14 +12,17 @@
 
 namespace cmd {
 
-enum ArgumentType
+// Use these to define argument types
+enum ArgumentTypeFlags
 {
-	ARGTYPE_STRING,
-	ARGTYPE_INT,
-	ARGTYPE_DOUBLE,
-	ARGTYPE_VECTOR3,
-	ARGTYPE_VECTOR2,
-	NUM_ARGTYPES,
+	ARGTYPE_VOID		= 0,
+	ARGTYPE_STRING		= 1 << 0,
+	ARGTYPE_INT			= 1 << 1,
+	ARGTYPE_DOUBLE		= 1 << 2,
+	ARGTYPE_VECTOR3		= 1 << 3,
+	ARGTYPE_VECTOR2		= 1 << 4,
+	// future types go here
+	ARGTYPE_OPTIONAL	= 1 << 16,
 };
 
 // One command argument, provides several getter methods
@@ -31,12 +34,16 @@ class Argument
 	Vector3 _vector3Value;
 	Vector2 _vector2Value;
 
+	// The type flags
+	std::size_t _type;
+
 public:
 	Argument() :
 		_doubleValue(0),
 		_intValue(0),
 		_vector3Value(0,0,0),
-		_vector2Value(0,0)
+		_vector2Value(0,0),
+		_type(ARGTYPE_VOID)
 	{}
 
 	Argument(const char* str) :
@@ -44,8 +51,12 @@ public:
 		_doubleValue(strToDouble(str)),
 		_intValue(strToInt(str)),
 		_vector3Value(Vector3(str)),
-		_vector2Value(Vector2(str))
-	{}
+		_vector2Value(Vector2(str)),
+		_type(ARGTYPE_STRING)
+	{
+		tryNumberConversion();
+		tryVectorConversion();
+	}
 
 	// String => Argument constructor
 	Argument(const std::string& str) :
@@ -53,8 +64,12 @@ public:
 		_doubleValue(strToDouble(str)),
 		_intValue(strToInt(str)),
 		_vector3Value(Vector3(str)),
-		_vector2Value(Vector2(str))
-	{}
+		_vector2Value(Vector2(str)),
+		_type(ARGTYPE_STRING)
+	{
+		tryNumberConversion();
+		tryVectorConversion();
+	}
 
 	// Double => Argument constructor
 	Argument(const double d) :
@@ -62,16 +77,23 @@ public:
 		_doubleValue(d),
 		_intValue(static_cast<int>(d)),
 		_vector3Value(d,d,d),
-		_vector2Value(d,d)
-	{}
+		_vector2Value(d,d),
+		_type(ARGTYPE_DOUBLE)
+	{
+		// Enable INT flag if double value is rounded
+		if (lrint(_doubleValue) == _intValue) {
+			_type |= ARGTYPE_INT;
+		}
+	}
 
-	// Double => Argument constructor
+	// Int => Argument constructor
 	Argument(const int i) :
 		_strValue(intToStr(i)),
 		_doubleValue(static_cast<double>(i)),
 		_intValue(i),
 		_vector3Value(i,i,i),
-		_vector2Value(i,i)
+		_vector2Value(i,i),
+		_type(ARGTYPE_INT|ARGTYPE_DOUBLE) // INT can be used as DOUBLE too
 	{}
 
 	// Vector3 => Argument constructor
@@ -80,7 +102,8 @@ public:
 		_doubleValue(v.getLength()),
 		_intValue(static_cast<int>(v.getLength())),
 		_vector3Value(v),
-		_vector2Value(v[0], v[1])
+		_vector2Value(v[0], v[1]),
+		_type(ARGTYPE_VECTOR3)
 	{}
 
 	// Vector2 => Argument constructor
@@ -89,7 +112,8 @@ public:
 		_doubleValue(v.getLength()),
 		_intValue(static_cast<int>(v.getLength())),
 		_vector3Value(v[0], v[1], 0),
-		_vector2Value(v)
+		_vector2Value(v),
+		_type(ARGTYPE_VECTOR2)
 	{}
 
 	// Copy Constructor
@@ -98,8 +122,13 @@ public:
 		_doubleValue(other._doubleValue),
 		_intValue(other._intValue),
 		_vector3Value(other._vector3Value),
-		_vector2Value(other._vector2Value)
+		_vector2Value(other._vector2Value),
+		_type(other._type)
 	{}
+
+	std::size_t getType() const {
+		return _type;
+	}
 
 	std::string getString() const {
 		return _strValue;
@@ -120,6 +149,49 @@ public:
 	Vector2 getVector2() const {
 		return _vector2Value;
 	}
+
+private:
+	void tryNumberConversion() {
+		// Try to cast the string value to numbers
+		try {
+			_intValue = boost::lexical_cast<int>(_strValue);
+			// cast succeeded
+			_type |= ARGTYPE_INT;
+		}
+		catch (boost::bad_lexical_cast) {}
+
+		try {
+			_doubleValue = boost::lexical_cast<double>(_strValue);
+			// cast succeeded
+			_type |= ARGTYPE_DOUBLE;
+		}
+		catch (boost::bad_lexical_cast) {}
+	}
+	
+	void tryVectorConversion() {
+		// Use a stringstream to parse the string
+        std::stringstream strm(_strValue);
+        strm << std::skipws;
+
+		// Try converting the first two values
+        strm >> _vector2Value.x();
+        strm >> _vector2Value.y();
+
+		if (!strm.fail()) {
+			_type |= ARGTYPE_VECTOR2;
+
+			// Try to parse the third value
+			strm >> _vector3Value.z();
+
+			if (!strm.fail()) {
+				// Third value successfully parsed
+				_type |= ARGTYPE_VECTOR3;
+				// Copy the two values from the parsed Vector2
+				_vector3Value.x() = _vector2Value.x();
+				_vector3Value.y() = _vector2Value.y();
+			}
+		}
+	}
 };
 
 typedef std::vector<Argument> ArgumentList;
@@ -135,29 +207,29 @@ typedef boost::function<void (const ArgumentList&)> Function;
 
 // A command signature consists just of arguments, no return types
 class Signature :
-	public std::vector<ArgumentType> 
+	public std::vector<std::size_t> 
 {
 public:
 	Signature()
 	{}
 
 	// Additional convenience constructors
-	Signature(ArgumentType type1) {
+	Signature(std::size_t type1) {
 		push_back(type1);
 	}
 
-	Signature(ArgumentType type1, ArgumentType type2) {
+	Signature(std::size_t type1, std::size_t type2) {
 		push_back(type1);
 		push_back(type2);
 	}
 
-	Signature(ArgumentType type1, ArgumentType type2, ArgumentType type3) {
+	Signature(std::size_t type1, std::size_t type2, std::size_t type3) {
 		push_back(type1);
 		push_back(type2);
 		push_back(type3);
 	}
 
-	Signature(ArgumentType type1, ArgumentType type2, ArgumentType type3, ArgumentType type4) {
+	Signature(std::size_t type1, std::size_t type2, std::size_t type3, std::size_t type4) {
 		push_back(type1);
 		push_back(type2);
 		push_back(type3);
