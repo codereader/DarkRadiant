8f4fbf1876453fa77e391a9862abae35cde8bf23
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2009-02-28 06:38:03 +0000

    More VFS functions exposed, added convenience script function readTextFile.

--- plugins/script/interfaces/FileSystemInterface.cpp
+++ plugins/script/interfaces/FileSystemInterface.cpp
@@ -1,6 +1,8 @@
 #include "FileSystemInterface.h"
 
 #include "generic/callback.h"
+#include "iarchive.h"
+#include "itextstream.h"
 
 namespace script {
 
@@ -15,6 +17,42 @@ void FileSystemInterface::forEachFile(const std::string& basedir,
 	);
 }
 
+std::string FileSystemInterface::readTextFile(const std::string& filename) {
+	ArchiveTextFilePtr file = GlobalFileSystem().openTextFile(filename);
+
+	if (file == NULL) return "";
+
+	TextInputStream& istream = file->getInputStream();
+
+	const std::size_t READSIZE = 16384;
+
+	std::string text;
+	char buffer[READSIZE];
+	std::size_t bytesRead = READSIZE;
+
+	do {
+		bytesRead = istream.read(buffer, READSIZE);
+
+		// Copy the stuff to the string
+		text.append(buffer, bytesRead);
+
+	} while (bytesRead == READSIZE);
+
+	return text;
+}
+
+int FileSystemInterface::getFileCount(const std::string& filename) {
+	return GlobalFileSystem().getFileCount(filename);
+}
+
+std::string FileSystemInterface::findFile(const std::string& name) {
+	return GlobalFileSystem().findFile(name);
+}
+
+std::string FileSystemInterface::findRoot(const std::string& name) {
+	return GlobalFileSystem().findRoot(name);
+}
+
 void FileSystemInterface::registerInterface(boost::python::object& nspace) {
 	// Expose the FileVisitor interface
 	nspace["FileVisitor"] = 
@@ -25,6 +63,10 @@ void FileSystemInterface::registerInterface(boost::python::object& nspace) {
 	// Add the VFS module declaration to the given python namespace
 	nspace["GlobalFileSystem"] = boost::python::class_<FileSystemInterface>("GlobalFileSystem")
 		.def("forEachFile", &FileSystemInterface::forEachFile)
+		.def("findFile", &FileSystemInterface::findFile)
+		.def("findRoot", &FileSystemInterface::findRoot)
+		.def("readTextFile", &FileSystemInterface::readTextFile)
+		.def("getFileCount", &FileSystemInterface::getFileCount)
 	;
 
 	// Now point the Python variable "GlobalFileSystem" to this instance
