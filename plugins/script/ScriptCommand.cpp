7429e4bd574efb45af1cb26551ef9c67445c2e2f
codereader <greebo@angua.at>
2009-03-02 15:10:14 +0000

    Added first python command which is dynamically added to the commandsystem by the script module.

--- plugins/script/ScriptCommand.cpp
+++ plugins/script/ScriptCommand.cpp
@@ -1,10 +1,19 @@
 #include "ScriptCommand.h"
 
+#include "icommandsystem.h"
+
 namespace script {
 
-ScriptCommand::ScriptCommand(const std::string& scriptFilename) :
+ScriptCommand::ScriptCommand(const std::string& name, const std::string& scriptFilename) :
+	_name(name),
 	_scriptFilename(scriptFilename)
 {
+	// Register this with the command system
+	GlobalCommandSystem().addStatement(_name, "RunScriptCommand " + _name);
+}
+
+ScriptCommand::~ScriptCommand() {
+	GlobalCommandSystem().removeCommand(_name);
 }
 
 } // namespace script
