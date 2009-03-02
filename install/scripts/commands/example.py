6188ab80b47fede232f31ac6a7c853677e468fcd
codereader <greebo@angua.at>
2009-03-02 15:23:33 +0000

    Renamed example python command script.

--- install/scripts/commands/example.py
+++ install/scripts/commands/example.py
@@ -1,6 +1,7 @@
 # Set the command name so that DarkRadiant recognises this file
-__commandName__ = 'ListShaders'
+__commandName__ = 'Example'
 
+# The actual algorithm called by DarkRadiant is contained in the execute() function
 def execute():
 	shader = GlobalShaderSystem.getShaderForName('bc_rat')
 	print(shader.getName())
