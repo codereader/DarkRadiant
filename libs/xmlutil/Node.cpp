a50a102aaa7b3c51b86d35f922db179690223da4
codereader <greebo@angua.at>
2009-03-30 12:32:55 +0000

    Fixed a memory leak in xml::Node.

--- libs/xmlutil/Node.cpp
+++ libs/xmlutil/Node.cpp
@@ -7,16 +7,17 @@ namespace xml
 
 Node::Node(xmlNodePtr node):
     _xmlNode(node)
-{
-}
+{}
 
 // Return the actual node ptr
-xmlNodePtr Node::getNodePtr() const {
+xmlNodePtr Node::getNodePtr() const
+{
 	return _xmlNode;
 }
 
 // Return the name of a node 
-const std::string Node::getName() const {
+const std::string Node::getName() const
+{
 	if (_xmlNode) {
 		return std::string( reinterpret_cast<const char*>(_xmlNode->name) );
 	}
@@ -26,8 +27,8 @@ const std::string Node::getName() const {
 }
 
 // Return a NodeList of all children of this node
-NodeList Node::getChildren() const {
-
+NodeList Node::getChildren() const
+{
     NodeList retval;
     
     // Iterate throught the list of children, adding each child node
@@ -40,7 +41,8 @@ NodeList Node::getChildren() const {
 }
 
 // Creates a new child with no content (i.e. <name />)
-Node Node::createChild(const std::string& name) {
+Node Node::createChild(const std::string& name)
+{
 	// Create a new child under the contained node
 	xmlNodePtr newChild = xmlNewChild(_xmlNode,	NULL, xmlCharStrdup(name.c_str()), NULL);
 	
@@ -50,8 +52,8 @@ Node Node::createChild(const std::string& name) {
 
 // Return a NodeList of named children of this node
 
-NodeList Node::getNamedChildren(const std::string& name) const {
-
+NodeList Node::getNamedChildren(const std::string& name) const
+{
     NodeList retval;
     
     // Iterate throught the list of children, adding each child node
@@ -63,19 +65,25 @@ NodeList Node::getNamedChildren(const std::string& name) const {
     }
     
     return retval;
-    
 }
 
 // Set the value of the given attribute
-void Node::setAttributeValue(const std::string& key, const std::string& value) {
-	xmlSetProp(_xmlNode, xmlCharStrdup(key.c_str()), xmlCharStrdup(value.c_str()));
+void Node::setAttributeValue(const std::string& key, const std::string& value)
+{
+	xmlChar* k = xmlCharStrdup(key.c_str());
+	xmlChar* v = xmlCharStrdup(value.c_str());
+
+	xmlSetProp(_xmlNode, k, v);
+
+	xmlMemFree(k);
+	xmlMemFree(v);
 }
 
 // Return the value of a given attribute, or throw AttributeNotFoundException
 // if the attribute does not exist.
 
-std::string Node::getAttributeValue(const std::string& key) const {
-
+std::string Node::getAttributeValue(const std::string& key) const
+{
     // Iterate through the chain of attributes to find the requested one.
     for (xmlAttrPtr attr = _xmlNode->properties; attr != NULL; attr = attr->next) {
         if (xmlStrcmp(attr->name, reinterpret_cast<const xmlChar*>(key.c_str())) == 0) {
@@ -85,13 +93,13 @@ std::string Node::getAttributeValue(const std::string& key) const {
 
     // Not found, return an empty string
     return "";
-    
 }
 
 // Return the textual content of a given node. This may be an empty string if there is no
 // content available.
 
-std::string Node::getContent() const {
+std::string Node::getContent() const
+{
 	if (_xmlNode->children && _xmlNode->children->content) {
 		return std::string(reinterpret_cast<const char*>(_xmlNode->children->content));
 	}
@@ -100,15 +108,19 @@ std::string Node::getContent() const {
 	}
 }
 
-void Node::addText(const std::string& text) {
+void Node::addText(const std::string& text)
+{
 	// Allocate a new text node
-	xmlNodePtr whitespace = xmlNewText(xmlCharStrdup(text.c_str()));
+	xmlNodePtr whitespace = xmlNewText(
+		reinterpret_cast<const xmlChar*>(text.c_str())
+	);
 	
 	// Add the newly allocated text as sibling of this node
 	xmlAddSibling(_xmlNode, whitespace);
 }
 
-void Node::erase() {
+void Node::erase()
+{
 	// unlink the node from the list first, otherwise: crashes ahead!
 	xmlUnlinkNode(_xmlNode);
 	
@@ -116,4 +128,4 @@ void Node::erase() {
 	xmlFreeNode(_xmlNode);
 }
 
-}
+} // namespace xml
