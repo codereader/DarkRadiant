eecc43bb7c137a44d1e374e793574187e5331a3f
Matthew Mott <orbweaver3d@gmail.com>
2009-04-07 09:11:21 +0000

    Moved some Matrix4 function bodies into .cpp file

--- libs/math/matrix.cpp
+++ libs/math/matrix.cpp
@@ -21,6 +21,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "matrix.h"
 
+// Named constructors
+
 // Identity matrix
 const Matrix4& Matrix4::getIdentity()
 {
@@ -33,8 +35,20 @@ const Matrix4& Matrix4::getIdentity()
     return _identity;
 }
 
+// Get a translation matrix for the given vector
+Matrix4 Matrix4::getTranslation(const Vector3& translation)
+{
+    return Matrix4(
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        translation.x(), translation.y(), translation.z(), 1
+    );
+}
+
 // Get a rotation from 2 vectors (named constructor)
-Matrix4 Matrix4::getRotation(const Vector3& a, const Vector3& b) {
+Matrix4 Matrix4::getRotation(const Vector3& a, const Vector3& b) 
+{
 	double angle = a.angle(b);
 	Vector3 axis = b.crossProduct(a).getNormalised();
 	// Pre-calculate the terms
@@ -50,5 +64,62 @@ Matrix4 Matrix4::getRotation(const Vector3& a, const Vector3& b) {
 		oneMinusCosPhi*z*x - sinPhi*y, oneMinusCosPhi*z*y + sinPhi*x, cosPhi + oneMinusCosPhi*z*z, 0,
 		0, 0, 0, 1
 	);
+}
+
+// Main explicit constructor
+Matrix4::Matrix4(double xx_, double xy_, double xz_, double xw_,
+                 double yx_, double yy_, double yz_, double yw_,
+                 double zx_, double zy_, double zz_, double zw_,
+                 double tx_, double ty_, double tz_, double tw_)
+{
+    xx() = xx_;
+    xy() = xy_;
+    xz() = xz_;
+    xw() = xw_;
+    yx() = yx_;
+    yy() = yy_;
+    yz() = yz_;
+    yw() = yw_;
+    zx() = zx_;
+    zy() = zy_;
+    zz() = zz_;
+    zw() = zw_;
+    tx() = tx_;
+    ty() = ty_;
+    tz() = tz_;
+    tw() = tw_;
+}
+
+// Transpose the matrix in-place
+void Matrix4::transpose()
+{
+    std::swap(_m[1], _m[4]); // xy <=> yx
+    std::swap(_m[2], _m[8]); // xz <=> zx
+    std::swap(_m[3], _m[12]); // xw <=> tx
+    std::swap(_m[6], _m[9]); // yz <=> zy
+    std::swap(_m[7], _m[13]); // yw <=> ty
+    std::swap(_m[11], _m[14]); // zw <=> tz
+}
+
+// Return transposed copy
+Matrix4 Matrix4::getTransposed() const
+{
+    return Matrix4(
+        xx(), yx(), zx(), tx(),
+        xy(), yy(), zy(), ty(),
+        xz(), yz(), zz(), tz(),
+        xw(), yw(), zw(), tw()
+    );
+}
+
+// Transform a vector
+Vector4 Matrix4::transform(const Vector4& vector4) const
+{
+    return Vector4(
+        _m[0] * vector4[0] + _m[4] * vector4[1] + _m[8]  * vector4[2] + _m[12] * vector4[3],
+        _m[1] * vector4[0] + _m[5] * vector4[1] + _m[9]  * vector4[2] + _m[13] * vector4[3],
+        _m[2] * vector4[0] + _m[6] * vector4[1] + _m[10] * vector4[2] + _m[14] * vector4[3],
+        _m[3] * vector4[0] + _m[7] * vector4[1] + _m[11] * vector4[2] + _m[15] * vector4[3]
+    );
 }
 
