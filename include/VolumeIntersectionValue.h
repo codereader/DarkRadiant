eb4e4f660689be9abf83933a6afa43420cde7ce0
Matthew Mott <orbweaver3d@gmail.com>
2009-04-18 16:48:00 +0000

    Added missing file

---
+++ include/VolumeIntersectionValue.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/**
+ * \brief
+ * Enumeration of possible intersection results between two volumes.
+ */
+enum VolumeIntersectionValue
+{
+    VOLUME_OUTSIDE,
+    VOLUME_INSIDE,
+    VOLUME_PARTIAL
+};
+
+
