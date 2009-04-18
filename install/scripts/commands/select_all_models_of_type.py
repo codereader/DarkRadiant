f7456f64e7d67db24d1a2352209d7529a8892097
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2009-04-18 16:20:59 +0000

    Extended ScriptSceneNode interface. Added new command "SelectAllModelsOfType", implemented in Python.

---
+++ install/scripts/commands/select_all_models_of_type.py
@@ -0,0 +1,44 @@
+# Set the command name so that DarkRadiant recognises this file
+__commandName__ = 'SelectAllModelsOfType'
+
+# The actual algorithm called by DarkRadiant is contained in the execute() function
+def execute():
+	# Collect all currently selected models
+	selectedModelNames = {}
+
+	class Walker(SelectionVisitor) :
+		def visit(self, node):
+			# Try to "cast" the node to an entity
+			entity = node.getEntity()
+
+			if not entity.isNull():
+				if not entity.getKeyValue('model') == '':
+					selectedModelNames[entity.getKeyValue('model')] = 1
+
+	visitor = Walker()
+	GlobalSelectionSystem.foreachSelected(visitor)
+
+	print('Unique models currently selected: ' + str(len(selectedModelNames)))
+
+	# Now traverse the scenegraph, selecting all of the same names
+	class SceneWalker(SceneNodeVisitor) :
+		def pre(self, node):
+			
+			# Try to "cast" the node to an entity
+			entity = node.getEntity()
+
+			if not entity.isNull():
+				modelName = entity.getKeyValue('model')
+
+				if not modelName == '' and modelName in selectedModelNames:
+					# match, select this node
+					node.setSelected(1); 
+
+			return 0 # don't traverse this entity's children
+
+	walker = SceneWalker()
+	GlobalSceneGraph.root().traverse(walker)
+
+# __executeCommand__ evaluates to true after DarkRadiant has successfully initialised
+if __executeCommand__:
+	execute()
