b2626e2417de212d613c3e568f26066b8e363905
Matthew Mott <orbweaver3d@gmail.com>
2009-04-18 20:31:54 +0000

    Added some basic buttons into AnglePropertyEditor

--- radiant/ui/einspector/AnglePropertyEditor.cpp
+++ radiant/ui/einspector/AnglePropertyEditor.cpp
@@ -1,5 +1,8 @@
 #include "AnglePropertyEditor.h"
 
+#include "iradiant.h"
+#include "gtkutil/IconTextButton.h"
+
 #include <gtk/gtk.h>
 
 namespace ui
@@ -8,7 +11,46 @@ namespace ui
 // Constructor
 AnglePropertyEditor::AnglePropertyEditor(Entity* entity, const std::string& key)
 {
-    _widget = gtk_button_new_with_label("Angle");
+    // Construct a 3x3 table to contain the directional buttons
+    GtkWidget* table = gtk_table_new(3, 3, TRUE);
+
+    // Add buttons
+    gtk_table_attach_defaults(
+        GTK_TABLE(table), 
+        gtkutil::IconTextButton(
+            "", GlobalRadiant().getLocalPixbuf("arrow_n24.png"), false
+        ),
+        1, 2,   // column boundaries
+        0, 1    // row boundaries
+    );
+    gtk_table_attach_defaults(
+        GTK_TABLE(table), 
+        gtkutil::IconTextButton(
+            "", GlobalRadiant().getLocalPixbuf("arrow_s24.png"), false
+        ),
+        1, 2,
+        2, 3
+    );
+    gtk_table_attach_defaults(
+        GTK_TABLE(table), 
+        gtkutil::IconTextButton(
+            "", GlobalRadiant().getLocalPixbuf("arrow_e24.png"), false
+        ),
+        2, 3,
+        1, 2
+    );
+    gtk_table_attach_defaults(
+        GTK_TABLE(table), 
+        gtkutil::IconTextButton(
+            "", GlobalRadiant().getLocalPixbuf("arrow_w24.png"), false
+        ),
+        0, 1,
+        1, 2
+    );
+
+    // Pack table into an hbox and set as the widget
+    _widget = gtk_vbox_new(FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(_widget), table, FALSE, FALSE, 0);
 }
 
 }
