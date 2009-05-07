3aa1d236f30970a5ccf38720949642e22be71fc8
Matthew Mott <orbweaver3d@gmail.com>
2009-05-07 16:11:01 +0000

    ARBDepthFillProgram method bodies moved into new .cpp file

--- radiant/render/backend/glprogram/ARBDepthFillProgram.h
+++ radiant/render/backend/glprogram/ARBDepthFillProgram.h
@@ -1,78 +1,23 @@
 #ifndef ARBDEPTHFILLPROGRAM_H_
 #define ARBDEPTHFILLPROGRAM_H_
 
-#include "render/backend/GLProgramFactory.h"
+#include "iglrender.h"
 
 namespace render {
-
-/* CONSTANTS */
-namespace {
-    const char* DEPTHFILL_VP_FILENAME = "zfill_vp.glp";
-    const char* DEPTHFILL_FP_FILENAME = "zfill_fp.glp";
-}
 
 class ARBDepthFillProgram : 
 	public GLProgram
 {
-public:
   GLuint m_vertex_program;
   GLuint m_fragment_program;
 
-  void create()
-  {
-    glEnable(GL_VERTEX_PROGRAM_ARB);
-    glEnable(GL_FRAGMENT_PROGRAM_ARB);
+public:
 
-    {
-      // Create the vertex program
-      glGenProgramsARB(1, &m_vertex_program);
-      glBindProgramARB(GL_VERTEX_PROGRAM_ARB, m_vertex_program);
-
-      GLProgramFactory::createARBProgram(
-        GLProgramFactory::getGLProgramPath(DEPTHFILL_VP_FILENAME), 
-        GL_VERTEX_PROGRAM_ARB
-      );
-
-      // Create the fragment program
-      glGenProgramsARB(1, &m_fragment_program);
-      glBindProgramARB(GL_FRAGMENT_PROGRAM_ARB, m_fragment_program);
-      
-      GLProgramFactory::createARBProgram(
-        GLProgramFactory::getGLProgramPath(DEPTHFILL_FP_FILENAME), 
-        GL_FRAGMENT_PROGRAM_ARB
-      );
-    }
-
-    glDisable(GL_VERTEX_PROGRAM_ARB);
-    glDisable(GL_FRAGMENT_PROGRAM_ARB);
-
-    GlobalOpenGL_debugAssertNoErrors();
-  }
-
-  void destroy()
-  {
-    glDeleteProgramsARB(1, &m_vertex_program);
-    glDeleteProgramsARB(1, &m_fragment_program);
-    GlobalOpenGL_debugAssertNoErrors();
-  }
-
-  void enable()
-  {
-    glEnable(GL_VERTEX_PROGRAM_ARB);
-    glEnable(GL_FRAGMENT_PROGRAM_ARB);
-    glBindProgramARB(GL_VERTEX_PROGRAM_ARB, m_vertex_program);
-    glBindProgramARB(GL_FRAGMENT_PROGRAM_ARB, m_fragment_program);
-
-    GlobalOpenGL_debugAssertNoErrors();
-  }
-
-  void disable()
-  {
-    glDisable(GL_VERTEX_PROGRAM_ARB);
-    glDisable(GL_FRAGMENT_PROGRAM_ARB);
-
-    GlobalOpenGL_debugAssertNoErrors();
-  }
+    /* GLProgram implementation */
+    void create();
+    void destroy();
+    void enable();
+    void disable();
 
     // Required set parameters function, just empty implementation
     void applyRenderParams(const Vector3& viewer,
