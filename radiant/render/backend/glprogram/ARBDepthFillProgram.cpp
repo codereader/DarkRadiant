dea5101d139596a549cc976e30792bb12d301dc6
Matthew Mott <orbweaver3d@gmail.com>
2009-05-07 16:11:01 +0000

    ARBDepthFillProgram method bodies moved into new .cpp file

---
+++ radiant/render/backend/glprogram/ARBDepthFillProgram.cpp
@@ -0,0 +1,70 @@
+#include "ARBDepthFillProgram.h"
+#include "render/backend/GLProgramFactory.h"
+
+namespace render
+{
+
+/* CONSTANTS */
+namespace {
+    const char* DEPTHFILL_VP_FILENAME = "zfill_vp.glp";
+    const char* DEPTHFILL_FP_FILENAME = "zfill_fp.glp";
+}
+
+void ARBDepthFillProgram::create()
+{
+    glEnable(GL_VERTEX_PROGRAM_ARB);
+    glEnable(GL_FRAGMENT_PROGRAM_ARB);
+
+    {
+      // Create the vertex program
+      glGenProgramsARB(1, &m_vertex_program);
+      glBindProgramARB(GL_VERTEX_PROGRAM_ARB, m_vertex_program);
+
+      GLProgramFactory::createARBProgram(
+        GLProgramFactory::getGLProgramPath(DEPTHFILL_VP_FILENAME), 
+        GL_VERTEX_PROGRAM_ARB
+      );
+
+      // Create the fragment program
+      glGenProgramsARB(1, &m_fragment_program);
+      glBindProgramARB(GL_FRAGMENT_PROGRAM_ARB, m_fragment_program);
+      
+      GLProgramFactory::createARBProgram(
+        GLProgramFactory::getGLProgramPath(DEPTHFILL_FP_FILENAME), 
+        GL_FRAGMENT_PROGRAM_ARB
+      );
+    }
+
+    glDisable(GL_VERTEX_PROGRAM_ARB);
+    glDisable(GL_FRAGMENT_PROGRAM_ARB);
+
+    GlobalOpenGL_debugAssertNoErrors();
+}
+
+void ARBDepthFillProgram::destroy()
+{
+    glDeleteProgramsARB(1, &m_vertex_program);
+    glDeleteProgramsARB(1, &m_fragment_program);
+    GlobalOpenGL_debugAssertNoErrors();
+}
+
+void ARBDepthFillProgram::enable()
+{
+    glEnable(GL_VERTEX_PROGRAM_ARB);
+    glEnable(GL_FRAGMENT_PROGRAM_ARB);
+    glBindProgramARB(GL_VERTEX_PROGRAM_ARB, m_vertex_program);
+    glBindProgramARB(GL_FRAGMENT_PROGRAM_ARB, m_fragment_program);
+
+    GlobalOpenGL_debugAssertNoErrors();
+}
+
+void ARBDepthFillProgram::disable()
+{
+    glDisable(GL_VERTEX_PROGRAM_ARB);
+    glDisable(GL_FRAGMENT_PROGRAM_ARB);
+
+    GlobalOpenGL_debugAssertNoErrors();
+}
+
+}
+
