d981a8cd6c0dc8c4ba7040809212db7fe9008256
codereader <greebo@angua.at>
2009-06-19 15:27:41 +0000

    Resolved issue #1338: Entities with "editor_rotatable" set to "1" can be rotated in all room directions. Float9 rotation struct is a class with actual methods now.

--- plugins/entity/rotation.h
+++ plugins/entity/rotation.h
@@ -30,172 +30,202 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "angle.h"
 
-typedef float Float9[9];
+class Float9
+{
+public:
+	float rotation[9];
 
-inline void default_rotation(Float9 rotation)
-{
-  rotation[0] = 1;
-  rotation[1] = 0;
-  rotation[2] = 0;
-  rotation[3] = 0;
-  rotation[4] = 1;
-  rotation[5] = 0;
-  rotation[6] = 0;
-  rotation[7] = 0;
-  rotation[8] = 1;
-}
-inline void write_rotation(const Float9 rotation, Entity* entity, const char* key = "rotation")
-{
-  if(rotation[0] == 1
-    && rotation[1] == 0
-    && rotation[2] == 0
-    && rotation[3] == 0
-    && rotation[4] == 1
-    && rotation[5] == 0
-    && rotation[6] == 0
-    && rotation[7] == 0
-    && rotation[8] == 1)
-  {
-    entity->setKeyValue(key, "");
-  }
-  else
-  {
-	std::ostringstream value;
-    value << rotation[0] << ' '
-      << rotation[1] << ' '
-      << rotation[2] << ' '
-      << rotation[3] << ' '
-      << rotation[4] << ' '
-      << rotation[5] << ' '
-      << rotation[6] << ' '
-      << rotation[7] << ' '
-      << rotation[8];
-    entity->setKeyValue(key, value.str());
-  }
-}
-inline void read_rotation(Float9 rotation, const std::string& value)
-{
-  if(!string_parse_vector(value.c_str(), rotation, rotation + 9))
-  {
-    default_rotation(rotation);
-  }
-}
+	Float9()
+	{
+		setIdentity();
+	}
 
-inline Matrix4 rotation_toMatrix(const Float9 rotation)
-{
-  return Matrix4::byColumns(
-    rotation[0],
-    rotation[1],
-    rotation[2],
-    0,
-    rotation[3],
-    rotation[4],
-    rotation[5],
-    0,
-    rotation[6],
-    rotation[7],
-    rotation[8],
-    0,
-    0,
-    0,
-    0,
-    1
-  );
-}
+	/** 
+     * Cast to float*, also enables operator[].
+	 */
+	operator float* () {
+		return rotation;	
+	}
 
-inline void rotation_fromMatrix(Float9 rotation, const Matrix4& matrix)
-{
-  rotation[0] = static_cast<float>(matrix.xx());
-  rotation[1] = static_cast<float>(matrix.xy());
-  rotation[2] = static_cast<float>(matrix.xz());
-  rotation[3] = static_cast<float>(matrix.yx());
-  rotation[4] = static_cast<float>(matrix.yy());
-  rotation[5] = static_cast<float>(matrix.yz());
-  rotation[6] = static_cast<float>(matrix.zx());
-  rotation[7] = static_cast<float>(matrix.zy());
-  rotation[8] = static_cast<float>(matrix.zz());
-}
+	/** 
+     * Cast to const float* to provide operator[] for const objects.
+	 */
+	operator const float* () const {
+		return rotation;
+	}
 
-inline void rotation_assign(Float9 rotation, const Float9 other)
-{
-  rotation[0] = other[0];
-  rotation[1] = other[1];
-  rotation[2] = other[2];
-  rotation[3] = other[3];
-  rotation[4] = other[4];
-  rotation[5] = other[5];
-  rotation[6] = other[6];
-  rotation[7] = other[7];
-  rotation[8] = other[8];
-}
+	// Reverts this rotation to the identity matrix
+	void setIdentity()
+	{
+		rotation[0] = 1;
+		rotation[1] = 0;
+		rotation[2] = 0;
+		rotation[3] = 0;
+		rotation[4] = 1;
+		rotation[5] = 0;
+		rotation[6] = 0;
+		rotation[7] = 0;
+		rotation[8] = 1;
+	}
 
-inline void rotation_rotate(Float9 rotation, const Quaternion& rotate)
-{
-  rotation_fromMatrix(rotation,
-    matrix4_multiplied_by_matrix4(
-      rotation_toMatrix(rotation),
-      matrix4_rotation_for_quaternion_quantised(rotate)
-    )
-  );
-}
+	void readFromString(const std::string& value)
+	{
+		if (!string_parse_vector(value.c_str(), rotation, rotation + 9))
+		{
+			// Parsing failed, fall back to the identity matrix
+			setIdentity();
+		}
+	}
 
-inline void read_angle(Float9 rotation, const std::string& value)
-{
-  float angle;
-  if(!string_parse_float(value.c_str(), angle))
-  {
-    default_rotation(rotation);
-  }
-  else
-  {
-    rotation_fromMatrix(rotation,  matrix4_rotation_for_z_degrees(angle));
-  }
-}
+	void writeToEntity(Entity* entity, const std::string& key = "rotation") const
+	{
+		if (rotation[0] == 1 && 
+			rotation[1] == 0 && 
+			rotation[2] == 0 && 
+			rotation[3] == 0 && 
+			rotation[4] == 1 && 
+			rotation[5] == 0 && 
+			rotation[6] == 0 && 
+			rotation[7] == 0 && 
+			rotation[8] == 1)
+		{
+			entity->setKeyValue(key, "");
+		}
+		else
+		{
+			std::ostringstream value;
+			value << rotation[0] << ' '
+				<< rotation[1] << ' '
+				<< rotation[2] << ' '
+				<< rotation[3] << ' '
+				<< rotation[4] << ' '
+				<< rotation[5] << ' '
+				<< rotation[6] << ' '
+				<< rotation[7] << ' '
+				<< rotation[8];
+
+			entity->setKeyValue(key, value.str());
+		}
+	}
+
+	Matrix4 getMatrix4() const
+	{
+		return Matrix4::byColumns(
+			rotation[0],
+			rotation[1],
+			rotation[2],
+			0,
+			rotation[3],
+			rotation[4],
+			rotation[5],
+			0,
+			rotation[6],
+			rotation[7],
+			rotation[8],
+			0,
+			0,
+			0,
+			0,
+			1
+		);
+	}
+
+	void setFromMatrix4(const Matrix4& matrix)
+	{
+		rotation[0] = static_cast<float>(matrix.xx());
+		rotation[1] = static_cast<float>(matrix.xy());
+		rotation[2] = static_cast<float>(matrix.xz());
+		rotation[3] = static_cast<float>(matrix.yx());
+		rotation[4] = static_cast<float>(matrix.yy());
+		rotation[5] = static_cast<float>(matrix.yz());
+		rotation[6] = static_cast<float>(matrix.zx());
+		rotation[7] = static_cast<float>(matrix.zy());
+		rotation[8] = static_cast<float>(matrix.zz());
+	}
+
+	// Assignment operator
+	const Float9& operator=(const Float9& other)
+	{
+		rotation[0] = other[0];
+		rotation[1] = other[1];
+		rotation[2] = other[2];
+		rotation[3] = other[3];
+		rotation[4] = other[4];
+		rotation[5] = other[5];
+		rotation[6] = other[6];
+		rotation[7] = other[7];
+		rotation[8] = other[8];
+
+		return *this;
+	}
+
+	void rotate(const Quaternion& rotate)
+	{
+		setFromMatrix4(
+			matrix4_multiplied_by_matrix4(
+				getMatrix4(),
+				matrix4_rotation_for_quaternion_quantised(rotate)
+			)
+		);
+	}
+
+	void setFromAngleString(const std::string& value)
+	{
+		float angle;
+
+		if (!string_parse_float(value.c_str(), angle))
+		{
+			setIdentity();
+		}
+		else
+		{
+			setFromMatrix4(matrix4_rotation_for_z_degrees(angle));
+		}
+	}
+};
 
 class RotationKey
 {
-  Callback m_rotationChanged;
+	Callback m_rotationChanged;
 public:
-  Float9 m_rotation;
+	Float9 m_rotation;
 
 
-  RotationKey(const Callback& rotationChanged)
-    : m_rotationChanged(rotationChanged)
-  {
-    default_rotation(m_rotation);
-  }
+	RotationKey(const Callback& rotationChanged) : 
+		m_rotationChanged(rotationChanged)
+	{}
 
-  void angleChanged(const std::string& value)
-  {
-    read_angle(m_rotation, value);
-    m_rotationChanged();
-  }
-  typedef MemberCaller1<RotationKey, const std::string&, &RotationKey::angleChanged> AngleChangedCaller;
+	void angleChanged(const std::string& value)
+	{
+		m_rotation.setFromAngleString(value);
+		m_rotationChanged();
+	}
+	typedef MemberCaller1<RotationKey, const std::string&, &RotationKey::angleChanged> AngleChangedCaller;
 
-  void rotationChanged(const std::string& value)
-  {
-    read_rotation(m_rotation, value);
-    m_rotationChanged();
-  }
-  typedef MemberCaller1<RotationKey, const std::string&, &RotationKey::rotationChanged> RotationChangedCaller;
+	void rotationChanged(const std::string& value)
+	{
+		m_rotation.readFromString(value);
+		m_rotationChanged();
+	}
+	typedef MemberCaller1<RotationKey, const std::string&, &RotationKey::rotationChanged> RotationChangedCaller;
 
-  void write(Entity* entity, bool isModel = false) const
-  {
-  	Vector3 euler = matrix4_get_rotation_euler_xyz_degrees(rotation_toMatrix(m_rotation));
-  	// greebo: Prevent the "angle" key from being used for models, they should always
-  	// have a rotation matrix written to their spawnargs. This should fix
-  	// the models hopping around after transforms
-    if(euler[0] == 0 && euler[1] == 0 && !isModel)
-    {
-      entity->setKeyValue("rotation", "");
-      write_angle(static_cast<float>(euler[2]), entity);
-    }
-    else
-    {
-      entity->setKeyValue("angle", "");
-      write_rotation(m_rotation, entity);
-    }
-  }
+	void write(Entity* entity, bool isModel = false) const
+	{
+		Vector3 euler = matrix4_get_rotation_euler_xyz_degrees(m_rotation.getMatrix4());
+		// greebo: Prevent the "angle" key from being used for models, they should always
+		// have a rotation matrix written to their spawnargs. This should fix
+		// the models hopping around after transforms
+		if(euler[0] == 0 && euler[1] == 0 && !isModel)
+		{
+			entity->setKeyValue("rotation", "");
+			write_angle(static_cast<float>(euler[2]), entity);
+		}
+		else
+		{
+			entity->setKeyValue("angle", "");
+			m_rotation.writeToEntity(entity);
+		}
+	}
 };
 
 #endif
