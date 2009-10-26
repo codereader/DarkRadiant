3a54918699654228e4db83ac5a66dc8aaf726dc5
codereader <greebo@angua.at>
2009-10-26 16:44:01 +0000

    Forgot to bump version 1.0.2...

--- include/version.h
+++ include/version.h
@@ -2,7 +2,7 @@
 #include <config.h>
 #define RADIANT_VERSION PACKAGE_VERSION
 #else
-#define RADIANT_VERSION "1.0.1"
+#define RADIANT_VERSION "1.0.2"
 #endif
 
 #define RADIANT_APPNAME "DarkRadiant"
