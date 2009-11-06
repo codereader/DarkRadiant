50310ac47b52765b2839d88eae36c211be932f02
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2009-11-06 10:38:03 +0000

    Added two Python scripts by Flanders.

---
+++ install/scripts/commands/shift_textures_upwards_randomly.py
@@ -0,0 +1,19 @@
+# Set the command name so that DarkRadiant recognises this file
+__commandName__ = 'ShiftTexturesUpwardsRandomly'
+
+# The actual algorithm called by DarkRadiant 
+# is contained in the execute() function
+
+def execute():
+	import random
+	s = random.randint(0, 256)
+ 
+	for i in range(0, s+1):
+		GlobalCommandSystem.execute('texshiftup "' + str(s) + '"')
+	else:		
+		print("texture translated over " + str(s) )
+
+# The variable __executeCommand__ evaluates to true 
+# when DarkRadiant executes this command
+if __executeCommand__:
+	execute()
