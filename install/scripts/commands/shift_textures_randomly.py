39dd96191a6c4deb98968f93ca8d5ac5a84bbcff
codereader <greebo@angua.at>
2009-11-06 10:38:03 +0000

    Added two Python scripts by Flanders.

---
+++ install/scripts/commands/shift_textures_randomly.py
@@ -0,0 +1,23 @@
+# Set the command name so that DarkRadiant recognises this file
+__commandName__ = 'ShiftTexturesRandomly'
+
+# The actual algorithm called by DarkRadiant 
+# is contained in the execute() function
+
+def execute():
+	import random
+	s = random.randint(0, 256)
+	t = random.randint(0, 256)
+	
+	for i in range(0, s+1):
+		GlobalCommandSystem.execute('texshiftright "' + str(s) + '"')
+	else:
+		for i in range(0, t+1):
+			GlobalCommandSystem.execute('texshiftup "' + str(t) + '"')
+		else:
+			print("texture translated over " + str(s) + " " + str(t))
+				
+# The variable __executeCommand__ evaluates to true 
+# when DarkRadiant executes this command
+if __executeCommand__:
+	execute()
