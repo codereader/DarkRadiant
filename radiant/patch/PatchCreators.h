c8b19aaf9a7d3a8d9cb727e11ab7af2d052ac61e
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2009-11-12 07:12:45 +0000

    Removed unneeded interface methods from PatchCreator plus some classes, things like these should be handled by a proper IPatch interface.

--- radiant/patch/PatchCreators.h
+++ radiant/patch/PatchCreators.h
@@ -9,64 +9,18 @@ namespace {
 	const std::string RKEY_PATCH_SUBDIVIDE_THRESHOLD = "user/ui/patch/subdivideThreshold";
 }
 
-/* greebo: This is the common patch creator class (for non-Doom3 patches), the base implementation
- * for the more specific patch creators (like Doom3PatchCreator).
- * 
- * Note: the implementation of createPatch() method is missing, this is implemented in Doom3PatchCreator,
- * so it will not be possible to create an actual instance of this very class.
- * 
- * Note: The abstract base class PatchCreator is defined in ipatch.h.  
- */ 
-class CommonPatchCreator : public PatchCreator {
-public:
-	// Save the current state of the patch.
-	void Patch_undoSave(scene::INodePtr patch) const {
-		// greebo: Get the patch member variable from the node and call its undoSave() function
-		// This creates an UndoMemento class where all the state-relevant variables of the patch are saved.  
-		Node_getPatch(patch)->undoSave();
-	}
-	
-	// Resize the patch (width and height of the control instances, e.g. 3x3 patch)
-	void Patch_resize(scene::INodePtr patch, std::size_t width, std::size_t height) const {
-		// Pass this call to the patch member variable of this node
-		Node_getPatch(patch)->setDims(width, height);
-	}
-	
-	// returns a PatchControlMatrix with width, height and the control vertices itself.
-	PatchControlMatrix Patch_getControlPoints(scene::INodePtr node) const {
-		// Retrieve a reference to the patch member variable of this node
-		Patch& patch = *Node_getPatch(node);
-		// Create a PatchControlMatrix with <height> and <width> and the list of control points.
-		return PatchControlMatrix(patch.getHeight(), patch.getWidth(), &patch.getControlPoints().front());
-	}
-	
-	// Notify the patch, that the control points have changed, so that the patch class can update itself accordingly
-	void Patch_controlPointsChanged(scene::INodePtr patch) const {
-		return Node_getPatch(patch)->controlPointsChanged();
-	}
-	
-	// Get the shadername of the patch stored in this node 
-	const std::string& Patch_getShader(scene::INodePtr patch) const {
-		return Node_getPatch(patch)->GetShader();
-	}
-  	
-  	// Set the shadername of the patch in this node
-	void Patch_setShader(scene::INodePtr patch, const std::string& shader) const {
-		Node_getPatch(patch)->SetShader(shader);
-	}
-}; // class CommonPatchCreator
-
-// ----------------------------------------------------------------------------------------------
-
-/* greebo: The Doom3PatchCreator inherits all implementations from CommonPatchCreator and adds the method createPatch(),
- * as required by the abstract base class PatchCreator (see ipatch.h). The nodes are allocated on the heap and can be 
- * released by calling the PatchNode::release() method (deletes itself).
+/* greebo: The Doom3PatchCreator implements the method createPatch(),
+ * as required by the abstract base class PatchCreator (see ipatch.h). 
+ * The nodes are allocated on the heap and can be released by calling 
+ * the PatchNode::release() method (deletes itself).
  */
 class Doom3PatchCreator : 
-	public CommonPatchCreator
+	public PatchCreator
 {
 public:
-	scene::INodePtr createPatch() {
+	// PatchCreator implementation
+	scene::INodePtr createPatch()
+	{
 		// Note the true as function argument: this means that patchDef3 = true in the PatchNode constructor.  
 		return scene::INodePtr(new PatchNode(true));
 	}
@@ -89,7 +43,7 @@ public:
 	}
 	
 	virtual void initialiseModule(const ApplicationContext& ctx) {
-		globalOutputStream() << "Doom3PatchDef3Creator::initialiseModule called.\n";
+		globalOutputStream() << "Doom3PatchDef3Creator::initialiseModule called." << std::endl;
 		
 		// Construct and Register the patch-related preferences
 		PreferencesPagePtr page = GlobalPreferenceSystem().getPage("Settings/Patch");
@@ -110,10 +64,12 @@ public:
 /* greebo: This is the same as the above, but makes sure that a patchDef2 node is created. 
  */
 class Doom3PatchDef2Creator : 
-	public CommonPatchCreator
+	public PatchCreator
 {
 public:
-	scene::INodePtr createPatch() {
+	// PatchCreator implementation
+	scene::INodePtr createPatch()
+	{
 		// The PatchNodeDoom3 constructor normally expects a bool, which defaults to false.
 		// this means that the patch is node def3, but def2
 		return scene::INodePtr(new PatchNode());
@@ -137,7 +93,7 @@ public:
 	}
 	
 	virtual void initialiseModule(const ApplicationContext& ctx) {
-		globalOutputStream() << "Doom3PatchDef2Creator::initialiseModule called.\n";
+		globalOutputStream() << "Doom3PatchDef2Creator::initialiseModule called." << std::endl;
 	}
 };
 
