05e40003eac05cd352db8c808f19cb9703a98533
codereader <greebo@angua.at>
2009-11-13 13:57:27 +0000

    Further extended brush interfaces.

---
+++ install/scripts/brushtest.py
@@ -0,0 +1,29 @@
+# Some interface tests
+
+# Test brush manipulation
+class BrushManipulator(SceneNodeVisitor) :
+	def pre(self, node):
+		# Try to get a brush from this node
+		brush = node.getBrush()
+
+		if not brush.isNull():
+			print('Brush information:')
+			print('Number of faces: ' + str(brush.getNumFaces()))
+			
+			i = 0
+			while i < brush.getNumFaces():
+				face = brush.getFace(i)
+				print('Face #' + str(i) +' has shader ' + face.getShader())
+				winding = face.getWinding()
+
+				#for w in winding:
+				#	print('w.vertex=' + w.vertex)
+				i += 1
+			
+		return 1
+
+walker = BrushManipulator()
+GlobalSceneGraph.root().traverse(walker)
+
+print('')
+
