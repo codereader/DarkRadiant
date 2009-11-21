b03a6f661499fe5a834659ef30030cbd41fceb88
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2009-11-21 09:13:54 +0000

    Replaced inline method with named constructor.

--- radiant/selection/ManipulateObserver.cpp
+++ radiant/selection/ManipulateObserver.cpp
@@ -1,24 +1,28 @@
 #include "ManipulateObserver.h"
 
 // Updates the internal event pointer
-void ManipulateObserver::setEvent(GdkEventButton* event) {
+void ManipulateObserver::setEvent(GdkEventButton* event)
+{
 	_event = event;
 }
 
 // greebo: Handles the mouseDown event and checks whether a manipulator can be made active 
-bool ManipulateObserver::mouseDown(DeviceVector position) {
-	return GlobalSelectionSystem().SelectManipulator(*_view, &position[0], &_epsilon[0]);
+bool ManipulateObserver::mouseDown(DeviceVector position)
+{
+	return GlobalSelectionSystem().SelectManipulator(*_view, position, _epsilon);
 }
 
 /* greebo: Pass the mouse movement to the current selection.
  * This is connected to the according mouse events by the RadiantWindowObserver class 
  */
-void ManipulateObserver::mouseMoved(DeviceVector position) {
+void ManipulateObserver::mouseMoved(DeviceVector position)
+{
 	GlobalSelectionSystem().MoveSelected(*_view, &position[0]);
 }
 
 // The mouse operation is finished, update the selection and unconnect the callbacks
-void ManipulateObserver::mouseUp(DeviceVector position) {
+void ManipulateObserver::mouseUp(DeviceVector position)
+{
 	// Notify the selectionsystem about the ended operation 
 	GlobalSelectionSystem().endMove();
 }
