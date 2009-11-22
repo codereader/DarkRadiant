27746093f4a63c0de451204d616dc8c46e9b0d6a
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2009-11-22 11:27:51 +0000

    Simplified InstanceWalkers a bit.

--- libs/scene/InstanceWalkers.cpp
+++ libs/scene/InstanceWalkers.cpp
@@ -4,13 +4,8 @@
 
 namespace scene {
 
-InstanceSubgraphWalker::InstanceSubgraphWalker(const scene::Path& path) :  
-	_path(path)
-{}
-
-bool InstanceSubgraphWalker::pre(const scene::INodePtr& node) {
-	_path.push(node);
-	
+bool InstanceSubgraphWalker::pre(const scene::INodePtr& node)
+{
 	// Instantiate this node with the reference to the current parent instance
 	node->instantiate();
 
@@ -20,32 +15,20 @@ bool InstanceSubgraphWalker::pre(const scene::INodePtr& node) {
 	return true;
 }
 
-void InstanceSubgraphWalker::post(const scene::INodePtr& node) {
-	// The subgraph has been traversed, remove the top stack elements
-	_path.pop();
-}
-
 // ==============================================================================================
 
-UninstanceSubgraphWalker::UninstanceSubgraphWalker(const scene::Path& parent) : 
-	_path(parent) // Initialise the path with the given parent path
-{}
-	
-bool UninstanceSubgraphWalker::pre(const scene::INodePtr& node) {
-	// Just remember what path we're going: push the node
-	_path.push(node);
+bool UninstanceSubgraphWalker::pre(const scene::INodePtr& node)
+{
 	return true;
 }
-	
-void UninstanceSubgraphWalker::post(const scene::INodePtr& node) {
+
+void UninstanceSubgraphWalker::post(const scene::INodePtr& node)
+{
 	// Instantiate this node with the reference to the current parent instance
 	node->uninstantiate();
 	
 	// Notify the Scenegraph about the upcoming deletion
 	GlobalSceneGraph().erase(node);
-
-	// We're done with this node, pop the path stack
-	_path.pop();
 }
 
 } // namespace scene
