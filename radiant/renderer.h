12a052b67bd7b610d329217d8122deb77edf9d26
codereader <greebo@angua.at>
2009-11-30 09:18:36 +0000

    Removed ForEachVisible template, plus some cleanup, WIP.

--- radiant/renderer.h
+++ radiant/renderer.h
@@ -1,40 +1,9 @@
-/*
-Copyright (C) 2001-2006, William Joseph.
-All Rights Reserved.
-
-This file is part of GtkRadiant.
-
-GtkRadiant is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-GtkRadiant is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with GtkRadiant; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
-#if !defined(INCLUDED_RENDERER_H)
-#define INCLUDED_RENDERER_H
-
-#include "render/frontend/ForEachVisible.h"
+#ifndef _RENDERER_H_
+#define _RENDERER_H_
 
 #include "irender.h"
-#include "ieclass.h"
 #include "irenderable.h"
-#include "iselection.h"
-#include "cullable.h"
-#include "scenelib.h"
-#include "math/frustum.h"
-
-inline RenderablePtr Node_getRenderable(const scene::INodePtr& node) {
-	return boost::dynamic_pointer_cast<Renderable>(node);
-}
+#include "render/frontend/RenderHighlighted.h"
 
 /**
  * Test the visibility of the given Node by intersecting with the given
@@ -42,7 +11,7 @@ inline RenderablePtr Node_getRenderable(const scene::INodePtr& node) {
  * intersection is partial, this node is tested, otherwise the parent's
  * visibility is used instead.
  */
-inline 
+/*inline 
 VolumeIntersectionValue Cullable_testVisible(const scene::INodePtr& node, 
 											 const VolumeTest& volume, 
 											 VolumeIntersectionValue parent)
@@ -61,9 +30,7 @@ VolumeIntersectionValue Cullable_testVisible(const scene::INodePtr& node,
 	// Parent was not partially visible or this instance is not Cullable, so
 	// pass back the parent's visibility
 	return parent;
-}
-
-#include "render/frontend/RenderHighlighted.h"
+}*/
 
 /**
  * Scene render function. Uses the visibility walkers to traverse the scene
@@ -82,4 +49,4 @@ inline void Scene_Render(RenderableCollector& collector, const VolumeTest& volum
 		RenderHighlighted::RenderCaller(RenderHighlighted(collector, volume)));
 }
 
-#endif
+#endif /* _RENDERER_H_ */
