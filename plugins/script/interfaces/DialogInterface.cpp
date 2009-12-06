64e6494dcf20a9b62d29731eefbcd3c354b23d92
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2009-12-06 19:17:21 +0000

    Exposed needed Dialog enums instead of casting integers. This was trickier than expected.

--- plugins/script/interfaces/DialogInterface.cpp
+++ plugins/script/interfaces/DialogInterface.cpp
@@ -14,12 +14,37 @@ ScriptDialog DialogManagerInterface::createDialog(const std::string& title, ui::
 void DialogManagerInterface::registerInterface(boost::python::object& nspace)
 {
 	// Add the declaration for the IDialog class
-	nspace["Dialog"] = boost::python::class_<ScriptDialog>("Dialog", 
-		boost::python::init<const ui::IDialogPtr&>())
+	boost::python::class_<ScriptDialog> dialog("Dialog", 
+		boost::python::init<const ui::IDialogPtr&>());
+
+	// Add the methods to the dialog object
+	dialog
 		.def("setTitle", &ScriptDialog::setTitle)
 		.def("setDialogType", &ScriptDialog::setDialogType)
 		.def("run", &ScriptDialog::run)
 		.def("destroy", &ScriptDialog::destroy)
+	;
+
+	// Register the dialog class name
+	nspace["Dialog"] = dialog;
+
+	// Switch to the Dialog's class scope
+	boost::python::scope dialogScope( dialog );
+
+	// Expose the enums in the Dialog's scope
+	boost::python::enum_<ui::IDialog::Result>("Result")
+		.value("CANCELLED", ui::IDialog::RESULT_CANCELLED)
+		.value("OK", ui::IDialog::RESULT_OK)
+		.value("NO", ui::IDialog::RESULT_NO)
+		.value("YES", ui::IDialog::RESULT_YES)
+		.export_values()
+	;
+
+	boost::python::enum_<ui::IDialog::Type>("Type")
+		.value("OK", ui::IDialog::DIALOG_OK)
+		.value("OK_CANCEL", ui::IDialog::DIALOG_OK_CANCEL)
+		.value("YES_NO", ui::IDialog::DIALOG_YES_NO)
+		.export_values()
 	;
 
 	// Add the module declaration to the given python namespace
