61ab8b8dd4b73b97b11750fe9118edf751209bc9
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2009-12-27 15:09:51 +0000

    First step to get filters working again.

--- plugins/filters/InstanceUpdateWalker.h
+++ plugins/filters/InstanceUpdateWalker.h
@@ -10,12 +10,32 @@
 
 namespace filters {
 
+// Walker: de-selects a complete subgraph
 class Deselector :
 	public scene::NodeVisitor
 {
 public:
 	bool pre(const scene::INodePtr& node) {
 		Node_setSelected(node, false);
+		return true;
+	}
+};
+
+// Walker: Shows or hides a complete subgraph
+class NodeVisibilityUpdater :
+	public scene::NodeVisitor
+{
+private:
+	bool _filtered;
+
+public:
+	NodeVisibilityUpdater(bool setFiltered) :
+		_filtered(setFiltered)
+	{}
+
+	bool pre(const scene::INodePtr& node)
+	{
+		node->setFiltered(_filtered);
 		return true;
 	}
 };
@@ -27,34 +47,51 @@ public:
 class InstanceUpdateWalker : 
 	public scene::NodeVisitor
 {
+private:
+	// Helper visitors to update subgraphs
+	NodeVisibilityUpdater _hideWalker;
+	NodeVisibilityUpdater _showWalker;
 public:
-	// Pre-descent walker function
-	bool pre(const scene::INodePtr& node) { 
+	InstanceUpdateWalker() :
+		_hideWalker(true),
+		_showWalker(false)
+	{}
 
+	// Pre-descent walker function
+	bool pre(const scene::INodePtr& node)
+	{
 		// Retrieve the parent entity and check its entity class.
 		Entity* entity = Node_getEntity(node);
-		if (entity != NULL) {
+		if (entity != NULL)
+		{
 			IEntityClassConstPtr eclass = entity->getEntityClass();
-			node->setFiltered(
-				!GlobalFilterSystem().isVisible("entityclass", eclass->getName())
+
+			Node_traverseSubgraph(
+				node, 
+				GlobalFilterSystem().isVisible("entityclass", eclass->getName()) ? _showWalker : _hideWalker
 			);
 		}
 		
 		// greebo: Update visibility of PatchInstances
-		if (Node_isPatch(node)) {
-			node->setFiltered(
-				!GlobalFilterSystem().isVisible("object", "patch")
+		if (Node_isPatch(node))
+		{
+			Node_traverseSubgraph(
+				node, 
+				GlobalFilterSystem().isVisible("object", "patch") ? _showWalker : _hideWalker
 			);
 		}
 		
 		// greebo: Update visibility of BrushInstances
-		if (Node_isBrush(node)) {
-			node->setFiltered(
-				!GlobalFilterSystem().isVisible("object", "brush")
+		if (Node_isBrush(node))
+		{
+			Node_traverseSubgraph(
+				node, 
+				GlobalFilterSystem().isVisible("object", "brush") ? _showWalker : _hideWalker
 			);
 		}
 
-		if (!node->visible()) {
+		if (!node->visible())
+		{
 			// de-select this node and all children
 			Deselector deselector;
 			Node_traverseSubgraph(node, deselector);
