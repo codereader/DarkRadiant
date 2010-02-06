71926cdb75031a9c5e165a9ca78ccd5c282bd951
codereader <greebo@angua.at>
2010-02-06 06:27:40 +0000

    Added empty MapFormatManager module.

--- include/imapformat.h
+++ include/imapformat.h
@@ -50,7 +50,35 @@ public:
 };
 typedef boost::shared_ptr<MapFormat> MapFormatPtr;
 
+/**
+ * greebo: This is the global map format manager. Use this class to 
+ * register any parsers.
+ */
+class IMapFormatManager :
+	public RegisterableModule
+{
+public:
+	/**
+	 * Returns a parser which can handle the named map format.
+	 */
+	virtual MapFormatPtr getMapFormat(const std::string& name) = 0;
+};
+typedef boost::shared_ptr<IMapFormatManager> IMapFormatManagerPtr;
 
 } // namespace map
+
+const std::string MODULE_MAPFORMATMANAGER("MapFormatManager");
+
+// Application-wide Accessor to the global map format manager
+inline map::IMapFormatManager& GlobalMapFormatManager()
+{
+	// Cache the reference locally
+	static map::IMapFormatManager& _mapFormatManager(
+		*boost::static_pointer_cast<map::IMapFormatManager>(
+		module::GlobalModuleRegistry().getModule(MODULE_MAPFORMATMANAGER)
+		)
+	);
+	return _mapFormatManager;
+}
 
 #endif /* _imapformat_h__ */
---
+++ radiant/map/MapFormatManager.cpp
@@ -0,0 +1,35 @@
+#include "MapFormatManager.h"
+
+#include "itextstream.h"
+#include "modulesystem/StaticModule.h"
+
+namespace map
+{
+
+MapFormatPtr MapFormatManager::getMapFormat(const std::string& name)
+{
+	return MapFormatPtr();
+}
+
+// RegisterableModule implementation
+const std::string& MapFormatManager::getName() const
+{
+	static std::string _name(MODULE_MAPFORMATMANAGER);
+	return _name;
+}
+
+const StringSet& MapFormatManager::getDependencies() const
+{
+	static StringSet _dependencies; // no deps
+	return _dependencies;
+}
+
+void MapFormatManager::initialiseModule(const ApplicationContext& ctx)
+{
+	globalOutputStream() << getName() << "::initialiseModule called." << std::endl;
+}
+
+// Creates the static module instance
+module::StaticModule<MapFormatManager> staticMapFormatManagerModule;
+
+}
