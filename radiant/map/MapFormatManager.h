31e3b9457b7d9366fbbb86e1a2b15dc0bd9aeb19
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2010-02-06 06:27:40 +0000

    Added empty MapFormatManager module.

---
+++ radiant/map/MapFormatManager.h
@@ -0,0 +1,23 @@
+#ifndef _MapFormatManager_h__
+#define _MapFormatManager_h__
+
+#include "imapformat.h"
+
+namespace map
+{
+
+class MapFormatManager :
+	public IMapFormatManager
+{
+public:
+	MapFormatPtr getMapFormat(const std::string& name);
+
+	// RegisterableModule implementation
+	const std::string& getName() const;
+	const StringSet& getDependencies() const;
+	void initialiseModule(const ApplicationContext& ctx);
+};
+
+}
+
+#endif // _MapFormatManager_h__
