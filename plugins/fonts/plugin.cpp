7df9b75172a7f87f0a10f434477228c4a265c0a8
codereader <greebo@angua.at>
2010-02-10 17:09:54 +0000

    Added empty fonts module.

---
+++ plugins/fonts/plugin.cpp
@@ -0,0 +1,16 @@
+#include "itextstream.h"
+#include "debugging/debugging.h"
+
+extern "C" void DARKRADIANT_DLLEXPORT RegisterModule(IModuleRegistry& registry)
+{
+	//registry.registerModule(shaders::Doom3ShaderSystemPtr(new shaders::Doom3ShaderSystem));
+	
+	// Initialise the streams using the given application context
+	module::initialiseStreams(registry.getApplicationContext());
+	
+	// Remember the reference to the ModuleRegistry
+	module::RegistryReference::Instance().setRegistry(registry);
+
+	// Set up the assertion handler
+	GlobalErrorHandler() = registry.getApplicationContext().getErrorHandlingFunction();
+}
