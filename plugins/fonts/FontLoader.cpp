53a429c77b08db6aee8e18decc5df3f5c8346daa
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2010-02-11 07:05:11 +0000

    Renamed FontFinder to FontLoader, added cpp file.

---
+++ plugins/fonts/FontLoader.cpp
@@ -0,0 +1,23 @@
+#include "FontLoader.h"
+
+namespace fonts 
+{
+
+void FontLoader::operator() (const std::string& fileName)
+{
+	// Construct the full VFS path
+	std::string fullPath = _basePath + fileName;
+	
+	// Open the file
+	/*ArchiveTextFilePtr file = GlobalFileSystem().openTextFile(fullPath);
+
+	if (file != NULL) {
+		std::istream is(&(file->getInputStream()));
+		parseShaderFile(is, fullPath);
+	} 
+	else {
+		throw std::runtime_error("Unable to read shaderfile: " + fullPath);  	
+	}*/
+}
+
+} // namespace fonts 
---
+++ plugins/fonts/FontLoader.h
@@ -0,0 +1,24 @@
+#include "ifonts.h"
+
+namespace fonts
+{
+
+class FontLoader
+{
+private:
+	// The base path for the shaders (e.g. "materials/")
+	std::string _basePath;
+
+public:
+	// Constructor. Set the base path of the search.
+	FontLoader(const std::string& path) : 
+		_basePath(path)
+	{}
+
+	// Required functor typedef
+	typedef const std::string& first_argument_type;
+
+	void operator() (const std::string& fileName);
+};
+
+} // namespace fonts
