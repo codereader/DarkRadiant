180541d54060bb0fc87e576149c8703f9dea95a3
codereader <greebo@angua.at>
2010-02-12 10:49:43 +0000

    Added empty RenderableFont class.

---
+++ plugins/fonts/RenderableFont.cpp
@@ -0,0 +1,34 @@
+#include "RenderableFont.h"
+
+namespace fonts
+{
+
+RenderableFont::RenderableFont(const FontInfoPtr& font) :
+	_font(font)
+{
+	// Ensure the font we're using is realised, i.e. all shaders are constructed
+	
+}
+
+void RenderableFont::renderSolid(RenderableCollector& collector, const VolumeTest& volume) const
+{
+	// TODO
+}
+
+void RenderableFont::renderWireframe(RenderableCollector& collector, const VolumeTest& volume) const
+{
+	renderSolid(collector, volume);
+}
+
+void RenderableFont::renderText(const std::string& text, const Vector3& pos)
+{
+	// Add ourselves as renderable
+}
+
+void RenderableFont::renderText(const std::string& text, 
+	const Vector3& areaTopLeft, const Vector3& areaBottomRight)
+{
+	// Add ourselves as renderable
+}
+
+} // namespace fonts
---
+++ plugins/fonts/RenderableFont.h
@@ -0,0 +1,42 @@
+#ifndef _RENDERABLE_FONT_H_
+#define _RENDERABLE_FONT_H_
+
+#include "irenderable.h"
+#include "math/Vector3.h"
+
+#include "FontInfo.h"
+
+namespace fonts
+{
+
+class RenderableFont :
+	public Renderable
+{
+private:
+	// The current state (used during front end rendering)
+	mutable ShaderPtr _curState;
+
+	// The font we're rendering
+	FontInfoPtr _font;
+
+public:
+	// Construct a renderable for the given font
+	RenderableFont(const FontInfoPtr& font);
+
+	// Renderable implementation, adds OpenGLRenderables to the collector
+	void renderSolid(RenderableCollector& collector, const VolumeTest& volume) const;
+	void renderWireframe(RenderableCollector& collector, const VolumeTest& volume) const;
+
+	// Renders the text at the given position
+	void renderText(const std::string& text, const Vector3& pos);
+
+	// Renders the given text in the given rectangle, starting
+	// from top left, wrapping the text at the borders.
+	void renderText(const std::string& text, 
+					const Vector3& areaTopLeft, 
+					const Vector3& areaBottomRight);
+};
+
+} // namespace fonts
+
+#endif /* _RENDERABLE_FONT_H_ */
