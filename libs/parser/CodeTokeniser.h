e1128cc6b335c17d1cba6afdd6962610f5d8977a
codereader <greebo@angua.at>
2010-02-16 18:47:37 +0000

    Fleshing out CodeTokeniser.

--- libs/parser/CodeTokeniser.h
+++ libs/parser/CodeTokeniser.h
@@ -12,6 +12,8 @@ namespace parser {
  * It is able to handle preprocessor statements like #include 
  * by maintaining several child tokenisers. This can be used
  * to parse code-like files as Doom 3 Scripts or GUIs.
+ *
+ * Note: Don't expect this tokeniser to be particularly fast.
  */
 class CodeTokeniser : 
 	public DefTokeniser
@@ -38,6 +40,16 @@ private:
 
 	NodeList::iterator _curNode;
 
+	// The next token which is not a pre-processor token
+	std::string _nextToken;
+
+	// A set of visited files to catch infinite include loops
+	typedef std::set<std::string> FileSet;
+	FileSet _visitedFiles;
+
+	typedef std::map<std::string, std::string> DefinitionMap;
+	DefinitionMap _definitions;
+
 public:
 
     /** 
@@ -47,26 +59,154 @@ public:
     {
 		_nodes.push_back(ParseNodePtr(new ParseNode(file)));
 		_curNode = _nodes.begin();
+
+		_visitedFiles.insert(file->getName());
+
+		loadNextRealToken();
 	}
         
     bool hasMoreTokens()
 	{
-        return (_curNode != _nodes.end() && (*_curNode)->tokeniser.hasMoreTokens());
+		return !_nextToken.empty();
     }
 
     std::string nextToken()
 	{
-		if (hasMoreTokens())
-		{
-			// Inspect this token (TODO)
-            return (*_curNode)->tokeniser.nextToken();
-		}
-        else
-		{
-            throw ParseException("CodeTokeniser: no more tokens");
-		}
+		std::string temp = _nextToken;
+
+		loadNextRealToken();
+
+		return temp;
     }
     
+private:
+	void loadNextRealToken()
+	{
+		while (_curNode != _nodes.end())
+		{
+			if (!(*_curNode)->tokeniser.hasMoreTokens())
+			{
+				++_curNode;
+			}
+
+			if (_curNode == _nodes.end())
+			{
+				_nextToken.clear();
+			}
+
+			_nextToken = (*_curNode)->tokeniser.nextToken();
+
+			if (!_nextToken.empty() && _nextToken[0] == '#')
+			{
+				// A pre-processor token is ahead
+				handlePreprocessorToken();
+				continue;
+			}
+
+			// Not a preprocessor token, break the loop
+			break;
+		}
+	}
+
+	void handlePreprocessorToken()
+	{
+		if (_nextToken == "#include")
+		{
+			std::string includeFile = (*_curNode)->tokeniser.nextToken();
+
+			ArchiveTextFilePtr file = GlobalFileSystem().openTextFile(includeFile);
+
+			if (file != NULL)
+			{
+				// Catch infinite recursions
+				std::pair<FileSet::iterator, bool> result = _visitedFiles.insert(file->getName());
+
+				if (result.second)
+				{
+					// Push a new parse node and switch
+					_curNode = _nodes.insert(_curNode, ParseNodePtr(new ParseNode(file)));
+				}
+				else
+				{
+					globalErrorStream() << "Caught infinite loop on parsing #include token: " 
+						<< includeFile << " in " << (*_curNode)->archive->getName() << std::endl;
+				}
+			}
+			else
+			{
+				globalWarningStream() << "Couldn't find include file: " 
+					<< includeFile << " in " << (*_curNode)->archive->getName() << std::endl;
+			}
+		}
+		else if (_nextToken == "#define")
+		{
+			std::string key = (*_curNode)->tokeniser.nextToken();
+			std::string value = (*_curNode)->tokeniser.nextToken();
+
+			std::pair<DefinitionMap::iterator, bool> result = _definitions.insert(
+				DefinitionMap::value_type(key, value)
+			);
+
+			if (!result.second)
+			{
+				globalWarningStream() << "Redefinition of " << key 
+					<< " in " << (*_curNode)->archive->getName() << std::endl;
+			}
+		}
+		else if (_nextToken == "#undef")
+		{
+			std::string key = (*_curNode)->tokeniser.nextToken();
+			_definitions.erase(key);
+		}
+		else if (_nextToken == "#ifdef")
+		{
+			std::string key = (*_curNode)->tokeniser.nextToken();
+			DefinitionMap::const_iterator found = _definitions.find(key);
+
+			if (found == _definitions.end())
+			{
+				skipUntilMatchingEndif();
+			}
+		}
+		else if (_nextToken == "#ifndef")
+		{
+			DefinitionMap::const_iterator found = _definitions.find(
+				(*_curNode)->tokeniser.nextToken());
+
+			if (found != _definitions.end())
+			{
+				skipUntilMatchingEndif();
+			}
+		}
+		else if (_nextToken == "#if")
+		{
+			(*_curNode)->tokeniser.skipTokens(1);
+		}
+	}
+
+	void skipUntilMatchingEndif()
+	{
+		// Not defined, skip everything until matching #endif
+		for (std::size_t level = 1; level > 0;)
+		{
+			if (!(*_curNode)->tokeniser.hasMoreTokens())
+			{
+				globalWarningStream() << "No matching #endif for #ifdef in "
+					<< (*_curNode)->archive->getName() << std::endl;
+			}
+
+			std::string token = (*_curNode)->tokeniser.nextToken();
+
+			if (token == "#endif")
+			{
+				level--;
+			}
+			else if (token == "#ifdef" || token == "#ifndef" || token == "#if")
+			{
+				level++;
+			}
+		}
+	}
 };
 
 } // namespace parser
