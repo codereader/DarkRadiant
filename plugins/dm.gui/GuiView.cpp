8a5d4fa8d251c85347853cc7fe164cdcb4a9b73d
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2010-02-16 13:25:12 +0000

    First, half-working implementation of the DeclFileInputStream class. This is still crashing when trying to load the mainmenu.gui.

--- plugins/dm.gui/GuiView.cpp
+++ plugins/dm.gui/GuiView.cpp
@@ -22,7 +22,6 @@ GuiView::GuiView() :
 		GDK_POINTER_MOTION_MASK);
 	g_signal_connect(G_OBJECT(glWidget), "expose-event", G_CALLBACK(onGLDraw), this);
 
-	setGui("guis/tdm_objectives.gui");
 	setGui("guis/mainmenu.gui");
 }
 
