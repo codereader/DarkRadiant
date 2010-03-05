45f3b3897c5d53a9d7fa7774a4b9c099fae1a145
codereader <greebo@angua.at>
2010-03-05 17:03:07 +0000

    Fixed issue #2150: Negative zeros are converted to 0 on export.

--- plugins/mapdoom3/primitivewriters/BrushDef3Exporter.h
+++ plugins/mapdoom3/primitivewriters/BrushDef3Exporter.h
@@ -11,11 +11,21 @@ namespace map
 namespace
 {
 	// Writes a double to the given stream and checks for NaN and infinity
-	inline void writeDoubleSafe(const double d, std::ostream& os) {
-		if (isValid(d)) {
-			os << d;
+	inline void writeDoubleSafe(const double d, std::ostream& os)
+	{
+		if (isValid(d))
+		{
+			if (d == -0.0)
+			{
+				os << 0; // convert -0 to 0
+			}
+			else
+			{
+				os << d;
+			}
 		}
-		else {
+		else
+		{
 			// Is infinity or NaN, write 0
 			os << "0";
 		}
