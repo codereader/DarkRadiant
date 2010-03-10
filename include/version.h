d873a679c9708e5a5c3f62c16a5670234c731d50
schtifu <schtifu@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2010-03-10 13:04:12 +0000

    Version adjusted

--- include/version.h
+++ include/version.h
@@ -2,7 +2,7 @@
 #include <config.h>
 #define RADIANT_VERSION PACKAGE_VERSION
 #else
-#define RADIANT_VERSION "1.1.1"
+#define RADIANT_VERSION "1.2.0"
 #endif
 
 #define RADIANT_APPNAME "DarkRadiant"
