a8bda5dd381fb7e7a7e5a4f0dd15511d6a52e99c
codereader <greebo@angua.at>
2010-03-24 09:24:31 +0000

    Added scripts for building the boost static libraries, for my own convenience.

---
+++ tools/scripts/build_boost_libs.cmd
@@ -0,0 +1,39 @@
+@echo off
+rem Launch this file in the boost/ folder.
+
+rem Check if we're in the correct folder
+if not exists libs goto :error
+if not exists boost goto :error
+
+cd libs\filesystem\build
+bjam --toolset=msvc link=static release stage
+bjam --toolset=msvc link=static debug stage
+
+cd ..\..\..\libs\system\build
+bjam --toolset=msvc link=static release stage
+bjam --toolset=msvc link=static debug stage
+
+cd ..\..\..\libs\python\build
+bjam --toolset=msvc link=static release stage
+bjam --toolset=msvc link=static debug stage
+
+cd ..\..\..\libs\regex\build
+bjam --toolset=msvc link=static release stage
+bjam --toolset=msvc link=static debug stage
+
+cd ..\..\..\stage
+start .
+
+goto :success
+
+:error
+echo Please launch this file in the boost folder you downloaded and extracted from sourceforge.
+echo Note that you need to have the path to bjam.exe in your PATH environment variable.
+echo 
+echo Example: 
+echo   cd c:\Downloads\boost_1_42_0\
+echo   c:\Games\DarkRadiant\tools\scripts\build_boost_libs.cmd
+goto :eof
+
+:success
+echo Successfully built the libraries, please copy them to the w32deps/boost/libs/ folder now.
