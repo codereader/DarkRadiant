85889cb0d58aa835ba2b3c2aa782c002754e5eab
codereader <greebo@angua.at>
2010-04-09 18:30:30 +0000

    Preference code for language selection (first draft).

--- radiant/settings/LanguageManager.h
+++ radiant/settings/LanguageManager.h
@@ -3,21 +3,44 @@
 
 #include "iregistry.h"
 #include "imodule.h"
+#include <vector>
+#include <map>
 
 namespace language
 {
 
-class LanguageManager;
-typedef boost::shared_ptr<LanguageManager> LanguageManagerPtr;
-
 class LanguageManager :
-	public RegisterableModule,
-	public RegistryKeyObserver
+	public RegisterableModule
 {
 private:
 	// The current language string (e.g. "en_US")
 	std::string _curLanguage;
 	std::string _languageSettingFile;
+
+	// The path where all the languages are stored
+	std::string _i18nPath;
+
+	struct Language
+	{
+		std::string twoDigitCode; // "en"
+		std::string displayName;  // "English"
+
+		Language()
+		{}
+
+		Language(const std::string& twoDigitCode_, const std::string& displayName_) :
+			twoDigitCode(twoDigitCode_),
+			displayName(displayName_)
+		{}
+	};
+
+	// Supported languages
+	typedef std::map<int, Language> LanguageMap;
+	LanguageMap _supportedLanguages;
+
+	// Available languages (for the preference page)
+	typedef std::vector<int> LanguageList;
+	LanguageList _availableLanguages;
 
 public:
 	// RegisterableModule implementation
@@ -25,9 +48,6 @@ public:
 	const StringSet& getDependencies() const;
 	void initialiseModule(const ApplicationContext& ctx);
 	void shutdownModule();
-
-	// RegistryKeyObserver implementation
-	void keyChanged(const std::string& key, const std::string& value);
 
 	// Method used to instantiate and register the module, and load settings
 	// This is called by main() even before any other modules are loaded
@@ -41,7 +61,17 @@ private:
 
 	// Saves the language setting to the .language in the user settings folder
 	void saveLanguageSetting(const std::string& language);
+
+	// Fills the array of supported languages
+	void loadSupportedLanguages();
+
+	// Searches for available language .mo files
+	void findAvailableLanguages();
+
+	// Returns the language index for the given two-digit code
+	int getLanguageIndex(const std::string& languageCode);
 };
+typedef boost::shared_ptr<LanguageManager> LanguageManagerPtr;
 
 } // namespace
 
