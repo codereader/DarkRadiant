3497a93483f66c2fcbf7bf65261b94c87a0a7fa6
codereader <greebo@angua.at>
2010-04-11 09:59:59 +0000

    Fixed linux build.

--- radiant/entity.cpp
+++ radiant/entity.cpp
@@ -21,6 +21,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "entity.h"
 
+#include "i18n.h"
 #include "ieventmanager.h"
 #include "icommandsystem.h"
 #include "ientity.h"
