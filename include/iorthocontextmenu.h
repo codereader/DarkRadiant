9aa37c89714e0d2aa16b2961b9342f3d336db853
codereader <greebo@angua.at>
2010-06-27 11:00:27 +0000

    Adapted OrthoContextMenu to take IMenuItems.

--- include/iorthocontextmenu.h
+++ include/iorthocontextmenu.h
@@ -2,39 +2,12 @@
 #define _IORTHOCONTEXT_MENU_H_
 
 #include "imodule.h"
+#include "imenu.h"
 
 typedef struct _GtkWidget GtkWidget;
 
 namespace ui
 {
-
-/**
- * Any element packed into the OrthoContextMenu should derive from this class.
- * A menu item must provide methods to test for sensibility and visibility,
- * which will be invoked each time before the menu is presented to the user.
- */
-class IOrthoContextMenuItem
-{
-public:
-	/**
-	 * Callback to check whether this item should be visible in the menu.
-	 * This is invoked by the OrthoContextMenu before presenting itself to the user.
-	 */
-	virtual bool isVisible() = 0; 
-
-	/**
-	 * Callback to check whether this item should be sensitive in the menu.
-	 * Insensitive items are displayed as "greyed out".
-	 * This is invoked by the OrthoContextMenu before presenting itself to the user.
-	 */
-	virtual bool isSensitive() = 0;
-
-	/**
-	 * Returns the widget needed for packing this into the menu.
-	 */
-	virtual GtkWidget* getWidget() = 0;
-};
-typedef boost::shared_ptr<IOrthoContextMenuItem> IOrthoContextMenuItemPtr;
 
 class IOrthoContextMenu :
 	public RegisterableModule
@@ -57,12 +30,12 @@ public:
 	 * It's possible to specify new sections by passing values > SECTION_USER.
 	 * Sections are visually separated by horizontal lines in the menu.
 	 */
-	virtual void addItem(const IOrthoContextMenuItemPtr& item, int section) = 0;
+	virtual void addItem(const IMenuItemPtr& item, int section) = 0;
 
 	/**
 	 * Removes a certain item from the menus.
 	 */
-	virtual void removeItem(const IOrthoContextMenuItemPtr& item) = 0;
+	virtual void removeItem(const IMenuItemPtr& item) = 0;
 };
 
 } // namespace
