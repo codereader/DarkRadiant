e4faacb0bd530aa6017c345904b5a6470126bf81
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2010-07-28 18:31:28 +0000

    Migrated AboutDialog.

--- radiant/ui/about/AboutDialog.cpp
+++ radiant/ui/about/AboutDialog.cpp
@@ -1,45 +1,55 @@
 #include "AboutDialog.h"
 
 #include "i18n.h"
-#include <gtk/gtk.h>
 #include "igl.h"
 #include "iregistry.h"
 #include "imainframe.h"
 #include "iuimanager.h"
 #include "version.h"
 #include "string/string.h"
+
 #include "gtkutil/LeftAlignedLabel.h"
 #include "gtkutil/LeftAlignment.h"
 #include "gtkutil/ScrolledFrame.h"
+
+#include <gtkmm/alignment.h>
+#include <gtkmm/button.h>
+#include <gtkmm/window.h>
+#include <gtkmm/image.h>
+#include <gtkmm/stock.h>
+#include <gtkmm/textview.h>
+#include <gtkmm/box.h>
+#include <gtkmmconfig.h>
+
 #include "modulesystem/ModuleRegistry.h"
 #include <boost/format.hpp>
 
 namespace ui {
 
-	namespace {
-		const std::string RKEY_SHOW_BUILD_TIME = "user/showBuildTime";
+	namespace
+	{
+		const char* const RKEY_SHOW_BUILD_TIME = "user/showBuildTime";
 		const char* const WINDOW_TITLE = N_("About DarkRadiant");
 	}
 
 AboutDialog::AboutDialog() :
 	BlockingTransientWindow(_(WINDOW_TITLE), GlobalMainFrame().getTopLevelWindow())
 {
-	gtk_container_set_border_width(GTK_CONTAINER(getWindow()), 12);
-	gtk_window_set_type_hint(GTK_WINDOW(getWindow()), GDK_WINDOW_TYPE_HINT_DIALOG);
+	set_border_width(12);
+	set_type_hint(Gdk::WINDOW_TYPE_HINT_DIALOG);
 	
 	// Create all the widgets
 	populateWindow();
 }
 
-void AboutDialog::populateWindow() {
-	GtkWidget* dialogVBox = gtk_vbox_new(FALSE, 6); 
+void AboutDialog::populateWindow()
+{
+	Gtk::VBox* dialogVBox = Gtk::manage(new Gtk::VBox(false, 6));
 	
-	GtkWidget* topHBox = gtk_hbox_new(FALSE, 12);
+	Gtk::HBox* topHBox = Gtk::manage(new Gtk::HBox(false, 12));
 	
-	GtkWidget* image = gtk_image_new_from_pixbuf(
-		GlobalUIManager().getLocalPixbuf("logo.png")->gobj()
-	);
-	gtk_box_pack_start(GTK_BOX(topHBox), image, FALSE, FALSE, 0);
+	Gtk::Image* image = Gtk::manage(new Gtk::Image(GlobalUIManager().getLocalPixbuf("logo.png")));
+	topHBox->pack_start(*image, false, false, 0);
 	
 	std::string date = __DATE__;
 	std::string time = __TIME__;
@@ -62,95 +72,97 @@ void AboutDialog::populateWindow() {
 		"DarkRadiant is based on the GPL version\n"
 		"of GtkRadiant (www.qeradiant.com)\n");
 
-	GtkWidget* title = gtkutil::LeftAlignedLabel(appNameStr + buildDateStr + descStr);
+	Gtk::Label* title = Gtk::manage(new gtkutil::LeftAlignedLabelmm(appNameStr + buildDateStr + descStr));
 
-	GtkWidget* alignment = gtk_alignment_new(0.0f, 0.0f, 1.0f, 0.0f);
-	gtk_container_add(GTK_CONTAINER(alignment), title);
-	gtk_box_pack_start(GTK_BOX(topHBox), alignment, TRUE, TRUE, 0);
-	
-	gtk_box_pack_start(GTK_BOX(dialogVBox), topHBox, FALSE, FALSE, 0);
+	Gtk::Alignment* alignment = Gtk::manage(new Gtk::Alignment(0.0, 0.0, 1.0, 0.0));
+	alignment->add(*title);
 
-	gtk_box_pack_start(GTK_BOX(dialogVBox), gtkutil::LeftAlignedLabel(
-		_("<b>GTK+ Properties</b>")), FALSE, FALSE, 0);
+	topHBox->pack_start(*alignment, true, true, 0);
 	
-	GtkWidget* gtkVersion = gtkutil::LeftAlignedLabel(
-		(boost::format(_("Version: %d.%d.%d")) % 
+	dialogVBox->pack_start(*topHBox, false, false, 0);
+
+	dialogVBox->pack_start(*Gtk::manage(new gtkutil::LeftAlignedLabelmm(
+		_("<b>GTK+ Properties</b>"))), false, false, 0);
+	
+	Gtk::Label* gtkVersion = Gtk::manage(new gtkutil::LeftAlignedLabelmm(
+		(boost::format(_("GTK+ Version: %d.%d.%d")) % 
 		gtk_major_version % 
 		gtk_minor_version % 
 		gtk_micro_version).str()
-	);
-	gtk_box_pack_start(GTK_BOX(dialogVBox), gtkutil::LeftAlignment(gtkVersion, 18), FALSE, FALSE, 0);
+	));
+	dialogVBox->pack_start(*Gtk::manage(new gtkutil::LeftAlignmentmm(*gtkVersion, 18)), false, false, 0);
 
-	gtk_box_pack_start(GTK_BOX(dialogVBox), gtkutil::LeftAlignedLabel(
-		_("<b>OpenGL Properties</b>")), FALSE, FALSE, 0);
+	Gtk::Label* gtkmmVersion = Gtk::manage(new gtkutil::LeftAlignedLabelmm(
+		(boost::format(_("gtkmm Version: %d.%d.%d")) % 
+		GTKMM_MAJOR_VERSION % 
+		GTKMM_MINOR_VERSION % 
+		GTKMM_MICRO_VERSION).str()
+	));
+	dialogVBox->pack_start(*Gtk::manage(new gtkutil::LeftAlignmentmm(*gtkmmVersion, 18)), false, false, 0);
+
+	dialogVBox->pack_start(*Gtk::manage(new gtkutil::LeftAlignedLabelmm(
+		_("<b>OpenGL Properties</b>"))), false, false, 0);
 	
 	// If anybody knows a better method to convert glubyte* to char*, please tell me...
 	std::string vendorStr = reinterpret_cast<const char*>(glGetString(GL_VENDOR));
 	std::string versionStr = reinterpret_cast<const char*>(glGetString(GL_VERSION));
 	std::string rendererStr = reinterpret_cast<const char*>(glGetString(GL_RENDERER));
 	
-	GtkWidget* glVendor = gtkutil::LeftAlignedLabel((boost::format(_("Vendor: %s")) % vendorStr).str());
-	GtkWidget* glVersion = gtkutil::LeftAlignedLabel((boost::format(_("Version: %s")) % versionStr).str());
-	GtkWidget* glRenderer = gtkutil::LeftAlignedLabel((boost::format(_("Renderer: %s")) % rendererStr).str());
+	Gtk::Label* glVendor = Gtk::manage(new gtkutil::LeftAlignedLabelmm((boost::format(_("Vendor: %s")) % vendorStr).str()));
+	Gtk::Label* glVersion = Gtk::manage(new gtkutil::LeftAlignedLabelmm((boost::format(_("Version: %s")) % versionStr).str()));
+	Gtk::Label* glRenderer = Gtk::manage(new gtkutil::LeftAlignedLabelmm((boost::format(_("Renderer: %s")) % rendererStr).str()));
 	
-	gtk_box_pack_start(GTK_BOX(dialogVBox), gtkutil::LeftAlignment(glVendor, 18), FALSE, FALSE, 0);
-	gtk_box_pack_start(GTK_BOX(dialogVBox), gtkutil::LeftAlignment(glVersion, 18), FALSE, FALSE, 0);
-	gtk_box_pack_start(GTK_BOX(dialogVBox), gtkutil::LeftAlignment(glRenderer, 18), FALSE, FALSE, 0);
+	dialogVBox->pack_start(*Gtk::manage(new gtkutil::LeftAlignmentmm(*glVendor, 18)), false, false, 0);
+	dialogVBox->pack_start(*Gtk::manage(new gtkutil::LeftAlignmentmm(*glVersion, 18)), false, false, 0);
+	dialogVBox->pack_start(*Gtk::manage(new gtkutil::LeftAlignmentmm(*glRenderer, 18)), false, false, 0);
 	
 	// OpenGL extensions
-	gtk_box_pack_start(GTK_BOX(dialogVBox), gtkutil::LeftAlignedLabel(
-		_("<b>OpenGL Extensions</b>")), FALSE, FALSE, 0);
+	dialogVBox->pack_start(*Gtk::manage(new gtkutil::LeftAlignedLabelmm(
+		_("<b>OpenGL Extensions</b>"))), false, false, 0);
 	
-	GtkWidget* textView = gtk_text_view_new();
-	gtk_text_view_set_editable(GTK_TEXT_VIEW(textView), FALSE);
-	gtk_text_buffer_set_text(
-		gtk_text_view_get_buffer(GTK_TEXT_VIEW(textView)), 
-		reinterpret_cast<const char*>(glGetString(GL_EXTENSIONS)), 
-		-1
-	);
-	gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(textView), GTK_WRAP_WORD);
+	Gtk::TextView* textView = Gtk::manage(new Gtk::TextView);
+	textView->set_editable(false);
+	textView->get_buffer()->set_text(reinterpret_cast<const char*>(glGetString(GL_EXTENSIONS)));
+	textView->set_wrap_mode(Gtk::WRAP_WORD);
 	
-	gtk_box_pack_start(GTK_BOX(dialogVBox), gtkutil::LeftAlignment(
-		gtkutil::ScrolledFrame(textView), 18, 1.0f), 
-		TRUE, TRUE, 0
-	);
+	dialogVBox->pack_start(*Gtk::manage(new gtkutil::LeftAlignmentmm(
+		*Gtk::manage(new gtkutil::ScrolledFramemm(*textView)), 18, 1.0f)), 
+		true, true, 0);
 	
 	// DarkRadiant modules
 	// OpenGL extensions
-	gtk_box_pack_start(GTK_BOX(dialogVBox), gtkutil::LeftAlignedLabel(
-		_("<b>DarkRadiant Modules</b>")), FALSE, FALSE, 0);
+	dialogVBox->pack_start(*Gtk::manage(new gtkutil::LeftAlignedLabelmm(
+		_("<b>DarkRadiant Modules</b>"))), false, false, 0);
 		
-	GtkWidget* moduleTextView = gtk_text_view_new();
-	gtk_text_view_set_editable(GTK_TEXT_VIEW(moduleTextView), FALSE);
-	gtk_text_buffer_set_text(
-		gtk_text_view_get_buffer(GTK_TEXT_VIEW(moduleTextView)), 
-		module::ModuleRegistry::Instance().getModuleList(", ").c_str(), 
-		-1
-	);
-	gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(moduleTextView), GTK_WRAP_WORD);
+	Gtk::TextView* moduleTextView = Gtk::manage(new Gtk::TextView);
+	moduleTextView->set_editable(false);
+	moduleTextView->get_buffer()->set_text(module::ModuleRegistry::Instance().getModuleList(", "));
+	moduleTextView->set_wrap_mode(Gtk::WRAP_WORD);
 	
-	gtk_box_pack_start(GTK_BOX(dialogVBox), gtkutil::LeftAlignment(
-		gtkutil::ScrolledFrame(moduleTextView), 18, 1.0f), 
-		TRUE, TRUE, 0
+	dialogVBox->pack_start(*Gtk::manage(new gtkutil::LeftAlignmentmm(
+		*Gtk::manage(new gtkutil::ScrolledFramemm(*moduleTextView)), 18, 1.0f)), 
+		true, true, 0
 	);
 	
 	// Create the close button
-	GtkWidget* buttonHBox = gtk_hbox_new(FALSE, 0);
-	GtkWidget* okButton = gtk_button_new_from_stock(GTK_STOCK_OK);
-	gtk_box_pack_end(GTK_BOX(buttonHBox), okButton, FALSE, FALSE, 0);
-	g_signal_connect(G_OBJECT(okButton), "clicked", G_CALLBACK(callbackClose), this);
+	Gtk::HBox* buttonHBox = Gtk::manage(new Gtk::HBox(false, 0));
+	Gtk::Button* okButton = Gtk::manage(new Gtk::Button(Gtk::Stock::OK));
+	okButton->signal_clicked().connect(sigc::mem_fun(*this, &AboutDialog::callbackClose));
+
+	buttonHBox->pack_end(*okButton, false, false, 0);
+	dialogVBox->pack_start(*buttonHBox, false, false, 0);
 	
-	gtk_box_pack_start(GTK_BOX(dialogVBox), buttonHBox, FALSE, FALSE, 0);
-	
-	gtk_container_add(GTK_CONTAINER(getWindow()), dialogVBox);
+	add(*dialogVBox);
 }
 
-void AboutDialog::callbackClose(GtkWidget* widget, AboutDialog* self) {
-	self->destroy();
+void AboutDialog::callbackClose()
+{
+	destroy();
 }
 
-void AboutDialog::showDialog(const cmd::ArgumentList& args) {
-	AboutDialog dialog; 
+void AboutDialog::showDialog(const cmd::ArgumentList& args)
+{
+	AboutDialog dialog;
 	dialog.show(); // blocks
 }
 
