7d52e2e940fc6c90e3a3671fbb48eb09a4ae32d9
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2010-08-01 05:54:25 +0000

    Migrated MapInfo dialog.

--- radiant/ui/mapinfo/EntityInfoTab.h
+++ radiant/ui/mapinfo/EntityInfoTab.h
@@ -3,33 +3,54 @@
 
 #include "map/EntityBreakdown.h"
 
-typedef struct _GtkWidget GtkWidget;
-typedef struct _GtkListStore GtkListStore;
+#include <gtkmm/liststore.h>
 
-namespace ui {
+namespace Gtk
+{
+	class Label;
+	class VBox;
+	class TreeView;
+	class Widget;
+}
+
+namespace ui
+{
 
 class EntityInfoTab
 {
+private:
 	// The "master" widget
-	GtkWidget* _widget;
+	Gtk::VBox* _widget;
 
 	// The helper class counting the entities in the map
 	map::EntityBreakdown _entityBreakdown;
 	
-	GtkWidget* _brushCount;
-	GtkWidget* _patchCount;
-	GtkWidget* _entityCount;
+	Gtk::Label* _brushCount;
+	Gtk::Label* _patchCount;
+	Gtk::Label* _entityCount;
+
+	// Treemodel definition
+	struct ListColumns : 
+		public Gtk::TreeModel::ColumnRecord
+	{
+		ListColumns() { add(eclass); add(count); }
+
+		Gtk::TreeModelColumn<Glib::ustring> eclass;
+		Gtk::TreeModelColumn<int> count;
+	};
+
+	ListColumns _columns;
 	
 	// The treeview containing the above liststore
-	GtkListStore* _listStore;
-	GtkWidget* _treeView;
+	Glib::RefPtr<Gtk::ListStore> _listStore;
+	Gtk::TreeView* _treeView;
 	
 public:
 	// Constructor
 	EntityInfoTab();
 
 	// Use this to pack the tab into a parent container
-	GtkWidget* getWidget();
+	Gtk::Widget& getWidget();
 
 	std::string getLabel();
 	std::string getIconName();
