c08eb2e418668eff658ba751cc123e7d7598a233
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2010-08-01 16:13:31 +0000

    Migrated ParticlesChooser.

--- radiant/ui/particles/ParticlesVisitor.h
+++ radiant/ui/particles/ParticlesVisitor.h
@@ -2,10 +2,7 @@
 #define PARTICLESVISITOR_H_
 
 #include "iparticles.h"
-
-#include <gtk/gtkliststore.h>
-
-#include <map>
+#include "ParticlesChooser.h"
 
 namespace ui
 {
@@ -16,38 +13,44 @@ namespace ui
  */
 class ParticlesVisitor
 {
+private:
 	// List store to populate
-	GtkListStore* _store;
+	Glib::RefPtr<Gtk::ListStore> _store;
+
+	const ParticlesChooser::ListColumns& _columns;
 
 	// Map of iters for fast lookup
-	typedef std::map<std::string, GtkTreeIter*> IterMap;
-	IterMap& _iterMap;
+	ParticlesChooser::IterMap& _iterMap;
 	
 public:
 	
 	/**
 	 * Constructor.
 	 */
-	ParticlesVisitor(GtkListStore* store, IterMap& map)
-	: _store(store), _iterMap(map)
-	{ }
+	ParticlesVisitor(const Glib::RefPtr<Gtk::ListStore>& store, 
+					 const ParticlesChooser::ListColumns& columns, 
+					 ParticlesChooser::IterMap& map)
+	: _store(store), 
+	  _columns(columns),
+	  _iterMap(map)
+	{}
 	
 	/**
 	 * Functor operator.
 	 */
-	void operator() (const IParticleDef& def) {
-		
+	void operator() (const IParticleDef& def)
+	{
 		// Add the ".prt" extension to the name fo display in the list
 		std::string prtName = def.getName() + ".prt";
 		
 		// Add the Def name to the list store
-		GtkTreeIter iter;
-		gtk_list_store_append(_store, &iter);
-		gtk_list_store_set(_store, &iter, 0, prtName.c_str(), -1);
+		Gtk::TreeModel::iterator iter = _store->append();
+
+		Gtk::TreeModel::Row row = *iter;
+		row[_columns.name] = prtName;
 		
 		// Save the iter in the iter map
-		GtkTreeIter* permanentIter = gtk_tree_iter_copy(&iter);
-		_iterMap.insert(IterMap::value_type(prtName, permanentIter));
+		_iterMap.insert(ParticlesChooser::IterMap::value_type(prtName, iter));
 	}
 };
 
