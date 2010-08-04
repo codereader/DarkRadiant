2349571a9310364fe5800d8464048656823b064d
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2010-08-04 16:18:43 +0000

    Restore LightTextureChooser window size code.

--- radiant/ui/einspector/LightTextureChooser.cpp
+++ radiant/ui/einspector/LightTextureChooser.cpp
@@ -51,7 +51,17 @@ LightTextureChooser::LightTextureChooser()
 	_selector(Gtk::manage(new ShaderSelector(this, getPrefixList(), true))) // true >> render a light texture
 {
 	// Set the default size of the window
-	Gdk::Rectangle rect = gtkutil::MultiMonitor::getMonitorForWindow(GlobalGroupDialog().getDialogWindow());
+	Gdk::Rectangle rect;
+
+	if (GlobalGroupDialog().getDialogWindow()->is_visible())
+	{
+		rect = gtkutil::MultiMonitor::getMonitorForWindow(GlobalGroupDialog().getDialogWindow());
+	}
+	else
+	{
+		rect = gtkutil::MultiMonitor::getMonitorForWindow(GlobalMainFrame().getTopLevelWindow());
+	}
+
 	set_default_size(static_cast<int>(rect.get_width()*0.6f), static_cast<int>(rect.get_height()*0.6f));
 	
 	// Construct main VBox, and pack in ShaderSelector and buttons panel
