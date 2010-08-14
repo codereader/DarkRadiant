912b9feceac2edbc92ab8ede900f1d2aad14c4d1
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2010-08-14 05:32:45 +0000

    Migrated EntityList module to gtkmm.

--- plugins/entitylist/GraphTreeModel.cpp
+++ plugins/entitylist/GraphTreeModel.cpp
@@ -1,6 +1,5 @@
 #include "GraphTreeModel.h"
 
-#include <gtk/gtk.h>
 #include "scenelib.h"
 #include <iostream>
 #include "nameable.h"
@@ -8,55 +7,67 @@
 #include "GraphTreeModelPopulator.h"
 #include "GraphTreeModelSelectionUpdater.h"
 
+#include <gtkmm/treestore.h>
+#include <gtkmm/treeselection.h>
+#include <gtkmm/treeview.h>
+
 namespace ui {
 
 	namespace {
-		inline NameablePtr Node_getNameable(const scene::INodePtr& node) {
+		inline NameablePtr Node_getNameable(const scene::INodePtr& node)
+		{
 			return boost::dynamic_pointer_cast<Nameable>(node);
 		}
 		
-		inline std::string node_get_name(const scene::INodePtr& node) {
+		inline std::string node_get_name(const scene::INodePtr& node)
+		{
 			NameablePtr nameable = Node_getNameable(node);
 			return (nameable != NULL) ? nameable->name() : "node";
 		}
 	
 		// Checks for NULL references and returns "" if node is NULL
-		inline std::string node_get_name_safe(const scene::INodePtr& node) {
+		inline std::string node_get_name_safe(const scene::INodePtr& node)
+		{
 			return (node == NULL) ? "" : node_get_name(node);
 		}
 	}
 
 GraphTreeModel::GraphTreeModel() :
-	_model(gtk_tree_store_new(NUM_COLS, G_TYPE_POINTER, G_TYPE_STRING))
+	_model(Gtk::TreeStore::create(_columns))
 {}
 
-GraphTreeModel::~GraphTreeModel() {
+GraphTreeModel::~GraphTreeModel()
+{
 	// Remove everything before shutting down
 	clear();
 }
 
-void GraphTreeModel::connectToSceneGraph() {
+void GraphTreeModel::connectToSceneGraph()
+{
 	// Subscribe to the scenegraph to get notified about insertions/deletions
 	GlobalSceneGraph().addSceneObserver(this);
 }
 
-void GraphTreeModel::disconnectFromSceneGraph() {
+void GraphTreeModel::disconnectFromSceneGraph()
+{
 	GlobalSceneGraph().removeSceneObserver(this);
 }
 
-const GraphTreeNodePtr& GraphTreeModel::insert(const scene::INodePtr& node) {
+const GraphTreeNodePtr& GraphTreeModel::insert(const scene::INodePtr& node)
+{
 	// Create a new GraphTreeNode
 	GraphTreeNodePtr gtNode(new GraphTreeNode(node));
 	
 	// Insert this iterator below a possible parent iterator
-	gtk_tree_store_insert(_model, gtNode->getIter(), findParentIter(node), 0);
-	
+	Gtk::TreeModel::iterator parentIter = findParentIter(node);
+
+	gtNode->getIter() = parentIter ? _model->append(parentIter->children()) : _model->append();
+
 	// Fill in the values
-	gtk_tree_store_set(_model, gtNode->getIter(), 
-		COL_NODE_POINTER, node.get(),
-		COL_NAME, getNodeCaption(node).c_str(),
-		-1
-	);
+	Gtk::TreeModel::Row row = *gtNode->getIter();
+
+	row[_columns.node] = node.get();
+	row[_columns.name] = getNodeCaption(node);
 	
 	// Insert this iterator into the node map to facilitate lookups
 	std::pair<NodeMap::iterator, bool> result = _nodemap.insert(
@@ -67,70 +78,79 @@ const GraphTreeNodePtr& GraphTreeModel::insert(const scene::INodePtr& node) {
 	return result.first->second;
 }
 
-void GraphTreeModel::erase(const scene::INodePtr& node) {
+void GraphTreeModel::erase(const scene::INodePtr& node)
+{
 	NodeMap::iterator found = _nodemap.find(scene::INodeWeakPtr(node));
 	
-	if (found != _nodemap.end()) {
+	if (found != _nodemap.end()) 
+	{
 		// Remove this from the GtkTreeStore...
-		gtk_tree_store_remove(_model, found->second->getIter());
+		_model->erase(found->second->getIter());
 		
 		// ...and from our lookup table
 		_nodemap.erase(found);
 	}
 }
 
-const GraphTreeNodePtr& GraphTreeModel::find(const scene::INodePtr& node) const {
+const GraphTreeNodePtr& GraphTreeModel::find(const scene::INodePtr& node) const
+{
 	NodeMap::const_iterator found = _nodemap.find(scene::INodeWeakPtr(node));
 	return (found != _nodemap.end()) ? found->second : _nullTreeNode;
 }
 
-void GraphTreeModel::clear() {
+void GraphTreeModel::clear()
+{
 	// Remove everything, GTK plus nodemap
-	gtk_tree_store_clear(_model);
 	_nodemap.clear();
+	_model->clear();
 }
 
-void GraphTreeModel::refresh() {
+void GraphTreeModel::refresh()
+{
 	// Instantiate a scenegraph walker and visit every node in the graph
 	// The walker also clears the graph in its constructor
 	GraphTreeModelPopulator populator(*this);
 	Node_traverseSubgraph(GlobalSceneGraph().root(), populator);
 }
 
-void GraphTreeModel::updateSelectionStatus(GtkTreeSelection* selection) {
+void GraphTreeModel::updateSelectionStatus(const Glib::RefPtr<Gtk::TreeSelection>& selection)
+{
 	GraphTreeModelSelectionUpdater updater(*this, selection);
 	Node_traverseSubgraph(GlobalSceneGraph().root(), updater);
 }
 
-void GraphTreeModel::updateSelectionStatus(GtkTreeSelection* selection, const scene::INodePtr& node) {
+void GraphTreeModel::updateSelectionStatus(const Glib::RefPtr<Gtk::TreeSelection>& selection, const scene::INodePtr& node)
+{
 	NodeMap::const_iterator found = _nodemap.find(scene::INodeWeakPtr(node));
 	
-	if (found != _nodemap.end()) {
-		if (Node_isSelected(node)) {
-
+	if (found != _nodemap.end())
+	{
+		if (Node_isSelected(node))
+		{
 			// Select the row in the TreeView
-			gtk_tree_selection_select_iter(selection, found->second->getIter());
+			selection->select(found->second->getIter());
 		
 			// Scroll to the row
-			GtkTreeView* tv = gtk_tree_selection_get_tree_view(selection);
-			GtkTreeModel* model = gtk_tree_view_get_model(tv);
-			GtkTreePath* selectedPath = gtk_tree_model_get_path(
-				model, found->second->getIter()
-			);
-			gtk_tree_view_expand_to_path(tv, selectedPath);
-			gtk_tree_view_scroll_to_cell(tv, selectedPath, NULL, FALSE, 0, 0);
-			gtk_tree_path_free(selectedPath);
+			Gtk::TreeView* tv = selection->get_tree_view();
+			
+			Gtk::TreeModel::Path selectedPath(found->second->getIter());
+			
+			tv->expand_to_path(selectedPath);
+			tv->scroll_to_row(selectedPath, 0.3f);
 		}
-		else {
-			gtk_tree_selection_unselect_iter(selection, found->second->getIter());
+		else
+		{
+			selection->unselect(found->second->getIter());
 		}
 	}
 }
 
-const GraphTreeNodePtr& GraphTreeModel::findParentNode(const scene::INodePtr& node) const {
+const GraphTreeNodePtr& GraphTreeModel::findParentNode(const scene::INodePtr& node) const
+{
 	scene::INodePtr parent = node->getParent();
 		
-	if (parent == NULL) {
+	if (parent == NULL)
+	{
 		// No parent, return the NULL pointer
 		return _nullTreeNode;
 	}
@@ -142,28 +162,39 @@ const GraphTreeNodePtr& GraphTreeModel::findParentNode(const scene::INodePtr& no
 	return (found != _nodemap.end()) ? found->second : _nullTreeNode;
 }
 
-GtkTreeIter* GraphTreeModel::findParentIter(const scene::INodePtr& node) const {
+Gtk::TreeModel::iterator GraphTreeModel::findParentIter(const scene::INodePtr& node) const
+{
 	// Find the parent's GraphTreeNode
 	const GraphTreeNodePtr& nodePtr = findParentNode(node);
-	// Return a NULL nodeptr if not found
-	return (nodePtr != NULL) ? nodePtr->getIter() : NULL;
+
+	// Return an empty iterator if not found
+	return (nodePtr != NULL) ? nodePtr->getIter() : Gtk::TreeModel::iterator();
 }
 
-std::string GraphTreeModel::getNodeCaption(const scene::INodePtr& node) {
+std::string GraphTreeModel::getNodeCaption(const scene::INodePtr& node)
+{
 	return node_get_name_safe(node);
 }
 
-GraphTreeModel::operator GtkTreeModel*() {
-	return GTK_TREE_MODEL(_model);
+const GraphTreeModel::TreeColumns& GraphTreeModel::getColumns() const
+{
+	return _columns;
+}
+
+Glib::RefPtr<Gtk::TreeModel> GraphTreeModel::getModel()
+{
+	return _model;
 }
 
 // Gets called when a new <instance> is inserted into the scenegraph
-void GraphTreeModel::onSceneNodeInsert(const scene::INodePtr& node) {
+void GraphTreeModel::onSceneNodeInsert(const scene::INodePtr& node)
+{
 	insert(node); // wrap to the actual insert() method
 }
 
 // Gets called when <instance> is removed from the scenegraph
-void GraphTreeModel::onSceneNodeErase(const scene::INodePtr& node) {
+void GraphTreeModel::onSceneNodeErase(const scene::INodePtr& node)
+{
 	erase(node); // wrap to the actual erase() method
 }
 
--- plugins/entitylist/GraphTreeNode.h
+++ plugins/entitylist/GraphTreeNode.h
@@ -1,28 +1,34 @@
 #ifndef GRAPHTREENODE_H_
 #define GRAPHTREENODE_H_
 
-namespace ui {
+#include <gtkmm/treemodel.h>
+
+namespace ui
+{
 
 /**
  * greebo: This structure contains information about the
  *         scene::INode displayed in the EntityList.
  * 
- * This includes a valid instance and a valid GtkTreeIter.
+ * This includes a valid instance and a valid Gtk::TreeModel::iterator.
  */
-class GraphTreeNode {
+class GraphTreeNode
+{
+private:
 	// A reference to the actual node
 	const scene::INodePtr& _node;
 	
-	// The GTK iterator pointing to the row in a GtkTreeStore
-	GtkTreeIter _iter;
+	// The GTK iterator pointing to the row in a Gtk::TreeStore
+	Gtk::TreeModel::iterator _iter;
 public:
 	GraphTreeNode(const scene::INodePtr& node) :
 		_node(node)
 	{}
 	
 	// Convenience accessor for GTK methods (hence raw pointer)
-	GtkTreeIter* getIter() {
-		return &_iter;
+	Gtk::TreeModel::iterator& getIter()
+	{
+		return _iter;
 	}
 	
 	const scene::INodePtr& getNode() const {
