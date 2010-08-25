3914de194301d4b006878314b06e7030436f77f6
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2010-08-25 12:40:47 +0000

    Version bump.

--- include/version.h
+++ include/version.h
@@ -2,7 +2,7 @@
 #include <config.h>
 #define RADIANT_VERSION PACKAGE_VERSION
 #else
-#define RADIANT_VERSION "1.4.0"
+#define RADIANT_VERSION "1.4.1"
 #endif
 
 #define RADIANT_APPNAME "DarkRadiant"
