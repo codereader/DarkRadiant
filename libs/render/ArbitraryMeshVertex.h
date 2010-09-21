c19c85e05ff648f118928ce6a2700690ebbf96a6
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2010-09-21 07:15:42 +0000

    Moved ArbitraryMeshTriangle routines to ArbitraryMeshVertex header. Removed ugly reinterpret_casts.

--- libs/render/ArbitraryMeshVertex.h
+++ libs/render/ArbitraryMeshVertex.h
@@ -7,8 +7,8 @@
 /**
  * Data structure representing a mesh vertex.
  */
-struct ArbitraryMeshVertex {
-	
+struct ArbitraryMeshVertex
+{
 	TexCoord2f texcoord;
 	Normal3f normal;
 	Vertex3f vertex;
@@ -81,6 +81,79 @@ inline std::ostream& operator<< (std::ostream& os, const ArbitraryMeshVertex& v)
 	   << " }";
 	
 	return os;
+}
+
+/// \brief Calculates the tangent vectors for a triangle \p a, \p b, \p c and stores the tangent in \p s and the bitangent in \p t.
+inline void ArbitraryMeshTriangle_calcTangents(const ArbitraryMeshVertex& a, 
+   const ArbitraryMeshVertex& b, const ArbitraryMeshVertex& c, 
+   Vector3& s, Vector3& t)
+{
+	s = Vector3(0, 0, 0);
+	t = Vector3(0, 0, 0);
+	Vector3 aVec, bVec, cVec;
+
+	{
+		aVec.set(a.vertex.x(), a.texcoord.s(), a.texcoord.t());
+		bVec.set(b.vertex.x(), b.texcoord.s(), b.texcoord.t());
+		cVec.set(c.vertex.x(), c.texcoord.s(), c.texcoord.t());
+
+		Vector3 cross( (bVec-aVec).crossProduct(cVec-aVec) );
+
+		if(fabs(cross.x()) > 0.000001f) {
+			s.x() = -cross.y() / cross.x();
+		}
+
+		if(fabs(cross.x()) > 0.000001f) {
+			t.x() = -cross.z() / cross.x();
+		}
+	}
+
+	{
+		aVec.set(a.vertex.y(), a.texcoord.s(), a.texcoord.t());
+		bVec.set(b.vertex.y(), b.texcoord.s(), b.texcoord.t());
+		cVec.set(c.vertex.y(), c.texcoord.s(), c.texcoord.t());
+
+		Vector3 cross( (bVec-aVec).crossProduct(cVec-aVec));
+
+		if(fabs(cross.x()) > 0.000001f) {
+			s.y() = -cross.y() / cross.x();
+		}
+
+		if(fabs(cross.x()) > 0.000001f) {
+			t.y() = -cross.z() / cross.x();
+		}
+	}
+
+	{
+		aVec.set(a.vertex.z(), a.texcoord.s(), a.texcoord.t());
+		bVec.set(b.vertex.z(), b.texcoord.s(), b.texcoord.t());
+		cVec.set(c.vertex.z(), c.texcoord.s(), c.texcoord.t());
+
+		Vector3 cross( (bVec-aVec).crossProduct(cVec-aVec));
+
+		if(fabs(cross.x()) > 0.000001f) {
+			s.z() = -cross.y() / cross.x();
+		}
+
+		if(fabs(cross.x()) > 0.000001f) {
+			t.z() = -cross.z() / cross.x();
+		}
+	}
+}
+
+inline void ArbitraryMeshTriangle_sumTangents(ArbitraryMeshVertex& a, ArbitraryMeshVertex& b, ArbitraryMeshVertex& c)
+{
+	Vector3 s, t;
+
+	ArbitraryMeshTriangle_calcTangents(a, b, c, s, t);
+
+	a.tangent += s;
+	b.tangent += s;
+	c.tangent += s;
+
+	a.bitangent += t;
+	b.bitangent += t;
+	c.bitangent += t;
 }
 
 #endif /*ARBITRARYMESHVERTEX_H_*/
