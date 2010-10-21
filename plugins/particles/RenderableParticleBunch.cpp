67c417a687966535d85a32f8f86ac4d146ca7e12
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2010-10-21 05:38:12 +0000

    Refactored classes into headers and implementation files.

---
+++ plugins/particles/RenderableParticleBunch.cpp
@@ -0,0 +1,495 @@
+#include "RenderableParticleBunch.h"
+
+#include "itextstream.h"
+#include "math/pi.h"
+
+namespace particles
+{
+
+void RenderableParticleBunch::update(std::size_t time)
+{
+	_bounds = AABB();
+	_quads.clear();
+
+	// Length of one cycle (duration + deadtime)
+	std::size_t cycleMsec = static_cast<std::size_t>(_stage.getCycleMsec());
+
+	if (cycleMsec == 0)
+	{
+		return;
+	}
+
+	// Reserve enough space for all the particles
+	_quads.reserve(_stage.getCount()*4);
+
+	// Normalise the global input time into local cycle time
+	// The cycleTime may be larger than the _stage.cycleMsec argument if bunching is turned off
+	std::size_t cycleTime = time - cycleMsec * _index;
+
+	// Reset the random number generator using our stored seed
+	_random.seed(_randSeed);
+
+	// Calculate the time between each particle spawn
+	// When bunching is set to 1 the spacing is 0, and vice versa.
+	std::size_t stageDurationMsec = static_cast<std::size_t>(SEC2MS(_stage.getDuration()));
+
+	float spawnSpacing = _stage.getBunching() * static_cast<float>(stageDurationMsec) / _stage.getCount();
+
+	// This is the spacing between each particle
+	std::size_t spawnSpacingMsec = static_cast<std::size_t>(spawnSpacing);
+
+	// Generate all particle quads, regardless of their visibility
+	// Visibility is considered by not rendering particles that haven't been spawned yet
+	for (std::size_t i = 0; i < static_cast<std::size_t>(_stage.getCount()); ++i)
+	{
+		// Consider bunching parameter
+		std::size_t particleStartTimeMsec = i * spawnSpacingMsec;
+
+		if (cycleTime < particleStartTimeMsec)
+		{
+			// This particle is not visible at the given time
+			continue;
+		}
+
+		assert(particleStartTimeMsec < stageDurationMsec);  // some sanity checks
+
+		// Get the "local particle time" in msecs
+		std::size_t particleTime = cycleTime - particleStartTimeMsec;
+
+		// Calculate the time fraction [0..1]
+		float timeFraction = static_cast<float>(particleTime) / stageDurationMsec;
+
+		// We need the particle time in seconds for the location/angle integrations
+		float particleTimeSecs = MS2SEC(particleTime);
+
+		// Calculate particle origin at time t
+		Vector3 particleOrigin = getOrigin(particleTimeSecs);
+
+		// Consider gravity, ignore "world" parameter for now
+		particleOrigin += Vector3(0,0,-1) * _stage.getGravity() * particleTimeSecs * particleTimeSecs * 0.5f;
+
+		// Get the initial angle value
+		float angle = _stage.getInitialAngle();
+
+		if (angle == 0)
+		{
+			// Use random angle
+			angle = 360 * static_cast<float>(_random()) / boost::rand48::max_value;
+		}
+
+		// Past this point, no more "randomness" is required, so let's check if we still need
+		// to render this particular particle. Don't dismiss particles too early, as each of them
+		// will change the RNG state in the calculations above. These state changes are important for
+		// all the subsequent particles.
+
+		// Each particle has a lifetime of <stage duration> at maximum
+		if (particleTime > stageDurationMsec)
+		{
+			continue; // particle has expired
+		}
+
+		// Calculate the time-dependent angle
+		// according to docs, half the quads have negative rotation speed
+		int rotFactor = i % 2 == 0 ? -1 : 1;
+		angle += rotFactor * integrate(_stage.getRotationSpeed(), particleTimeSecs);
+
+		// Calculate render colour for this particle (pass the index)
+		Vector4 colour = getColour(timeFraction, i);
+
+		// Consider aspect ratio
+		float aspect = _stage.getAspect().evaluate(timeFraction);
+
+		// Consider animation frames
+		std::size_t animFrames = static_cast<std::size_t>(_stage.getAnimationFrames());
+
+		if (animFrames > 0)
+		{
+			// At a given time, two particles can be visible at most
+			float frameRate = _stage.getAnimationRate();
+
+			// The time interval for cross-fading, fall back to entire duration * 3 for zero animation rates
+			float frameIntervalSecs = frameRate > 0 ? 1.0f / frameRate : 3 * _stage.getDuration();
+
+			// Calculate the current frame number, wrap around
+			std::size_t curFrame = static_cast<std::size_t>(floor(particleTimeSecs / frameIntervalSecs)) % animFrames;
+
+			// Wrap next frame around animationFrame count for looping
+			std::size_t nextFrame = (curFrame + 1) % animFrames;
+
+			// Calculate the time within the frame, relative to frame start
+			float frameMicrotime = float_mod(particleTimeSecs, frameIntervalSecs);
+
+			// As a fading lasts as long as the entire interval, the alpha gradient is the same as the FPS value
+			// The "current" particle is always fading out, the nextFrame is fading in
+			float curAlpha = 1.0f - frameRate * frameMicrotime;
+			float nextAlpha = frameRate * frameMicrotime;
+
+			Vector4 curColour = colour * curAlpha;
+			Vector4 nextColour = colour* nextAlpha;
+
+			// The width of a single frame in texture space
+			float sWidth = 1.0f / animFrames;
+
+			// Calculate the texture space for each frame and push the quads
+			pushQuad(particleOrigin, _stage.getSize().evaluate(timeFraction), aspect, angle, curColour, sWidth * curFrame, sWidth);
+			pushQuad(particleOrigin, _stage.getSize().evaluate(timeFraction), aspect, angle, nextColour, sWidth * nextFrame, sWidth);
+		}
+		else
+		{
+			// Generate a single quad using the given parameters
+			pushQuad(particleOrigin, _stage.getSize().evaluate(timeFraction), aspect, angle, colour);
+		}
+	}
+}
+
+void RenderableParticleBunch::render(const RenderInfo& info) const
+{ 
+	if (_quads.empty()) return;
+
+	glVertexPointer(3, GL_DOUBLE, sizeof(ParticleQuad::Vertex), &(_quads.front().verts[0].vertex));
+	glTexCoordPointer(2, GL_DOUBLE, sizeof(ParticleQuad::Vertex), &(_quads.front().verts[0].texcoord));
+	glNormalPointer(GL_DOUBLE, sizeof(ParticleQuad::Vertex), &(_quads.front().verts[0].normal));
+	glColorPointer(4, GL_DOUBLE, sizeof(ParticleQuad::Vertex), &(_quads.front().verts[0].colour));
+	
+	glDrawArrays(GL_QUADS, 0, static_cast<GLsizei>(_quads.size())*4);
+}
+
+const AABB& RenderableParticleBunch::getBounds()
+{
+	if (!_bounds.isValid())
+	{
+		calculateBounds();
+	}
+
+	return _bounds;
+}
+
+Vector4 RenderableParticleBunch::getColour(float timeFraction, std::size_t particleIndex)
+{
+	Vector4 colour = _stage.getColour();
+
+	// Consider fade index fraction, which can spawn particles already faded to some extent
+	float fadeIndexFraction = _stage.getFadeIndexFraction();
+
+	if (fadeIndexFraction > 0)
+	{
+		// greebo: The linear fading function goes like this:
+		// frac(t) = (startFrac - t) / (startFrac - 1) with t in [0..1]
+		// Boundary conditions: frac(1) = 1 and frac(startFrac) = 0
+
+		// Use the particle index as "time", normalised to [0..1]
+		// such that particle with higher index start more faded
+		float pIdx = static_cast<float>(particleIndex) / _stage.getCount();
+
+		// Calculate how much we should be faded already
+		float startFrac = 1.0f - fadeIndexFraction;
+		float frac = (startFrac - pIdx) / (startFrac - 1.0f);
+		
+		// Ignore negative fraction values, this also takes care that only
+		// those particles with time >= fadeIndexFraction get faded.
+		if (frac > 0)
+		{
+			colour = lerpColour(colour, _stage.getFadeColour(), frac);
+		}
+	}
+
+	float fadeInFraction = _stage.getFadeInFraction();
+
+	if (fadeInFraction > 0 && timeFraction <= fadeInFraction)
+	{
+		colour = lerpColour(_stage.getFadeColour(), _stage.getColour(), timeFraction / fadeInFraction); 
+	}
+
+	float fadeOutFraction = _stage.getFadeOutFraction();
+	float fadeOutFractionInverse = 1.0f - fadeOutFraction;
+
+	if (fadeOutFraction > 0 && timeFraction >= fadeOutFractionInverse)
+	{
+		colour = lerpColour(_stage.getColour(), _stage.getFadeColour(), (timeFraction - fadeOutFractionInverse) / fadeOutFraction);
+	}
+
+	return colour;
+}
+
+Vector3 RenderableParticleBunch::getOrigin(float particleTimeSecs)
+{
+	// Consider offset
+	Vector3 particleOrigin = _offset;
+
+	switch (_stage.getCustomPathType())
+	{
+	case IParticleStage::PATH_STANDARD: // Standard path calculation
+		{
+			// Consider particle distribution
+			Vector3 distributionOffset = getDistributionOffset(_distributeParticlesRandomly);
+
+			// Add this to the origin
+			particleOrigin += distributionOffset;
+
+			// Calculate particle direction, pass distribution offset (this is needed for DIRECTION_OUTWARD)
+			Vector3 particleDirection = getDirection(_direction, distributionOffset);
+			
+			// Consider speed
+			particleOrigin += particleDirection * integrate(_stage.getSpeed(), particleTimeSecs);
+		}
+		break;
+
+	case IParticleStage::PATH_FLIES:
+		{
+			// greebo: "Flies" particles are moving on the surface of a sphere of radius <size>
+			// The radial and axial speeds are chosen at random (but never 0) and are constant
+			// during the lifetime of a particle. Starting position appears to be random,
+			// but different to the "distribution sphere" type (i.e. it is not evenly distributed,
+			// instead the particles seem to bunch themselves at the poles).
+
+			// Sphere radius
+			float radius = _stage.getCustomPathParm(2);
+
+			// Generate starting conditions speed (+/-50%)
+			float rand = 2 * (static_cast<float>(_random()) / boost::rand48::max_value) - 1.0f;
+			float radialSpeedFactor = 1.0f + 0.5f * rand * rand;
+
+			// greebo: factor 0.4 is empirical, I measured a few D3 particles for their circulation times
+			float radialSpeed = _stage.getCustomPathParm(0) * radialSpeedFactor * 0.4f;
+
+			rand = 2 * (static_cast<float>(_random()) / boost::rand48::max_value) - 1.0f;
+			float axialSpeedFactor = 1.0f + 0.5f * rand * rand;
+			float axialSpeed = _stage.getCustomPathParm(1) * axialSpeedFactor * 0.4f;
+
+			float phi0 = 2 * static_cast<float>(c_pi) * static_cast<float>(_random()) / boost::rand48::max_value;
+			float theta0 = static_cast<float>(c_pi) * static_cast<float>(_random()) / boost::rand48::max_value;
+
+			// Calculate angles at the given particleTime
+			float phi = phi0 + axialSpeed * particleTimeSecs;
+			float theta = theta0 + radialSpeed * particleTimeSecs;
+
+			// Move the particle origin
+			particleOrigin += Vector3(radius * cos(theta) * sin(phi), radius * sin(theta) * sin(phi), radius * cos(phi));
+		}
+		break;
+
+	case IParticleStage::PATH_HELIX:
+		{
+			// greebo: Helical movement is describing an elliptic cylinder, its shape is determined by
+			// sizeX, sizeY and sizeZ. Particles are spawned randomly on that cylinder surface,
+			// their velocities (radial and axial) are also random (both negative and positive
+			// velocities are allowed).
+
+			float sizeX = _stage.getCustomPathParm(0);
+			float sizeY = _stage.getCustomPathParm(1);
+			float sizeZ = _stage.getCustomPathParm(2);
+
+			float radialSpeed = _stage.getCustomPathParm(3) * (2 * (static_cast<float>(_random()) / boost::rand48::max_value) - 1.0f);
+			float axialSpeed = _stage.getCustomPathParm(4) * (2 * (static_cast<float>(_random()) / boost::rand48::max_value) - 1.0f);
+
+			float phi0 = 2 * static_cast<float>(c_pi) * static_cast<float>(_random()) / boost::rand48::max_value;
+			float z0 = sizeZ * (2 * (static_cast<float>(_random()) / boost::rand48::max_value) - 1.0f);
+
+			float x = sizeX * cos(phi0 + radialSpeed * particleTimeSecs);
+			float y = sizeY * sin(phi0 + radialSpeed * particleTimeSecs);
+			float z = z0 + axialSpeed * particleTimeSecs;
+
+			particleOrigin += Vector3(x, y, z);
+		}
+		break;
+
+	case IParticleStage::PATH_ORBIT:
+	case IParticleStage::PATH_DRIP:
+		// These are actually unsupported by the engine ("bad path type")
+		globalWarningStream() << "Unsupported path type (drip/orbit)." << std::endl;
+		break;
+
+	default:
+		// Nothing
+		break;
+	};
+
+	return particleOrigin;
+}
+
+// baseDirection should be normalised and not degenerate
+Vector3 RenderableParticleBunch::getDirection(const Vector3& baseDirection, const Vector3& distributionOffset)
+{
+	if (baseDirection.getLengthSquared() == 0)
+	{
+		return Vector3(0,0,1); // degenerate input
+	}
+
+	switch (_stage.getDirectionType())
+	{
+	case IParticleStage::DIRECTION_CONE:
+		{
+			Vector3 dir = baseDirection.getNormalised();
+
+			// Find a normal vector to the base direction
+			Vector3 base1 = abs(dir.x()) < abs(dir.y()) ? 
+				(abs(dir.x()) < abs(dir.z()) ? Vector3(1,0,0) : Vector3(0,0,1)) : 
+				(abs(dir.y()) < abs(dir.z()) ? Vector3(0,1,0) : Vector3(0,0,1));
+
+			base1 = dir.crossProduct(base1);
+			base1.normalise();
+
+			// Another vector perpendicular to baseDir, forming the second base
+			Vector3 base2 = dir.crossProduct(base1);
+
+			// Pick a random point in the disc of radius discRadius
+			float coneAngle = _stage.getDirectionParm(0);
+			float discRadius = tan(coneAngle);
+
+			// Use sqrt(r) to fix bunching at the disc center
+			float r = discRadius * sqrt(static_cast<float>(_random()) / boost::rand48::max_value);
+			float phi = 2 * static_cast<float>(c_pi) * static_cast<float>(_random()) / boost::rand48::max_value;
+
+			Vector3 endPoint = baseDirection + base1 * r * cos(phi) + base2 * r * sin(phi);
+			
+			return endPoint.getNormalised();
+		}
+	case IParticleStage::DIRECTION_OUTWARD:
+		{
+			// This heavily relies on particles being distributed randomly within the spawn area
+			Vector3 direction = distributionOffset.getNormalised();
+
+			// Consider upwards bias
+			direction.z() += _stage.getDirectionParm(0);
+
+			return direction;
+		}
+	default:
+		return Vector3(0,0,1);
+	};
+}
+
+Vector3 RenderableParticleBunch::getDistributionOffset(bool distributeParticlesRandomly)
+{
+	switch (_stage.getDistributionType())
+	{
+		// Rectangular distribution
+		case IParticleStage::DISTRIBUTION_RECT:
+		{
+			// Factors to use for the random distribution
+			float randX = 1.0f;
+			float randY = 1.0f;
+			float randZ = 1.0f;
+
+			if (distributeParticlesRandomly)
+			{
+				// Rectangular spawn zone
+				randX = 2 * static_cast<float>(_random()) / boost::rand48::max_value - 1.0f;
+				randY = 2 * static_cast<float>(_random()) / boost::rand48::max_value - 1.0f;
+				randZ = 2 * static_cast<float>(_random()) / boost::rand48::max_value - 1.0f;
+			}
+
+			// If random distribution is off, particles get spawned at <sizex, sizey, sizez>
+
+			return Vector3(randX * _stage.getDistributionParm(0), 
+						   randY * _stage.getDistributionParm(1), 
+						   randZ * _stage.getDistributionParm(2));
+		}
+
+		case IParticleStage::DISTRIBUTION_CYLINDER:
+		{
+			// Get the cylinder dimensions
+			float sizeX = _stage.getDistributionParm(0);
+			float sizeY = _stage.getDistributionParm(1);
+			float sizeZ = _stage.getDistributionParm(2);
+			float ringFrac = _stage.getDistributionParm(3);
+
+			// greebo: Some tests showed that for the cylinder type
+			// the fourth parameter ("ringfraction") is only effective if >1,
+			// it effectively scales the elliptic shape by that factor. 
+			// Values < 1.0 didn't have any effect (?) Someone could double-check that.
+			// Interestingly, the built-in particle editor doesn't really allow editing that parameter.
+			if (ringFrac > 1.0f) 
+			{
+				sizeX *= ringFrac;
+				sizeY *= ringFrac;
+			}
+
+			if (distributeParticlesRandomly)
+			{
+				// Get a random angle in [0..2pi]
+				float angle = static_cast<float>(2*c_pi) * static_cast<float>(_random()) / boost::rand48::max_value;
+
+				float xPos = cos(angle) * sizeX;
+				float yPos = sin(angle) * sizeY;
+				float zPos = sizeZ * (2 * static_cast<float>(_random()) / boost::rand48::max_value - 1.0f);
+
+				return Vector3(xPos, yPos, zPos);
+			}
+			else
+			{
+				// Random distribution is off, particles get spawned at <sizex, sizey, sizez>
+				return Vector3(sizeX, sizeY, sizeZ);
+			}
+		}
+
+		case IParticleStage::DISTRIBUTION_SPHERE:
+		{
+			// Get the sphere dimensions
+			float maxX = _stage.getDistributionParm(0);
+			float maxY = _stage.getDistributionParm(1);
+			float maxZ = _stage.getDistributionParm(2);
+			float ringFrac = _stage.getDistributionParm(3);
+
+			float minX = maxX * ringFrac;
+			float minY = maxY * ringFrac;
+			float minZ = maxZ * ringFrac;
+
+			if (distributeParticlesRandomly)
+			{
+				// The following is modeled after http://mathworld.wolfram.com/SpherePointPicking.html
+				float u = static_cast<float>(_random()) / boost::rand48::max_value;
+				float v = static_cast<float>(_random()) / boost::rand48::max_value;
+
+				float theta = 2 * static_cast<float>(c_pi) * u;
+				float phi = acos(2*v - 1);
+
+				// Take the sqrt(radius) to correct bunching at the center of the sphere
+				float r = sqrt(static_cast<float>(_random()) / boost::rand48::max_value);
+
+				float x = (minX + (maxX - minX) * r) * cos(theta) * sin(phi);
+				float y = (minY + (maxY - minY) * r) * sin(theta) * sin(phi);
+				float z = (minZ + (maxZ - minZ) * r) * cos(phi);
+
+				return Vector3(x,y,z);
+			}
+			else
+			{
+				// Random distribution is off, particles get spawned at <sizex, sizey, sizez>
+				return Vector3(maxX, maxY, maxZ);
+			}
+		}
+
+		// Default case, should not be reachable
+		default:
+			return Vector3(0,0,0);
+	};
+}
+
+// Generates a new quad using the given origin as centroid, angle is in degrees
+void RenderableParticleBunch::pushQuad(const Vector3& origin, float size, float aspect, float angle, 
+									   const Vector4& colour, float s0, float sWidth)
+{
+	// greebo: Create a (rotated) quad facing the z axis
+	// then rotate it to fit the requested orientation
+	// finally translate it to its position.
+
+	const Vector3& normal = _viewRotation.z().getVector3();
+
+	_quads.push_back(ParticleQuad(size, aspect, angle, colour, normal, s0, sWidth));
+	_quads.back().transform(_viewRotation);
+	_quads.back().translate(origin);
+}
+
+void RenderableParticleBunch::calculateBounds()
+{
+	for (Quads::const_iterator i = _quads.begin(); i != _quads.end(); ++i)
+	{
+		_bounds.includePoint(i->verts[0].vertex);
+		_bounds.includePoint(i->verts[1].vertex);
+		_bounds.includePoint(i->verts[2].vertex);
+		_bounds.includePoint(i->verts[3].vertex);
+	}
+}
+
+} // namespace
