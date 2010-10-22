324901d6b50921e49c374468969e3d306ae82d77
codereader <greebo@angua.at>
2010-10-22 11:24:28 +0000

    Speed up EntityClassChooser tree population, the default sort function by gtkmm seems to be 3 times slower than mine.

--- radiant/ui/entitychooser/EntityClassChooser.cpp
+++ radiant/ui/entitychooser/EntityClassChooser.cpp
@@ -207,11 +207,11 @@ Gtk::Widget& EntityClassChooser::createTreeView()
 	Gtk::TreeViewColumn* col = Gtk::manage(
 		new gtkutil::IconTextColumn(_("Classname"), _columns.name, _columns.icon)
 	);
-	col->set_sort_column_id(_columns.name);
+	col->set_sort_column(_columns.name);
 
 	_treeView->append_column(*col);
 
-	_treeStore->set_sort_column_id(_columns.name, Gtk::SORT_ASCENDING);
+	gtkutil::TreeModel::applyFoldersFirstSortFunc(_treeStore, _columns.name, _columns.isFolder);
 
 	// Pack treeview into a scrolled frame and return
 	return *Gtk::manage(new gtkutil::ScrolledFrame(*_treeView));
