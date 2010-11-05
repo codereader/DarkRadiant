ff3e77c4198d3d9915a4626be8e69f1f29612bfe
Matthew Mott <orbweaver3d@gmail.com>
2010-11-05 14:24:20 +0000

    Set terminate() as Glibmm exception handler
    Otherwise the C++-based Glibmm catches exceptions thrown from signal handlers
    and silently discards them. This restores the previous behaviour.

--- radiant/main.cpp
+++ radiant/main.cpp
@@ -25,6 +25,8 @@
 #include <gtkmm/gl/init.h>
 #include <gtksourceviewmm/init.h>
 
+#include <exception>
+
 #ifdef _PROFILE
 #include "Profile.h"
 #endif
@@ -43,118 +45,119 @@ void crt_init()
 /**
  * Main entry point for the application.
  */
-int main (int argc, char* argv[]) {
-	
-	// Initialise the debug flags
-	crt_init();
+int main (int argc, char* argv[])
+{
+    // Initialise the debug flags
+    crt_init();
 
-	// Set the stream references for globalOutputStream(), redirect std::cout, etc.
-	applog::initialiseLogStreams();
+    // Set the stream references for globalOutputStream(), redirect std::cout, etc.
+    applog::initialiseLogStreams();
 
-	// Initialise the context (application path / settings path, is OS-specific)
-	module::ModuleRegistry::Instance().initialiseContext(argc, argv);
+    // Initialise the context (application path / settings path, is OS-specific)
+    module::ModuleRegistry::Instance().initialiseContext(argc, argv);
 
-	// Acquire the appplication context ref (shortcut)
-	const ApplicationContext& ctx = module::getRegistry().getApplicationContext();
+    // Acquire the appplication context ref (shortcut)
+    const ApplicationContext& ctx = module::getRegistry().getApplicationContext();
 
-	// The settings path is set, start logging now
-	applog::LogFile::create("darkradiant.log");
+    // The settings path is set, start logging now
+    applog::LogFile::create("darkradiant.log");
 
-	// Initialise the language based on the settings in the user settings folder
-	// This needs to happen before gtk_init() to set up the environment for GTK
-	language::LanguageManager().init(ctx);
+    // Initialise the language based on the settings in the user settings folder
+    // This needs to happen before gtk_init() to set up the environment for GTK
+    language::LanguageManager().init(ctx);
 
-	// Initialise gtkmm (don't set locale)
-	Gtk::Main gtkmm_main(argc, argv, false);
+    // Initialise gtkmm (don't set locale)
+    Gtk::Main gtkmm_main(argc, argv, false);
+    Glib::add_exception_handler(&std::terminate);
 
-	// Initialise gtksourceviewmm
-	gtksourceview::init();
+    // Initialise gtksourceviewmm
+    gtksourceview::init();
 
     // Initialise GTKGLExtmm
-	Gtk::GL::init(argc, argv);
+    Gtk::GL::init(argc, argv);
 
-	// reset some locale settings back to standard c
+    // reset some locale settings back to standard c
     // this is e.g. needed for parsing float values from textfiles 
     setlocale(LC_NUMERIC, "C");
     setlocale(LC_TIME, "C");
 
-	// Now that GTK is ready, activate the Popup Error Handler
-	module::ModuleRegistry::Instance().initErrorHandler();
+    // Now that GTK is ready, activate the Popup Error Handler
+    module::ModuleRegistry::Instance().initErrorHandler();
 
-	{
-		// Create the radiant.pid file in the settings folder 
-		// (emits a warning if the file already exists (due to a previous startup failure)) 
-		applog::PIDFile pidFile(PID_FILENAME);
+    {
+        // Create the radiant.pid file in the settings folder 
+        // (emits a warning if the file already exists (due to a previous startup failure)) 
+        applog::PIDFile pidFile(PID_FILENAME);
 
-		ui::Splash::Instance().show_all();
-	
-		// Initialise the Reference in the GlobalModuleRegistry() accessor. 
-		module::RegistryReference::Instance().setRegistry(module::getRegistry());
-	
-		ui::Splash::Instance().setProgressAndText(_("Searching for Modules"), 0.0f);
-	
-		// Invoke the ModuleLoad routine to load the DLLs from modules/ and plugins/
+        ui::Splash::Instance().show_all();
+    
+        // Initialise the Reference in the GlobalModuleRegistry() accessor. 
+        module::RegistryReference::Instance().setRegistry(module::getRegistry());
+    
+        ui::Splash::Instance().setProgressAndText(_("Searching for Modules"), 0.0f);
+    
+        // Invoke the ModuleLoad routine to load the DLLs from modules/ and plugins/
 #if defined(POSIX) && defined(PKGLIBDIR)
         // Load modules from compiled-in path (e.g. /usr/lib/darkradiant)
         module::Loader::loadModules(PKGLIBDIR);
 #else
         // Load modules from application-relative path
-		module::Loader::loadModules(ctx.getApplicationPath());
+        module::Loader::loadModules(ctx.getApplicationPath());
 #endif
-	
-		module::getRegistry().initialiseModules();
-	
-		ui::Splash::Instance().setProgressAndText(_("Creating Preference Dialog"), 0.85f);
+    
+        module::getRegistry().initialiseModules();
+    
+        ui::Splash::Instance().setProgressAndText(_("Creating Preference Dialog"), 0.85f);
 
-		Radiant_Initialise();
-		
-		// Initialise the mediabrowser
-		ui::Splash::Instance().setProgressAndText(_("Initialising MediaBrowser"), 0.92f);
-		ui::MediaBrowser::init();
+        Radiant_Initialise();
+        
+        // Initialise the mediabrowser
+        ui::Splash::Instance().setProgressAndText(_("Initialising MediaBrowser"), 0.92f);
+        ui::MediaBrowser::init();
 
-		ui::Splash::Instance().setProgressAndText(_("Starting MainFrame"), 0.95f);
+        ui::Splash::Instance().setProgressAndText(_("Starting MainFrame"), 0.95f);
 
-		// Initialise the mainframe
-		GlobalMainFrame().construct();
+        // Initialise the mainframe
+        GlobalMainFrame().construct();
 
-		// Load the shortcuts from the registry
-   		GlobalEventManager().loadAccelerators();
-	   	
-   		// Update all accelerators, at this point all commands should be setup
-   		GlobalUIManager().getMenuManager().updateAccelerators();
-	  
-		ui::Splash::Instance().setProgressAndText(_("DarkRadiant Startup Complete"), 1.0f);  
+        // Load the shortcuts from the registry
+        GlobalEventManager().loadAccelerators();
+        
+        // Update all accelerators, at this point all commands should be setup
+        GlobalUIManager().getMenuManager().updateAccelerators();
+      
+        ui::Splash::Instance().setProgressAndText(_("DarkRadiant Startup Complete"), 1.0f);  
 
-		// Delete the splash screen here
-		ui::Splash::Instance().destroy();
+        // Delete the splash screen here
+        ui::Splash::Instance().destroy();
 
-		// Scope ends here, PIDFile is deleted by its destructor
-	}
+        // Scope ends here, PIDFile is deleted by its destructor
+    }
 
 #ifdef _PROFILE
-	// greebo: In profile builds, check if we should run an automated test
-	if (!profile::CheckAutomatedTestRun()) {
-		// Start the GTK main loop. This will run until a quit command is given by
-		// the user
-		Gtk::Main::run();
-	}
+    // greebo: In profile builds, check if we should run an automated test
+    if (!profile::CheckAutomatedTestRun()) {
+        // Start the GTK main loop. This will run until a quit command is given by
+        // the user
+        Gtk::Main::run();
+    }
 #else 
-	// Start the GTK main loop. This will run until a quit command is given by
-	// the user
-	Gtk::Main::run();
+    // Start the GTK main loop. This will run until a quit command is given by
+    // the user
+    Gtk::Main::run();
 #endif
-	
-	GlobalMap().freeMap();
+    
+    GlobalMap().freeMap();
 
-	GlobalMainFrame().destroy();
+    GlobalMainFrame().destroy();
 
-	// Issue a shutdown() call to all the modules
-  	module::GlobalModuleRegistry().shutdownModules();
+    // Issue a shutdown() call to all the modules
+    module::GlobalModuleRegistry().shutdownModules();
 
-	// Close the logfile 
-	applog::LogFile::close();
-	applog::shutdownStreams();
+    // Close the logfile 
+    applog::LogFile::close();
+    applog::shutdownStreams();
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
