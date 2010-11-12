8b7c6da6b2deb6ad23dedacf7537005cd04d03e9
Matthew Mott <orbweaver3d@gmail.com>
2010-11-12 13:34:53 +0000

    Connect correct Gtk::TreeModels
    Objective entities list now populated correctly.
    
    NOTE: New .cpp file

--- plugins/dm.objectives/ObjectiveEntityFinder.h
+++ plugins/dm.objectives/ObjectiveEntityFinder.h
@@ -1,5 +1,6 @@
-#ifndef OBJECTIVEENTITYFINDER_H_
-#define OBJECTIVEENTITYFINDER_H_
+#pragma once
+
+#include "ObjectiveEntity.h"
 
 #include "i18n.h"
 #include "scenelib.h"
@@ -102,50 +103,9 @@ public:
 	/**
 	 * @see scene::NodeVisitor::pre()
 	 */
-	bool pre(const scene::INodePtr& node)
-	{
-
-		// Get the entity and check the classname
-		Entity* ePtr = Node_getEntity(node);
-		if (!ePtr)
-			return true;
-
-		// We have an entity at this point
-
-		if (ePtr->getKeyValue("classname") == "worldspawn")
-		{
-			_worldSpawn = ePtr;
-			return false; // Don't traverse worldspawn children
-		}
-
-		// Check for objective entity or worldspawn
-		for (std::size_t i = 0; i < _classNames.size(); ++i)
-		{
-			if (ePtr->getKeyValue("classname") == _classNames[i])
-			{
-				// Construct the display string
-				std::string name = ePtr->getKeyValue("name");
-
-				// Add the entity to the list
-				Gtk::TreeModel::Row row = *_store->append();
-
-				row[_columns.displayName] = (boost::format(_("<b>%s</b> at [ %s ]")) % name % ePtr->getKeyValue("origin")).str();
-				row[_columns.entityName] = name;
-				row[_columns.startActive] = false;
-
-				// Construct an ObjectiveEntity with the node, and add to the map
-				ObjectiveEntityPtr oe(new ObjectiveEntity(node));
-				_map.insert(ObjectiveEntityMap::value_type(name, oe));
-
-				break;
-			}
-		}
-
-		return false; // don't traverse entity children
-	}
+	bool pre(const scene::INodePtr& node);
 
 };
 
 }
 
-#endif /*OBJECTIVEENTITYFINDER_H_*/
