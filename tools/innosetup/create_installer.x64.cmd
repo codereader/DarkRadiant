222dcb6e42593c684ac897607189891526938169
codereader <greebo@angua.at>
2010-12-11 08:20:21 +0000

    Added some build scripts for my own convenience.

---
+++ tools/innosetup/create_installer.x64.cmd
@@ -0,0 +1,2 @@
+@echo For this script to run, please make sure that InnoSetup's compil32 is found via the PATH environment variable
+compil32 /cc darkradiant.x64.iss
---
+++ tools/scripts/compile_release_package_all.cmd
@@ -0,0 +1,7 @@
+call compile_release_package.x86.cmd
+
+cd ..\scripts
+
+call compile_release_package.x64.cmd
+
+start ..\innosetup\.
