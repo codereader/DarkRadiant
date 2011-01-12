b2eb29f8ab2ffea78816c4572fc7b427b9a184b5
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2011-01-12 22:02:12 +0000

    Fix issue #2541: Copy & Paste doesn't work.

--- libs/gtkutil/clipboard.cpp
+++ libs/gtkutil/clipboard.cpp
@@ -21,7 +21,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "clipboard.h"
 
-#include "stream/memstream.h"
+#include "stream/BufferInputStream.h"
 
 #include <memory.h>
 #include <sstream>
