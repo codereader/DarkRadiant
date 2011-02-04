c0c50e8e1a7730fb31e2315e742efa34ff77d83a
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2011-02-04 14:45:17 +0000

    Added progress dialog to mapexporter.

--- radiant/map/algorithm/MapExporter.h
+++ radiant/map/algorithm/MapExporter.h
@@ -4,15 +4,22 @@
 #include "imapformat.h"
 #include "igame.h"
 
+#include "gtkutil/ModalProgressDialog.h"
+#include "EventRateLimiter.h"
+
 namespace map
 {
 
 /**
  * Walker class which passes the visited scene nodes to the
  * attached MapExporter class, for writing it to the given
- * string output stream. The exporter gets a chance to catch
- * various events during scene traversal, like PreMap(), 
- * PostMap(), PreEntity(), PostEntity(), PrePrimitive(), etc.
+ * string output stream. The exporter is using a IMapWriter class
+ * to dispatch various calls like beginWriteEntity(), 
+ * beginMap(), endWriteBrush() during scene traversal etc.
+ *
+ * If the progress dialog is enabled (i.e. nodeCount > 0 in constructor)
+ * a gtkutil::OperationAbortedException& might be thrown during traversal, 
+ * the calling code needs to be able to handle that.
  */
 class MapExporter :
 	public scene::NodeVisitor
@@ -27,18 +34,33 @@ private:
 	// The root node of the subgraph to be exported
 	scene::INodePtr _root;
 
+	// The progress dialog
+	gtkutil::ModalProgressDialogPtr _dialog;
+
+    // Event rate limiter for the progress dialog
+    EventRateLimiter _dialogEventLimiter;
+
+	// The total number, used for progress measurement
+	std::size_t _totalNodeCount;
+	std::size_t	_curNodeCount;
+
 public:
 	// The constructor prepares the scene and the output stream
-	MapExporter(IMapWriter& writer, const scene::INodePtr& root, std::ostream& mapStream);
+	MapExporter(IMapWriter& writer, const scene::INodePtr& root, std::ostream& mapStream, std::size_t nodeCount = 0);
 
 	// Cleans up the scene on destruction
 	~MapExporter();
+
+	void enableProgressDialog();
+	void disableProgressDialog();
 
 	// NodeVisitor implementation, is called by the traversal func passed to MapResource
 	bool pre(const scene::INodePtr& node);
 	void post(const scene::INodePtr& node);
 
 private:
+	void onNodeProgress();
+
 	// Is called before exporting the scene to prepare func_* groups.
 	void prepareScene();
 
