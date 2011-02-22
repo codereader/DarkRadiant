6c2302324d21c605253a1fca5ac3e09eeda377aa
codereader <greebo@angua.at>
2011-02-22 12:19:25 +0000

    Remove debug code.

--- radiant/renderer.h
+++ radiant/renderer.h
@@ -1,7 +1,6 @@
 #ifndef _RENDERER_H_
 #define _RENDERER_H_
 
-#include "itextstream.h"
 #include "irender.h"
 #include "irenderable.h"
 #include "render/frontend/RenderHighlighted.h"
@@ -9,47 +8,12 @@
 namespace render
 {
 
-class TestVisitor :
-	public scene::Graph::Walker
-{
-public:
-	TestVisitor() :
-		_renderables(0),
-		_nonRenderables(0)
-	{
-	}
-
-	std::size_t _renderables;
-	std::size_t _nonRenderables;
-
-	bool visit(const scene::INodePtr& node)
-	{
-		bool isRenderable = boost::dynamic_pointer_cast<Renderable>(node);
-
-		if (isRenderable)
-		{
-			_renderables++;
-		}
-		else
-		{
-			_nonRenderables++;
-		}
-
-		return true;
-	}
-};
-
 /**
  * Scene render function. Uses the visibility walkers to traverse the scene
  * graph and submit all visible objects to the provided RenderableCollector.
  */
 inline void collectRenderablesInScene(RenderableCollector& collector, const VolumeTest& volume)
 {
-	TestVisitor visitor;
-	GlobalSceneGraph().foreachVisibleNodeInVolume(volume, visitor);
-
-	globalOutputStream() << "Renderables: " << visitor._renderables << ", Non-renderables: " << visitor._nonRenderables << std::endl;
-
 	// Instantiate a new walker class
 	RenderHighlighted renderHighlightWalker(collector, volume);
 
