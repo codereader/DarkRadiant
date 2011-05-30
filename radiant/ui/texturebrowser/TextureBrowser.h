ea31cd4d52df1455a9cb35fb703215caa788ce12
codereader <greebo@angua.at>
2011-05-30 08:23:36 +0000

    Some cleanup.

--- radiant/ui/texturebrowser/TextureBrowser.h
+++ radiant/ui/texturebrowser/TextureBrowser.h
@@ -26,7 +26,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "icommandsystem.h"
 #include "gtkutil/nonmodal.h"
 #include "gtkutil/GLWidget.h"
-#include "gtkutil/cursor_old.h"
 #include "gtkutil/FreezePointer.h"
 #include "gtkutil/DeferredAdjustment.h"
 #include "texturelib.h"
