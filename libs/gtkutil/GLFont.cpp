d63eb79cc264855fa0821c47996b9243bca5586a
codereader <greebo@angua.at>
2011-05-31 06:34:41 +0000

    Change case in source files too.

--- libs/gtkutil/GLFont.cpp
+++ libs/gtkutil/GLFont.cpp
@@ -19,7 +19,7 @@ along with GtkRadiant; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include "glfont.h"
+#include "GLFont.h"
 
 #include "igl.h"
 #include <gtkmm/gl/widget.h>
