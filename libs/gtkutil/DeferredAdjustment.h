64e468801d602b4e91132b335240607628a64dfe
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2011-06-01 16:13:51 +0000

    Removed miniscule .cpp file.

--- libs/gtkutil/DeferredAdjustment.h
+++ libs/gtkutil/DeferredAdjustment.h
@@ -22,15 +22,28 @@ private:
 public:
 	DeferredAdjustment(const ValueChangedFunction& function, 
 					   double value, double lower, double upper, double step_increment = 1.0,
-					   double page_increment = 10.0, double page_size = 0.0);
+					   double page_increment = 10.0, double page_size = 0.0) :
+		Gtk::Adjustment(value, lower, upper, step_increment, page_increment, page_size),
+		_function(function)
+	{}
 
-	void flush();
+	void flush()
+	{
+		flushIdleCallback();
+	}
 
 protected:
-	void onGtkIdle();
+	void onGtkIdle()
+	{
+		_function(_value);
+	}
 
 	// gtkmm signal handler
-	void on_value_changed();
+	void on_value_changed()
+	{
+		_value = get_value();
+		requestIdleCallback();
+	}
 };
 
 } // namespace gtkutil
