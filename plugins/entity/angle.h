000411fa4378ee19dbe2a2349223d7cbbf1d5d87
codereader <greebo@angua.at>
2011-06-17 04:08:57 +0000

    Removed Matrix4 header dependency.

--- plugins/entity/angle.h
+++ plugins/entity/angle.h
@@ -25,6 +25,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "ientity.h"
 
 #include "math/quaternion.h"
+#include "math/Matrix4.h"
 #include "string/string.h"
 #include <boost/function.hpp>
 
