92a2f80acd6db8b340805c4ea7ad6e53b3df7910
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2011-08-09 10:29:44 +0000

    Use a local operator/operand stack for each recursion level. Add support for multiplication, division and subtraction.

--- plugins/shaders/ShaderExpression.cpp
+++ plugins/shaders/ShaderExpression.cpp
@@ -24,9 +24,8 @@ class ShaderExpressionParser
 private:
 	parser::DefTokeniser& _tokeniser;
 
-	// Two stacks, one for operands, one for operators
-	std::stack<IShaderExpressionPtr> _operands;
-	std::stack<BinaryExpressionPtr> _operators;
+	typedef std::stack<IShaderExpressionPtr> OperandStack;
+	typedef std::stack<BinaryExpressionPtr> OperatorStack;
 
 public:
 	ShaderExpressionParser(parser::DefTokeniser& tokeniser) :
@@ -35,6 +34,10 @@ public:
 
 	IShaderExpressionPtr getExpression()
 	{
+		// The local variable and operator stack
+		OperandStack operands;
+		OperatorStack operators;
+
 		while (_tokeniser.hasMoreTokens())
 		{
 			std::string token = _tokeniser.nextToken();
@@ -60,7 +63,7 @@ public:
 
 			if (term)
 			{
-				_operands.push(term);
+				operands.push(term);
 				continue;
 			}
 			
@@ -71,53 +74,69 @@ public:
 
 			if (op)
 			{
-				if (_operands.empty())
+				if (operands.empty())
 				{
 					throw parser::ParseException("Missing operand for operator: " + token);
 				}
 
+				// Check precedence if we have previous operators
+				if (!operators.empty())
+				{
+					if (operators.top()->getPrecedence() <= op->getPrecedence())
+					{
+						finaliseOperator(operands, operators);
+					}
+				}
+
 				// Push this one on the operator stack
-				_operators.push(op);
+				operators.push(op);
 			}
 		}
 
 		// Roll up the operations
-		while (!_operators.empty())
+		while (!operators.empty())
 		{
-			// Need two operands for an operator
-			if (_operands.size() < 2)
-			{
-				throw parser::ParseException("Too few operands for operator.");
-			}
-
-			const BinaryExpressionPtr& op = _operators.top();
-
-			op->setA(_operands.top());
-			_operands.pop();
-
-			op->setB(_operands.top());
-			_operands.pop();
-
-			// Push the result back on the stack, as operand
-			_operands.push(op);
-
-			_operators.pop();
+			finaliseOperator(operands, operators);
 		}
 
-		if (_operands.empty())
+		if (operands.empty())
 		{
 			throw parser::ParseException("Missing expression");
 		}
 		
-		IShaderExpressionPtr rv = _operands.top();
-		_operands.pop();
+		IShaderExpressionPtr rv = operands.top();
+		operands.pop();
 
-		assert(_operands.empty()); // there should be nothing left on the stack
+		assert(operands.empty()); // there should be nothing left on the stack
 
 		return rv;
 	}
 
 private:
+	void finaliseOperator(OperandStack& operands, OperatorStack& operators)
+	{
+		// Need two operands for an operator
+		if (operands.size() < 2)
+		{
+			throw parser::ParseException("Too few operands for operator.");
+		}
+
+		const BinaryExpressionPtr& op = operators.top();
+
+		// Set operand B first, we're dealing with a LIFO stack
+		op->setB(operands.top());
+		operands.pop();
+
+		op->setA(operands.top());
+		operands.pop();
+
+		// Push the result back on the stack, as operand
+		operands.push(op);
+
+		// Remove the operator, after we've copied it to the operand stack
+		operators.pop();
+	}
+
 	IShaderExpressionPtr getTerm(const std::string& token)
 	{
 		IShaderExpressionPtr rv;
@@ -186,140 +205,26 @@ private:
 	// Helper routines
 	BinaryExpressionPtr getOperatorForToken(const std::string& token)
 	{
-		BinaryExpressionPtr rv;
-
 		if (token == "+")
 		{
-			rv.reset(new AddExpression);
+			return BinaryExpressionPtr(new AddExpression);
+		}
+		else if (token == "-")
+		{
+			return BinaryExpressionPtr(new SubtractExpression);
+		}
+		else if (token == "*")
+		{
+			return BinaryExpressionPtr(new MultiplyExpression);
+		}
+		else if (token == "/")
+		{
+			return BinaryExpressionPtr(new DivideExpression);
 		}
 
-		return rv;
+		return BinaryExpressionPtr();
 	}
 };
-
-#if 0
-IShaderExpressionPtr ShaderExpression::createFromTokens(parser::StringTokeniser& tokeniser)
-{
-	std::string token = tokeniser.nextToken();
-
-	// The first expression (must not be an operator)
-	IShaderExpressionPtr first;
-
-	if (token == "(")
-	{
-		// A paranthesis always suggests a new token, enter recursion to retrieve the first
-		first = createFromTokens(tokeniser);
-	}
-	else if (boost::algorithm::istarts_with(token, "parm"))
-	{
-		// This is a shaderparm, get the number
-		int shaderParmNum = strToInt(token.substr(4));
-		
-		if (shaderParmNum >= 0 && shaderParmNum <= MAX_SHADERPARM_INDEX)
-		{
-			first.reset(new ShaderParmExpression(shaderParmNum));
-		}
-		else
-		{
-			throw new parser::ParseException("Shaderparm index out of bounds");
-		}
-	}
-	else if (token == "time")
-	{
-		first.reset(new TimeExpression);
-	}
-	else 
-	{
-		// Check if this keyword is a material lookup table
-		TableDefinitionPtr table = GetShaderSystem()->getTableForName(token);
-
-		if (table != NULL)
-		{
-			// Got it, this is a table name
-			// We need an opening '[' to have valid syntax
-			tokeniser.assertNextToken("[");
-
-			// The lookup expression itself has to be parsed afresh, enter recursion
-			IShaderExpressionPtr lookupValue = createFromTokens(tokeniser);
-
-			if (lookupValue == NULL)
-			{
-				throw new parser::ParseException("Missing or invalid expression in table lookup operator[]");
-			}
-
-			// Construct a new table lookup expression and link them together
-			first.reset(new TableLookupExpression(table, lookupValue));
-		}
-		else
-		{
-			// Attempt to convert the token into a floating point value
-			try
-			{
-				float value = boost::lexical_cast<float>(token);
-				first.reset(new ConstantExpression(value));
-			}
-			catch (boost::bad_lexical_cast& ex)
-			{
-				throw new parser::ParseException(
-					"Expected time, parm<N> , table name or floating point value, found: " +
-					token + " (" + ex.what() + ")");
-			}
-		}
-	}
-
-	// The potential operator and second operand
-	BinaryExpressionPtr op;
-	IShaderExpressionPtr second;
-
-	// The first expression has been parsed and is non-NULL, are there any more tokens?
-	while (tokeniser.hasMoreTokens())
-	{
-		token = tokeniser.nextToken();
-
-		// Check if this is an operator
-		BinaryExpressionPtr candidate = getOperatorForToken(token);
-
-		if (candidate)
-		{
-			if (op && !second)
-			{
-				// Syntax error
-				throw parser::ParseException("Operand expected.");
-			}
-
-#if 0
-			// Close any pending operators
-			if (op && second)
-			{
-				// We already have an operator in our stack, check which one takes precedence
-				if (candidate->getPrecedence() > op->getPrecedence())
-				{
-
-				}
-			}
-
-			op = candidate;
-
-			// Link the first operand to this operator
-			op->setA(first);
-#endif
-		}
-		else if (token == "]")
-		{
-			// End of table lookup - break the loop and return
-			break;
-		}
-	}
-
-	// If we don't have an operator at hand this is a "standalone" expression
-	if (!op)
-	{
-		return first;
-	}
-
-	return IShaderExpressionPtr();
-}
-#endif
 
 } // namespace expressions
 
