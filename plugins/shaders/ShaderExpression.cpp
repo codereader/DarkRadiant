2a70135ab18cf0b52c6137e5c0e09a8b3f42e665
codereader <greebo@angua.at>
2011-08-09 09:08:08 +0000

    More WIP parsing.

--- plugins/shaders/ShaderExpression.cpp
+++ plugins/shaders/ShaderExpression.cpp
@@ -4,8 +4,14 @@
 
 #include "string/string.h"
 #include <boost/algorithm/string/predicate.hpp>
+#include "Doom3ShaderSystem.h"
+
+#include <stack>
 
 namespace shaders
+{
+
+namespace expressions
 {
 
 namespace
@@ -13,23 +19,200 @@ namespace
 	const int MAX_SHADERPARM_INDEX = 11;
 }
 
+class ShaderExpressionParser
+{
+private:
+	parser::StringTokeniser& _tokeniser;
+
+	// Two stacks, one for operands, one for operators
+	std::stack<IShaderExpressionPtr> _operands;
+	std::stack<BinaryExpressionPtr> _operators;
+
+public:
+	ShaderExpressionParser(parser::StringTokeniser& tokeniser) :
+		_tokeniser(tokeniser)
+	{}
+
+	IShaderExpressionPtr getExpression()
+	{
+		while (_tokeniser.hasMoreTokens())
+		{
+			std::string token = _tokeniser.nextToken();
+
+			// Get a new term, push it on the stack
+			IShaderExpressionPtr term = getTerm(token);
+
+			if (term)
+			{
+				_operands.push(term);
+				continue;
+			}
+			
+			// Not a term, do we have an operator at hand?
+
+			// Get an operator
+			BinaryExpressionPtr op = getOperatorForToken(token);
+
+			if (op)
+			{
+				if (_operands.empty())
+				{
+					throw parser::ParseException("Missing operand for operator: " + token);
+				}
+
+				// Push this one on the operator stack
+				_operators.push(op);
+			}
+		}
+
+		// Roll up the operations
+		while (!_operators.empty())
+		{
+			// Need two operands for an operator
+			if (_operands.size() < 2)
+			{
+				throw parser::ParseException("Too few operands for operator.");
+			}
+
+			const BinaryExpressionPtr& op = _operators.top();
+
+			op->setA(_operands.top());
+			_operands.pop();
+
+			op->setB(_operands.top());
+			_operands.pop();
+
+			// Push the result back on the stack, as operand
+			_operands.push(op);
+
+			_operators.pop();
+		}
+
+		if (_operands.empty())
+		{
+			throw parser::ParseException("Missing expression");
+		}
+		
+		IShaderExpressionPtr rv = _operands.top();
+		_operands.pop();
+
+		assert(_operands.empty()); // there should be nothing left on the stack
+
+		return rv;
+	}
+
+private:
+	IShaderExpressionPtr getTerm(const std::string& token)
+	{
+		IShaderExpressionPtr rv;
+
+		if (token == "(")
+		{
+			// A paranthesis always suggests a new token, enter recursion to retrieve the first
+			rv = getExpression();
+		}
+		else if (token == ")")
+		{
+			// Should not happen?
+			assert(false);
+		}
+		else if (boost::algorithm::istarts_with(token, "parm"))
+		{
+			// This is a shaderparm, get the number
+			int shaderParmNum = strToInt(token.substr(4));
+		
+			if (shaderParmNum >= 0 && shaderParmNum <= MAX_SHADERPARM_INDEX)
+			{
+				rv.reset(new ShaderParmExpression(shaderParmNum));
+			}
+			else
+			{
+				throw new parser::ParseException("Shaderparm index out of bounds");
+			}
+		}
+		else if (token == "time")
+		{
+			rv.reset(new TimeExpression);
+		}
+		else if (token == "+" || token == "-")
+		{
+			// TODO: This is a sign operator
+			rv = getTerm(_tokeniser.nextToken());
+		}
+		else 
+		{
+			// Check if this keyword is a material lookup table
+			TableDefinitionPtr table = GetShaderSystem()->getTableForName(token);
+
+			if (table != NULL)
+			{
+				// Got it, this is a table name
+				// We need an opening '[' to have valid syntax
+				_tokeniser.assertNextToken("[");
+
+				// The lookup expression itself has to be parsed afresh, enter recursion
+				IShaderExpressionPtr lookupValue = getExpression();
+
+				if (lookupValue == NULL)
+				{
+					throw new parser::ParseException("Missing or invalid expression in table lookup operator[]");
+				}
+
+				// Construct a new table lookup expression and link them together
+				rv.reset(new TableLookupExpression(table, lookupValue));
+			}
+			else
+			{
+				// Attempt to convert the token into a floating point value
+				try
+				{
+					float value = boost::lexical_cast<float>(token);
+					rv.reset(new ConstantExpression(value));
+				}
+				catch (boost::bad_lexical_cast& ex)
+				{
+					throw new parser::ParseException(
+						"Expected time, parm<N> , table name or floating point value, found: " +
+						token + " (" + ex.what() + ")");
+				}
+			}
+		}
+
+		return rv;
+	}
+
+	// Helper routines
+	BinaryExpressionPtr getOperatorForToken(const std::string& token)
+	{
+		BinaryExpressionPtr rv;
+
+		if (token == "+")
+		{
+			rv.reset(new AddExpression);
+		}
+
+		return rv;
+	}
+};
+
+#if 0
 IShaderExpressionPtr ShaderExpression::createFromTokens(parser::StringTokeniser& tokeniser)
 {
 	std::string token = tokeniser.nextToken();
 
-	// The first expression
+	// The first expression (must not be an operator)
 	IShaderExpressionPtr first;
 
 	if (token == "(")
 	{
 		// A paranthesis always suggests a new token, enter recursion to retrieve the first
 		first = createFromTokens(tokeniser);
-	}                                                                                        
+	}
 	else if (boost::algorithm::istarts_with(token, "parm"))
 	{
 		// This is a shaderparm, get the number
 		int shaderParmNum = strToInt(token.substr(4));
-
+		
 		if (shaderParmNum >= 0 && shaderParmNum <= MAX_SHADERPARM_INDEX)
 		{
 			first.reset(new ShaderParmExpression(shaderParmNum));
@@ -43,33 +226,100 @@ IShaderExpressionPtr ShaderExpression::createFromTokens(parser::StringTokeniser&
 	{
 		first.reset(new TimeExpression);
 	}
-	else
+	else 
 	{
-		// TODO Check if this keyword is a material lookup table
+		// Check if this keyword is a material lookup table
+		TableDefinitionPtr table = GetShaderSystem()->getTableForName(token);
 
-		// Attempt to convert the token into a floating point value
-		try
+		if (table != NULL)
 		{
-			float value = boost::lexical_cast<float>(token);
-			first.reset(new ConstantExpression(value));
+			// Got it, this is a table name
+			// We need an opening '[' to have valid syntax
+			tokeniser.assertNextToken("[");
+
+			// The lookup expression itself has to be parsed afresh, enter recursion
+			IShaderExpressionPtr lookupValue = createFromTokens(tokeniser);
+
+			if (lookupValue == NULL)
+			{
+				throw new parser::ParseException("Missing or invalid expression in table lookup operator[]");
+			}
+
+			// Construct a new table lookup expression and link them together
+			first.reset(new TableLookupExpression(table, lookupValue));
 		}
-		catch (boost::bad_lexical_cast& ex)
+		else
 		{
-			throw new parser::ParseException(
-				"Expected time, parm<N> , table name or floating point value, found: " +
-				token + " (" + ex.what() + ")");
+			// Attempt to convert the token into a floating point value
+			try
+			{
+				float value = boost::lexical_cast<float>(token);
+				first.reset(new ConstantExpression(value));
+			}
+			catch (boost::bad_lexical_cast& ex)
+			{
+				throw new parser::ParseException(
+					"Expected time, parm<N> , table name or floating point value, found: " +
+					token + " (" + ex.what() + ")");
+			}
 		}
 	}
 
-	// The first expression has been parsed, are there any more tokens?
+	// The potential operator and second operand
+	BinaryExpressionPtr op;
+	IShaderExpressionPtr second;
+
+	// The first expression has been parsed and is non-NULL, are there any more tokens?
 	while (tokeniser.hasMoreTokens())
 	{
-		// TODO
-		tokeniser.nextToken();
+		token = tokeniser.nextToken();
+
+		// Check if this is an operator
+		BinaryExpressionPtr candidate = getOperatorForToken(token);
+
+		if (candidate)
+		{
+			if (op && !second)
+			{
+				// Syntax error
+				throw parser::ParseException("Operand expected.");
+			}
+
+#if 0
+			// Close any pending operators
+			if (op && second)
+			{
+				// We already have an operator in our stack, check which one takes precedence
+				if (candidate->getPrecedence() > op->getPrecedence())
+				{
+
+				}
+			}
+
+			op = candidate;
+
+			// Link the first operand to this operator
+			op->setA(first);
+#endif
+		}
+		else if (token == "]")
+		{
+			// End of table lookup - break the loop and return
+			break;
+		}
+	}
+
+	// If we don't have an operator at hand this is a "standalone" expression
+	if (!op)
+	{
+		return first;
 	}
 
 	return IShaderExpressionPtr();
 }
+#endif
+
+} // namespace expressions
 
 IShaderExpressionPtr ShaderExpression::createFromString(const std::string& exprStr)
 {
@@ -77,7 +327,9 @@ IShaderExpressionPtr ShaderExpression::createFromString(const std::string& exprS
 
 	try
 	{
-		return createFromTokens(tokeniser);
+		expressions::ShaderExpressionParser parser(tokeniser);
+
+		return parser.getExpression();
 	}
 	catch (parser::ParseException& ex)
 	{
--- plugins/shaders/ShaderExpression.h
+++ plugins/shaders/ShaderExpression.h
@@ -2,20 +2,22 @@
 
 #include "ishaders.h"
 #include "parser/Tokeniser.h"
+#include "TableDefinition.h"
 
 namespace shaders
 {
 
+// The base class containing the factory methods
 class ShaderExpression :
 	public IShaderExpression
 {
 public:
 	static IShaderExpressionPtr createFromString(const std::string& exprStr);
-
-private:
-	// Recursive parsing method
-	static IShaderExpressionPtr createFromTokens(parser::StringTokeniser& tokeniser);
 };
+
+// Detail namespace
+namespace expressions
+{
 
 // Specialised implementations of the ShaderExpression below
 
@@ -90,5 +92,109 @@ public:
 		return getValue();
 	}
 };
+
+// An expression looking up a value in a table def
+class TableLookupExpression :
+	public ShaderExpression
+{
+private:
+	TableDefinitionPtr _tableDef;
+	IShaderExpressionPtr _lookupExpr;
+
+public:
+	// Pass the table and the expression used to perform the lookup 
+	TableLookupExpression(const TableDefinitionPtr& tableDef, 
+						  const IShaderExpressionPtr& lookupExpr) :
+		ShaderExpression(),
+		_tableDef(tableDef),
+		_lookupExpr(lookupExpr)
+	{
+		assert(_tableDef);
+		assert(_lookupExpr);
+	}
+
+	virtual float getValue()
+	{
+		float lookupVal = _lookupExpr->getValue();
+		return _tableDef->getValue(lookupVal);
+	}
+
+	virtual float evaluate()
+	{
+		// no saving so far
+		return getValue();
+	}
+};
+
+// Abstract base class for an expression taking two sub-expression as arguments
+// Additions, Multiplications, Divisions, Modulo, comparisons, etc.
+class BinaryExpression :
+	public ShaderExpression
+{
+public:
+	// The operator precedence, smaller values mean higher priority
+	enum Precedence
+	{
+		ADD,
+		MUL,
+	};
+
+protected:
+	IShaderExpressionPtr _a;
+	IShaderExpressionPtr _b;
+	Precedence _precedence;
+
+public:
+	// Pass the table and the expression used to perform the lookup 
+	BinaryExpression(Precedence precedence,
+					 const IShaderExpressionPtr& a = IShaderExpressionPtr(), 
+				     const IShaderExpressionPtr& b = IShaderExpressionPtr()) :
+		ShaderExpression(),
+		_a(a),
+		_b(b),
+		_precedence(precedence)
+	{}
+
+	Precedence getPrecedence() const
+	{
+		return _precedence;
+	}
+
+	void setA(const IShaderExpressionPtr& a)
+	{
+		_a = a;
+	}
+
+	void setB(const IShaderExpressionPtr& b)
+	{
+		_b = b;
+	}
+};
+typedef boost::shared_ptr<BinaryExpression> BinaryExpressionPtr;
+
+// An expression adding the value of two expressions
+class AddExpression :
+	public BinaryExpression
+{
+public:
+	// Pass the table and the expression used to perform the lookup 
+	AddExpression(const IShaderExpressionPtr& a = IShaderExpressionPtr(), 
+				  const IShaderExpressionPtr& b = IShaderExpressionPtr()) :
+		BinaryExpression(ADD, a, b)
+	{}
+
+	virtual float getValue()
+	{
+		return _a->getValue() + _b->getValue();
+	}
+
+	virtual float evaluate()
+	{
+		// no saving so far
+		return getValue();
+	}
+};
+
+} // namespace
 
 } // namespace
