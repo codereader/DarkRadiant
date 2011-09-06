307b0ff3a08fe16916482c84806e2fe63275ebfb
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2011-09-06 06:03:06 +0000

    Introducing a common base for Preview widgets, WIP.

---
+++ libs/gtkutil/preview/RenderPreview.h
@@ -0,0 +1,137 @@
+#pragma once
+
+#include <gtkmm/frame.h>
+
+#include "math/Matrix4.h"
+#include "../GLWidget.h"
+#include "../Timer.h"
+
+#include "iscenegraph.h"
+#include "irender.h"
+
+#include "render/ShaderStateRenderer.h"
+#include "render/NopVolumeTest.h"
+
+namespace Gtk
+{
+	class ToolButton;
+	class ToggleToolButton;
+	class Toolbar;
+	class HBox;
+}
+
+namespace gtkutil
+{
+
+/**
+ * greebo: This class acts as base for widgets featuring 
+ * a real time openGL render preview. It offers
+ * its own local SceneGraph, backend and frontend renderer
+ * plus all the logic for camera handling and filtering.
+ *
+ * Override the protected methods to have the scene set up 
+ * in special ways or add custom toolbar items.
+ *
+ * After construction the local scene graph will be empty.
+ */
+class RenderPreview :
+	public Gtk::Frame
+{
+private:
+	// The scene we're rendering
+	scene::GraphPtr _scene;
+
+protected:
+	// GL widget
+	gtkutil::GLWidget* _glWidget;
+
+	Gtk::HBox* _toolHBox;
+
+	Gtk::ToolButton* _startButton;
+	Gtk::ToolButton* _pauseButton;
+	Gtk::ToolButton* _stopButton;
+
+	// The backend rendersystem instance
+	RenderSystemPtr _renderSystem;
+
+	// The front-end renderer, collecting the OpenGLRenderables
+	render::ShaderStateRenderer _renderer;
+	render::NopVolumeTest _volumeTest;
+
+	// Current distance between camera and preview
+	GLfloat _camDist;
+
+	// Current rotation matrix
+	Matrix4 _rotation;
+
+	// Mutex flag to avoid draw call bunching
+	bool _renderingInProgress;
+
+	gtkutil::Timer _timer;
+
+	int _previewWidth;
+	int _previewHeight;
+
+public:
+	RenderPreview();
+
+	virtual ~RenderPreview();
+
+	void setSize(int width, int height);
+
+	/**
+	 * Initialise the GL preview. This clears the window and sets up the
+	 * initial matrices and lights.
+	 */
+	void initialisePreview();
+
+protected:
+	const scene::GraphPtr& getScene();
+
+	// Add another one to the toolbar hbox
+	void addToolbar(Gtk::Toolbar& toolbar);
+
+	// Subclasses should at least add a single node as scene root, such that
+	// the rendersystem can be associated. This is called after initialisePreview()
+	virtual void setupSceneGraph();
+
+	virtual Matrix4 getProjectionMatrix(float near_z, float far_z, float fieldOfView, int width, int height);
+	virtual Matrix4 getModelViewMatrix();
+
+	virtual void startPlayback();
+	virtual void stopPlayback();
+
+	// Override this to deliver accurate scene bounds, used for mousewheel-zooming
+	virtual AABB getSceneBounds();
+
+	// Called right before rendering, returning false will cancel the render algorithm
+	virtual bool onPreRender();
+
+	// Called after the render phase, can be used to draw custom stuff on the GL widget
+	virtual void onPostRender() {}
+
+	// Use this to render a wireframe view of the scene
+	void renderWireFrame();
+
+private:
+	void associateRenderSystem();
+
+	// gtkmm callbacks
+	bool onGLDraw(GdkEventExpose*);
+	bool onGLMotion(GdkEventMotion*);
+	bool onGLScroll(GdkEventScroll*);
+	void onStart();
+	void onPause();
+	void onStop();
+	void onStepForward();
+	void onStepBack();
+	void onSizeAllocate(Gtk::Allocation& allocation);
+
+	void drawTime();
+
+	// Called each frame by gtkutil::Timer
+	static gboolean _onFrame(gpointer data);
+};
+typedef boost::shared_ptr<RenderPreview> RenderPreviewPtr;
+
+} // namespace
--- plugins/uimanager/ParticlePreview.cpp
+++ plugins/uimanager/ParticlePreview.cpp
@@ -1,16 +1,12 @@
 #include "ParticlePreview.h"
 
-#include "gtkutil/GLWidgetSentry.h"
 #include "iuimanager.h"
 #include "ieventmanager.h"
 #include "ientity.h"
 #include "ieclass.h"
-#include "iscenegraphfactory.h"
 #include "iparticles.h"
 #include "i18n.h"
 
-#include "math/AABB.h"
-#include "math/Frustum.h"
 #include "entitylib.h"
 
 #include "string/string.h"
@@ -31,108 +27,22 @@
 namespace ui
 {
 
-/* CONSTANTS */
-
-namespace {
-
-	const GLfloat PREVIEW_FOV = 60;
-	const unsigned int MSEC_PER_FRAME = 16;
+namespace
+{
 	const char* const FUNC_EMITTER_CLASS = "func_emitter";
-
-	// Set the bool to true for the lifetime of this object
-	class ScopedBoolLock
-	{
-	private:
-		bool& _target;
-
-	public:
-		ScopedBoolLock(bool& target) :
-			_target(target)
-		{
-			_target = true;
-		}
-
-		~ScopedBoolLock()
-		{
-			_target = false;
-		}
-	};
 }
 
 // Construct the widgets
 
 ParticlePreview::ParticlePreview() :
-	Gtk::Frame(),
-	_glWidget(Gtk::manage(new gtkutil::GLWidget(true, "ParticlePreview"))),
-	_startButton(NULL),
-	_pauseButton(NULL),
-	_stopButton(NULL),
-	_scene(GlobalSceneGraphFactory().createSceneGraph()),
-	_renderSystem(GlobalRenderSystemFactory().createRenderSystem()),
-	_renderingInProgress(false),
-	_timer(MSEC_PER_FRAME, _onFrame, this),
-	_previewWidth(0),
-	_previewHeight(0)
+	gtkutil::RenderPreview()
 {
-	// Main vbox - above is the GL widget, below is the toolbar
-	Gtk::VBox* vbx = Gtk::manage(new Gtk::VBox(false, 0));
-
-	// Cast the GLWidget object to GtkWidget for further use
-	vbx->pack_start(*_glWidget, true, true, 0);
-
-	// Connect up the signals
-	_glWidget->set_events(Gdk::EXPOSURE_MASK | Gdk::BUTTON_PRESS_MASK | Gdk::BUTTON_RELEASE_MASK |
-						 Gdk::POINTER_MOTION_MASK | Gdk::SCROLL_MASK);
-
-	_glWidget->signal_expose_event().connect(sigc::mem_fun(*this, &ParticlePreview::callbackGLDraw));
-	_glWidget->signal_motion_notify_event().connect(sigc::mem_fun(*this, &ParticlePreview::callbackGLMotion));
-	_glWidget->signal_scroll_event().connect(sigc::mem_fun(*this, &ParticlePreview::callbackGLScroll));
-	_glWidget->signal_size_allocate().connect(sigc::mem_fun(*this, &ParticlePreview::onSizeAllocate));
-
-	// The HBox containing the toolbar and the menubar
-	Gtk::HBox* toolHBox = Gtk::manage(new Gtk::HBox(false, 0));
-	vbx->pack_end(*toolHBox, false, false, 0);
-
-	// Create the toolbar
+	// Add one additional toolbar for particle-related stuff
 	Gtk::Toolbar* toolbar = Gtk::manage(new Gtk::Toolbar);
 	toolbar->set_toolbar_style(Gtk::TOOLBAR_ICONS);
 
-	_startButton = Gtk::manage(new Gtk::ToolButton);
-	_startButton->signal_clicked().connect(sigc::mem_fun(*this, &ParticlePreview::callbackStart));
-	_startButton->set_icon_widget(*Gtk::manage(new Gtk::Image(Gtk::Stock::MEDIA_PLAY, Gtk::ICON_SIZE_MENU)));
-	_startButton->set_tooltip_text(_("Start time"));
-
-	_pauseButton = Gtk::manage(new Gtk::ToolButton);
-	_pauseButton->signal_clicked().connect(sigc::mem_fun(*this, &ParticlePreview::callbackPause));
-	_pauseButton->set_icon_widget(*Gtk::manage(new Gtk::Image(Gtk::Stock::MEDIA_PAUSE, Gtk::ICON_SIZE_MENU)));
-	_pauseButton->set_tooltip_text(_("Pause time"));
-
-	_stopButton = Gtk::manage(new Gtk::ToolButton);
-	_stopButton->signal_clicked().connect(sigc::mem_fun(*this, &ParticlePreview::callbackStop));
-	_stopButton->set_icon_widget(*Gtk::manage(new Gtk::Image(Gtk::Stock::MEDIA_STOP, Gtk::ICON_SIZE_MENU)));
-	_stopButton->set_tooltip_text(_("Stop time"));
-
-	Gtk::ToolButton* nextButton = Gtk::manage(new Gtk::ToolButton);
-	nextButton->signal_clicked().connect(sigc::mem_fun(*this, &ParticlePreview::callbackStepForward));
-	nextButton->set_icon_widget(*Gtk::manage(new Gtk::Image(Gtk::Stock::MEDIA_NEXT, Gtk::ICON_SIZE_MENU)));
-	nextButton->set_tooltip_text(_("Next frame"));
-
-	Gtk::ToolButton* prevButton = Gtk::manage(new Gtk::ToolButton);
-	prevButton->signal_clicked().connect(sigc::mem_fun(*this, &ParticlePreview::callbackStepBack));
-	prevButton->set_icon_widget(*Gtk::manage(new Gtk::Image(Gtk::Stock::MEDIA_PREVIOUS, Gtk::ICON_SIZE_MENU)));
-	prevButton->set_tooltip_text(_("Previous frame"));
-
-	toolbar->insert(*_startButton, 0);
-	toolbar->insert(*_pauseButton, 0);
-	toolbar->insert(*nextButton, 0);
-	toolbar->insert(*prevButton, 0);
-	toolbar->insert(*_stopButton, 0);
-
-	Gtk::Toolbar* toolbar2 = Gtk::manage(new Gtk::Toolbar);
-	toolbar2->set_toolbar_style(Gtk::TOOLBAR_ICONS);
-
 	_showAxesButton = Gtk::manage(new Gtk::ToggleToolButton);
-	_showAxesButton->signal_toggled().connect(sigc::mem_fun(*this, &ParticlePreview::callbackToggleAxes));
+	_showAxesButton->signal_toggled().connect(sigc::mem_fun(*this, &ParticlePreview::onToggleAxes));
 	_showAxesButton->set_icon_widget(*Gtk::manage(new Gtk::Image(
 		GlobalUIManager().getLocalPixbufWithMask("axes.png"))));
 	_showAxesButton->set_tooltip_text(_("Show coordinate axes"));
@@ -148,83 +58,12 @@ ParticlePreview::ParticlePreview() :
 		GlobalUIManager().getLocalPixbufWithMask("wireframe.png"))));
 	_showWireFrameButton->set_tooltip_text(_("Show wireframe"));
 
-	toolbar2->insert(*_showAxesButton, 0);
-	toolbar2->insert(*_showWireFrameButton, 0);
-	toolbar2->insert(*reloadButton, 0);
+	toolbar->insert(*_showAxesButton, 0);
+	toolbar->insert(*_showWireFrameButton, 0);
+	toolbar->insert(*reloadButton, 0);
 
-	toolHBox->pack_start(*toolbar, true, true, 0);
-	toolHBox->pack_start(*toolbar2, true, true, 0);
-
-	// Pack into a frame and return
-	add(*vbx);
-
-	// Setup the scene graph
-	setupSceneGraph();
+	addToolbar(*toolbar);
 }
-
-ParticlePreview::~ParticlePreview()
-{
-	if (_timer.isEnabled())
-	{
-		_timer.disable();
-	}
-}
-
-// Set the size request for the widget
-
-void ParticlePreview::setSize(int width, int height)
-{
-	_glWidget->set_size_request(width, height);
-}
-
-// Initialise the preview GL stuff
-
-void ParticlePreview::initialisePreview()
-{
-	// Grab the GL widget with sentry object
-	gtkutil::GLWidgetSentry sentry(*_glWidget);
-
-	// Clear the window
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LEQUAL);
-	glClearColor(0.0, 0.0, 0.0, 0);
-	glClearDepth(100.0);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	// Set up the camera
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(PREVIEW_FOV, 1, 0.1, 10000);
-
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
-	// Set up the lights
-	glEnable(GL_LIGHTING);
-
-	glEnable(GL_LIGHT0);
-	GLfloat l0Amb[] = { 0.3f, 0.3f, 0.3f, 1.0f };
-	GLfloat l0Dif[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-	GLfloat l0Pos[] = { 1.0f, 1.0f, 1.0f, 0.0f };
-	glLightfv(GL_LIGHT0, GL_AMBIENT, l0Amb);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, l0Dif);
-	glLightfv(GL_LIGHT0, GL_POSITION, l0Pos);
-
-	glEnable(GL_LIGHT1);
-	GLfloat l1Dif[] = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat l1Pos[] = { 0.0, 0.0, 1.0, 0.0 };
-	glLightfv(GL_LIGHT1, GL_DIFFUSE, l1Dif);
-	glLightfv(GL_LIGHT1, GL_POSITION, l1Pos);
-
-    if (_renderSystem->shaderProgramsAvailable())
-    {
-        _renderSystem->setShaderProgram(
-            RenderSystem::SHADER_PROGRAM_INTERACTION
-        );
-    }
-}
-
-// Set the model, this also resets the camera
 
 void ParticlePreview::setParticle(const std::string& name)
 {
@@ -241,6 +80,12 @@ void ParticlePreview::setParticle(const std::string& name)
 		_particle.reset();
 		stopPlayback();
 		return;
+	}
+
+	// Set up the scene
+	if (!_entity)
+	{
+		setupSceneGraph();
 	}
 
 	scene::INodePtr node = GlobalParticlesManager().getParticleNode(nameClean);
@@ -284,236 +129,42 @@ void ParticlePreview::setParticle(const std::string& name)
 	_glWidget->queueDraw();
 }
 
-Gtk::Widget* ParticlePreview::getWidget()
-{
-	return this;
-}
-
-void ParticlePreview::startPlayback()
-{
-	if (_timer.isEnabled())
-	{
-		// Timer is already running, just reset the preview time
-		_renderSystem->setTime(0);
-	}
-	else
-	{
-		// Timer is not enabled, we're paused or stopped
-		_timer.enable();
-	}
-
-	_pauseButton->set_sensitive(true);
-	_stopButton->set_sensitive(true);
-}
-
-void ParticlePreview::stopPlayback()
-{
-	_renderSystem->setTime(0);
-	_timer.disable();
-
-	_pauseButton->set_sensitive(false);
-	_stopButton->set_sensitive(false);
-
-	_glWidget->queue_draw();
-}
-
-void ParticlePreview::callbackStart()
-{
-	startPlayback();
-}
-
-void ParticlePreview::callbackPause()
-{
-	// Disable the button
-	_pauseButton->set_sensitive(false);
-
-	if (_timer.isEnabled())
-	{
-		_timer.disable();
-	}
-	else
-	{
-		_timer.enable(); // re-enable playback
-	}
-}
-
-void ParticlePreview::callbackStop()
-{
-	stopPlayback();
-}
-
-void ParticlePreview::callbackStepForward()
-{
-	// Disable the button
-	_pauseButton->set_sensitive(false);
-
-	if (_timer.isEnabled())
-	{
-		_timer.disable();
-	}
-
-	_renderSystem->setTime(_renderSystem->getTime() + MSEC_PER_FRAME);
-	_glWidget->queue_draw();
-}
-
-void ParticlePreview::callbackStepBack()
-{
-	// Disable the button
-	_pauseButton->set_sensitive(false);
-
-	if (_timer.isEnabled())
-	{
-		_timer.disable();
-	}
-
-	if (_renderSystem->getTime() > 0)
-	{
-		_renderSystem->setTime(_renderSystem->getTime() - MSEC_PER_FRAME);
-	}
-
-	_glWidget->queue_draw();
-}
-
-void ParticlePreview::callbackToggleAxes()
+void ParticlePreview::onToggleAxes()
 {
 	_glWidget->queue_draw();
-}
-
-void ParticlePreview::onSizeAllocate(Gtk::Allocation& allocation)
-{
-	_previewWidth = allocation.get_width();
-	_previewHeight = allocation.get_height();
-}
-
-Matrix4 ParticlePreview::getProjectionMatrix(float near_z, float far_z, float fieldOfView, int width, int height)
-{
-	const float half_width = near_z * tan(degrees_to_radians(fieldOfView * 0.5f));
-	const float half_height = half_width * (static_cast<float>(height) / static_cast<float>(width));
-
-	return Matrix4::getProjectionForFrustum(
-		-half_width,
-		half_width,
-		-half_height,
-		half_height,
-		near_z,
-		far_z
-	);
 }
 
 void ParticlePreview::setupSceneGraph()
 {
+	RenderPreview::setupSceneGraph();
+
 	_entity = GlobalEntityCreator().createEntity(
 		GlobalEntityClassManager().findClass(FUNC_EMITTER_CLASS));
 
 	// This entity is acting as our root node in the scene
-	_scene->setRoot(_entity);
-
-	// Associate our rendersystem with the graph
-	_scene->root()->setRenderSystem(_renderSystem);
-
-	// Set our render time to 0
-	_renderSystem->setTime(0);
+	getScene()->setRoot(_entity);
 }
 
-bool ParticlePreview::callbackGLDraw(GdkEventExpose* ev)
+AABB ParticlePreview::getSceneBounds()
 {
-	if (_renderingInProgress) return false; // avoid double-entering this method
-
-	ScopedBoolLock lock(_renderingInProgress); // will be set to false on method exit
-
-	// Create scoped sentry object to swap the GLWidget's buffers
-	gtkutil::GLWidgetSentry sentry(*_glWidget);
-
-	glViewport(0, 0, _previewWidth, _previewHeight);
-
-	// Set up the render and clear the drawing area in any case
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	if (_particle == NULL)
+	if (!_particle)
 	{
-		drawTime();
-		return false; // nothing to do
+		return RenderPreview::getSceneBounds();
 	}
 
-	// Update the particle
-	_particle->getParticle()->update(_rotation);
+	return _particle->getParticle()->getBounds();
+}
 
-	// Front-end render phase, collect OpenGLRenderable objects from the
-	// particle system
-	_particle->getParticle()->renderSolid(_renderer, _volumeTest);
+bool ParticlePreview::onPreRender()
+{
+	return _particle != NULL;
+}
 
-	RenderStateFlags flags = RENDER_COLOURWRITE
-                             | RENDER_ALPHATEST
-                             | RENDER_BLEND
-                             | RENDER_CULLFACE
-                             | RENDER_COLOURARRAY
-                             | RENDER_OFFSETLINE
-                             | RENDER_POLYGONSMOOTH
-                             | RENDER_LINESMOOTH
-                             | RENDER_COLOURCHANGE
-							 | RENDER_FILL
-							 | RENDER_LIGHTING
-							 | RENDER_TEXTURE_2D
-							 | RENDER_SMOOTH
-							 | RENDER_SCALED 
-							 | RENDER_FILL
-							 | RENDER_TEXTURE_CUBEMAP
-							 | RENDER_BUMP
-							 | RENDER_PROGRAM
-							 | RENDER_MATERIAL_VCOL
-							 | RENDER_VCOL_INVERT
-							 | RENDER_SCREEN;
-
-	// Allow forced activation of client state GL_COLOR_ARRAY, some shaders don't require this state
-	flags |= RENDER_FORCE_COLORARRAY;
-
-	// Set up the camera
-	Matrix4 projection = getProjectionMatrix(0.1f, 10000, PREVIEW_FOV, _previewWidth, _previewHeight);
-
-	// Premultiply with the translations
-	Matrix4 modelview;
-	{
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-
-		glTranslatef(0, 0, _camDist); // camera translation
-		glMultMatrixf(_rotation); // post multiply with rotations
-
-		// Load the matrix from openGL
-		glGetFloatv(GL_MODELVIEW_MATRIX, modelview);
-	}
-
-	// Launch the back end rendering
-	_renderSystem->render(flags, modelview, projection);
-
+void ParticlePreview::onPostRender()
+{
 	if (_showWireFrameButton->get_active())
 	{
-		flags = RENDER_COLOURWRITE
-                //| RENDER_DEPTHWRITE
-                | RENDER_ALPHATEST
-                | RENDER_BLEND
-                | RENDER_CULLFACE
-                | RENDER_COLOURARRAY
-                | RENDER_OFFSETLINE
-                | RENDER_POLYGONSMOOTH
-                | RENDER_LINESMOOTH
-                | RENDER_COLOURCHANGE
-				| RENDER_LIGHTING
-				| RENDER_SMOOTH
-				| RENDER_SCALED
-				| RENDER_TEXTURE_CUBEMAP
-				| RENDER_BUMP
-				| RENDER_PROGRAM
-				| RENDER_MATERIAL_VCOL
-				| RENDER_VCOL_INVERT
-				| RENDER_SCREEN;
-
-		flags |= RENDER_FORCE_COLORARRAY;
-
-		_particle->getParticle()->renderSolid(_renderer, _volumeTest);
-
-		_renderSystem->render(flags, modelview, projection);
+		renderWireFrame();
 	}
 
 	// Draw coordinate axes for better orientation
@@ -521,60 +172,6 @@ bool ParticlePreview::callbackGLDraw(GdkEventExpose* ev)
 	{
 		drawAxes();
 	}
-
-	drawTime();
-	//drawDebugInfo();
-
-	return false;
-}
-
-gboolean ParticlePreview::_onFrame(gpointer data)
-{
-	ParticlePreview* self = reinterpret_cast<ParticlePreview*>(data);
-
-	if (!self->_renderingInProgress)
-	{
-		self->_renderSystem->setTime(self->_renderSystem->getTime() + MSEC_PER_FRAME);
-		self->_glWidget->queue_draw();
-	}
-
-	// Return true, so that the timer gets called again
-	return TRUE;
-}
-
-void ParticlePreview::drawTime()
-{
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0, static_cast<float>(_previewWidth), 0, static_cast<float>(_previewHeight), -100, 100);
-
-	glScalef(1, -1, 1);
-	glTranslatef(0, -static_cast<float>(_previewHeight), 0);
-
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
-	if (GLEW_VERSION_1_3)
-	{
-		glClientActiveTexture(GL_TEXTURE0);
-		glActiveTexture(GL_TEXTURE0);
-	}
-
-	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-	glDisableClientState(GL_NORMAL_ARRAY);
-	glDisableClientState(GL_COLOR_ARRAY);
-
-	glDisable(GL_TEXTURE_2D);
-	glDisable(GL_LIGHTING);
-	glDisable(GL_COLOR_MATERIAL);
-	glDisable(GL_DEPTH_TEST);
-
-	glColor3f( 1.f, 1.f, 1.f );
-	glLineWidth(1);
-
-	glRasterPos3f(1.0f, static_cast<float>(_previewHeight) - 1.0f, 0.0f);
-
-	GlobalOpenGL().drawString((boost::format("%.3f sec.") % (_renderSystem->getTime() * 0.001f)).str());
 }
 
 void ParticlePreview::drawAxes()
@@ -600,68 +197,6 @@ void ParticlePreview::drawAxes()
 	glVertex3f(0,0,5);
 
 	glEnd();
-}
-
-bool ParticlePreview::callbackGLMotion(GdkEventMotion* ev)
-{
-	if (ev->state & GDK_BUTTON1_MASK) // dragging with mouse button
-	{
-		static gdouble _lastX = ev->x;
-		static gdouble _lastY = ev->y;
-
-		// Calculate the mouse delta as a vector in the XY plane, and store the
-		// current position for the next event.
-		Vector3 deltaPos(static_cast<float>(ev->x - _lastX),
-						 static_cast<float>(_lastY - ev->y),
-						 0);
-		_lastX = ev->x;
-		_lastY = ev->y;
-
-		// Calculate the axis of rotation. This is the mouse vector crossed with the Z axis,
-		// to give a rotation axis in the XY plane at right-angles to the mouse delta.
-		static Vector3 _zAxis(0, 0, 1);
-		Vector3 axisRot = deltaPos.crossProduct(_zAxis);
-
-		// Grab the GL widget, and update the modelview matrix with the
-		// additional rotation
-		if (gtkutil::GLWidget::makeCurrent(*_glWidget))
-		{
-			// Premultiply the current modelview matrix with the rotation,
-			// in order to achieve rotations in eye space rather than object
-			// space. At this stage we are only calculating and storing the
-			// matrix for the GLDraw callback to use.
-			glLoadIdentity();
-			glRotatef(-2, axisRot.x(), axisRot.y(), axisRot.z());
-			glMultMatrixf(_rotation);
-
-			// Save the new GL matrix for GL draw
-			glGetFloatv(GL_MODELVIEW_MATRIX, _rotation);
-
-			_glWidget->queueDraw(); // trigger the GLDraw method to draw the actual model
-		}
-	}
-
-	return false;
-}
-
-bool ParticlePreview::callbackGLScroll(GdkEventScroll* ev)
-{
-	if (_particle == NULL) return false;
-
-	// Scroll increment is a fraction of the AABB radius
-	float inc = static_cast<float>(_particle->getParticle()->getBounds().getRadius()) * 0.1f;
-
-	if (ev->direction == GDK_SCROLL_UP)
-		_camDist += inc;
-	else if (ev->direction == GDK_SCROLL_DOWN)
-		_camDist -= inc;
-
-	if (!_renderingInProgress)
-	{
-		_glWidget->queueDraw();
-	}
-
-	return false;
 }
 
 } // namespace ui
