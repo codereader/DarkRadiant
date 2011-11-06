92eacb6816cf8b090be228c36a26338276fd6c2c
greebo_darkmod <greebo_darkmod@80d3f8d2-1b0e-0410-a445-ca0696aa8c50>
2011-11-06 05:08:41 +0000

    Keep .pdb files when creating installation pacakges, just don't add them to the installer.

--- tools/scripts/copy_install_files.x64.cmd
+++ tools/scripts/copy_install_files.x64.cmd
@@ -6,7 +6,7 @@ cd DarkRadiant_install.x64
 del *.* /S /Q
 xcopy ..\DarkRadiant\install\*.* /s .
 del *.exp /S /Q
-del *.pdb /S /Q
+rem del *.pdb /S /Q
 del *.lib /S /Q
 del *.suo /S /Q
 del *.pgd /S /Q
