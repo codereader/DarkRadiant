a11256c2943c40e6a8e4a59487d803ae5bab76d1
Matthew Mott <orbweaver3d@gmail.com>
2011-12-29 11:46:47 +0000

    Font loading changes implemented on Linux
    - Using the same local font-loading code on both Linux and Windows for
      simplicity, fontconfig not needed.
    - FTGL added to build scripts and linked to radiant and gtkutil.
    - Local .ttf files installed into destination directory.

--- libs/gtkutil/GLFont.cpp
+++ libs/gtkutil/GLFont.cpp
@@ -5,7 +5,6 @@
 #include <gtkmm/gl/widget.h>
 #include <gdkmm/gl/font.h>
 #include <iostream>
-#include <fontconfig/fontconfig.h>
 
 namespace gtkutil
 {
@@ -14,59 +13,13 @@ GLFont::GLFont(Style style, unsigned int size) :
 	_pixelHeight(0),
 	_ftglFont(NULL)
 {
-#ifndef WIN32
-	const std::string fontName = style == FONT_SANS ? "Sans" : "Mono";
-
-	// get filename for requested font
-	FcPattern* pattern = FcNameParse(reinterpret_cast<const FcChar8*>(fontName.c_str()));
-
-	// No font substitution in Win32
-	FcConfigSubstituteWithPat(NULL, pattern, NULL, FcMatchPattern);
-	FcDefaultSubstitute(pattern);
-	
-	FcResult res;
-	pattern = FcFontMatch(NULL,pattern, &res);
-
-	if (res != FcResultMatch)
-	{
-		const char *fc_error;
-
-		switch(res)
-		{
-			case FcResultNoMatch:
-				fc_error = "No Match found";
-				break;
-			case FcResultTypeMismatch:
-				fc_error = "Result type mismatch";
-				break;
-			case FcResultNoId:
-				fc_error = "Invalid id value specified";
-				break;
-			default:
-				fc_error = "unknown error";
-		}
-
-		g_critical("The requested font \"%s\" could not be matched. FcFontMatch returned: %s", fontName, fc_error);
-	}
-
-
-	FcChar8* fcstring;
-	FcPatternGetString(pattern, "file", 0, &fcstring);
-
-	std::string fontpath(reinterpret_cast<const char*>(fcstring));
-
-	FcPatternDestroy(pattern);
-
-#else // Win32
-
+    // Load the locally-provided TTF font file
 	std::string fontpath = module::GlobalModuleRegistry()
                            .getApplicationContext()
                            .getRuntimeDataPath()
                            + "ui/fonts/";
 
 	fontpath += style == FONT_SANS ? "FreeSans.ttf" : "FreeMono.ttf";
-
-#endif
 
 	_ftglFont = FTGL::ftglCreatePixmapFont(fontpath.c_str());
 
