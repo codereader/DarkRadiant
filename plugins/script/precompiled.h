e35e35bce11ae9b4a5685987f0b3775ff322f122
codereader <greebo@angua.at>
2012-01-11 16:05:59 +0000

    Add precompiled headers to scripting module.

---
+++ plugins/script/precompiled.h
@@ -0,0 +1,15 @@
+// Precompiled header used by the Script module
+#pragma once
+
+// Include this first
+#include <boost/python.hpp>
+
+#define DR_PRECOMPILED_BOOST
+#define DR_PRECOMPILED_INTERFACES
+#define DR_PRECOMPILED_MATH
+
+#include "precompiled_main.h"
+
+// Some specifics
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
+#include <boost/python/suite/indexing/map_indexing_suite.hpp>
