36d3462855234e931dbe9d345d4fbc905c992780
codereader <greebo@angua.at>
2012-01-11 15:49:37 +0000

    Add precompiled headers to S/R editor.

---
+++ plugins/dm.stimresponse/precompiled.h
@@ -0,0 +1,23 @@
+// Precompiled header used by the S/R Editor module
+#pragma once
+
+#define DR_PRECOMPILED_BOOST
+#define DR_PRECOMPILED_GTKMM
+
+#include "precompiled_main.h"
+
+// Some objectives editor specifics
+#include "gtkutil/ScrolledFrame.h"
+#include "gtkutil/TreeModel.h"
+#include "gtkutil/LeftAlignedLabel.h"
+#include "gtkutil/StockIconMenuItem.h"
+#include "gtkutil/LeftAlignment.h"
+#include "gtkutil/RightAlignment.h"
+#include "gtkutil/ScrolledFrame.h"
+#include "gtkutil/MultiMonitor.h"
+#include "gtkutil/TextColumn.h"
+#include "gtkutil/IconTextColumn.h"
+#include "gtkutil/dialog/MessageBox.h"
+
+#include <boost/format.hpp>
+#include "string/string.h"
