510f0d7340cfabd0d7bd2aed91a2fb7057f118ad
codereader <greebo@angua.at>
2012-01-11 15:43:35 +0000

    Add precompiled headers to the objectives editor plugin (44 sec. => 13 sec.).

---
+++ plugins/dm.objectives/precompiled.h
@@ -0,0 +1,13 @@
+// Precompiled header used by the Objectives module
+#pragma once
+
+#define DR_PRECOMPILED_BOOST
+#define DR_PRECOMPILED_GTKMM
+
+#include "precompiled_main.h"
+
+// Some objectives editor specifics
+#include "gtkutil/LeftAlignment.h"
+#include "gtkutil/LeftAlignedLabel.h"
+#include "gtkutil/RightAlignment.h"
+#include "string/string.h"
