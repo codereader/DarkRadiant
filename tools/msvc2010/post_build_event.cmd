94fc8f83553a5999bcb6afc923287f61474b3e6c
codereader <greebo@angua.at>
2012-05-03 17:34:10 +0200

    Adjust post build event scripts to take the platform toolset version as argument, to enable compilation in studio versions other than VC++ 2010.

--- tools/msvc2010/post_build_event.cmd
+++ tools/msvc2010/post_build_event.cmd
@@ -21,27 +21,27 @@ IF "%1" == "Debug" GOTO GTK_DEBUG
 
 	xcopy ..\..\w32deps\gtk2\bin\lib\gtk-2.0\engines\*.dll ..\..\install\lib\gtk-2.0\engines\. /S /Y
 
-	copy ..\..\w32deps\gtk2\bin\atk-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\cairo-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\cairo-gobject-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\gdk-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\gdk-pixbuf-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\gio-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\glib-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\gmodule-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\gobject-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\gthread-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\gtk-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\pangocairo-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\pango-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\pangoft2-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\pangowin32-vc100.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\atk-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\cairo-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\cairo-gobject-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\gdk-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\gdk-pixbuf-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\gio-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\glib-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\gmodule-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\gobject-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\gthread-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\gtk-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\pangocairo-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\pango-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\pangoft2-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\pangowin32-%2.dll ..\..\install /Y
 
-	copy ..\..\w32deps\fontconfig\bin\fontconfig-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\ftgl\bin\ftgl-vc100.dll ..\..\install /Y
+	copy ..\..\w32deps\fontconfig\bin\fontconfig-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\ftgl\bin\ftgl-%2.dll ..\..\install /Y
 
-	copy ..\..\w32deps\gettext\bin\gettext-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\pixman\bin\pixman-vc100.dll ..\..\install /Y
+	copy ..\..\w32deps\gettext\bin\gettext-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\pixman\bin\pixman-%2.dll ..\..\install /Y
 
 GOTO GTK_END
 
@@ -50,27 +50,27 @@ GOTO GTK_END
 	@rem Use the debug version of the libwimp.dll
 	xcopy ..\..\w32deps\gtk2\bin\lib\gtk-2.0\engines\debug\*.dll ..\..\install\lib\gtk-2.0\engines\. /S /Y
 
-	copy ..\..\w32deps\gtk2\bin\atk-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\cairo-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\cairo-gobject-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\gdk-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\gdk-pixbuf-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\gio-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\glib-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\gmodule-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\gobject-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\gthread-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\gtk-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\pangocairo-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\pango-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\pangoft2-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtk2\bin\pangowin32-d-vc100.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\atk-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\cairo-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\cairo-gobject-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\gdk-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\gdk-pixbuf-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\gio-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\glib-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\gmodule-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\gobject-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\gthread-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\gtk-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\pangocairo-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\pango-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\pangoft2-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtk2\bin\pangowin32-d-%2.dll ..\..\install /Y
 
-	copy ..\..\w32deps\fontconfig\bin\fontconfig-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\ftgl\bin\ftgl-d-vc100.dll ..\..\install /Y
+	copy ..\..\w32deps\fontconfig\bin\fontconfig-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\ftgl\bin\ftgl-d-%2.dll ..\..\install /Y
 
-	copy ..\..\w32deps\gettext\bin\gettext-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\pixman\bin\pixman-d-vc100.dll ..\..\install /Y
+	copy ..\..\w32deps\gettext\bin\gettext-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\pixman\bin\pixman-d-%2.dll ..\..\install /Y
 
 :GTK_END
 
@@ -81,32 +81,32 @@ IF "%1" == "Debug" GOTO GTKMM_DEBUG
 
 :GTKMM_RELEASE
 	@echo Copying GTKmm release binaries
-	copy ..\..\w32deps\gtkmm\bin\atkmm-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtkmm\bin\cairomm-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtkmm\bin\gdkmm-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtkmm\bin\giomm-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtkmm\bin\glibmm-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtkmm\bin\gtkmm-vc100.dll ..\..\install /Y
-	copy "..\..\w32deps\gtkmm\bin\libsigc++-vc100.dll" ..\..\install /Y
-	copy ..\..\w32deps\gtkmm\bin\pangomm-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtksourceviewmm\bin\gtksourceviewmm-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtkglextmm\bin\gdkglextmm-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtkglextmm\bin\gtkglextmm-vc100.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkmm\bin\atkmm-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkmm\bin\cairomm-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkmm\bin\gdkmm-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkmm\bin\giomm-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkmm\bin\glibmm-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkmm\bin\gtkmm-%2.dll ..\..\install /Y
+	copy "..\..\w32deps\gtkmm\bin\libsigc++-%2.dll" ..\..\install /Y
+	copy ..\..\w32deps\gtkmm\bin\pangomm-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtksourceviewmm\bin\gtksourceviewmm-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkglextmm\bin\gdkglextmm-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkglextmm\bin\gtkglextmm-%2.dll ..\..\install /Y
 GOTO GTKMM_END
 
 :GTKMM_DEBUG
 	@echo Copying GTKmm debug binaries
-	copy ..\..\w32deps\gtkmm\bin\atkmm-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtkmm\bin\cairomm-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtkmm\bin\gdkmm-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtkmm\bin\giomm-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtkmm\bin\glibmm-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtkmm\bin\gtkmm-d-vc100.dll ..\..\install /Y
-	copy "..\..\w32deps\gtkmm\bin\libsigc++-d-vc100.dll" ..\..\install /Y
-	copy ..\..\w32deps\gtkmm\bin\pangomm-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtksourceviewmm\bin\gtksourceviewmm-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtkglextmm\bin\gdkglextmm-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtkglextmm\bin\gtkglextmm-d-vc100.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkmm\bin\atkmm-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkmm\bin\cairomm-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkmm\bin\gdkmm-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkmm\bin\giomm-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkmm\bin\glibmm-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkmm\bin\gtkmm-d-%2.dll ..\..\install /Y
+	copy "..\..\w32deps\gtkmm\bin\libsigc++-d-%2.dll" ..\..\install /Y
+	copy ..\..\w32deps\gtkmm\bin\pangomm-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtksourceviewmm\bin\gtksourceviewmm-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkglextmm\bin\gdkglextmm-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkglextmm\bin\gtkglextmm-d-%2.dll ..\..\install /Y
 
 :GTKMM_END
 
@@ -115,13 +115,13 @@ IF "%1" == "Debug" GOTO GTKSOURCEVIEW_DEBUG
 
 :GTKSOURCEVIEW_RELEASE
 	@echo Copying gtksourceview release binaries
-	copy ..\..\w32deps\gtksourceview-2.0\bin\gtksourceview-vc100.dll ..\..\install /Y
+	copy ..\..\w32deps\gtksourceview-2.0\bin\gtksourceview-%2.dll ..\..\install /Y
 
 GOTO GTKSOURCEVIEW_END
 
 :GTKSOURCEVIEW_DEBUG
 	@echo Copying gtksourceview debug binaries
-	copy ..\..\w32deps\gtksourceview-2.0\bin\gtksourceview-d-vc100.dll ..\..\install /Y
+	copy ..\..\w32deps\gtksourceview-2.0\bin\gtksourceview-d-%2.dll ..\..\install /Y
 
 :GTKSOURCEVIEW_END
 
@@ -130,15 +130,15 @@ IF "%1" == "Debug" GOTO GTKGLEXT_DEBUG
 
 :GTKGLEXT_RELEASE
 	@echo Copying GTKGlext release binaries
-	copy ..\..\w32deps\gtkglext\bin\gdkglext-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtkglext\bin\gtkglext-vc100.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkglext\bin\gdkglext-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkglext\bin\gtkglext-%2.dll ..\..\install /Y
 
 GOTO GTKGLEXT_END
 
 :GTKGLEXT_DEBUG
 	@echo Copying GTKGlext debug binaries
-	copy ..\..\w32deps\gtkglext\bin\gdkglext-d-vc100.dll ..\..\install /Y
-	copy ..\..\w32deps\gtkglext\bin\gtkglext-d-vc100.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkglext\bin\gdkglext-d-%2.dll ..\..\install /Y
+	copy ..\..\w32deps\gtkglext\bin\gtkglext-d-%2.dll ..\..\install /Y
 
 :GTKGLEXT_END
 
@@ -147,12 +147,12 @@ IF "%1" == "Debug" GOTO LIBXML2_DEBUG
 
 :LIBXML2_RELEASE
 	@echo Copying libxml2 release binaries
-	copy ..\..\w32deps\libxml2\bin\libxml2-vc100.dll ..\..\install /Y
+	copy ..\..\w32deps\libxml2\bin\libxml2-%2.dll ..\..\install /Y
 
 GOTO LIBXML2_END
 
 :LIBXML2_DEBUG
 	@echo Copying libxml2 debug binaries
-	copy ..\..\w32deps\libxml2\bin\libxml2-d-vc100.dll ..\..\install /Y
+	copy ..\..\w32deps\libxml2\bin\libxml2-d-%2.dll ..\..\install /Y
 
 :LIBXML2_END
