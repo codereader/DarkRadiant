codereader/DarkRadiant

commit b7454aaea45544a2e254d2234665743f97ad24ce

Use Glib::signal_idle() instead of SingleIdleCallback class

radiant/selection/RadiantSelectionSystem.cpp
@@ -268,8 +268,9 @@ std::size_t RadiantSelectionSystem::countSelectedComponents() const {
 }
 
 // This is called if the selection changes, so that the local list of selected nodes can be updated
-void RadiantSelectionSystem::onSelectedChanged(const scene::INodePtr& node, const Selectable& selectable) {
-
+void RadiantSelectionSystem::onSelectedChanged(const scene::INodePtr& node,
+                                               const Selectable& selectable)
+{
     // Cache the selection state
     bool isSelected = selectable.isSelected();
     int delta = isSelected ? +1 : -1;
@@ -304,7 +305,7 @@ void RadiantSelectionSystem::onSelectedChanged(const scene::INodePtr& node, cons
     ASSERT_MESSAGE(_selection.size() == _countPrimitive, "selection-tracking error");
 
     // Schedule an idle callback
-    requestIdleCallback();
+    scheduleUpdate();
 
     _requestWorkZoneRecalculation = true;
     _requestSceneGraphChange = true;
@@ -337,7 +338,7 @@ void RadiantSelectionSystem::onComponentSelection(const scene::INodePtr& node, c
     ASSERT_MESSAGE(_componentSelection.size() == _countComponent, "component selection-tracking error");
 
     // Schedule an idle callback
-    requestIdleCallback();
+    scheduleUpdate();
 
     _requestWorkZoneRecalculation = true;
     _requestSceneGraphChange = true;
@@ -769,7 +770,7 @@ void RadiantSelectionSystem::MoveSelected(const View& view, const Vector2& devic
         _requestWorkZoneRecalculation = true;
         _requestSceneGraphChange = true;
 
-        requestIdleCallback();
+        scheduleUpdate();
     }
 }
 
@@ -857,7 +858,7 @@ void RadiantSelectionSystem::freezeTransforms()
     _requestWorkZoneRecalculation = true;
     _requestSceneGraphChange = true;
 
-    requestIdleCallback();
+    scheduleUpdate();
 }
 
 // End the move, this freezes the current transforms
@@ -912,7 +913,7 @@ void RadiantSelectionSystem::endMove() {
 const selection::WorkZone& RadiantSelectionSystem::getWorkZone()
 {
     // Flush any pending idle callbacks, we need the workzone now
-    flushIdleCallback();
+    update();
 
     return _workZone;
 }
@@ -1031,7 +1032,7 @@ void RadiantSelectionSystem::onSceneBoundsChanged()
     pivotChanged();
 
     _requestWorkZoneRecalculation = true;
-    requestIdleCallback();
+    scheduleUpdate();
 }
 
 // RegisterableModule implementation
@@ -1098,7 +1099,14 @@ void RadiantSelectionSystem::shutdownModule()
     destroyStatic();
 }
 
-void RadiantSelectionSystem::onGtkIdle()
+void RadiantSelectionSystem::scheduleUpdate()
+{
+    Glib::signal_idle().connect_once(
+        sigc::mem_fun(this, &RadiantSelectionSystem::update)
+    );
+}
+
+void RadiantSelectionSystem::update()
 {
     // System is idle, check for pending tasks
 

radiant/selection/RadiantSelectionSystem.h
@@ -1,12 +1,10 @@
-#ifndef RADIANTSELECTIONSYSTEM_H_
-#define RADIANTSELECTIONSYSTEM_H_
+#pragma once
 
 #include "iregistry.h"
 #include "irenderable.h"
 #include "iselection.h"
 #include "selectionlib.h"
 #include "math/Matrix4.h"
-#include "gtkutil/event/SingleIdleCallback.h"
 #include "Manipulator.h"
 #include "Manipulatables.h"
 #include "TranslateManipulator.h"
@@ -56,8 +54,7 @@ class RadiantSelectionSystem :
 	public Translatable,
 	public Rotatable,
 	public Scalable,
-	public Renderable,
-	protected gtkutil::SingleIdleCallback
+	public Renderable
 {
 	mutable Matrix4 _pivot2world;
 	Matrix4 _pivot2worldStart;
@@ -226,8 +223,8 @@ public:
 	virtual void shutdownModule();
 
 protected:
-	// Called when GTK is idle to recalculate the workzone (if necessary)
-	virtual void onGtkIdle();
+	void update();
+    void scheduleUpdate();
 
 	// Traverses the scene and adds any selectable nodes matching the given SelectionTest to the "targetList".
 	void testSelectScene(SelectablesList& targetList, SelectionTest& test,
@@ -237,5 +234,3 @@ protected:
 private:
 	void notifyObservers(const scene::INodePtr& node, bool isComponent);
 };
-
-#endif /*RADIANTSELECTIONSYSTEM_H_*/

libs/gtkutil/DeferredAdjustment.h
@@ -3,14 +3,11 @@
 #include <gtkmm/adjustment.h>
 #include <boost/function.hpp>
 
-#include "event/SingleIdleCallback.h"
-
 namespace gtkutil
 {
 
-class DeferredAdjustment :
-	public Gtk::Adjustment,
-	protected SingleIdleCallback
+/// Adjustment that calls a callback function when Gtk is idle
+class DeferredAdjustment : public Gtk::Adjustment
 {
 public:
 	typedef boost::function<void(double)> ValueChangedFunction;
@@ -29,20 +26,19 @@ public:
 
 	void flush()
 	{
-		flushIdleCallback();
+		_function(_value);
 	}
 
 protected:
-	void onGtkIdle()
-	{
-		_function(_value);
-	}
 
 	// gtkmm signal handler
 	void on_value_changed()
 	{
 		_value = get_value();
-		requestIdleCallback();
+
+        Glib::signal_idle().connect_once(
+            sigc::mem_fun(this, &DeferredAdjustment::flush)
+        );
 	}
 };
 

libs/gtkutil/DeferredMotion.h
@@ -3,8 +3,6 @@
 #include <gdk/gdkevents.h>
 #include <boost/function.hpp>
 
-#include "event/SingleIdleCallback.h"
-
 namespace gtkutil
 {
 
@@ -13,8 +11,7 @@ namespace gtkutil
  * It is buffering the motion calls until GTK is idle, in which case the 
  * attached callback is invoked with the buffered x,y and state parameters.
  */
-class DeferredMotion :
-	protected SingleIdleCallback
+class DeferredMotion
 {
 public:
 	// The motion function to invoke when GTK is idle
@@ -28,28 +25,30 @@ private:
 	gdouble _y;
 	guint _state;
 
+private:
+	void invokeCallback()
+	{
+		_motionCallback(_x, _y, _state);
+	}
+
 public:
 	DeferredMotion(const MotionCallback& motionCallback) :
 		_motionCallback(motionCallback)
 	{}
 
-	// greebo: This is the actual callback method that gets connected via to the "motion_notify_event"
+    // greebo: This is the actual callback method that gets connected via to the
+    // "motion_notify_event"
 	bool onMouseMotion(GdkEventMotion* ev)
 	{
 		_x = ev->x;
 		_y = ev->y;
 		_state = ev->state;
 
-		requestIdleCallback();
+        Glib::signal_idle().connect_once(
+            sigc::mem_fun(this, &DeferredMotion::invokeCallback)
+        );
 		
 		return false;
-	}
-
-protected:
-	// GTK idle callback
-	void onGtkIdle()
-	{
-		_motionCallback(_x, _y, _state);
 	}
 };
 

libs/gtkutil/DeferredMotionDelta.h
@@ -1,11 +1,8 @@
 #pragma once
 
-#include <sigc++/connection.h>
-#include <glib.h>
+#include <glibmm.h>
 #include <gdk/gdkevents.h>
 #include <boost/function.hpp>
-
-#include "event/SingleIdleCallback.h"
 
 namespace gtkutil
 {
@@ -14,8 +11,7 @@ namespace gtkutil
  * A class accumulating mouse motion delta calls. When GTK is idle, 
  * the attached function object is called with the stored x,y delta values.
  */
-class DeferredMotionDelta :
-	private SingleIdleCallback
+class DeferredMotionDelta
 {
 public:
 	typedef boost::function<void(int, int)> MotionDeltaFunction;
@@ -29,6 +25,8 @@ private:
 	MotionDeltaFunction _function;
 
 public:
+
+    /// Initialise with function to call
 	DeferredMotionDelta(const MotionDeltaFunction& function) : 
 		_deltaX(0), 
 		_deltaY(0),
@@ -37,24 +35,21 @@ public:
 
 	void flush()
 	{
-		flushIdleCallback();
+		_function(_deltaX, _deltaY);
+
+		_deltaX = 0;
+		_deltaY = 0;
 	}
 
+    /// Supply motion delta and invoke the callback when idle
 	void onMouseMotionDelta(int x, int y, guint state)
 	{
 		_deltaX += x;
 		_deltaY += y;
 
-		requestIdleCallback();
-	}
-
-private:
-	void onGtkIdle()
-	{
-		_function(_deltaX, _deltaY);
-
-		_deltaX = 0;
-		_deltaY = 0;
+        Glib::signal_idle().connect_once(
+            sigc::mem_fun(this, &DeferredMotionDelta::flush)
+        );
 	}
 };
 

libs/gtkutil/event/SingleIdleCallback.h
@@ -1,104 +1,0 @@
-#pragma once
-
-#include <glib.h>
-
-#include <iostream>
-
-namespace gtkutil
-{
-
-/**
- * Base class for classes which wish to receive a single callback when GTK is
- * idle, to perform processing that does not block the GUI.
- *
- * A class which derives from this must invoke the requestIdleCallback() method
- * whenever it is ready to receive an idle callback. Subsequently, it will have
- * its onGtkIdle() method invoked once during the GTK idle period.
- */
-class SingleIdleCallback
-{
-private:
-	// ID of the registered callback (for subsequent removal)
-	guint _id;
-
-private:
-
-	// Actual GTK idle callback static function, which invokes the child class'
-	// implementing method
-	static gboolean _onIdle(gpointer self)
-	{
-		SingleIdleCallback* cb = reinterpret_cast<SingleIdleCallback*>(self);
-
-		// Call the virtual function
-		cb->onGtkIdle();
-
-		cb->_id = 0;
-
-		return FALSE; // automatically deregister callback
-	}
-
-	// Remove the callback
-	void deregisterCallback()
-	{
-		if (_id != 0)
-		{
-			g_source_remove(_id);
-			_id = 0;
-		}
-	}
-
-protected:
-
-	/**
-	 * Request an idle callback. The onGtkIdle() method will be invoked during
-	 * the next GTK idle period.
-	 */
-	void requestIdleCallback()
-	{
-		if (_id != 0) return; // avoid duplicate registrations
-
-		_id = g_idle_add(_onIdle, this);
-	}
-
-	// TRUE if an idle callback is pending
-	bool callbacksPending() const
-	{
-		return _id != 0;
-	}
-
-	// Flushes any pending events, forces a call to onGtkIdle, if necessary
-	void flushIdleCallback()
-	{
-		if (_id != 0)
-		{
-			// Cancel the event and force an onGtkIdle() call
-			deregisterCallback();
-
-			// Invoke the idle callback
-			onGtkIdle();
-		}
-	}
-
-	/**
-	 * Implementing method for the idle callback. Code in this method will
-	 * be executed only when GTK is idle.
-	 */
-	virtual void onGtkIdle() { }
-
-public:
-
-	SingleIdleCallback() :
-		_id(0)
-	{}
-
-	/**
-	 * Destructor. De-registers the callback from GTK, so that the method will
-	 * not be invoked after the object is destroyed.
-	 */
-	virtual ~SingleIdleCallback()
-	{
-		deregisterCallback();
-	}
-};
-
-}

plugins/uimanager/StatusBarManager.cpp
@@ -88,7 +88,9 @@ void StatusBarManager::setText(const std::string& name, const std::string& text)
 		found->second->text = text;
 
 		// Request an idle callback
-		requestIdleCallback();
+        Glib::signal_idle().connect_once(
+            sigc::mem_fun(this, &StatusBarManager::onGtkIdle)
+        );
 	}
 	else
 	{

plugins/uimanager/StatusBarManager.h
@@ -1,9 +1,7 @@
-#ifndef _STATUS_BAR_MANAGER_H_
-#define _STATUS_BAR_MANAGER_H_
+#pragma once
 
 #include "iuimanager.h"
 #include <map>
-#include "gtkutil/event/SingleIdleCallback.h"
 
 namespace Gtk
 {
@@ -16,8 +14,7 @@ namespace ui
 {
 
 class StatusBarManager :
-	public IStatusBarManager,
-	protected gtkutil::SingleIdleCallback
+	public IStatusBarManager
 {
 	struct StatusBarElement
 	{
@@ -97,11 +94,10 @@ public:
 	 */
 	void setText(const std::string& name, const std::string& text);
 
-protected:
+private:
 	// Gets called when GTK is idle - this fills in the status text
 	void onGtkIdle();
 
-private:
 	// Returns an integer position which is not used yet.
 	int getFreePosition(int desiredPosition);
 
@@ -113,5 +109,3 @@ private:
 };
 
 } // namespace ui
-
-#endif /* _STATUS_BAR_MANAGER_H_ */

commit 76260f1a113b3cb62867ad1f46a8ca7a5d8a6a9c

Fixed hang during "Loading textures" for a large map
Commit b7454aaea45544a2e254d2234665743f97ad24ce inadvertently removed an
important performance optimisation that avoided scheduling additional idle
callbacks if one was already scheduled. This behaviour is now corrected.

radiant/selection/RadiantSelectionSystem.cpp
@@ -54,6 +54,7 @@ void matrix4_assign_rotation_for_pivot(Matrix4& matrix, const scene::INodePtr& n
 // --------- RadiantSelectionSystem Implementation ------------------------------------------
 
 RadiantSelectionSystem::RadiantSelectionSystem() :
+    _updatePending(false),
     _requestSceneGraphChange(false),
     _requestWorkZoneRecalculation(true),
     _undoBegun(false),
@@ -1101,9 +1102,14 @@ void RadiantSelectionSystem::shutdownModule()
 
 void RadiantSelectionSystem::scheduleUpdate()
 {
-    Glib::signal_idle().connect_once(
-        sigc::mem_fun(this, &RadiantSelectionSystem::update)
-    );
+    if (!_updatePending)
+    {
+        _updatePending = true;
+
+        Glib::signal_idle().connect_once(
+            sigc::mem_fun(this, &RadiantSelectionSystem::update)
+        );
+    }
 }
 
 void RadiantSelectionSystem::update()
@@ -1148,6 +1154,8 @@ void RadiantSelectionSystem::update()
 
         GlobalSceneGraph().sceneChanged();
     }
+
+    _updatePending = false;
 }
 
 // Define the static SelectionSystem module

radiant/selection/RadiantSelectionSystem.h
@@ -73,6 +73,7 @@ class RadiantSelectionSystem :
 	// When this is set to TRUE, the idle callback will emit a scenegraph change call
 	// This is to avoid massive calls to GlobalSceneGraph().sceneChanged() on each
 	// and every selection change.
+    mutable bool _updatePending;
 	mutable bool _requestSceneGraphChange;
 	mutable bool _requestWorkZoneRecalculation;
 