a5fbb0f4ec7a1d13b0312485eb7c8b6d3999a263
codereader <greebo@angua.at>
2012-10-10 14:20:49 +0200

    Move method definitions to .cpp file.

--- radiant/render/View.cpp
+++ radiant/render/View.cpp
@@ -19,7 +19,11 @@ along with GtkRadiant; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include "view.h"
+#include "View.h"
+
+#if defined(_DEBUG)
+#define DEBUG_CULLING
+#endif
 
 #if defined(DEBUG_CULLING)
 
@@ -45,7 +49,6 @@ void Cull_ResetStats()
 #endif
 }
 
-
 const char* Cull_GetStats()
 {
 #if defined(DEBUG_CULLING)
@@ -54,4 +57,128 @@ const char* Cull_GetStats()
 #else
   return "";
 #endif
+}
+
+#if defined(DEBUG_CULLING)
+#define INC_COUNTER(x) (++(x))
+#else
+#define INC_COUNTER(x)
+#endif
+
+View::View(bool fill) :
+	_modelview(Matrix4::getIdentity()),
+	_projection(Matrix4::getIdentity()),
+	_scissor(Matrix4::getIdentity()),
+	_fill(fill)
+{}
+
+void View::Construct(const Matrix4& projection, const Matrix4& modelview, std::size_t width, std::size_t height)
+{
+	// modelview
+	_modelview = modelview;
+
+	// projection
+	_projection = projection;
+
+	// viewport
+	_viewport = Matrix4::getIdentity();
+	_viewport[0] = float(width/2);
+	_viewport[5] = float(height/2);
+	if(fabs(_projection[11]) > 0.0000001)
+		_viewport[10] = _projection[0] * _viewport[0];
+	else
+		_viewport[10] = 1 / _projection[10];
+
+	construct();
+}
+
+void View::EnableScissor(float min_x, float max_x, float min_y, float max_y)
+{
+	_scissor = Matrix4::getIdentity();
+	_scissor[0] = (max_x - min_x) * 0.5f;
+	_scissor[5] = (max_y - min_y) * 0.5f;
+	_scissor[12] = (min_x + max_x) * 0.5f;
+	_scissor[13] = (min_y + max_y) * 0.5f;
+	_scissor.invertFull();
+
+	construct();
+}
+
+void View::DisableScissor()
+{
+	_scissor = Matrix4::getIdentity();
+
+	construct();
+}
+
+bool View::TestPoint(const Vector3& point) const
+{
+	return _viewproj.testPoint(point);
+}
+
+bool View::TestLine(const Segment& segment) const
+{
+	return _frustum.testLine(segment);
+}
+
+bool View::TestPlane(const Plane3& plane) const
+{
+	INC_COUNTER(g_count_planes);
+	return _viewer.testPlane(plane);
+}
+
+bool View::TestPlane(const Plane3& plane, const Matrix4& localToWorld) const
+{
+	INC_COUNTER(g_count_oriented_planes);
+	return _viewer.testPlane(plane, localToWorld);
+}
+
+VolumeIntersectionValue View::TestAABB(const AABB& aabb) const
+{
+	INC_COUNTER(g_count_bboxs);
+    return _frustum.testIntersection(aabb);
+}
+
+VolumeIntersectionValue View::TestAABB(const AABB& aabb, const Matrix4& localToWorld) const
+{
+	INC_COUNTER(g_count_oriented_bboxs);
+	return _frustum.testIntersection(aabb, localToWorld);
+}
+
+const Matrix4& View::GetViewMatrix() const
+{
+	return _viewproj;
+}
+
+const Matrix4& View::GetViewport() const
+{
+	return _viewport;
+}
+
+const Matrix4& View::GetModelview() const
+{
+	return _modelview;
+}
+
+const Matrix4& View::GetProjection() const
+{
+	return _projection;
+}
+
+bool View::fill() const
+{
+	return _fill;
+}
+
+const Vector3& View::getViewer() const
+{
+	return _viewer.getVector3();
+}
+
+void View::construct()
+{
+	_viewproj = _scissor.getMultipliedBy(_projection).getMultipliedBy(_modelview);
+
+	_frustum = Frustum::createFromViewproj(_viewproj);
+	_viewer = Viewer::createFromViewProjection(_viewproj);
 }
--- radiant/render/View.h
+++ radiant/render/View.h
@@ -1,215 +1,63 @@
-/*
-Copyright (C) 2001-2006, William Joseph.
-All Rights Reserved.
-
-This file is part of GtkRadiant.
-
-GtkRadiant is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-GtkRadiant is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with GtkRadiant; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
 #pragma once
 
 #include "ivolumetest.h"
+
 #include "math/Frustum.h"
 #include "math/ViewProjection.h"
 #include "math/Viewer.h"
-
-
-#if defined(_DEBUG)
-#define DEBUG_CULLING
-#endif
-
-
-#if defined(DEBUG_CULLING)
-
-extern int g_count_dots;
-extern int g_count_planes;
-extern int g_count_oriented_planes;
-extern int g_count_bboxs;
-extern int g_count_oriented_bboxs;
-
-#endif
-
-inline void debug_count_dot()
-{
-#if defined(DEBUG_CULLING)
-  ++g_count_dots;
-#endif
-}
-
-inline void debug_count_plane()
-{
-#if defined(DEBUG_CULLING)
-  ++g_count_planes;
-#endif
-}
-
-inline void debug_count_oriented_plane()
-{
-#if defined(DEBUG_CULLING)
-  ++g_count_oriented_planes;
-#endif
-}
-
-inline void debug_count_bbox()
-{
-#if defined(DEBUG_CULLING)
-  ++g_count_bboxs;
-#endif
-}
-
-inline void debug_count_oriented_bbox()
-{
-#if defined(DEBUG_CULLING)
-  ++g_count_oriented_bboxs;
-#endif
-}
-
-
-
 
 /// \brief View-volume culling and transformations.
 class View : 
 	public VolumeTest
 {
-  /// modelview matrix
-  Matrix4 m_modelview;
-  /// projection matrix
-  Matrix4 m_projection;
-  /// device-to-screen transform
-  Matrix4 m_viewport;
+private:
+	/// modelview matrix
+	Matrix4 _modelview;
 
-  Matrix4 m_scissor;
+	/// projection matrix
+	Matrix4 _projection;
+
+	/// device-to-screen transform
+	Matrix4 _viewport;
+
+	Matrix4 _scissor;
 
 	/// combined modelview and projection matrix
-	ViewProjection m_viewproj;
+	ViewProjection _viewproj;
 
 	/// camera position in world space
-	Viewer m_viewer;
+	Viewer _viewer;
 
-  /// view frustum in world space
-  Frustum m_frustum;
+	/// view frustum in world space
+	Frustum _frustum;
 
-  bool m_fill;
-
-  void construct()
-  {
-	  m_viewproj = m_scissor.getMultipliedBy(m_projection).getMultipliedBy(m_modelview);
-
-    m_frustum = Frustum::createFromViewproj(m_viewproj);
-	m_viewer = Viewer::createFromViewProjection(m_viewproj);
-  }
+	bool _fill;
+	
 public:
-  View(bool fill = false) :
-    m_modelview(Matrix4::getIdentity()),
-    m_projection(Matrix4::getIdentity()),
-    m_scissor(Matrix4::getIdentity()),
-    m_fill(fill)
-  {
-  }
-  void Construct(const Matrix4& projection, const Matrix4& modelview, std::size_t width, std::size_t height)
-  {
-    // modelview
-    m_modelview = modelview;
+	View(bool fill = false);
 
-    // projection
-    m_projection = projection;
+	void Construct(const Matrix4& projection, const Matrix4& modelview, std::size_t width, std::size_t height);
 
-    // viewport
-    m_viewport = Matrix4::getIdentity();
-    m_viewport[0] = float(width/2);
-    m_viewport[5] = float(height/2);
-    if(fabs(m_projection[11]) > 0.0000001)
-      m_viewport[10] = m_projection[0] * m_viewport[0];
-    else
-      m_viewport[10] = 1 / m_projection[10];
+	void EnableScissor(float min_x, float max_x, float min_y, float max_y);
+	void DisableScissor();
 
-    construct();
-  }
-  void EnableScissor(float min_x, float max_x, float min_y, float max_y)
-  {
-    m_scissor = Matrix4::getIdentity();
-    m_scissor[0] = (max_x - min_x) * 0.5f;
-    m_scissor[5] = (max_y - min_y) * 0.5f;
-    m_scissor[12] = (min_x + max_x) * 0.5f;
-    m_scissor[13] = (min_y + max_y) * 0.5f;
-    m_scissor.invertFull();
+	bool TestPoint(const Vector3& point) const;
+	bool TestLine(const Segment& segment) const;
+	bool TestPlane(const Plane3& plane) const;
+	bool TestPlane(const Plane3& plane, const Matrix4& localToWorld) const;
 
-    construct();
-  }
-  void DisableScissor()
-  {
-    m_scissor = Matrix4::getIdentity();
+    VolumeIntersectionValue TestAABB(const AABB& aabb) const;
+	VolumeIntersectionValue TestAABB(const AABB& aabb, const Matrix4& localToWorld) const;
 
-    construct();
-  }
+	const Matrix4& GetViewMatrix() const;
+	const Matrix4& GetViewport() const;
+	const Matrix4& GetModelview() const;
+	const Matrix4& GetProjection() const;
 
-	bool TestPoint(const Vector3& point) const
-	{
-		return m_viewproj.testPoint(point);
-	}
+	bool fill() const;
+	
+	const Vector3& getViewer() const;
 
-  bool TestLine(const Segment& segment) const
-  {
-    return m_frustum.testLine(segment);
-  }
-  bool TestPlane(const Plane3& plane) const
-  {
-    debug_count_plane();
-	return m_viewer.testPlane(plane);
-  }
-  bool TestPlane(const Plane3& plane, const Matrix4& localToWorld) const
-  {
-    debug_count_oriented_plane();
-	return m_viewer.testPlane(plane, localToWorld);
-  }
-
-    VolumeIntersectionValue TestAABB(const AABB& aabb) const
-    {
-        debug_count_bbox();
-        return m_frustum.testIntersection(aabb);
-    }
-
-	VolumeIntersectionValue TestAABB(const AABB& aabb, const Matrix4& localToWorld) const
-	{
-		debug_count_oriented_bbox();
-		return m_frustum.testIntersection(aabb, localToWorld);
-	}
-
-  const Matrix4& GetViewMatrix() const
-  {
-    return m_viewproj;
-  }
-  const Matrix4& GetViewport() const
-  {
-    return m_viewport;
-  };
-  const Matrix4& GetModelview() const
-  {
-    return m_modelview;
-  }
-  const Matrix4& GetProjection() const
-  {
-    return m_projection;
-  }
-
-  bool fill() const
-  {
-    return m_fill;
-  }
-  const Vector3& getViewer() const
-  {
-    return m_viewer.getVector3();
-  }
+private:
+	void construct();
 };
