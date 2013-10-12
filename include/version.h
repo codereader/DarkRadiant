fc44f65a29e61b344b6ea965b45e2980944d5f76
codereader <greebo@angua.at>
2013-10-12 08:44:13 +0200

    Version bump

--- include/version.h
+++ include/version.h
@@ -2,7 +2,7 @@
 #include <config.h>
 #define RADIANT_VERSION PACKAGE_VERSION
 #else
-#define RADIANT_VERSION "1.7.4"
+#define RADIANT_VERSION "1.8.0"
 #endif
 
 #define RADIANT_APPNAME "DarkRadiant"
