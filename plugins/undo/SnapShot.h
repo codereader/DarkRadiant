c3b5d6fae2a66663d3513b2b05360a94157434ca
codereader <greebo@angua.at>
2014-01-04 08:26:46 +0100

    Simplify Undo SnapShot class.

--- plugins/undo/SnapShot.h
+++ plugins/undo/SnapShot.h
@@ -2,7 +2,38 @@
 
 #include "iundo.h"
 
-/* greebo: Basically, this class can contain a whole list of Undoables and all their UndoMementos,
+namespace undo
+{
+
+/**
+ * greebo: A StateApplicator applies the saved state to
+ * an Undoable. The pointers to the Undables and their
+ * UndoMementos are stored internally.
+ */
+class StateApplicator
+{
+public:
+	Undoable& _undoable;
+private:
+	UndoMemento* _data;
+public:
+	// Constructor
+	StateApplicator(Undoable& undoable) :
+		_undoable(undoable), 
+		_data(_undoable.exportState())
+	{}
+
+	void restore() {
+		_undoable.importState(_data);
+	}
+
+	void release() {
+		_data->release();
+	}
+};
+
+/** 
+ * greebo: Basically, this class can contain a whole list of Undoables and all their UndoMementos,
  * as there can be multiple Undoables whose states have to be saved in a Snapshot.
  *
  * What happens on save(): The Undable is queried for its UndoMemento (the actual data)
@@ -11,70 +42,33 @@
  * Upon request (restore() or release()) the UndoMementos are restored back to their according
  * Undoables or released from memory, resp.
  */
-
-namespace undo {
-
-class Snapshot
+class Snapshot :
+	public std::list<StateApplicator>
 {
-	/* greebo: A StateApplicator applies the saved state to
-	 * an Undoable. The pointers to the Undables and their
-	 * UndoMementos are stored internally.
-	 */
-	class StateApplicator
-	{
-	public:
-		Undoable& _undoable;
-	private:
-		UndoMemento* _data;
-	public:
-		// Constructor
-		StateApplicator(Undoable& undoable) :
-			_undoable(undoable), 
-			_data(_undoable.exportState())
-		{}
-
-		void restore() {
-			_undoable.importState(_data);
-		}
-
-		void release() {
-			_data->release();
-		}
-	};
-
-	typedef std::list<StateApplicator> StateApplicatorList;
-	StateApplicatorList _states;
-
 public:
-
-	bool empty() const {
-		return _states.empty();
-	}
-
-	std::size_t size() const {
-		return _states.size();
-	}
-
 	// Adds a StateApplicator to the internal list. The Undoable pointer is saved as well as
 	// the pointer to its UndoMemento (queried by exportState().
-	void save(Undoable& undoable) {
-		_states.push_front(StateApplicator(undoable));
+	void save(Undoable& undoable)
+	{
+		push_front(StateApplicator(undoable));
 	}
 
 	// Cycles through all the StateApplicators and tells them to restore the state.
-	void restore() {
-		for (StateApplicatorList::iterator i = _states.begin(); i != _states.end(); ++i) {
-			i->restore();
-		}
+	void restore()
+	{
+		std::for_each(begin(), end(), [&] (StateApplicator& state)
+		{
+			state.restore();
+		});
 	}
 
 	// Releases all the UndoMemento from the heap by cycling through the StateApplicators
 	void release()
 	{
-		for (StateApplicatorList::iterator i = _states.begin(); i != _states.end(); ++i)
+		std::for_each(begin(), end(), [&] (StateApplicator& state)
 		{
-			i->release();
-		}
+			state.release();
+		});
 	}
 
 }; // class Snapshot
