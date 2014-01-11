af0b62d84a88f104e7ba4de102e802850dc005b7
codereader <greebo@angua.at>
2014-01-11 14:38:18 +0100

    Script updates

--- tools/scripts/compile_release_package.x64.cmd
+++ tools/scripts/compile_release_package.x64.cmd
@@ -1,3 +1,3 @@
-msbuild ..\msvc2010\DarkRadiant.sln /p:configuration=release /t:rebuild /p:platform=x64 /maxcpucount:4 
+msbuild ..\msvc2012\DarkRadiant.sln /p:configuration=release /t:rebuild /p:platform=x64 /maxcpucount:4 
 call copy_install_files.x64.cmd
 call ..\innosetup\create_installer.x64.cmd
--- tools/scripts/compile_release_package.x86.cmd
+++ tools/scripts/compile_release_package.x86.cmd
@@ -1,3 +1,3 @@
-msbuild ..\msvc2010\DarkRadiant.sln /p:configuration=release /t:rebuild /p:platform=Win32 /maxcpucount:4 
+msbuild ..\msvc2012\DarkRadiant.sln /p:configuration=release /t:rebuild /p:platform=Win32 /maxcpucount:4 
 call copy_install_files.cmd
 call ..\innosetup\create_installer.x86.cmd
