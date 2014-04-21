cff7b36fa24a9eeefbc46857d58607e0ccc345ba
codereader <greebo@angua.at>
2014-04-21 13:39:45 +0200

    Start migrating RenderPreview widgets.

--- libs/gtkutil/preview/RenderPreview.cpp
+++ libs/gtkutil/preview/RenderPreview.cpp
@@ -10,11 +10,556 @@
 #include "math/AABB.h"
 #include "util/ScopedBoolLock.h"
 
+#include "../GLWidget.h"
+#include <wx/sizer.h>
+#include <wx/menu.h>
+#include <wx/dcclient.h>
+
 #include <gtkmm/box.h>
 #include <gtkmm/toolbar.h>
 #include <gtkmm/stock.h>
 
 #include "../GLWidgetSentry.h"
+#include <boost/bind.hpp>
+
+namespace wxutil
+{
+
+namespace
+{
+    const GLfloat PREVIEW_FOV = 60;
+    const unsigned int MSEC_PER_FRAME = 16;
+
+    // Widget names
+    const std::string BOTTOM_BOX("bottomBox");
+    const std::string PAUSE_BUTTON("pauseButton");
+    const std::string STOP_BUTTON("stopButton");
+}
+
+RenderPreview::RenderPreview(wxWindow* parent, bool enableAnimation) :
+    _mainPanel(loadNamedPanel(parent, "RenderPreviewPanel")),
+	_glWidget(new wxutil::GLWidget(_mainPanel, boost::bind(&RenderPreview::drawPreview, this), "RenderPreview")),
+    _renderSystem(GlobalRenderSystemFactory().createRenderSystem()),
+    _sceneWalker(_renderer, _volumeTest),
+    _renderingInProgress(false),
+    _timer(this),
+    _previewWidth(0),
+    _previewHeight(0),
+    _filtersMenu(GlobalUIManager().createFilterMenu())
+{
+	Connect(wxEVT_TIMER, wxTimerEventHandler(RenderPreview::_onFrame), NULL, this);
+
+    // Insert GL widget
+	_mainPanel->GetSizer()->Prepend(_glWidget, 1, wxEXPAND);
+
+
+	_glWidget->Connect(wxEVT_SIZE, wxSizeEventHandler(RenderPreview::onSizeAllocate), NULL, this);
+	_glWidget->Connect(wxEVT_MOUSEWHEEL, wxMouseEventHandler(RenderPreview::onGLScroll), NULL, this);
+	_glWidget->Connect(wxEVT_MOTION, wxMouseEventHandler(RenderPreview::onGLMotion), NULL, this);
+	
+	wxToolBar* toolbar = findNamedObject<wxToolBar>(_mainPanel, "RenderPreviewAnimToolbar");
+
+    // Set up the toolbar
+    if (enableAnimation)
+    {
+        connectToolbarSignals();
+    }
+    else
+    {
+		toolbar->GetParent()->RemoveChild(toolbar);
+		toolbar->Destroy();
+    }
+
+	// Add filters menu to end of bottom hbox
+	toolbar->GetContainingSizer()->Add(_filtersMenu->getMenuBarWidget(), 0, wxEXPAND);
+
+    // Get notified of filter changes
+    GlobalFilterSystem().filtersChangedSignal().connect(
+        sigc::mem_fun(this, &RenderPreview::filtersChanged)
+    );
+}
+
+void RenderPreview::connectToolbarSignals()
+{
+	wxToolBar* toolbar = findNamedObject<wxToolBar>(_mainPanel, "RenderPreviewAnimToolbar");
+
+	toolbar->Connect(getToolBarToolByLabel(toolbar, "startTimeButton")->GetId(), 
+		wxEVT_TOOL, wxCommandEventHandler(RenderPreview::onStartPlaybackClick), NULL, this);
+	toolbar->Connect(getToolBarToolByLabel(toolbar, "pauseTimeButton")->GetId(), 
+		wxEVT_TOOL, wxCommandEventHandler(RenderPreview::onPausePlaybackClick), NULL, this);
+	toolbar->Connect(getToolBarToolByLabel(toolbar, "stopTimeButton")->GetId(), 
+		wxEVT_TOOL, wxCommandEventHandler(RenderPreview::onStopPlaybackClick), NULL, this);
+
+	toolbar->Connect(getToolBarToolByLabel(toolbar, "prevButton")->GetId(), 
+		wxEVT_TOOL, wxCommandEventHandler(RenderPreview::onStepForwardClick), NULL, this);
+	toolbar->Connect(getToolBarToolByLabel(toolbar, "nextButton")->GetId(), 
+		wxEVT_TOOL, wxCommandEventHandler(RenderPreview::onStepBackClick), NULL, this);
+}
+
+RenderPreview::~RenderPreview()
+{
+    _timer.Stop();
+}
+
+void RenderPreview::filtersChanged()
+{
+    if (!getScene()->root()) return;
+
+    GlobalFilterSystem().updateSubgraph(getScene()->root());
+    queueDraw();
+}
+
+void RenderPreview::addToolbar(wxToolBar* toolbar)
+{
+	wxToolBar* existing = findNamedObject<wxToolBar>(_mainPanel, "RenderPreviewAnimToolbar");
+	existing->GetContainingSizer()->Add(toolbar, 0, wxEXPAND);
+}
+
+void RenderPreview::queueDraw()
+{
+	_glWidget->Refresh();
+}
+
+void RenderPreview::setSize(int width, int height)
+{
+	_glWidget->SetClientSize(width, height);
+}
+
+void RenderPreview::initialisePreview()
+{
+#if 0
+#ifdef WIN32
+    // greebo: Unfortunate hack to fix the grey GL renderviews in the EntityChooser
+    // and other windows that are hidden instead of destroyed when closed.
+    Gtk::Container* container = get_parent();
+    bool wasShown = get_visible();
+
+    if (container != NULL)
+    {
+        if (wasShown)
+        {
+            hide();
+        }
+
+        container->remove(*this);
+        container->add(*this);
+
+        if (wasShown)
+        {
+            show();
+        }
+    }
+
+#endif
+#endif
+	// Grab the GL widget with sentry object
+	wxPaintDC dc(_glWidget);
+	_glWidget->SetCurrent(GlobalOpenGL().getwxGLContext());
+	
+    // Set up the camera
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(PREVIEW_FOV, 1, 0.1, 10000);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    // Set up the lights
+    glEnable(GL_LIGHTING);
+
+    glEnable(GL_LIGHT0);
+    GLfloat l0Amb[] = { 0.3f, 0.3f, 0.3f, 1.0f };
+    GLfloat l0Dif[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    GLfloat l0Pos[] = { 1.0f, 1.0f, 1.0f, 0.0f };
+    glLightfv(GL_LIGHT0, GL_AMBIENT, l0Amb);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, l0Dif);
+    glLightfv(GL_LIGHT0, GL_POSITION, l0Pos);
+
+    glEnable(GL_LIGHT1);
+    GLfloat l1Dif[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat l1Pos[] = { 0.0, 0.0, 1.0, 0.0 };
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, l1Dif);
+    glLightfv(GL_LIGHT1, GL_POSITION, l1Pos);
+
+    if (_renderSystem->shaderProgramsAvailable())
+    {
+        _renderSystem->setShaderProgram(
+            RenderSystem::SHADER_PROGRAM_INTERACTION
+        );
+    }
+}
+
+const scene::GraphPtr& RenderPreview::getScene()
+{
+    if (!_scene)
+    {
+        _scene = GlobalSceneGraphFactory().createSceneGraph();
+
+        setupSceneGraph();
+
+        associateRenderSystem();
+    }
+
+    return _scene;
+}
+
+void RenderPreview::setupSceneGraph()
+{
+    // Set our render time to 0
+    _renderSystem->setTime(0);
+}
+
+void RenderPreview::associateRenderSystem()
+{
+    if (_scene && _scene->root())
+    {
+        _scene->root()->setRenderSystem(_renderSystem);
+    }
+}
+
+Matrix4 RenderPreview::getProjectionMatrix(float near_z, float far_z, float fieldOfView, int width, int height)
+{
+    const float half_width = near_z * tan(degrees_to_radians(fieldOfView * 0.5f));
+    const float half_height = half_width * (static_cast<float>(height) / static_cast<float>(width));
+
+    return Matrix4::getProjectionForFrustum(
+        -half_width,
+        half_width,
+        -half_height,
+        half_height,
+        near_z,
+        far_z
+    );
+}
+
+Matrix4 RenderPreview::getModelViewMatrix()
+{
+    // Premultiply with the translations
+    Matrix4 modelview;
+
+    {
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
+        glTranslatef(0, 0, _camDist); // camera translation
+        glMultMatrixd(_rotation); // post multiply with rotations
+
+        // Load the matrix from openGL
+        glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
+    }
+
+    return modelview;
+}
+
+void RenderPreview::startPlayback()
+{
+	if (_timer.IsRunning())
+    {
+        // Timer is already running, just reset the preview time
+        _renderSystem->setTime(0);
+    }
+    else
+    {
+        // Timer is not enabled, we're paused or stopped
+        _timer.Start();
+    }
+
+	wxToolBar* toolbar = findNamedObject<wxToolBar>(_mainPanel, "RenderPreviewAnimToolbar");
+
+	toolbar->EnableTool(getToolBarToolByLabel(toolbar, "pauseTimeButton")->GetId(), true);
+	toolbar->EnableTool(getToolBarToolByLabel(toolbar, "stopTimeButton")->GetId(), true);
+}
+
+void RenderPreview::stopPlayback()
+{
+    _renderSystem->setTime(0);
+    _timer.Stop();
+
+	wxToolBar* toolbar = findNamedObject<wxToolBar>(_mainPanel, "RenderPreviewAnimToolbar");
+
+	toolbar->EnableTool(getToolBarToolByLabel(toolbar, "pauseTimeButton")->GetId(), false);
+	toolbar->EnableTool(getToolBarToolByLabel(toolbar, "stopTimeButton")->GetId(), false);
+
+    _glWidget->Refresh();
+}
+
+bool RenderPreview::onPreRender()
+{
+    return true;
+}
+
+RenderStateFlags RenderPreview::getRenderFlagsFill()
+{
+    return  RENDER_MASKCOLOUR |
+            RENDER_ALPHATEST |
+            RENDER_BLEND |
+            RENDER_CULLFACE |
+            RENDER_OFFSETLINE |
+            RENDER_VERTEX_COLOUR |
+            RENDER_FILL |
+            RENDER_LIGHTING |
+            RENDER_TEXTURE_2D |
+            RENDER_SMOOTH |
+            RENDER_SCALED |
+            RENDER_FILL |
+            RENDER_TEXTURE_CUBEMAP |
+            RENDER_BUMP |
+            RENDER_PROGRAM;
+}
+
+RenderStateFlags RenderPreview::getRenderFlagsWireframe()
+{
+    return RENDER_MASKCOLOUR |
+           RENDER_ALPHATEST |
+           RENDER_BLEND |
+           RENDER_CULLFACE |
+           RENDER_OFFSETLINE |
+           RENDER_VERTEX_COLOUR |
+           RENDER_LIGHTING |
+           RENDER_SMOOTH |
+           RENDER_SCALED |
+           RENDER_TEXTURE_CUBEMAP |
+           RENDER_BUMP |
+           RENDER_PROGRAM;
+}
+
+void RenderPreview::drawPreview()
+{
+    if (_renderingInProgress) return; // avoid double-entering this method
+
+    util::ScopedBoolLock lock(_renderingInProgress);
+
+    glViewport(0, 0, _previewWidth, _previewHeight);
+
+    // Set up the render and clear the drawing area in any case
+    glDepthMask(GL_TRUE);
+    glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Set up the camera
+    Matrix4 projection = getProjectionMatrix(0.1f, 10000, PREVIEW_FOV, _previewWidth, _previewHeight);
+
+    // Keep the modelview matrix in the volumetest class up to date
+    _volumeTest.setModelView(getModelViewMatrix());
+    _volumeTest.setProjection(projection);
+
+    // Pre-Render event
+    if (!onPreRender())
+    {
+        // a return value of false means to cancel rendering
+        drawTime();
+        return;
+    }
+
+    // Front-end render phase, collect OpenGLRenderable objects from the scene
+    getScene()->foreachVisibleNodeInVolume(_volumeTest, _sceneWalker);
+
+    RenderStateFlags flags = getRenderFlagsFill();
+
+    // Launch the back end rendering
+    _renderSystem->render(flags, _volumeTest.GetModelview(), projection);
+
+    // Give subclasses an opportunity to render their own on-screen stuff
+    onPostRender();
+
+    // Draw the render time
+    drawTime();
+}
+
+void RenderPreview::renderWireFrame()
+{
+    RenderStateFlags flags = getRenderFlagsWireframe();
+
+    // Set up the camera
+    Matrix4 projection = getProjectionMatrix(0.1f, 10000, PREVIEW_FOV, _previewWidth, _previewHeight);
+
+    // Front-end render phase, collect OpenGLRenderable objects from the scene
+    getScene()->foreachVisibleNodeInVolume(_volumeTest, _sceneWalker);
+
+    // Launch the back end rendering
+    _renderSystem->render(flags, _volumeTest.GetModelview(), projection);
+}
+
+void RenderPreview::onGLMotion(wxMouseEvent& ev)
+{
+	if (ev.LeftIsDown()) // dragging with mouse button
+    {
+        static gdouble _lastX = ev.GetX();
+        static gdouble _lastY = ev.GetY();
+
+        // Calculate the mouse delta as a vector in the XY plane, and store the
+        // current position for the next event.
+        Vector3 deltaPos(static_cast<float>(ev.GetX() - _lastX),
+                         static_cast<float>(_lastY - ev.GetY()),
+                         0);
+        _lastX = ev.GetX();
+        _lastY = ev.GetY();
+
+        // Calculate the axis of rotation. This is the mouse vector crossed with the Z axis,
+        // to give a rotation axis in the XY plane at right-angles to the mouse delta.
+        static Vector3 _zAxis(0, 0, 1);
+        Vector3 axisRot = deltaPos.crossProduct(_zAxis);
+
+        // Grab the GL widget, and update the modelview matrix with the
+        // additional rotation
+		// This is required even though dc is not used otherwise.
+		wxPaintDC dc(_glWidget);
+
+		// Grab the contex for this widget
+		if (_glWidget->SetCurrent(GlobalOpenGL().getwxGLContext()))
+        {
+            // Premultiply the current modelview matrix with the rotation,
+            // in order to achieve rotations in eye space rather than object
+            // space. At this stage we are only calculating and storing the
+            // matrix for the GLDraw callback to use.
+            glLoadIdentity();
+            glRotated(-2, axisRot.x(), axisRot.y(), axisRot.z());
+            glMultMatrixd(_rotation);
+
+            // Save the new GL matrix for GL draw
+            glGetDoublev(GL_MODELVIEW_MATRIX, _rotation);
+
+            _glWidget->Refresh(); // trigger the GLDraw method to draw the actual model
+        }
+    }
+}
+
+AABB RenderPreview::getSceneBounds()
+{
+    return AABB(Vector3(0,0,0), Vector3(64,64,64));
+}
+
+void RenderPreview::onGLScroll(wxMouseEvent& ev)
+{
+    // Scroll increment is a fraction of the AABB radius
+    float inc = static_cast<float>(getSceneBounds().getRadius()) * 0.1f;
+
+	if (ev.GetWheelRotation() > 0)
+    {
+        _camDist += inc;
+    }
+    else if (ev.GetWheelRotation() < 0)
+    {
+        _camDist -= inc;
+    }
+
+    if (!_renderingInProgress)
+    {
+        _glWidget->Refresh();
+    }
+}
+
+void RenderPreview::onStartPlaybackClick(wxCommandEvent& ev)
+{
+	startPlayback();
+}
+
+void RenderPreview::onStopPlaybackClick(wxCommandEvent& ev)
+{
+	stopPlayback();
+}
+
+void RenderPreview::onPausePlaybackClick(wxCommandEvent& ev)
+{
+	// Disable the button
+	wxToolBar* toolbar = findNamedObject<wxToolBar>(_mainPanel, "RenderPreviewAnimToolbar");
+	toolbar->EnableTool(getToolBarToolByLabel(toolbar, "pauseTimeButton")->GetId(), false);
+	
+	if (_timer.IsRunning())
+    {
+        _timer.Stop();
+    }
+    else
+    {
+        _timer.Start(); // re-enable playback
+    }
+}
+
+void RenderPreview::onStepForwardClick(wxCommandEvent& ev)
+{
+    // Disable the button
+	wxToolBar* toolbar = findNamedObject<wxToolBar>(_mainPanel, "RenderPreviewAnimToolbar");
+	toolbar->EnableTool(getToolBarToolByLabel(toolbar, "pauseTimeButton")->GetId(), false);
+
+	if (_timer.IsRunning())
+    {
+        _timer.Stop();
+    }
+
+    _renderSystem->setTime(_renderSystem->getTime() + MSEC_PER_FRAME);
+    _glWidget->Refresh();
+}
+
+void RenderPreview::onStepBackClick(wxCommandEvent& ev)
+{
+    // Disable the button
+    wxToolBar* toolbar = findNamedObject<wxToolBar>(_mainPanel, "RenderPreviewAnimToolbar");
+	toolbar->EnableTool(getToolBarToolByLabel(toolbar, "pauseTimeButton")->GetId(), false);
+
+	if (_timer.IsRunning())
+    {
+        _timer.Stop();
+    }
+
+    if (_renderSystem->getTime() > 0)
+    {
+        _renderSystem->setTime(_renderSystem->getTime() - MSEC_PER_FRAME);
+    }
+
+    _glWidget->Refresh();
+}
+
+void RenderPreview::onSizeAllocate(wxSizeEvent& ev)
+{
+	_previewWidth = ev.GetSize().GetWidth();
+    _previewHeight = ev.GetSize().GetHeight();
+}
+
+void RenderPreview::drawTime()
+{
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, static_cast<float>(_previewWidth), 0, static_cast<float>(_previewHeight), -100, 100);
+
+    glScalef(1, -1, 1);
+    glTranslatef(0, -static_cast<float>(_previewHeight), 0);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    if (GLEW_VERSION_1_3)
+    {
+        glClientActiveTexture(GL_TEXTURE0);
+        glActiveTexture(GL_TEXTURE0);
+    }
+
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+    glDisableClientState(GL_NORMAL_ARRAY);
+    glDisableClientState(GL_COLOR_ARRAY);
+
+    glDisable(GL_TEXTURE_2D);
+    glDisable(GL_LIGHTING);
+    glDisable(GL_COLOR_MATERIAL);
+    glDisable(GL_DEPTH_TEST);
+
+    glColor3f( 1.f, 1.f, 1.f );
+    glLineWidth(1);
+
+    glRasterPos3f(1.0f, static_cast<float>(_previewHeight) - 1.0f, 0.0f);
+
+    GlobalOpenGL().drawString((boost::format("%.3f sec.") % (_renderSystem->getTime() * 0.001f)).str());
+}
+
+void RenderPreview::_onFrame(wxTimerEvent& ev)
+{
+    if (!_renderingInProgress)
+    {
+        _renderSystem->setTime(_renderSystem->getTime() + MSEC_PER_FRAME);
+        _glWidget->Refresh();
+    }
+}
+
+} // namespace
 
 namespace gtkutil
 {
--- libs/gtkutil/preview/RenderPreview.h
+++ libs/gtkutil/preview/RenderPreview.h
@@ -1,10 +1,11 @@
 #pragma once
 
-#include <gtkmm/frame.h>
+#include <wx/panel.h>
+#include <wx/timer.h>
 
 #include "math/Matrix4.h"
 #include "../Timer.h"
-#include "../GladeWidgetHolder.h"
+#include "../XmlResourceBasedWidget.h"
 
 #include "ifiltermenu.h"
 #include "iscenegraph.h"
@@ -13,6 +14,158 @@
 #include "render/ShaderStateRenderer.h"
 #include "render/SceneRenderWalker.h"
 #include "render/NopVolumeTest.h"
+
+namespace wxutil
+{
+
+class GLWidget;
+
+/**
+ * greebo: This class acts as base for widgets featuring 
+ * a real time openGL render preview. It offers
+ * its own local SceneGraph, backend and frontend renderer
+ * plus all the logic for camera handling and filtering.
+ *
+ * Override the protected methods to have the scene set up 
+ * in special ways or add custom toolbar items.
+ *
+ * After construction the local scene graph will be empty.
+ */
+class RenderPreview :
+    public wxEvtHandler,
+    private XmlResourceBasedWidget
+{
+private:
+    // The scene we're rendering
+    scene::GraphPtr _scene;
+
+    // GL widget
+    GLWidget* _glWidget;
+
+private:
+    void connectToolbarSignals();
+    void drawPreview();
+    void onGLMotion(wxMouseEvent& ev);
+    void onGLScroll(wxMouseEvent& ev);
+
+	void onStartPlaybackClick(wxCommandEvent& ev);
+	void onStopPlaybackClick(wxCommandEvent& ev);
+	void onPausePlaybackClick(wxCommandEvent& ev);
+    void onStepForwardClick(wxCommandEvent& ev);
+    void onStepBackClick(wxCommandEvent& ev);
+
+    void onSizeAllocate(wxSizeEvent& ev);
+    void filtersChanged();
+
+    void drawTime();
+
+    // Called each frame by wxTimer
+    void _onFrame(wxTimerEvent& ev);
+
+protected:
+	wxPanel* _mainPanel;
+
+    // The backend rendersystem instance
+    RenderSystemPtr _renderSystem;
+
+    // The front-end renderer, collecting the OpenGLRenderables
+    render::ShaderStateRenderer _renderer;
+    render::NopVolumeTest _volumeTest;
+
+    // The scene adaptor passing nodes into our front-end renderer
+    render::SceneRenderWalker _sceneWalker;
+
+    // Current distance between camera and preview
+    GLfloat _camDist;
+
+    // Current rotation matrix
+    Matrix4 _rotation;
+
+    // Mutex flag to avoid draw call bunching
+    bool _renderingInProgress;
+
+    wxTimer _timer;
+
+    int _previewWidth;
+    int _previewHeight;
+
+    // The filters menu
+    ui::IFilterMenuPtr _filtersMenu;
+
+protected:
+    const scene::GraphPtr& getScene();
+
+    /// Add another one to the toolbar hbox
+    void addToolbar(wxToolBar* toolbar);
+
+    /// Schedule a GL widget redraw operation
+    void queueDraw();
+
+    // Subclasses should at least add a single node as scene root, such that
+    // the rendersystem can be associated. This is called after initialisePreview()
+    virtual void setupSceneGraph();
+
+    virtual Matrix4 getProjectionMatrix(float near_z, float far_z, float fieldOfView, int width, int height);
+    virtual Matrix4 getModelViewMatrix();
+
+    virtual void startPlayback();
+    virtual void stopPlayback();
+
+    // Override this to deliver accurate scene bounds, used for mousewheel-zooming
+    virtual AABB getSceneBounds();
+
+    // Called right before rendering, returning false will cancel the render algorithm
+    virtual bool onPreRender();
+
+    // Called after the render phase, can be used to draw custom stuff on the GL widget
+    virtual void onPostRender() {}
+
+    // Use this to render a wireframe view of the scene
+    void renderWireFrame();
+
+    // Override these to define the flags to render a fill/wireframe scene
+    virtual RenderStateFlags getRenderFlagsFill();
+    virtual RenderStateFlags getRenderFlagsWireframe();
+
+    void associateRenderSystem();
+
+    /**
+     * \brief
+     * Construct a RenderPreview
+     *
+     * \param enableAnimation
+     * If true, display animation controls in toolbar, otherwise hide the
+     * animation controls.
+     */
+    RenderPreview(wxWindow* parent, bool enableAnimation = true);
+
+    virtual ~RenderPreview();
+
+public:
+	wxPanel* getWidget() const
+	{
+		return _mainPanel;
+	}
+
+    void setSize(int width, int height);
+
+    /**
+     * Initialise the GL preview. This clears the window and sets up the
+     * initial matrices and lights.
+     */
+    void initialisePreview();
+
+    /// Get the RenderSystem used by the preview
+    const RenderSystemPtr& getRenderSystem()
+    {
+        return _renderSystem;
+    }
+};
+
+} // namespace
+
+#include <gtkmm/frame.h>
+#include "../GladeWidgetHolder.h"
 
 namespace Gtk
 {
