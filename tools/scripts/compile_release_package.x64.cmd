ac3d61b8c737ae55127d3ef159a45f2670f9b2a4
codereader <greebo@angua.at>
2014-07-17 12:37:49 +0200

    More build script updates.

--- tools/scripts/compile_release_package.x64.cmd
+++ tools/scripts/compile_release_package.x64.cmd
@@ -1,3 +1,5 @@
 msbuild ..\msvc2013\DarkRadiant.sln /p:configuration=release /t:rebuild /p:platform=x64 /maxcpucount:4 
 call copy_install_files.x64.cmd 
-call ..\innosetup\create_installer.x64.cmd
+cd ..\innosetup
+@echo For this script to run, please make sure that InnoSetup's compil32 is found via the PATH environment variable
+compil32 /cc darkradiant.x64.iss
