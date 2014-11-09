c354f028650f9fb47d2f9700f9c2d8197a8da789
codereader <greebo@angua.at>
2014-11-09 11:03:00 +0100

    Update comments, smaller methods.

--- plugins/eventmanager/GlobalKeyEventFilter.cpp
+++ plugins/eventmanager/GlobalKeyEventFilter.cpp
@@ -30,83 +30,103 @@ int GlobalKeyEventFilter::FilterEvent(wxEvent& event)
     if (eventType == wxEVT_KEY_DOWN || eventType == wxEVT_KEY_UP)
     {
         wxKeyEvent& keyEvent = static_cast<wxKeyEvent&>(event);
-        wxObject* eventObject = keyEvent.GetEventObject();
-
-        // Don't eat key events of text controls
-        if (wxDynamicCast(eventObject, wxTextCtrl) || wxDynamicCast(eventObject, wxStyledTextCtrl))
+        
+        if (shouldConsiderEvent(keyEvent))
         {
-            return Event_Skip;
+            // Attempt to find an accelerator
+            bool acceleratorFound = handleAccelerator(keyEvent);
+
+            // Update the general key state in any case
+            _eventManager.updateKeyState(keyEvent, eventType == wxEVT_KEY_DOWN);
+
+            return acceleratorFound ? Event_Processed : Event_Skip;
         }
-
-        // Check if the event object can handle the event
-        wxWindow* window = dynamic_cast<wxWindow*>(keyEvent.GetEventObject());
-
-        if (!window) return Event_Skip;
-
-        if (window->GetEventHandler()->ProcessEvent(keyEvent))
-        {
-            // The control handled this event, so don't check for accelerators
-            return Event_Skip;
-        }
-
-        // Special handling for our treeviews with type ahead search
-
-        // Treeviews are special, the actual wxWindows receiving/generating the event are the
-        // privately implemented wxDataViewMainWindows, so let's attempt identifying that case
-        // Check for keys with modifers, these are not handled by the treeview search
-        if (!keyEvent.HasAnyModifiers() && 
-            wxString(eventObject->GetClassInfo()->GetClassName()) == "wxDataViewMainWindow")
-        {
-            // We have a modifier-less key event in a wxutil::TreeView. It will be passed through
-            // in the general case. The ESC key will be caught if the treeview is not search mode.
-            
-            if (keyEvent.GetKeyCode() == WXK_ESCAPE)
-            {
-                // The ESC key should only be passed to the dataview if the search popup is actually shown
-                wxutil::TreeView* treeView = dynamic_cast<wxutil::TreeView*>(window->GetParent());
-
-                if (treeView && treeView->HasActiveSearchPopup())
-                {
-                    return Event_Skip;
-                }
-            }
-            else // non-escape key, no modifiers, don't process it
-            {
-                return Event_Skip;
-            }
-        }
-
-        // The event stays unhandled by the control that it's triggered on
-        // check if an accelerator matches this event
-        // Try to find a matching accelerator
-        AcceleratorList accelList = _eventManager.findAccelerator(keyEvent);
-
-        if (!accelList.empty())
-        {
-            // Release any modifiers
-            _eventManager.clearModifierState();
-
-            // Pass the execute() call to all found accelerators
-            for (AcceleratorList::iterator i = accelList.begin(); i != accelList.end(); ++i)
-            {
-                if (eventType == wxEVT_KEY_DOWN)
-                {
-                    i->keyDown();
-                }
-                else
-                {
-                    i->keyUp();
-                }
-            }
-
-            return Event_Processed;
-        }
-
-        _eventManager.updateKeyState(keyEvent, eventType == wxEVT_KEY_DOWN);
     }
 
     // Continue processing the event normally as well.
     return Event_Skip;
 }
 
+bool GlobalKeyEventFilter::shouldConsiderEvent(wxKeyEvent& keyEvent)
+{
+    wxObject* eventObject = keyEvent.GetEventObject();
+
+    // Don't eat key events of text controls
+    if (wxDynamicCast(eventObject, wxTextCtrl) || wxDynamicCast(eventObject, wxStyledTextCtrl))
+    {
+        return false;
+    }
+
+    // Check if the event object can handle the event
+    wxWindow* window = dynamic_cast<wxWindow*>(keyEvent.GetEventObject());
+
+    if (!window) return false;
+
+    if (window->GetEventHandler()->ProcessEvent(keyEvent))
+    {
+        // The control handled this event, so don't check for accelerators
+        return false;
+    }
+
+    // Special handling for our treeviews with type ahead search
+
+    // Treeviews are special, the actual wxWindows receiving/generating the event are the
+    // privately implemented wxDataViewMainWindows, so let's attempt identifying that case
+    // Check for keys with modifers, these are not handled by the treeview search
+    if (!keyEvent.HasAnyModifiers() &&
+        wxString(eventObject->GetClassInfo()->GetClassName()) == "wxDataViewMainWindow")
+    {
+        // We have a modifier-less key event in a wxutil::TreeView. It will be passed through
+        // in the general case. The ESC key will be caught if the treeview is not search mode.
+
+        if (keyEvent.GetKeyCode() == WXK_ESCAPE)
+        {
+            // The ESC key should only be passed to the dataview if the search popup is actually shown
+            wxutil::TreeView* treeView = dynamic_cast<wxutil::TreeView*>(window->GetParent());
+
+            if (treeView && treeView->HasActiveSearchPopup())
+            {
+                return false;
+            }
+        }
+        else // non-escape key, no modifiers, don't process it
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
+
+bool GlobalKeyEventFilter::handleAccelerator(wxKeyEvent& keyEvent)
+{
+    // The event stays unhandled by the control that it's triggered on
+    // check if an accelerator matches this event
+    // Try to find a matching accelerator
+    AcceleratorList accelList = _eventManager.findAccelerator(keyEvent);
+
+    if (!accelList.empty())
+    {
+        // Release any modifiers
+        _eventManager.clearModifierState();
+
+        // Pass the execute() call to all found accelerators
+        for (AcceleratorList::iterator i = accelList.begin(); i != accelList.end(); ++i)
+        {
+            if (keyEvent.GetEventType() == wxEVT_KEY_DOWN)
+            {
+                i->keyDown();
+            }
+            else
+            {
+                i->keyUp();
+            }
+        }
+
+        return true; // handled
+    }
+
+    return false; // no accelerator found
+}
+
+} // namespace
--- plugins/eventmanager/GlobalKeyEventFilter.h
+++ plugins/eventmanager/GlobalKeyEventFilter.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <wx/event.h>
 #include <wx/eventfilter.h>
 #include <memory>
 
@@ -9,7 +10,10 @@ namespace ui
 {
 
 /**
-* TODO
+* Global filter owned by the EventManager module. Scans every
+* key event in the application and invokes matching accelerators,
+* unless the control in focus prevents this (like an active wxTextCtrl
+* or wxutil::TreeView).
 */
 class GlobalKeyEventFilter :
     public wxEventFilter
@@ -22,6 +26,13 @@ public:
     virtual ~GlobalKeyEventFilter();
 
     virtual int FilterEvent(wxEvent& event);
+
+private:
+    // Returns true if this event applies for accelerator
+    bool shouldConsiderEvent(wxKeyEvent& keyEvent);
+
+    // Attempts to fire an accelerator for this event. Returns false if no accelerator matched.
+    bool handleAccelerator(wxKeyEvent& keyEvent);
 };
 typedef std::shared_ptr<GlobalKeyEventFilter> GlobalKeyEventFilterPtr;
 
