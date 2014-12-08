1bec09e597ed9e492540e6356c621b195852f6e0
codereader <greebo@angua.at>
2014-12-08 18:19:48 +0100

    Let the release package script create a 7-zip package too.

--- tools/scripts/compile_release_package.x64.cmd
+++ tools/scripts/compile_release_package.x64.cmd
@@ -3,3 +3,4 @@ call copy_install_files.x64.cmd
 cd ..\innosetup
 @echo For this script to run, please make sure that InnoSetup's compil32 is found via the PATH environment variable
 compil32 /cc darkradiant.x64.iss
+"C:\Program Files\7-Zip\7z.exe" a -r -x!*.pdb -mx9 ..\innosetup\darkradiant-X.Y.ZpreV.x64.7z ..\..\..\DarkRadiant_install.x64\*.*
