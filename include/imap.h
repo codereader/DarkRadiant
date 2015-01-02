730b72b913a32f08806bb6255609180b573b7ec1
codereader <greebo@angua.at>
2015-01-02 05:22:07 +0100

    The root node is a scene::INode too.

--- include/imap.h
+++ include/imap.h
@@ -1,35 +1,10 @@
-/*
-Copyright (C) 2001-2006, William Joseph.
-All Rights Reserved.
-
-This file is part of GtkRadiant.
-
-GtkRadiant is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-GtkRadiant is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with GtkRadiant; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
-#if !defined(INCLUDED_IMAP_H)
-#define INCLUDED_IMAP_H
+#pragma once
 
 #include "imodule.h"
 #include "inode.h"
 
 // Registry setting for suppressing the map load progress dialog
-const std::string RKEY_MAP_SUPPRESS_LOAD_STATUS_DIALOG = "user/ui/map/suppressMapLoadDialog";
-
-// Forward declaration
-namespace parser { class DefTokeniser; }
+const char* const RKEY_MAP_SUPPRESS_LOAD_STATUS_DIALOG = "user/ui/map/suppressMapLoadDialog";
 
 // Namespace forward declaration
 class INamespace;
@@ -39,7 +14,8 @@ typedef std::shared_ptr<INamespace> INamespacePtr;
  * greebo: A root node is the top level element of a map.
  * It also owns the namespace of the corresponding map.
  */
-class IMapRootNode
+class IMapRootNode :
+    public virtual scene::INode
 {
 public:
     virtual ~IMapRootNode() {}
@@ -89,5 +65,3 @@ inline IMap& GlobalMapModule() {
 	);
 	return _mapModule;
 }
-
-#endif
