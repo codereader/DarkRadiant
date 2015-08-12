5d7d182df769f2d53a5759c77b760d259227a7eb
Matthew Mott <orbweaver3d@gmail.com>
2015-08-12 19:12:49 +0100

    Added support for "angles" key to represent rotation (courtesy of motorsep)
    New behaviour is triggered by the addition of a mapFormat/entityRotationKey
    node in the .game file with a value of "angles".

--- plugins/entity/RotationKey.cpp
+++ plugins/entity/RotationKey.cpp
@@ -1,17 +1,27 @@
 #include "RotationKey.h"
 
+#include "gamelib.h"
+
+namespace
+{
+    const char* GKEY_ROTATIONKEY = "/mapFormat/entityRotationKey";
+
+    const char* ANGLES_KEY = "angles";
+    const char* ROTATION_KEY = "rotation";
+}
+
 void RotationKey::write(Entity* entity, bool isModel) const
 {
-    // greebo: Prevent the "angle" key from being used for models, they should always
-    // have a rotation matrix written to their spawnargs. This should fix
-    // the models hopping around after transforms
+    Vector3 euler = m_rotation.getMatrix4().getEulerAnglesXYZDegrees();
+
+    // greebo: Prevent the "angle" key from being used for models, they should
+    // always have a rotation matrix written to their spawnargs. This should
+    // fix the models hopping around after transforms
     if (!isModel)
     {
-        Vector3 euler = m_rotation.getMatrix4().getEulerAnglesXYZDegrees();
-
         if (euler[0] == 0 && euler[1] == 0)
         {
-            entity->setKeyValue("rotation", "");
+            entity->setKeyValue(ROTATION_KEY, "");
             write_angle(euler[2], entity);
             return;
         }
@@ -25,7 +35,17 @@ void RotationKey::write(Entity* entity, bool isModel) const
 
     entity->setKeyValue("angle", "");
 
+    // Set an angles key if the game calls for it
+    if (game::current::getValue<std::string>(GKEY_ROTATIONKEY) == ANGLES_KEY)
+    {
+        std::string anglesVal = std::to_string(euler[1]) + ' '
+                              + std::to_string(euler[2]) + ' '
+                              + std::to_string(euler[0]);
+
+        entity->setKeyValue(ANGLES_KEY, anglesVal);
+    }
+
     // This call will usually update the m_rotation member again
-    entity->setKeyValue("rotation", keyVal);
+    entity->setKeyValue(ROTATION_KEY, keyVal);
 }
 
