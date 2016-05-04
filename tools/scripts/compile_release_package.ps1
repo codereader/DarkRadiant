72f919893ff7cbee48275ba19edbd77325658ac3
codereader <greebo@angua.at>
2016-05-04 13:34:55 +0200

    Add new build script for doing fewer things manually

---
+++ tools/scripts/compile_release_package.ps1
@@ -0,0 +1,78 @@
+# Check input arguments
+if ($args.Count -eq 0 -or ($args[0] -ne "x64" -and $args[0] -ne "x86"))
+{
+    Write-Host "Usage: compile_release_package.ps1 <x64|x86>"
+    Write-Host ""
+    Write-Host "e.g. to compile a 64 bit build: .\compile_release_package.ps1 x64"
+    Write-Host ""
+    return
+}
+
+# Check tool reachability
+if ((Get-Command "compil32" -ErrorAction SilentlyContinue) -eq $null)
+{
+    Write-Host -ForegroundColor Red "For this script to run, please make sure that InnoSetup's compil32 is found via the PATH environment variable"
+    return
+}
+
+if ((Get-Command "msbuild" -ErrorAction SilentlyContinue) -eq $null)
+{
+    Write-Host -ForegroundColor Red "For this script to run, please make sure that you've opened the corresponding studio developer command prompt."
+    return
+}
+
+# ----------------------------------------------
+
+$target = $args[0]
+
+Write-Host ("Compiling for target: {0}" -f $target)
+
+$versionRegex = "AppVerName=DarkRadiant (.*) $target"
+$portableFilenameTemplate = "darkradiant-{0}-$target.7z"
+$pdbFilenameTemplate = "darkradiant-{0}-$target.pdb.7z"
+
+if ($target -eq "x86")
+{
+    $platform = "Win32"
+    $copyFilesCmd = ".\copy_install_files.cmd"
+    $issFile = "..\innosetup\darkradiant.iss"
+    $portableFilesFolder = "DarkRadiant_install"
+} 
+else
+{
+    $platform = "x64"
+    $copyFilesCmd = ".\copy_install_files.x64.cmd"
+    $issFile = "..\innosetup\darkradiant.x64.iss"
+    $portableFilesFolder = "DarkRadiant_install.x64"
+}
+
+#Start-Process "msbuild" -ArgumentList ("..\msvc2013\DarkRadiant.sln", "/p:configuration=release", "/t:rebuild", "/p:platform=$platform", "/maxcpucount:4") -NoNewWindow -Wait
+
+Start-Process $copyFilesCmd -NoNewWindow -Wait
+
+# Get the version from the innosetup file
+$content = Get-Content $issFile
+$defaultVersionString = "X.Y.ZpreV"
+$foundVersionString = $defaultVersionString
+
+foreach ($line in $content)
+{
+    if ($line -match $versionRegex)
+    {
+        $foundVersionString = $matches[1] 
+        Write-Host ("Version is {0}" -f $matches[1])
+        break
+    }
+}
+
+$portableFilename = ($portableFilenameTemplate -f $foundVersionString)
+$pdbFilename = ($pdbFilenameTemplate -f $foundVersionString)
+
+# Compile the installer package
+Start-Process "compil32" -ArgumentList ("/cc", $issFile)
+
+# Compress to portable 7z package 
+Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList ("a", "-r", "-x!*.pdb", "-mx9", "-mmt2", "..\innosetup\$portableFilename", "..\..\..\$portableFilesFolder\*.*")
+
+# Compress Program Database Files
+Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList ("a", "-r", "-mx9", "-mmt2", "..\innosetup\$pdbFilename", "..\..\..\$portableFilesFolder\*.pdb") -Wait
--- tools/scripts/compile_release_package.x64.cmd
+++ tools/scripts/compile_release_package.x64.cmd
@@ -1,7 +1,1 @@
-msbuild ..\msvc2013\DarkRadiant.sln /p:configuration=release /t:rebuild /p:platform=x64 /maxcpucount:4 
-call copy_install_files.x64.cmd 
-cd ..\innosetup
-@echo For this script to run, please make sure that InnoSetup's compil32 is found via the PATH environment variable
-compil32 /cc darkradiant.x64.iss
-"C:\Program Files\7-Zip\7z.exe" a -r -x!*.pdb -mx9 ..\innosetup\darkradiant-X.Y.ZpreV-x64.7z ..\..\..\DarkRadiant_install.x64\*.*
-"C:\Program Files\7-Zip\7z.exe" a -r -mx9 ..\innosetup\darkradiant-X.Y.ZpreV-x64.pdb.7z ..\..\..\DarkRadiant_install.x64\*.pdb
+powershell.exe .\compile_release_package.ps1 x64
