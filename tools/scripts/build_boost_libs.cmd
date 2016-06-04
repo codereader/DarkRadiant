3996f7d863c89e31d21dd3f1fb0a91f1922e2689
codereader <greebo@angua.at>
2016-06-04 19:56:03 +0200

    Update boost build scripts to use b2.

--- tools/scripts/build_boost_libs.cmd
+++ tools/scripts/build_boost_libs.cmd
@@ -4,45 +4,22 @@ rem Launch this file in the boost/ folder.
 rem Check if we're in the correct folder
 if not exist libs goto :error
 if not exist boost goto :error
+if not exist b2.exe goto :error
 
 mkdir stage
 
-cd libs\filesystem\build
-bjam --toolset=msvc link=static threading=multi release stage
-bjam --toolset=msvc link=static threading=multi debug stage
-copy stage\*.lib ..\..\..\stage
+b2 toolset=msvc variant=release,debug link=static threading=multi stage /boost/python /boost/filesystem /boost/regex /boost/system
 
-cd ..\..\..\libs\system\build
-bjam --toolset=msvc link=static threading=multi release stage
-bjam --toolset=msvc link=static threading=multi debug stage
-copy stage\*.lib ..\..\..\stage
-
-cd ..\..\..\libs\python\build
-bjam --toolset=msvc link=static threading=multi release stage
-bjam --toolset=msvc link=static threading=multi debug stage
-copy stage\*.lib ..\..\..\stage
-
-cd ..\..\..\libs\regex\build
-bjam --toolset=msvc link=static threading=multi release stage
-bjam --toolset=msvc link=static threading=multi debug stage
-copy stage\*.lib ..\..\..\stage
-
-rem cd ..\..\..\libs\signals\build
-rem bjam --toolset=msvc link=static threading=multi release stage
-rem bjam --toolset=msvc link=static threading=multi debug stage
-rem copy stage\*.lib ..\..\..\stage
-
-cd ..\..\..\stage
-start .
+start stage
 
 goto :success
 
 :error
 echo Please launch this file in the boost folder you downloaded and extracted from sourceforge.
-echo Note that you need to have the path to bjam.exe in your PATH environment variable.
-echo 
+echo Run the bootstrap.bat file to generate the b2.exe file needed for the build process.
+echo __________________________________________________________________________________________
 echo Example: 
-echo   cd c:\Downloads\boost_1_55_0\
+echo   cd c:\Downloads\boost_1_61_0\
 echo   c:\Games\DarkRadiant\tools\scripts\build_boost_libs.cmd
 goto :eof
 
