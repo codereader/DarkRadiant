69fdc2853c676a624a1f779db237e7403518e4be
codereader <greebo@angua.at>
2016-06-05 08:36:53 +0200

    Compilation fixes and build changes for wxWidgets 3.1.0

--- tools/msvc2015/post_build_event_x64.cmd
+++ tools/msvc2015/post_build_event_x64.cmd
@@ -11,14 +11,14 @@ IF "%1" == "Debug" (SET WXLIB_SUFFIX=ud) ELSE (SET WXLIB_SUFFIX=u)
 
 @echo Copying wxWidgets binaries
 
-copy ..\..\w64deps\wxWidgets\bin\wxmsw30%WXLIB_SUFFIX%_gl_%2_x64.dll	   ..\..\install /Y
-copy ..\..\w64deps\wxWidgets\bin\wxmsw30%WXLIB_SUFFIX%_html_%2_x64.dll  ..\..\install /Y
-copy ..\..\w64deps\wxWidgets\bin\wxmsw30%WXLIB_SUFFIX%_xrc_%2_x64.dll   ..\..\install /Y
-copy ..\..\w64deps\wxWidgets\bin\wxmsw30%WXLIB_SUFFIX%_stc_%2_x64.dll   ..\..\install /Y
-copy ..\..\w64deps\wxWidgets\bin\wxbase30%WXLIB_SUFFIX%_%2_x64.dll	   ..\..\install /Y
-copy ..\..\w64deps\wxWidgets\bin\wxbase30%WXLIB_SUFFIX%_xml_%2_x64.dll  ..\..\install /Y
-copy ..\..\w64deps\wxWidgets\bin\wxmsw30%WXLIB_SUFFIX%_adv_%2_x64.dll   ..\..\install /Y
-copy ..\..\w64deps\wxWidgets\bin\wxmsw30%WXLIB_SUFFIX%_core_%2_x64.dll  ..\..\install /Y
+copy ..\..\w64deps\wxWidgets\bin\wxmsw310%WXLIB_SUFFIX%_gl_%2_x64.dll	   ..\..\install /Y
+copy ..\..\w64deps\wxWidgets\bin\wxmsw310%WXLIB_SUFFIX%_html_%2_x64.dll  ..\..\install /Y
+copy ..\..\w64deps\wxWidgets\bin\wxmsw310%WXLIB_SUFFIX%_xrc_%2_x64.dll   ..\..\install /Y
+copy ..\..\w64deps\wxWidgets\bin\wxmsw310%WXLIB_SUFFIX%_stc_%2_x64.dll   ..\..\install /Y
+copy ..\..\w64deps\wxWidgets\bin\wxbase310%WXLIB_SUFFIX%_%2_x64.dll	   ..\..\install /Y
+copy ..\..\w64deps\wxWidgets\bin\wxbase310%WXLIB_SUFFIX%_xml_%2_x64.dll  ..\..\install /Y
+copy ..\..\w64deps\wxWidgets\bin\wxmsw310%WXLIB_SUFFIX%_adv_%2_x64.dll   ..\..\install /Y
+copy ..\..\w64deps\wxWidgets\bin\wxmsw310%WXLIB_SUFFIX%_core_%2_x64.dll  ..\..\install /Y
 
 IF "%1" == "Debug" (SET DEBUG_SUFFIX=-d) ELSE (SET DEBUG_SUFFIX=)
 
