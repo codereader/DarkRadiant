6bd10546f3ac217f8d06fc59ff6f286317c83d87
codereader <greebo@angua.at>
2016-07-26 20:38:09 +0200

    Add some checks to GroupSelected command

--- radiant/selection/group/SelectionGroupManager.cpp
+++ radiant/selection/group/SelectionGroupManager.cpp
@@ -1,5 +1,6 @@
 #include "SelectionGroupManager.h"
 
+#include "i18n.h"
 #include "imap.h"
 #include "itextstream.h"
 #include "imapinfofile.h"
@@ -9,6 +10,8 @@
 #include "imainframe.h"
 #include "modulesystem/StaticModule.h"
 
+#include "wxutil/dialog/MessageBox.h"
+#include "selectionlib.h"
 #include "SelectionGroup.h"
 #include "SelectableNode.h"
 #include "SelectionGroupInfoFileModule.h"
@@ -149,7 +152,53 @@ void SelectionGroupManager::deleteAllSelectionGroupsCmd(const cmd::ArgumentList&
 
 void SelectionGroupManager::groupSelectedCmd(const cmd::ArgumentList& args)
 {
-	// TODO: Maybe check if the current selection already is member of the same group
+	if (GlobalSelectionSystem().Mode() != SelectionSystem::ePrimitive)
+	{
+		rError() << "Must be in primitive selection mode to form groups." << std::endl;
+		wxutil::Messagebox::ShowError(_("Groups can be formed in Primitive selection mode only"));
+		return;
+	}
+
+	if (GlobalSelectionSystem().getSelectionInfo().totalCount == 0)
+	{
+		rError() << "Nothing selected, cannot group anything." << std::endl;
+		wxutil::Messagebox::ShowError(_("Nothing selected, cannot group anything"));
+		return;
+	}
+	
+	if (GlobalSelectionSystem().getSelectionInfo().totalCount == 1)
+	{
+		rError() << "Select more than one element to form a group." << std::endl;
+		wxutil::Messagebox::ShowError(_("Select more than one element to form a group"));
+		return;
+	}
+
+	// Check if the current selection already is member of the same group
+	std::set<std::size_t> groupIds;
+	bool hasUngroupedNode = false;
+
+	GlobalSelectionSystem().foreachSelected([&](const scene::INodePtr& node)
+	{
+		std::shared_ptr<IGroupSelectable> selectable = std::dynamic_pointer_cast<IGroupSelectable>(node);
+
+		if (!selectable) return;
+
+		if (!selectable->getGroupIds().empty())
+		{
+			groupIds.insert(selectable->getMostRecentGroupId());
+		}
+		else
+		{
+			hasUngroupedNode = true;
+		}
+	});
+
+	if (!hasUngroupedNode && groupIds.size() == 1)
+	{
+		rError() << "The selected elements already form a group" << std::endl;
+		wxutil::Messagebox::ShowError(_("The selected elements already form a group"));
+		return;
+	}
 
 	ISelectionGroupPtr group = createSelectionGroup();
 
