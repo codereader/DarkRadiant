fd4a5d1f95e3427a919d048a18b5ae9c0c19dcd5
codereader <greebo@angua.at>
2017-01-29 18:19:28 +0100

    Tighten the os/dir.h header a bit

--- libs/os/dir.h
+++ libs/os/dir.h
@@ -4,20 +4,21 @@
 /// \brief OS directory-listing object.
 #include <boost/filesystem.hpp>
 
-namespace fs = boost::filesystem;
-
 #include <string>
 #include <stdexcept>
+#include <functional>
+
+namespace fs = boost::filesystem;
 
 namespace os
 {
 
 /**
- * Exception thrown by Directory_forEach to indicate that the given directory
+ * Exception thrown by foreachItemInDirectory to indicate that the given directory
  * does not exist.
  */
-class DirectoryNotFoundException
-: public std::runtime_error
+class DirectoryNotFoundException : 
+	public std::runtime_error
 {
 public:
     DirectoryNotFoundException(const std::string& what) : 
@@ -26,29 +27,20 @@ public:
 };
 
 // Invoke functor for all items in a directory
-inline void foreachItemInDirectory(const std::string& path, const std::function<void (const fs::path&)>& functor)
+inline void foreachItemInDirectory(const std::string& path, const std::function<void(const fs::path&)>& functor)
 {
 	fs::path start(path);
 
 	if (!fs::exists(start))
 	{
-		throw DirectoryNotFoundException(
-            "foreachItemInDirectory(): invalid directory '" + path + "'"
-        );
+		throw DirectoryNotFoundException("foreachItemInDirectory(): invalid directory '" + path + "'");
 	}
 
 	for (fs::directory_iterator it(start); it != fs::directory_iterator(); ++it)
 	{
-		const fs::path& candidate = *it;
-
-		functor(candidate);
+		functor(*it);
 	}
 }
-
-} // namespace
-
-namespace os
-{
 
 /**
  * \brief
@@ -62,18 +54,18 @@ inline bool makeDirectory(const std::string& name)
 {
 	try
 	{
-		fs::path path(name);
+		fs::path dirPath(name);
 
 		// Create the directory 
-		if (fs::create_directory(path))
+		if (fs::create_directory(dirPath))
 		{
 			// Directory has been created, set permissions
-			rConsole() << "Directory " << path << " created succesfully." << std::endl;
+			rConsole() << "Directory " << dirPath << " created succesfully." << std::endl;
 
 			// Set permissions to rwxrwxr_x
-			fs::permissions(path, fs::add_perms |
-				fs::owner_exe | fs::owner_write | fs::owner_read |
-				fs::group_exe | fs::group_write | fs::group_read |
+			fs::permissions(dirPath, fs::add_perms |
+				fs::owner_exe  | fs::owner_write | fs::owner_read |
+				fs::group_exe  | fs::group_write | fs::group_read |
 				fs::others_exe | fs::others_read);
 		}
 		
@@ -91,4 +83,3 @@ inline bool makeDirectory(const std::string& name)
 }
 
 } // namespace os
-
