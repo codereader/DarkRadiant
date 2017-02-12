c9d5268924f081ab7de573106e48f3fbe6ea6d8e
codereader <greebo@angua.at>
2017-02-12 09:58:34 +0100

    Add xmlregistry module. Build output needs to be tested, right now stuff goes into the Application Folder.

--- radiant/modulesystem/ModuleLoader.cpp
+++ radiant/modulesystem/ModuleLoader.cpp
@@ -24,6 +24,8 @@ Loader::Loader(const std::string& path) :
 	_path(path),
 #if defined(WIN32)
 	  _ext(".dll")
+#elif defined(__APPLE__) // TODO: Makefile is using .so at the moment
+	  _ext(".dylib")
 #elif defined(POSIX)
 	  _ext(".so")
 #endif
@@ -57,8 +59,15 @@ void Loader::loadModules(const std::string& root) {
 
     // Get standardised paths
     std::string stdRoot = os::standardPathWithSlash(root);
+    
+#if defined(__APPLE__)
+    // Xcode output goes to the application folder right now
+    std::string modulesPath = stdRoot;
+    std::string pluginsPath = stdRoot;
+#else
     std::string modulesPath = stdRoot + MODULES_DIR;
     std::string pluginsPath = stdRoot + PLUGINS_DIR;
+#endif
 
     // Load modules and plugins
 	Loader modulesLoader(modulesPath);
@@ -66,15 +75,18 @@ void Loader::loadModules(const std::string& root) {
 
 	os::foreachItemInDirectory(modulesPath, modulesLoader);
 
-    // Plugins are optional, so catch the exception
-    try
-	{
-    	os::foreachItemInDirectory(pluginsPath, pluginsLoader);
-    }
-    catch (os::DirectoryNotFoundException&)
-	{
-        rConsole() << "Loader::loadModules(): plugins directory '"
+    if (pluginsPath != modulesPath)
+    {
+        // Plugins are optional, so catch the exception
+        try
+        {
+            os::foreachItemInDirectory(pluginsPath, pluginsLoader);
+        }
+        catch (os::DirectoryNotFoundException&)
+        {
+            rConsole() << "Loader::loadModules(): plugins directory '"
                   << pluginsPath << "' not found." << std::endl;
+        }
     }
 }
 
