157f9ebb2a52223b93a3f89107b0d4a26a1da705
codereader <greebo@angua.at>
2017-07-12 15:26:23 +0200

    Bump version. Update .gitignore list.

--- include/version.h
+++ include/version.h
@@ -2,7 +2,7 @@
 #include <config.h>
 #define RADIANT_VERSION PACKAGE_VERSION
 #else
-#define RADIANT_VERSION "2.2.2pre1"
+#define RADIANT_VERSION "2.3.0pre1"
 #endif
 
 #define RADIANT_APPNAME "DarkRadiant"
