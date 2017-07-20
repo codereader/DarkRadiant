a12f3e06b910d6c989000db867b48202d8f646e5
codereader <greebo@angua.at>
2017-07-20 18:42:56 +0200

    Migrated RegistryInterface

--- plugins/script/interfaces/RegistryInterface.h
+++ plugins/script/interfaces/RegistryInterface.h
@@ -1,11 +1,11 @@
-#ifndef _REGISTRY_INTERFACE_H_
-#define _REGISTRY_INTERFACE_H_
+#pragma once
 
 #include "iregistry.h"
 #include "iscript.h"
-#include <boost/python.hpp>
+#include <pybind11/pybind11.h>
 
-namespace script {
+namespace script
+{
 
 /**
  * greebo: This class provides the script interface for the GlobalRegistry module.
@@ -14,28 +14,27 @@ class RegistryInterface :
 	public IScriptInterface
 {
 public:
-	std::string get(const std::string& key) {
+	std::string get(const std::string& key)
+	{
 		return GlobalRegistry().get(key);
 	}
 
-	void set(const std::string& key, const std::string& value) {
+	void set(const std::string& key, const std::string& value)
+	{
 		GlobalRegistry().set(key, value);
 	}
 
 	// IScriptInterface implementation
-	void registerInterface(boost::python::object& nspace) {
+	void registerInterface(py::module& scope, py::dict& globals) override
+	{
 		// Add the module declaration to the given python namespace
-		nspace["GlobalRegistry"] = boost::python::class_<RegistryInterface>("GlobalRegistry")
-			.def("get", &RegistryInterface::get)
-			.def("set", &RegistryInterface::set)
-		;
+		py::class_<RegistryInterface> registry(scope, "Registry");
+		registry.def("get", &RegistryInterface::get);
+		registry.def("set", &RegistryInterface::set);
 
 		// Now point the Python variable "GlobalRegistry" to this instance
-		nspace["GlobalRegistry"] = boost::python::ptr(this);
+		globals["GlobalRegistry"] = this;
 	}
 };
-typedef std::shared_ptr<RegistryInterface> RegistryInterfacePtr;
 
 } // namespace script
-
-#endif /* _REGISTRY_INTERFACE_H_ */
