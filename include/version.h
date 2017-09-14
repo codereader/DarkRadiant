e6ce8dd031706c94a87daec36f3ea9a693eb5750
codereader <greebo@angua.at>
2017-09-14 09:24:12 +0200

    Add game file for Prey. Fix Quake 4 base folder name. Thanks to grml4d for the contribution!

--- include/version.h
+++ include/version.h
@@ -2,7 +2,7 @@
 #include <config.h>
 #define RADIANT_VERSION PACKAGE_VERSION
 #else
-#define RADIANT_VERSION "2.4.0pre1"
+#define RADIANT_VERSION "2.4.0pre4"
 #endif
 
 #define RADIANT_APPNAME "DarkRadiant"
