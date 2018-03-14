010e5b97567e9f4793941a053a019f72b59c300d
Matthew Mott <orbweaver3d@gmail.com>
2018-03-14 21:48:12 +0000

    Added missing PicoModelModule.cpp

---
+++ radiant/modelfile/PicoModelModule.cpp
@@ -0,0 +1,11 @@
+#include "PicoModelModule.h"
+
+#include "modulesystem/StaticModule.h"
+
+namespace model
+{
+
+// Static module instance
+module::StaticModule<PicoModelModule> picoModelModule;
+
+}
