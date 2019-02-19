6caff69856481136cc7fb6826d5b794ac360935c
Matthew Mott <orbweaver3d@gmail.com>
2019-02-19 20:27:14 +0000

    Resurrect tests using the include-only version of Boost.Test
    Tests were previously disabled to avoid linking with the Boost.Test dynamic
    library. This can be avoided by using the header-only version, which imposes
    the limitation that each test module can only contain a single translation
    unit. Currently the only test module is the facePlaneTest.

--- radiant/test/facePlaneTest.cpp
+++ radiant/test/facePlaneTest.cpp
@@ -1,6 +1,5 @@
-#define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE facePlaneTest
-#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
 
 #include "radiant/brush/FacePlane.h"
 #include "math/Matrix4.h"
