26a840a577e48ad9b58fd9b402832d37790099fd
Matthew Mott <orbweaver3d@gmail.com>
2019-03-07 21:21:57 +0000

    Add more dependencies of ShaderFileLoader into unit test
    Actually calling the parseFiles() method generates code which requires
    additional dependencies. Some of these are added as .cpp files in the Makefile,
    while the call to GetTextureManager() is linked to a local implementation so we
    can avoid depending on the modulesystem.

--- radiant/test/vfsTest.cpp
+++ radiant/test/vfsTest.cpp
@@ -3,6 +3,7 @@
 
 #include "radiant/vfs/Doom3FileSystem.h"
 #include "radiant/shaders/ShaderFileLoader.h"
+#include "radiant/shaders/textures/GLTextureManager.h"
 
 struct VFSFixture
 {
@@ -100,14 +101,35 @@ BOOST_FIXTURE_TEST_CASE(handleAssetsLst, VFSFixture)
     BOOST_TEST(fileVis.count("assets.lst") == 0);
 }
 
+namespace shaders
+{
+
+// Provide a local implementation of GetTextureManager since the application
+// version calls the module registry.
+GLTextureManager& GetTextureManager()
+{
+    static GLTextureManager manager;
+    return manager;
+}
+
+}
+
 using namespace shaders;
 
+// Replacement for ShaderLibrary used in tests
 struct MockShaderLibrary
 {
+    bool addTableDefinition(const TableDefinitionPtr& def)
+    { return true; }
+
+    bool addDefinition(const std::string& name, const ShaderDefinition& def)
+    { return true; }
 };
 
 BOOST_FIXTURE_TEST_CASE(loaderShaderFiles, VFSFixture)
 {
     MockShaderLibrary library;
     shaders::ShaderFileLoader<MockShaderLibrary> loader("materials", library);
+
+    loader.parseFiles();
 }
