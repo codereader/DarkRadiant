98e01bb3e221b09dff353dc200ad08156d795879
Matthew Mott <orbweaver3d@gmail.com>
2019-03-13 20:52:19 +0000

    Improve handling of 'srcdir' environment variable
    Throw a more meaningful exception if the srcdir variable is not set, rather
    than a construction error from inside std::string.

--- radiant/test/VFSFixture.h
+++ radiant/test/VFSFixture.h
@@ -12,6 +12,16 @@ struct VFSFixture
     vfs::VirtualFileSystem::ExtensionSet pakExtensions;
     vfs::SearchPaths searchPaths;
 
+    // Get the srcdir environment variable (set by Automake)
+    std::string srcdir() const
+    {
+        const char* envVal = getenv("srcdir");
+        if (envVal)
+            return std::string(envVal);
+        else
+            throw std::runtime_error("srcdir not set");
+    }
+
     VFSFixture()
     {
         // Setup the output stream
@@ -19,9 +29,7 @@ struct VFSFixture
 
         // Configure search paths and extensions
         pakExtensions.insert("pk4");
-        searchPaths.insertIfNotExists(
-            std::string(getenv("srcdir")) + "/test/data/vfs_root"
-        );
+        searchPaths.insertIfNotExists(srcdir() + "/test/data/vfs_root");
 
         // Initialise the VFS
         fs.initialise(searchPaths, pakExtensions);
