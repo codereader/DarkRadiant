0f7674bc63dc0de6f806942001aae0465f75a9f8
codereader <greebo@angua.at>
2019-12-08 10:04:03 +0100

    Move user guide URL to registry and let it point to darkradiant.net. Invoke the browser using that setting.

--- radiant/ui/Documentation.cpp
+++ radiant/ui/Documentation.cpp
@@ -1,15 +1,19 @@
 #include "Documentation.h"
-#include "modulesystem/ModuleRegistry.h"
+#include "registry/registry.h"
 
 #include <wx/utils.h>
 
 namespace ui
 {
 
+namespace
+{
+	const char* const RKEY_USER_GUIDE_URL = "user/ui/userGuideUrl";
+}
+
 void Documentation::showUserGuide(const cmd::ArgumentList&)
 {
-    const ApplicationContext& ctx = module::ModuleRegistry::Instance().getApplicationContext();
-    wxLaunchDefaultBrowser(ctx.getHTMLPath() + "manual.html");
+    wxLaunchDefaultBrowser(registry::getValue<std::string>(RKEY_USER_GUIDE_URL));
 }
 
 }
