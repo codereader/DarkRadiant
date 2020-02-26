28358ed0253e5ddcb152be6d0372daf34ea35dc6
Matthew Mott <orbweaver3d@gmail.com>
2020-02-26 21:36:27 +0000

    Fix assertion failure on opening Difficulty editor
    Tab images were placed into a wxImageList but this list was never set on the
    wxNotebook with SetImageList(), causing an assertion failure.

--- plugins/dm.difficulty/DifficultyDialog.cpp
+++ plugins/dm.difficulty/DifficultyDialog.cpp
@@ -23,118 +23,119 @@ namespace ui
 
 namespace
 {
-	const char* const WINDOW_TITLE = N_("Difficulty Editor");
+    const char* const WINDOW_TITLE = N_("Difficulty Editor");
 }
 
 DifficultyDialog::DifficultyDialog() :
-	DialogBase(_(WINDOW_TITLE))
+    DialogBase(_(WINDOW_TITLE))
 {
-	// Load the settings
-	_settingsManager.loadSettings();
+    // Load the settings
+    _settingsManager.loadSettings();
 
-	// Create the widgets
-	populateWindow();
+    // Create the widgets
+    populateWindow();
 }
 
 void DifficultyDialog::createDifficultyEditors()
 {
-	int numLevels = game::current::getValue<int>(GKEY_DIFFICULTY_LEVELS);
+    int numLevels = game::current::getValue<int>(GKEY_DIFFICULTY_LEVELS);
 
-	for (int i = 0; i < numLevels; i++)
-	{
-		// Acquire the settings object
-		difficulty::DifficultySettingsPtr settings = _settingsManager.getSettings(i);
+    for (int i = 0; i < numLevels; i++)
+    {
+        // Acquire the settings object
+        difficulty::DifficultySettingsPtr settings = _settingsManager.getSettings(i);
 
-		if (settings != NULL)
-		{
-			_editors.push_back(
-				DifficultyEditorPtr(new DifficultyEditor(
-					_notebook, _settingsManager.getDifficultyName(i), settings)
-				)
-			);
-		}
-	}
+        if (settings != NULL)
+        {
+            _editors.push_back(
+                DifficultyEditorPtr(new DifficultyEditor(
+                    _notebook, _settingsManager.getDifficultyName(i), settings)
+                )
+            );
+        }
+    }
 
-	// A new image list for the notebook tab icons
-	_imageList.reset(new wxImageList(16, 16));
+    // A new image list for the notebook tab icons
+    _imageList.reset(new wxImageList(16, 16));
+    _notebook->SetImageList(_imageList.get());
 
-	// Pack the editors into the notebook
-	for (std::size_t i = 0; i < _editors.size(); i++)
-	{
-		DifficultyEditor& editor = *_editors[i];
+    // Pack the editors into the notebook
+    for (std::size_t i = 0; i < _editors.size(); i++)
+    {
+        DifficultyEditor& editor = *_editors[i];
 
-		wxWindow* editorWidget = editor.getEditor();
-		std::string icon = editor.getNotebookIconName();
+        wxWindow* editorWidget = editor.getEditor();
+        std::string icon = editor.getNotebookIconName();
 
-		// Load the icon
-		int imageId = icon.empty() ? -1 : 
-			_imageList->Add(wxArtProvider::GetBitmap(GlobalUIManager().ArtIdPrefix() + icon));
-	
-		editorWidget->Reparent(_notebook);
-		_notebook->AddPage(editorWidget, editor.getNotebookLabel(), false, imageId);
-	}
+        // Load the icon
+        int imageId = icon.empty() ? -1 : 
+            _imageList->Add(wxArtProvider::GetBitmap(GlobalUIManager().ArtIdPrefix() + icon));
+    
+        editorWidget->Reparent(_notebook);
+        _notebook->AddPage(editorWidget, editor.getNotebookLabel(), false, imageId);
+    }
 }
 
 void DifficultyDialog::populateWindow()
 {
-	SetSizer(new wxBoxSizer(wxVERTICAL));
+    SetSizer(new wxBoxSizer(wxVERTICAL));
 
-	// Create the notebook and add it to the vbox
-	_notebook = new wxNotebook(this, wxID_ANY);
-	_notebook->SetMinClientSize(wxSize(800, 400));
+    // Create the notebook and add it to the vbox
+    _notebook = new wxNotebook(this, wxID_ANY);
+    _notebook->SetMinClientSize(wxSize(800, 400));
 
-	// Create and pack the editors
-	createDifficultyEditors();
+    // Create and pack the editors
+    createDifficultyEditors();
 
-	GetSizer()->Add(_notebook, 1, wxEXPAND | wxALL, 12);
+    GetSizer()->Add(_notebook, 1, wxEXPAND | wxALL, 12);
 
-	wxButton* okButton = new wxButton(this, wxID_OK);
-	wxButton* cancelButton = new wxButton(this, wxID_CANCEL);
+    wxButton* okButton = new wxButton(this, wxID_OK);
+    wxButton* cancelButton = new wxButton(this, wxID_CANCEL);
 
-	okButton->Bind(wxEVT_BUTTON, [&] (wxCommandEvent&) { EndModal(wxID_OK); });
-	cancelButton->Bind(wxEVT_BUTTON, [&] (wxCommandEvent&) { EndModal(wxID_CANCEL); });
+    okButton->Bind(wxEVT_BUTTON, [&] (wxCommandEvent&) { EndModal(wxID_OK); });
+    cancelButton->Bind(wxEVT_BUTTON, [&] (wxCommandEvent&) { EndModal(wxID_CANCEL); });
 
-	wxBoxSizer* buttonSizer = new wxBoxSizer(wxHORIZONTAL);
-	buttonSizer->Add(cancelButton, 0, wxRIGHT, 6);
-	buttonSizer->Add(okButton, 0, wxRIGHT, 6);
+    wxBoxSizer* buttonSizer = new wxBoxSizer(wxHORIZONTAL);
+    buttonSizer->Add(cancelButton, 0, wxRIGHT, 6);
+    buttonSizer->Add(okButton, 0, wxRIGHT, 6);
 
-	GetSizer()->Add(buttonSizer, 0, wxALIGN_RIGHT | wxALL, 12);
+    GetSizer()->Add(buttonSizer, 0, wxALIGN_RIGHT | wxALL, 12);
 
-	Layout();
-	Fit();
+    Layout();
+    Fit();
 }
 
 void DifficultyDialog::save()
 {
-	// Consistency check can go here
+    // Consistency check can go here
 
-	// Scoped undo object
-	UndoableCommand command("editDifficulty");
+    // Scoped undo object
+    UndoableCommand command("editDifficulty");
 
-	// Save the working set to the entity
-	_settingsManager.saveSettings();
+    // Save the working set to the entity
+    _settingsManager.saveSettings();
 }
 
 int DifficultyDialog::ShowModal()
 {
-	int returnCode = DialogBase::ShowModal();
+    int returnCode = DialogBase::ShowModal();
 
-	if (returnCode == wxID_OK)
-	{
-		save();
-	}
+    if (returnCode == wxID_OK)
+    {
+        save();
+    }
 
-	return returnCode;
+    return returnCode;
 }
 
 // Static command target
 void DifficultyDialog::ShowDialog(const cmd::ArgumentList& args)
 {
-	// Construct a new instance, this enters the main loop
-	DifficultyDialog* editor = new DifficultyDialog;
+    // Construct a new instance, this enters the main loop
+    DifficultyDialog* editor = new DifficultyDialog;
 
-	editor->ShowModal();
-	editor->Destroy();
+    editor->ShowModal();
+    editor->Destroy();
 }
 
 } // namespace ui
