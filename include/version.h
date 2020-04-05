9f6eadd74413fa48173992d299d7bddb865bd8e1
codereader <greebo@angua.at>
2020-04-05 18:21:28 +0200

    Bump version for a first pre-release build.

--- include/version.h
+++ include/version.h
@@ -2,7 +2,7 @@
 #include <config.h>
 #define RADIANT_VERSION PACKAGE_VERSION
 #else
-#define RADIANT_VERSION "2.7.0"
+#define RADIANT_VERSION "2.8.0pre1"
 #endif
 
 #define RADIANT_APPNAME "DarkRadiant"
