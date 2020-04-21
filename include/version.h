bb74f5561ac17dbfe2d37c0d819ac23e4d6d1bf3
codereader <greebo@angua.at>
2020-04-21 06:54:56 +0200

    Another attempt to fix msbuild hanging after launched through the compile_release_package PS script.

--- include/version.h
+++ include/version.h
@@ -2,7 +2,7 @@
 #include <config.h>
 #define RADIANT_VERSION PACKAGE_VERSION
 #else
-#define RADIANT_VERSION "2.8.0pre3"
+#define RADIANT_VERSION "2.8.0pre4"
 #endif
 
 #define RADIANT_APPNAME "DarkRadiant"
