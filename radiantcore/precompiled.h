64de2a375ee4adc1135267ede3561867b8920e76
codereader <greebo@angua.at>
2020-05-09 07:03:11 +0200

    #5231: Configure pre-compiled headers in the core module

---
+++ radiantcore/precompiled.h
@@ -0,0 +1,12 @@
+/**
+ * greebo: Precompiled header file used by the main module.
+ */
+#pragma once
+
+#define DR_PRECOMPILED_WXWIDGETS
+#define DR_PRECOMPILED_INTERFACES
+#define DR_PRECOMPILED_MATH
+#define DR_PRECOMPILED_REGISTRY
+
+ // The actual inclusions are handled by the main precompiled header file
+#include "precompiled_main.h"
