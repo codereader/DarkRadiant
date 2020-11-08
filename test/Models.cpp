2034a9defe060e8b18e194f559b00db3aca3782e
codereader <greebo@angua.at>
2020-11-08 17:26:57 +0100

    #5383: Add unit test asserting the LWO and ASE poly count after loading

---
+++ test/Models.cpp
@@ -0,0 +1,28 @@
+#include "RadiantTest.h"
+
+#include "imodelcache.h"
+
+namespace test
+{
+
+using ModelTest = RadiantTest;
+
+TEST_F(ModelTest, LwoPolyCount)
+{
+    auto model = GlobalModelCache().getModel("models/darkmod/test/unit_cube.lwo");
+    EXPECT_TRUE(model);
+
+    // The unit cube should have 12 polys (6 quads = 12 tris)
+    EXPECT_EQ(model->getPolyCount(), 12);
+}
+
+TEST_F(ModelTest, AsePolyCount)
+{
+    auto model = GlobalModelCache().getModel("models/darkmod/test/unit_cube.ase");
+    EXPECT_TRUE(model);
+
+    // The unit cube should have 12 polys (6 quads = 12 tris)
+    EXPECT_EQ(model->getPolyCount(), 12);
+}
+
+}
