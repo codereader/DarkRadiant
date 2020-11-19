e1d4b7fa9f4155e24078663e6af1b8391594c475
Matthew Mott <orbweaver3d@gmail.com>
2020-11-19 21:07:55 +0000

    Tweak architecture names on Linux
    Linux users will be more familiar with the architecture names 'amd64' or
    'i386', as used by package managers, so we now show these in the About dialog.
    Windows users should continue to see 'x86' or 'x64' as they would expect.

--- include/version.h
+++ include/version.h
@@ -9,9 +9,23 @@
 #define RADIANT_BLANK " "
 
 #if defined(_M_X64) || defined(__amd64__) || defined(_WIN64)
+
+// 64 bit architecture names (according to platform convention)
+#if defined(__linux__)
+    #define RADIANT_PLATFORM "amd64"
+#else
 	#define RADIANT_PLATFORM "x64"
+#endif
+
+#else
+
+// 32 bit architecture names (according to platform convention)
+#if defined(__linux)
+    #define RADIANT_PLATFORM "i386"
 #else
 	#define RADIANT_PLATFORM "x86"
+#endif
+
 #endif
 
 #include <string>
