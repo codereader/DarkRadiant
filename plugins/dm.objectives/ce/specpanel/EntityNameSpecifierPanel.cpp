62a4ab7918e76bd2264278b8c5d66ffab40e1af7
codereader <greebo@angua.at>
2020-12-25 09:39:01 +0100

    #5406: Upgrade "Name of single entity" SpecifierEditor to a combo box listing all the map's entities.

--- plugins/dm.objectives/ce/specpanel/EntityNameSpecifierPanel.cpp
+++ plugins/dm.objectives/ce/specpanel/EntityNameSpecifierPanel.cpp
@@ -1,10 +1,69 @@
 #include "EntityNameSpecifierPanel.h"
 
+#include <wx/sizer.h>
+#include "inode.h"
+#include "imap.h"
+#include "ientity.h"
+
 namespace objectives
 {
 
 namespace ce
 {
+
+EntityNameSpecifierPanel::EntityNameSpecifierPanel() :
+    _editCombo(nullptr)
+{}
+
+EntityNameSpecifierPanel::EntityNameSpecifierPanel(wxWindow* parent) :
+    _editCombo(nullptr)
+{
+    _editCombo = new wxComboBox(parent, wxID_ANY);
+
+    // Bind both events to the same callback
+    _editCombo->Bind(wxEVT_TEXT, &EntityNameSpecifierPanel::onComboBoxChanged, this);
+    _editCombo->Bind(wxEVT_COMBOBOX, &EntityNameSpecifierPanel::onComboBoxChanged, this);
+
+    // Collect all entity names in the scene
+    GlobalMapModule().getRoot()->foreachNode([&](const scene::INodePtr& node)
+    {
+        if (Node_isEntity(node))
+        {
+            _editCombo->AppendString(Node_getEntity(node)->getKeyValue("name"));
+        }
+
+        return true;
+    });
+}
+
+EntityNameSpecifierPanel::~EntityNameSpecifierPanel()
+{
+    delete _editCombo;
+    _editCombo = nullptr;
+}
+
+wxWindow* EntityNameSpecifierPanel::getWidget()
+{
+    return _editCombo;
+}
+
+void EntityNameSpecifierPanel::setValue(const std::string& value)
+{
+    _editCombo->SetValue(value);
+}
+
+std::string EntityNameSpecifierPanel::getValue() const
+{
+    return _editCombo->GetValue().ToStdString();
+}
+
+void EntityNameSpecifierPanel::onComboBoxChanged(wxCommandEvent& ev)
+{
+    if (_valueChanged)
+    {
+        _valueChanged(); // usually points to the abstract ComponentEditor::writeToComponent
+    }
+}
 
 // Reg helper
 EntityNameSpecifierPanel::RegHelper EntityNameSpecifierPanel::_regHelper;
--- plugins/dm.objectives/ce/specpanel/EntityNameSpecifierPanel.h
+++ plugins/dm.objectives/ce/specpanel/EntityNameSpecifierPanel.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include "SpecifierPanel.h"
-#include "TextSpecifierPanel.h"
+#include "SpecifierPanelFactory.h"
+#include <wx/combobox.h>
 
 namespace objectives
 {
@@ -13,12 +14,46 @@ namespace ce
  * SpecifierPanel subclass for the SPEC_NAME (name of single entity) specifier
  * type.
  */
-class EntityNameSpecifierPanel
-: public TextSpecifierPanel
+class EntityNameSpecifierPanel : 
+    public wxEvtHandler,
+    public SpecifierPanel
 {
+protected:
+    wxComboBox* _editCombo;
+
+    // The change callback, invoked when the text changes
+    std::function<void()> _valueChanged;
+
+    EntityNameSpecifierPanel();
+
+public:
+    EntityNameSpecifierPanel(wxWindow* parent);
+
+    virtual ~EntityNameSpecifierPanel();
+
+    /* SpecifierPanel implementation */
+    virtual SpecifierPanelPtr create(wxWindow* parent) const override
+    {
+        return std::make_shared<EntityNameSpecifierPanel>(parent);
+    }
+
+    void setChangedCallback(const std::function<void()>& callback) override
+    {
+        _valueChanged = callback;
+    }
+
+    virtual wxWindow* getWidget() override;
+    void setValue(const std::string& value) override;
+    std::string getValue() const override;
+
+private:
+    void onComboBoxChanged(wxCommandEvent& ev);
+
 	// Map registration
-	static struct RegHelper {
-		RegHelper() {
+	static struct RegHelper
+    {
+		RegHelper()
+        {
 			SpecifierPanelFactory::registerType(
 				SpecifierType::SPEC_NAME().getName(),
 				SpecifierPanelPtr(new EntityNameSpecifierPanel())
