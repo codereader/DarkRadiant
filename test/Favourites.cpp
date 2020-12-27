cf98ee4885c311282e235a1d30f12ae84d4b2495
codereader <greebo@angua.at>
2020-12-27 17:37:40 +0100

    #5127: Add unit test covering the import of legacy media browser favourites

--- test/Favourites.cpp
+++ test/Favourites.cpp
@@ -1,11 +1,40 @@
 #include "RadiantTest.h"
 
 #include "ifavourites.h"
+#include "iregistry.h"
 
 namespace test
 {
 
-using FavouritesTest = RadiantTest;
+class FavouritesTest :
+    public RadiantTest
+{
+protected:
+    void copyUserXmlFileToSettingsPath(const std::string& userXmlFile)
+    {
+        fs::path sourcePath = _context.getTestResourcePath();
+        sourcePath /= "settings/";
+        sourcePath /= userXmlFile;
+
+        fs::path targetPath = _context.getSettingsPath();
+        targetPath /= "user.xml";
+
+        fs::remove(targetPath);
+        fs::copy(sourcePath, targetPath);
+    }
+};
+
+class FavouritesTestWithLegacyFavourites :
+    public FavouritesTest
+{
+public:
+    void SetUp() override
+    {
+        copyUserXmlFileToSettingsPath("old_mediabrowser_favourites.xml");
+
+        RadiantTest::SetUp();
+    }
+};
 
 TEST_F(FavouritesTest, AddingAndRemovingFavourites)
 {
@@ -73,6 +102,27 @@ TEST_F(FavouritesTest, AddingEmptyPaths)
 
     // Empty strings always evaluate to false
     EXPECT_FALSE(GlobalFavouritesManager().isFavourite(decl::Type::Material, ""));
+}
+
+TEST_F(FavouritesTestWithLegacyFavourites, LegacyFavouritesAreImported)
+{
+    // The settings in the old location in the user.xml file should have been imported
+    EXPECT_FALSE(GlobalFavouritesManager().getFavourites(decl::Type::Material).empty());
+
+    // These were present in the legacy nodes
+    EXPECT_TRUE(GlobalFavouritesManager().isFavourite(decl::Type::Material, "textures/base_trim/gotrustcol1"));
+    EXPECT_TRUE(GlobalFavouritesManager().isFavourite(decl::Type::Material, "textures/common/caulk"));
+    EXPECT_TRUE(GlobalFavouritesManager().isFavourite(decl::Type::Material, "textures/common/clip"));
+    EXPECT_TRUE(GlobalFavouritesManager().isFavourite(decl::Type::Material, "textures/common/monster_clip"));
+    EXPECT_TRUE(GlobalFavouritesManager().isFavourite(decl::Type::Material, "textures/darkmod/stone/cobblestones/blocks_mixedsize01_dark"));
+    EXPECT_TRUE(GlobalFavouritesManager().isFavourite(decl::Type::Material, "textures/darkmod/stone/cobblestones/blocks_mixedsize03_mossy"));
+
+    // This one is already saved in the new location and should have been loaded too
+    EXPECT_TRUE(GlobalFavouritesManager().isFavourite(decl::Type::Material, "textures/common/ladder"));
+
+    // The old node should have been automatically removed
+    auto legacyNodes = GlobalRegistry().findXPath("user/ui/mediaBrowser/favourites");
+    EXPECT_TRUE(legacyNodes.empty());
 }
 
 }
