f532d6e518e677a6904a5c768e45e91b422c8f09
Matthew Mott <orbweaver3d@gmail.com>
2021-01-18 20:30:39 +0000

    Merge GenericEntity with GenericEntityNode
    Remove a largely useless level of indirection by merging these two classes.

--- radiantcore/entity/generic/GenericEntityNode.cpp
+++ radiantcore/entity/generic/GenericEntityNode.cpp
@@ -1,21 +1,56 @@
 #include "GenericEntityNode.h"
+#include "../EntitySettings.h"
 
 #include "math/Frustum.h"
 
-namespace entity {
+namespace entity
+{
 
 GenericEntityNode::GenericEntityNode(const IEntityClassPtr& eclass) :
 	EntityNode(eclass),
-	m_contained(*this),
+	m_originKey(std::bind(&GenericEntityNode::originChanged, this)),
+	m_origin(ORIGINKEY_IDENTITY),
+	m_angleKey(std::bind(&GenericEntityNode::angleChanged, this)),
+	m_angle(AngleKey::IDENTITY),
+	m_rotationKey(std::bind(&GenericEntityNode::rotationChanged, this)),
+	m_arrow(m_ray),
+	m_aabb_solid(m_aabb_local),
+	m_aabb_wire(m_aabb_local),
+	_allow3Drotations(_entity.getKeyValue("editor_rotatable") == "1"),
     _solidAABBRenderMode(SolidBoxes)
 {}
 
 GenericEntityNode::GenericEntityNode(const GenericEntityNode& other) :
 	EntityNode(other),
 	Snappable(other),
-	m_contained(other.m_contained, *this),
+	m_originKey(std::bind(&GenericEntityNode::originChanged, this)),
+	m_origin(ORIGINKEY_IDENTITY),
+	m_angleKey(std::bind(&GenericEntityNode::angleChanged, this)),
+	m_angle(AngleKey::IDENTITY),
+	m_rotationKey(std::bind(&GenericEntityNode::rotationChanged, this)),
+	m_arrow(m_ray),
+	m_aabb_solid(m_aabb_local),
+	m_aabb_wire(m_aabb_local),
+	_allow3Drotations(_entity.getKeyValue("editor_rotatable") == "1"),
     _solidAABBRenderMode(other._solidAABBRenderMode)
 {}
+
+GenericEntityNode::~GenericEntityNode()
+{
+	if (!_allow3Drotations)
+	{
+		// Ordinary rotation (2D around z axis), use angle key observer
+		removeKeyObserver("angle", _angleObserver);
+	}
+	else
+	{
+		// Full 3D rotations allowed, observe both keys using the rotation key observer
+		removeKeyObserver("angle", _angleObserver);
+		removeKeyObserver("rotation", _rotationObserver);
+	}
+
+	removeKeyObserver("origin", m_originKey);
+}
 
 GenericEntityNodePtr GenericEntityNode::Create(const IEntityClassPtr& eclass)
 {
@@ -29,24 +64,53 @@ void GenericEntityNode::construct()
 {
 	EntityNode::construct();
 
-	m_contained.construct();
+	m_aabb_local = _entity.getEntityClass()->getBounds();
+	m_ray.origin = m_aabb_local.getOrigin();
+	m_ray.direction = Vector3(1, 0, 0);
+	m_rotation.setIdentity();
+
+	if (!_allow3Drotations)
+	{
+		_angleObserver.setCallback(std::bind(&AngleKey::angleChanged, &m_angleKey, std::placeholders::_1));
+
+		// Ordinary rotation (2D around z axis), use angle key observer
+		addKeyObserver("angle", _angleObserver);
+	}
+	else
+	{
+		_angleObserver.setCallback(std::bind(&RotationKey::angleChanged, &m_rotationKey, std::placeholders::_1));
+		_rotationObserver.setCallback(std::bind(&RotationKey::rotationChanged, &m_rotationKey, std::placeholders::_1));
+
+		// Full 3D rotations allowed, observe both keys using the rotation key observer
+		addKeyObserver("angle", _angleObserver);
+		addKeyObserver("rotation", _rotationObserver);
+	}
+
+	addKeyObserver("origin", m_originKey);
 }
 
-// Snappable implementation
-void GenericEntityNode::snapto(float snap) {
-	m_contained.snapto(snap);
+void GenericEntityNode::snapto(float snap)
+{
+	m_originKey.snap(snap);
+	m_originKey.write(_entity);
 }
 
-// Bounded implementation
-const AABB& GenericEntityNode::localAABB() const {
-	return m_contained.localAABB();
+const AABB& GenericEntityNode::localAABB() const
+{
+	return m_aabb_local;
 }
 
 void GenericEntityNode::testSelect(Selector& selector, SelectionTest& test)
 {
 	EntityNode::testSelect(selector, test);
 
-	m_contained.testSelect(selector, test, localToWorld());
+	test.BeginMesh(localToWorld());
+
+	SelectionIntersection best;
+	aabb_testselect(m_aabb_local, test, best);
+	if(best.isValid()) {
+		selector.addIntersection(best);
+	}
 }
 
 scene::INodePtr GenericEntityNode::clone() const
@@ -63,36 +127,109 @@ GenericEntityNode::SolidAAABBRenderMode GenericEntityNode::getSolidAABBRenderMod
     return _solidAABBRenderMode;
 }
 
+void GenericEntityNode::renderArrow(const ShaderPtr& shader, RenderableCollector& collector,
+	const VolumeTest& volume, const Matrix4& localToWorld) const
+{
+	if (EntitySettings::InstancePtr()->getShowEntityAngles())
+	{
+		collector.addRenderable(*shader, m_arrow, localToWorld);
+	}
+}
+
 void GenericEntityNode::renderSolid(RenderableCollector& collector, const VolumeTest& volume) const
 {
 	EntityNode::renderSolid(collector, volume);
 
-	m_contained.renderSolid(collector, volume, localToWorld());
+	const ShaderPtr& shader = getSolidAABBRenderMode() == GenericEntityNode::WireFrameOnly ?
+		getWireShader() : getFillShader();
+
+	collector.addRenderable(*shader, m_aabb_solid, localToWorld());
+	renderArrow(shader, collector, volume, localToWorld());
 }
 
 void GenericEntityNode::renderWireframe(RenderableCollector& collector, const VolumeTest& volume) const
 {
 	EntityNode::renderWireframe(collector, volume);
 
-	m_contained.renderWireframe(collector, volume, localToWorld());
+	collector.addRenderable(*getWireShader(), m_aabb_wire, localToWorld());
+	renderArrow(getWireShader(), collector, volume, localToWorld());
 }
 
 const Vector3& GenericEntityNode::getDirection() const
 {
-	// Return the direction as specified by the angle/rotation keys
-	return m_contained.getDirection();
+	return m_ray.direction;
+}
+
+void GenericEntityNode::rotate(const Quaternion& rotation)
+{
+	if (_allow3Drotations)
+	{
+        m_rotation.rotate(rotation);
+	}
+	else
+	{
+		m_angle = AngleKey::getRotatedValue(m_angle, rotation);
+	}
+}
+
+void GenericEntityNode::revertTransform()
+{
+	m_origin = m_originKey.get();
+
+	if (_allow3Drotations)
+	{
+		m_rotation = m_rotationKey.m_rotation;
+	}
+	else
+	{
+		m_angle = m_angleKey.getValue();
+	}
+}
+
+void GenericEntityNode::freezeTransform()
+{
+	m_originKey.set(m_origin);
+	m_originKey.write(_entity);
+
+	if (_allow3Drotations)
+	{
+		m_rotationKey.m_rotation = m_rotation;
+		m_rotationKey.m_rotation.writeToEntity(&_entity);
+	}
+	else
+	{
+		m_angleKey.setValue(m_angle);
+		m_angleKey.write(&_entity);
+	}
+}
+
+void GenericEntityNode::updateTransform()
+{
+	localToParent() = Matrix4::getTranslation(m_origin);
+
+	if (_allow3Drotations)
+	{
+		// greebo: Use the z-direction as base for rotations
+		m_ray.direction = m_rotation.getMatrix4().transformDirection(Vector3(0,0,1));
+	}
+	else
+	{
+		m_ray.direction = Matrix4::getRotationAboutZDegrees(m_angle).transformDirection(Vector3(1, 0, 0));
+	}
+
+	transformChanged();
 }
 
 void GenericEntityNode::_onTransformationChanged()
 {
 	if (getType() == TRANSFORM_PRIMITIVE)
 	{
-		m_contained.revertTransform();
+		revertTransform();
 
-		m_contained.translate(getTranslation());
-		m_contained.rotate(getRotation());
+		m_origin += getTranslation();
+		rotate(getRotation());
 
-		m_contained.updateTransform();
+		updateTransform();
 	}
 }
 
@@ -100,18 +237,18 @@ void GenericEntityNode::_applyTransformation()
 {
 	if (getType() == TRANSFORM_PRIMITIVE)
 	{
-		m_contained.revertTransform();
+		revertTransform();
 
-		m_contained.translate(getTranslation());
-		m_contained.rotate(getRotation());
+		m_origin += getTranslation();
+		rotate(getRotation());
 
-		m_contained.freezeTransform();
+		freezeTransform();
 	}
 }
 
 const Vector3& GenericEntityNode::getUntransformedOrigin()
 {
-    return m_contained.getUntransformedOrigin();
+    return m_originKey.get();
 }
 
 void GenericEntityNode::onChildAdded(const scene::INodePtr& child) 
@@ -154,5 +291,30 @@ void GenericEntityNode::onChildRemoved(const scene::INodePtr& child)
         return true;
     });
 }
+
+void GenericEntityNode::originChanged()
+{
+	m_origin = m_originKey.get();
+	updateTransform();
+}
+
+void GenericEntityNode::angleChanged()
+{
+	// Ignore the angle key when 3D rotations are enabled
+	if (_allow3Drotations) return;
+
+	m_angle = m_angleKey.getValue();
+	updateTransform();
+}
+
+void GenericEntityNode::rotationChanged()
+{
+	// Ignore the rotation key, when in 2D "angle" mode
+	if (!_allow3Drotations) return;
+
+	m_rotation = m_rotationKey.m_rotation;
+	updateTransform();
+}
+
 
 } // namespace entity
