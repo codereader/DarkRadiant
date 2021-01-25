61dd80d59782fb1c4bb60e731aa9d7b2b54fde6f
Matthew Mott <orbweaver3d@gmail.com>
2021-01-25 20:22:07 +0000

    Further testing of light inheritance in EntityTest
    Check that entities which derive from light (directly or through multiple
    levels) all have isLight() set correctly, but torch_brazier is not considered a
    light (it is a func_static with a light attached).

--- test/Entity.cpp
+++ test/Entity.cpp
@@ -18,7 +18,7 @@ TEST_F(EntityTest, LookupEntityClass)
     EXPECT_TRUE(lightCls);
 }
 
-TEST_F(EntityTest, LightClassRecognisedAsLight)
+TEST_F(EntityTest, LightEntitiesRecognisedAsLights)
 {
     // The 'light' class should be recognised as an actual light
     auto lightCls = GlobalEntityClassManager().findClass("light");
@@ -28,6 +28,19 @@ TEST_F(EntityTest, LightClassRecognisedAsLight)
     auto notLightCls = GlobalEntityClassManager().findClass("dr:entity_using_modeldef");
     EXPECT_TRUE(notLightCls);
     EXPECT_FALSE(notLightCls->isLight());
+
+    // Anything deriving from the light class should also be a light
+    auto derived1 = GlobalEntityClassManager().findClass("atdm:light_base");
+    EXPECT_TRUE(derived1->isLight());
+
+    // Second level derivations too
+    auto derived2 = GlobalEntityClassManager().findClass("light_extinguishable");
+    EXPECT_TRUE(derived2->isLight());
+
+    // torch_brazier is not a light itself, but has a light attached, so it
+    // should not have isLight() == true
+    auto brazier = GlobalEntityClassManager().findClass("atdm:torch_brazier");
+    EXPECT_FALSE(brazier->isLight());
 }
 
 }
