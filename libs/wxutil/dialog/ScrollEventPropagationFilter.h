8272a95c7a95cfced351dbd45b63cc4f2d8d5a43
codereader <greebo@angua.at>
2021-01-28 18:22:36 +0100

    #5506: Fix the endless loop triggered by the ScrollEventPropagationFilter

--- libs/wxutil/dialog/ScrollEventPropagationFilter.h
+++ libs/wxutil/dialog/ScrollEventPropagationFilter.h
@@ -3,6 +3,7 @@
 #include <memory>
 #include <wx/eventfilter.h>
 #include "../GLWidget.h"
+#include "util/ScopedBoolLock.h"
 
 namespace wxutil
 {
@@ -11,34 +12,37 @@ namespace wxutil
  * greebo: Propagate the mousewheel event to the GL window below the cursor
  * not the one that might happen to have focus in MS Windows
  */
-class ScrollEventPropagationFilter : 
+class ScrollEventPropagationFilter final : 
 	public wxEventFilter
 {
+private:
+	bool _filterDisabled;
 public:
-	ScrollEventPropagationFilter()
+	ScrollEventPropagationFilter() :
+		_filterDisabled(false)
 	{
 		wxEvtHandler::AddFilter(this);
 	}
 
-	virtual ~ScrollEventPropagationFilter()
+	~ScrollEventPropagationFilter()
 	{
 		wxEvtHandler::RemoveFilter(this);
 	}
 
-	virtual int FilterEvent(wxEvent& ev)
+	int FilterEvent(wxEvent& ev) override
 	{
-		if (ev.GetEventType() != wxEVT_MOUSEWHEEL)
+		if (_filterDisabled || ev.GetEventType() != wxEVT_MOUSEWHEEL)
 		{
 			return Event_Skip;
 		}
 
 		wxPoint mousePos = wxGetMousePosition();
-		wxWindow* windowAtPoint = wxFindWindowAtPointer(mousePos);
+		wxWindow* windowAtPoint = wxFindWindowAtPoint(mousePos);
 
         if (windowAtPoint)
         {
             // Special case for wxDataViewCtrls. wxDataViewMainWindow is the one that is returned by
-            // the wxFindWindowAtPointer method, but it doesn't process the mouse wheel events, it's
+            // the wxFindWindowAtPoint method, but it doesn't process the mouse wheel events, it's
             // its parent wxDataViewCtrl taking care of that - so with this special knowledge...
             if (wxString(windowAtPoint->GetClassInfo()->GetClassName()) == "wxDataViewMainWindow")
             {
@@ -48,6 +52,9 @@ public:
 
             if (windowAtPoint)
             {
+				// Disable this filter before passing it to the target window
+				// it's possible that the wxEvtHandler of this window passes the event back to this filter
+				util::ScopedBoolLock lock(_filterDisabled);
                 return windowAtPoint->GetEventHandler()->ProcessEvent(ev) ? Event_Processed : Event_Skip;
             }
         }
