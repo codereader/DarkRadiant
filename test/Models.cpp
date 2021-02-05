90d10fc8fc54193e80d875c46ecf9a8661d35c8e
codereader <greebo@angua.at>
2021-02-05 18:03:36 +0100

    #4644: Add unit test to check the material fallback behaviour in the TDM game configuration

--- test/Models.cpp
+++ test/Models.cpp
@@ -25,4 +25,14 @@ TEST_F(ModelTest, AsePolyCount)
     EXPECT_EQ(model->getPolyCount(), 12);
 }
 
+// #4644: If the *BITMAP material cannot be resolved, the code should not fall back to *MATERIAL_NAME (in TDM/idTech4)
+TEST_F(ModelTest, BitmapFieldPreferredOverMaterialName)
+{
+    auto model = GlobalModelCache().getModel("models/missing_texture.ase");
+    EXPECT_TRUE(model);
+
+    EXPECT_EQ(model->getSurfaceCount(), 1);
+    EXPECT_EQ(model->getSurface(0).getDefaultMaterial(), "textures/doesnt_exist");
+}
+
 }
