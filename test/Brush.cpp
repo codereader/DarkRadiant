3bc8e301adc2eb0f2cb78c41b592918084f55940
Matthew Mott <orbweaver3d@gmail.com>
2021-03-14 13:54:11 +0000

    Merge FaceTest and FacePlaneTest into a single BrushTest suite

--- test/Brush.cpp
+++ test/Brush.cpp
@@ -1,25 +1,21 @@
 #include "RadiantTest.h"
 
 #include "ibrush.h"
-#include "icommandsystem.h"
+#include "imap.h"
 #include "iselection.h"
 #include "itransformable.h"
-#include "imap.h"
 #include "scenelib.h"
-#include "math/Matrix4.h"
 #include "math/Quaternion.h"
-#include "math/Vector3.h"
 
 namespace test
 {
 
-class FacePlaneTest :
-    public RadiantTest
+class BrushTest: public RadiantTest
 {
 protected:
     scene::INodePtr _brushNode;
 
-    void performTest(const std::function<bool(const IBrushNodePtr&)>& functor)
+    void testFacePlane(const std::function<bool(const IBrushNodePtr&)>& functor)
     {
         auto worldspawn = GlobalMapModule().findOrInsertWorldspawn();
 
@@ -44,9 +40,70 @@ protected:
     }
 };
 
-TEST_F(FacePlaneTest, FacePlaneRotateWithMatrix)
+inline bool isSane(const Matrix4& matrix)
 {
-    performTest([this](const IBrushNodePtr& brush)
+    for (std::size_t i = 0; i < 15; ++i)
+    {
+        if (std::isnan(matrix[i]) || std::isinf(matrix[i]))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+TEST_F(BrushTest, FitTextureWithZeroScale)
+{
+    auto worldspawn = GlobalMapModule().findOrInsertWorldspawn();
+
+    auto brushNode = GlobalBrushCreator().createBrush();
+    worldspawn->addChildNode(brushNode);
+
+    GlobalSelectionSystem().setSelectedAll(false);
+    Node_setSelected(brushNode, true);
+
+    const double size = 15;
+    Vector3 startPos(-size, -size, -size);
+    Vector3 endPos(size, size, size);
+    GlobalCommandSystem().executeCommand("ResizeSelectedBrushesToBounds",
+        startPos, endPos, std::string("shader"));
+
+    GlobalSelectionSystem().setSelectedAll(false);
+
+    auto brush = std::dynamic_pointer_cast<IBrushNode>(brushNode);
+    brush->getIBrush().evaluateBRep();
+
+    // Apply a texdef with a 0 scale component to the first face
+    ShiftScaleRotation scr;
+
+    scr.shift[0] = 0.0f;
+    scr.shift[1] = 0.0f;
+    scr.scale[0] = 1.0f;
+    scr.scale[1] = 0.0f; // zero scale
+    scr.rotate = 0.0f;
+
+    auto& face = brush->getIBrush().getFace(0);
+
+    face.setShiftScaleRotation(scr);
+
+    auto matrix = face.getProjectionMatrix();
+    EXPECT_FALSE(isSane(matrix)); // 5th matrix component is INF at the least
+
+    // Now fit the texture
+    face.fitTexture(1, 1);
+
+    matrix = face.getProjectionMatrix();
+
+    // The whole matrix should be sane now
+    EXPECT_TRUE(isSane(matrix)) << "Texture Projection Matrix is not sane after fitting";
+
+    scene::removeNodeFromParent(brushNode);
+}
+
+TEST_F(BrushTest, FacePlaneRotateWithMatrix)
+{
+    testFacePlane([this](const IBrushNodePtr& brush)
     {
         // Get the plane facing down the x axis and check it
         for (std::size_t i = 0; i < brush->getIBrush().getNumFaces(); ++i)
@@ -75,9 +132,9 @@ TEST_F(FacePlaneTest, FacePlaneRotateWithMatrix)
     });
 }
 
-TEST_F(FacePlaneTest, FacePlaneTranslate)
+TEST_F(BrushTest, FacePlaneTranslate)
 {
-    performTest([this](const IBrushNodePtr& brush)
+    testFacePlane([this](const IBrushNodePtr& brush)
     {
         // Get the plane facing down the x axis and check it
         for (std::size_t i = 0; i < brush->getIBrush().getNumFaces(); ++i)
