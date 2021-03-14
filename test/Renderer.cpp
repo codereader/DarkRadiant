2aa1cb28be4a54b82d71c2da307ee3e7cdd35fda
Matthew Mott <orbweaver3d@gmail.com>
2021-03-14 13:15:32 +0000

    Add test for light origin's effect on matrix
    Confirm that the light matrix reflects the position of the light in world
    space, since the matrix is intended to map world space coordinates to light
    texture coordinates.

--- test/Renderer.cpp
+++ test/Renderer.cpp
@@ -50,4 +50,32 @@ TEST_F(RendererTest, GetLightTextureTransform)
                                       0, 0, 0, 1));
 }
 
+TEST_F(RendererTest, LightMatrixInWorldSpace)
+{
+    auto light = createByClassName("light");
+    Entity* lightEnt = Node_getEntity(light);
+    ASSERT_TRUE(lightEnt);
+
+    // Set both an origin and a radius
+    const Vector3 ORIGIN(128, 64, -192);
+    const Vector3 RADIUS(32, 32, 32);
+    lightEnt->setKeyValue("origin", string::to_string(ORIGIN));
+    lightEnt->setKeyValue("light_radius", string::to_string(RADIUS));
+
+    // Get the texture matrix transform
+    const RendererLight& rLight = Node_getLightNode(light)->getRendererLight();
+    Matrix4 texMat = rLight.getLightTextureTransformation();
+
+    // Light matrix should subtract the origin scaled to the light bounds (twice
+    // the radius), then add the 0.5 offset to get to [0, 1].
+    const Vector3 BOUNDS = RADIUS * 2;
+    EXPECT_EQ(
+        texMat,
+        Matrix4::byRows(0.5 / RADIUS.x(), 0, 0, 0.5 - ORIGIN.x() / BOUNDS.x(),
+                        0, 0.5 / RADIUS.y(), 0, 0.5 - ORIGIN.y() / BOUNDS.y(),
+                        0, 0, 0.5 / RADIUS.z(), 0.5 - ORIGIN.z() / BOUNDS.z(),
+                        0, 0, 0, 1)
+    );
+}
+
 }
