4ce21a8b626ba1474c2b2eee1f82934b9d820076
Matthew Mott <orbweaver3d@gmail.com>
2021-03-21 20:34:30 +0000

    Add unit tests for Quaternion::createForX/Y/Z
    Confirm that the generated Quaternion contains the values derived from the
    half-angle, and is equal to that returned from createForMatrix() with the
    equivalent rotation matrix.

--- libs/math/Quaternion.h
+++ libs/math/Quaternion.h
@@ -42,7 +42,7 @@ public:
 	 */
 	static Quaternion createForUnitVectors(const Vector3& from, const Vector3& to);
 
-	/** 
+	/**
 	 * Constructs a rotation quaternion for the given euler angles.
 	 * Each component of the given vector refers to an angle (in degrees)
 	 * of one of the x-/y-/z-axis.
@@ -54,7 +54,7 @@ public:
 	 */
 	static Quaternion createForAxisAngle(const Vector3& axis, double angle);
 
-	/** 
+	/**
 	 * Constructs a rotation quaternion about a given axis.
 	 */
 	static Quaternion createForX(double angle);
@@ -227,3 +227,10 @@ inline Vector3 Quaternion::transformPoint(const Vector3& point) const
 
 const double c_half_sqrt2 = 0.70710678118654752440084436210485;
 const float c_half_sqrt2f = static_cast<float>(c_half_sqrt2);
+
+/// Stream insertion for Quaternion
+inline std::ostream& operator<< (std::ostream& s, const Quaternion& q)
+{
+    return s << "Quaternion(x=" << q.x() << ", y=" << q.y() << ", z=" << q.z()
+             << ", w=" << q.w() << ")";
+}
