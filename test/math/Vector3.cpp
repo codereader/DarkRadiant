412aeb07f3eda2735bae2e3b05c1a3c2a388eab1
Matthew Mott <orbweaver3d@gmail.com>
2021-04-01 21:13:43 +0100

    Add some simple tests for Vector3/4
    Test conversion between Vector3 and Vector3 in both directions (either gaining
    a W coordinate of 1 or losing the W coordinate), and calculating the length and
    squared length.

--- test/math/Vector3.cpp
+++ test/math/Vector3.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 
 #include "math/Vector3.h"
+#include "math/Vector4.h"
 
 namespace test
 {
@@ -12,6 +13,29 @@ TEST(MathTest, ConstructVector3)
     EXPECT_EQ(vec.x(), 1.0);
     EXPECT_EQ(vec.y(), 2.0);
     EXPECT_EQ(vec.z(), 3.5);
+}
+
+TEST(MathTest, PromoteVector3To4)
+{
+    Vector3 v3(8, 12, -5);
+    Vector4 v4(v3);
+
+    EXPECT_EQ(v4, Vector4(8, 12, -5, 1));
+}
+
+TEST(MathTest, DemoteVector4To3)
+{
+    Vector4 v4(1, -96, 0.125);
+    Vector3 v3 = v4.getVector3();
+
+    EXPECT_EQ(v3, Vector3(1, -96, 0.125));
+}
+
+TEST(MathTest, VectorLength)
+{
+    Vector3 v3(3, 4, 5);
+    EXPECT_EQ(v3.getLengthSquared(), 3*3 + 4*4 + 5*5);
+    EXPECT_NEAR(v3.getLength(), sqrt(3*3 + 4*4 + 5*5), 1E-6);
 }
 
 TEST(MathTest, Vector3IsPacked)
