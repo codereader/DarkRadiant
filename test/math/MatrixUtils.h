079e9bdd39c92a5730194168789de08949f2e3ca
codereader <greebo@angua.at>
2021-04-02 10:00:49 +0200

    #5567: Fix unit tests running into double equality problems

--- test/math/MatrixUtils.h
+++ test/math/MatrixUtils.h
@@ -5,29 +5,30 @@
 namespace test
 {
 
-// EXPECT that two matrices are close to each other (using GTest's own
-// EXPECT_DOUBLE_EQ)
+// EXPECT that two matrices are close to each other (using GTest's EXPECT_NEAR)
 inline void expectNear(const Matrix4& m1, const Matrix4& m2)
 {
-    EXPECT_DOUBLE_EQ(m1.xx(), m2.xx());
-    EXPECT_DOUBLE_EQ(m1.xy(), m2.xy());
-    EXPECT_DOUBLE_EQ(m1.xz(), m2.xz());
-    EXPECT_DOUBLE_EQ(m1.xw(), m2.xw());
+    constexpr double TestEpsilon = 0.0001;
 
-    EXPECT_DOUBLE_EQ(m1.yx(), m2.yx());
-    EXPECT_DOUBLE_EQ(m1.yy(), m2.yy());
-    EXPECT_DOUBLE_EQ(m1.yz(), m2.yz());
-    EXPECT_DOUBLE_EQ(m1.yw(), m2.yw());
+    EXPECT_NEAR(m1.xx(), m2.xx(), TestEpsilon);
+    EXPECT_NEAR(m1.xy(), m2.xy(), TestEpsilon);
+    EXPECT_NEAR(m1.xz(), m2.xz(), TestEpsilon);
+    EXPECT_NEAR(m1.xw(), m2.xw(), TestEpsilon);
 
-    EXPECT_DOUBLE_EQ(m1.zx(), m2.zx());
-    EXPECT_DOUBLE_EQ(m1.zy(), m2.zy());
-    EXPECT_DOUBLE_EQ(m1.zz(), m2.zz());
-    EXPECT_DOUBLE_EQ(m1.zw(), m2.zw());
+    EXPECT_NEAR(m1.yx(), m2.yx(), TestEpsilon);
+    EXPECT_NEAR(m1.yy(), m2.yy(), TestEpsilon);
+    EXPECT_NEAR(m1.yz(), m2.yz(), TestEpsilon);
+    EXPECT_NEAR(m1.yw(), m2.yw(), TestEpsilon);
 
-    EXPECT_DOUBLE_EQ(m1.tx(), m2.tx());
-    EXPECT_DOUBLE_EQ(m1.ty(), m2.ty());
-    EXPECT_DOUBLE_EQ(m1.tz(), m2.tz());
-    EXPECT_DOUBLE_EQ(m1.tw(), m2.tw());
+    EXPECT_NEAR(m1.zx(), m2.zx(), TestEpsilon);
+    EXPECT_NEAR(m1.zy(), m2.zy(), TestEpsilon);
+    EXPECT_NEAR(m1.zz(), m2.zz(), TestEpsilon);
+    EXPECT_NEAR(m1.zw(), m2.zw(), TestEpsilon);
+
+    EXPECT_NEAR(m1.tx(), m2.tx(), TestEpsilon);
+    EXPECT_NEAR(m1.ty(), m2.ty(), TestEpsilon);
+    EXPECT_NEAR(m1.tz(), m2.tz(), TestEpsilon);
+    EXPECT_NEAR(m1.tw(), m2.tw(), TestEpsilon);
 }
 
 }
