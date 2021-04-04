9cccf6b259f2e274336a87ebe5337c8a383226f3
codereader <greebo@angua.at>
2021-04-04 12:59:04 +0200

    #5576: Remove picomodel references from AseModelLoader.

--- radiantcore/model/import/AseModelLoader.cpp
+++ radiantcore/model/import/AseModelLoader.cpp
@@ -3,25 +3,13 @@
 #include <istream>
 #include "AseModel.h"
 
-#include "../picomodel/lib/picomodel.h"
 #include "os/path.h"
 #include "string/case_conv.h"
 
 #include "../StaticModel.h"
-#include "../picomodel/PicoModelLoader.h"
 
 namespace model
 {
-
-namespace
-{
-
-size_t picoInputStreamReam(void* inputStream, unsigned char* buffer, size_t length)
-{
-    return reinterpret_cast<InputStream*>(inputStream)->read(buffer, length);
-}
-
-}
 
 AseModelLoader::AseModelLoader() :
     ModelImporterBase("ASE")
@@ -40,44 +28,16 @@ IModelPtr AseModelLoader::loadModelFromPath(const std::string& path)
         return IModelPtr();
     }
 
-#if 0
-    auto* model = PicoModuleLoadModelStream(
-        &picoModuleASE,
-        &file->getInputStream(),
-        picoInputStreamReam,
-        file->size(),
-        0
-    );
-
-    // greebo: Check if the model load was successful
-    if (!model || model->numSurfaces == 0)
-    {
-        // Model is either NULL or has no surfaces, this must've failed
-        return IModelPtr();
-    }
-
-    auto surfaces = PicoModelLoader::CreateSurfaces(model, string::to_lower_copy(getExtension()));
-
-    auto modelObj = std::make_shared<StaticModel>(surfaces);
-    // Set the filename
-    modelObj->setFilename(os::getFilename(file->getName()));
-    modelObj->setModelPath(path);
-
-    PicoFreeModel(model);
-
-    return modelObj;
-#else 
     std::istream stream(&(file->getInputStream()));
     auto model = AseModel::CreateFromStream(stream);
 
-    auto modelObj = std::make_shared<StaticModel>(model->getSurfaces());
+    auto staticModel = std::make_shared<StaticModel>(model->getSurfaces());
     
     // Set the filename
-    modelObj->setFilename(os::getFilename(file->getName()));
-    modelObj->setModelPath(path);
+    staticModel->setFilename(os::getFilename(file->getName()));
+    staticModel->setModelPath(path);
 
-    return modelObj;
-#endif
+    return staticModel;
 }
 
 }
