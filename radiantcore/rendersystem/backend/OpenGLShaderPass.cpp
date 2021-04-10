codereader/DarkRadiant

commit e71c0c156d9bd1c5862bd709ed47a18df2d710c8

Remove unsafe Matrix4 pointer in TransformedRenderable
TransformedRenderable was storing a non-owning C pointer to each renderable
object's transform matrix, which is inherently unsafe if any object has
returned a matrix calculated on the fly (i.e. an rvalue) rather than by
reference. Storing a copy of the matrix might be very slightly slower but
avoids this potential source of bugs.

radiantcore/rendersystem/backend/OpenGLShaderPass.cpp
@@ -703,8 +703,8 @@ void OpenGLShaderPass::renderAllContained(const Renderables& renderables,
                                           const Vector3& viewer,
                                           std::size_t time)
 {
-    // Keep a pointer to the last transform matrix and render entity used
-    const Matrix4* transform = 0;
+    // Keep a copy of the last transform matrix and render entity used
+    Matrix4 transform = Matrix4::getIdentity();
 
     glPushMatrix();
 
@@ -713,17 +713,16 @@ void OpenGLShaderPass::renderAllContained(const Renderables& renderables,
     {
         // If the current iteration's transform matrix was different from the
         // last, apply it and store for the next iteration
-        if (transform == NULL ||
-            (transform != r.transform && !transform->isAffineEqual(*r.transform)))
+        if (!transform.isAffineEqual(r.transform))
         {
             transform = r.transform;
             glPopMatrix();
             glPushMatrix();
-            glMultMatrixd(*transform);
+            glMultMatrixd(transform);
 
             // Determine the face direction
             if (current.testRenderFlag(RENDER_CULLFACE)
-                && transform->getHandedness() == Matrix4::RIGHTHANDED)
+                && transform.getHandedness() == Matrix4::RIGHTHANDED)
             {
                 glFrontFace(GL_CW);
             }
@@ -738,7 +737,7 @@ void OpenGLShaderPass::renderAllContained(const Renderables& renderables,
         const RendererLight* light = r.light;
         if (current.glProgram && light)
         {
-            setUpLightingCalculation(current, light, viewer, *transform, time);
+            setUpLightingCalculation(current, light, viewer, transform, time);
         }
 
         // Render the renderable

radiantcore/rendersystem/backend/OpenGLShaderPass.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "math/Vector3.h"
+#include "math/Matrix4.h"
 #include "iglrender.h"
 
 #include <vector>
@@ -12,8 +13,8 @@ class OpenGLRenderable;
 class RendererLight;
 
 namespace render
-{ 
-    
+{
+
 class OpenGLShader;
 
 /**
@@ -40,7 +41,7 @@ class OpenGLShaderPass
     	const OpenGLRenderable* renderable;
 
     	// The modelview transform for this renderable
-    	const Matrix4* transform;
+    	const Matrix4 transform;
 
 		// The light falling on this obejct
     	const RendererLight* light;
@@ -54,7 +55,7 @@ class OpenGLShaderPass
 							  const RendererLight* l,
 							  const IRenderEntity* e)
 		: renderable(&r),
-		  transform(&t),
+		  transform(t),
 		  light(l),
 		  entity(e)
 		{}
@@ -63,7 +64,7 @@ class OpenGLShaderPass
 	// Vector of transformed renderables using this state
 	typedef std::vector<TransformedRenderable> Renderables;
 	Renderables _renderablesWithoutEntity;
-	
+
 	// Renderables sorted by RenderEntity
 	typedef std::map<const IRenderEntity*, Renderables> RenderablesByEntity;
 	RenderablesByEntity _renderables;

commit b68926f4dd9344c61a1c3505f2bbf6288db2074b

Fix a disappearing brush issue
Commit e71c0c156d9bd1c5862b introduced a rendering bug causing brushes to
disappear in some circumstances. The changes to renderAllContained() were
excessive - there is nothing wrong with using a Matrix4 pointer locally in this
method, since the pointed Matrix4 object is stored in each
TransformedRenderable which outlives the local pointer - and the modified code
failed to correctly process the first renderable (when transform == nullptr).

radiantcore/rendersystem/backend/OpenGLShaderPass.cpp
@@ -703,8 +703,8 @@ void OpenGLShaderPass::renderAllContained(const Renderables& renderables,
                                           const Vector3& viewer,
                                           std::size_t time)
 {
-    // Keep a copy of the last transform matrix and render entity used
-    Matrix4 transform = Matrix4::getIdentity();
+    // Keep a pointer to the last transform matrix used
+    const Matrix4* transform = nullptr;
 
     glPushMatrix();
 
@@ -713,16 +713,16 @@ void OpenGLShaderPass::renderAllContained(const Renderables& renderables,
     {
         // If the current iteration's transform matrix was different from the
         // last, apply it and store for the next iteration
-        if (!transform.isAffineEqual(r.transform))
+        if (!transform || !transform->isAffineEqual(r.transform))
         {
-            transform = r.transform;
+            transform = &r.transform;
             glPopMatrix();
             glPushMatrix();
-            glMultMatrixd(transform);
+            glMultMatrixd(*transform);
 
             // Determine the face direction
             if (current.testRenderFlag(RENDER_CULLFACE)
-                && transform.getHandedness() == Matrix4::RIGHTHANDED)
+                && transform->getHandedness() == Matrix4::RIGHTHANDED)
             {
                 glFrontFace(GL_CW);
             }
@@ -737,7 +737,7 @@ void OpenGLShaderPass::renderAllContained(const Renderables& renderables,
         const RendererLight* light = r.light;
         if (current.glProgram && light)
         {
-            setUpLightingCalculation(current, light, viewer, transform, time);
+            setUpLightingCalculation(current, light, viewer, *transform, time);
         }
 
         // Render the renderable