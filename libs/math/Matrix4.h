a75b266633b39895e42d46c3a4bd73dead56f6f2
codereader <greebo@angua.at>
2021-04-12 19:52:39 +0200

    Silence more compiler warnings occurring in Eigen

--- libs/math/Matrix4.h
+++ libs/math/Matrix4.h
@@ -12,7 +12,8 @@
 // Silence C++17 deprecation warnings from Eigen\src\Core\util\Meta.h(373), will be unsilenced at the bottom of this file
 #ifdef _MSC_VER
 #pragma warning(push)
-#pragma warning(disable : 4996) 
+#pragma warning(disable : 4996)
+#pragma warning(disable : 4127)
 #endif
 
 #include <Eigen/Geometry>
