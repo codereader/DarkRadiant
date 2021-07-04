318f5e207941c2f8b7dd9f911a32494b35e19dea
codereader <greebo@angua.at>
2021-07-04 13:16:17 +0200

    #5662: Add empty plugin project

---
+++ plugins/vcs/plugin.cpp
@@ -0,0 +1,14 @@
+#include "itextstream.h"
+
+#include "debugging/debugging.h"
+
+/**
+ * greebo: This is the module entry point which the main binary will look for.
+ * The symbol RegisterModule is called with the singleton ModuleRegistry as argument.
+ */
+extern "C" void DARKRADIANT_DLLEXPORT RegisterModule(IModuleRegistry & registry)
+{
+    module::performDefaultInitialisation(registry);
+
+    //registry.registerModule(script::ScriptingSystemPtr(new script::ScriptingSystem));
+}
