3e147ae519e5a44332aeaea37f3867b9381f0816
stgatilov <none@none>
2021-07-31 11:01:59 +0700

    Moved all module-related stuff to the bottom.

--- plugins/dm.gameconnection/GameConnection.cpp
+++ plugins/dm.gameconnection/GameConnection.cpp
@@ -235,116 +235,6 @@ GameConnection::~GameConnection() {
 
 //-------------------------------------------------------------
 
-const std::string& GameConnection::getName() const
-{
-    static std::string _name("GameConnection");
-    return _name;
-}
-
-const StringSet& GameConnection::getDependencies() const
-{
-    static StringSet _dependencies {
-        MODULE_CAMERA_MANAGER, MODULE_COMMANDSYSTEM, MODULE_MAP,
-        MODULE_SCENEGRAPH, MODULE_SELECTIONSYSTEM, MODULE_EVENTMANAGER,
-        MODULE_MENUMANAGER, MODULE_MAINFRAME
-    };
-    return _dependencies;
-}
-
-void GameConnection::initialiseModule(const IApplicationContext& ctx)
-{
-    // Construct toggles
-    _camSyncToggle = GlobalEventManager().addAdvancedToggle(
-        "GameConnectionToggleCameraSync",
-        [this](bool v) { return setCameraSyncEnabled(v); }
-    );
-    GlobalEventManager().addAdvancedToggle(
-        "GameConnectionToggleAutoMapReload",
-        [this](bool v) { return setAutoReloadMapEnabled(v); }
-    );
-    GlobalEventManager().addAdvancedToggle(
-        "GameConnectionToggleHotReload",
-        [this](bool v) { return setMapHotReload(v); }
-    );
-
-    // Add one-shot commands and associated toolbar buttons
-    GlobalCommandSystem().addCommand("GameConnectionBackSyncCamera",
-        [this](const cmd::ArgumentList&) { backSyncCamera(); });
-    _camSyncBackButton = GlobalEventManager().addCommand(
-        "GameConnectionBackSyncCamera", "GameConnectionBackSyncCamera", false
-    );
-    GlobalCommandSystem().addCommand("GameConnectionReloadMap",
-        [this](const cmd::ArgumentList&) { reloadMap(); });
-    GlobalCommandSystem().addCommand("GameConnectionUpdateMap",
-        [this](const cmd::ArgumentList&) { doUpdateMap(); });
-    GlobalCommandSystem().addCommand("GameConnectionPauseGame",
-        [this](const cmd::ArgumentList&) { togglePauseGame(); });
-    GlobalCommandSystem().addCommand("GameConnectionRespawnSelected",
-        [this](const cmd::ArgumentList&) { respawnSelectedEntities(); });
-
-    // Add menu items
-    ui::menu::IMenuManager& mm = GlobalMenuManager();
-    mm.insert("main/help", "connection", ui::menu::ItemType::Folder, _("Connection"), "", "");
-
-    mm.add("main/connection", "cameraSyncEnable", ui::menu::ItemType::Item,
-           _("Game position follows DarkRadiant camera"), "", "GameConnectionToggleCameraSync");
-    mm.add("main/connection", "backSyncCamera", ui::menu::ItemType::Item,
-           _("Move camera to current game position"), "", "GameConnectionBackSyncCamera");
-    mm.add("main/connection", "postCameraSep", ui::menu::ItemType::Separator);
-
-    mm.add("main/connection", "reloadMapAutoEnable", ui::menu::ItemType::Item,
-           _("Game reloads .map file on save"), "", "GameConnectionToggleAutoMapReload");
-    mm.add("main/connection", "reloadMap", ui::menu::ItemType::Item,
-           _("Tell game to reload .map file now"), "", "GameConnectionReloadMap");
-    mm.add("main/connection", "postMapFileSep", ui::menu::ItemType::Separator);
-
-    mm.add("main/connection", "mapHotReload", ui::menu::ItemType::Item,
-           _("Update entities on every change"), "", "GameConnectionToggleHotReload");
-    mm.add("main/connection", "updateMap", ui::menu::ItemType::Item,
-           _("Update entities now"), "", "GameConnectionUpdateMap");
-    mm.add("main/connection", "postHotReloadSep", ui::menu::ItemType::Separator);
-
-    mm.add("main/connection", "pauseGame", ui::menu::ItemType::Item,
-           _("Pause game"), "", "GameConnectionPauseGame");
-    mm.add("main/connection", "respawnSelected", ui::menu::ItemType::Item,
-           _("Respawn selected entities"), "", "GameConnectionRespawnSelected");
-
-    // Toolbar button(s)
-    GlobalMainFrame().signal_MainFrameConstructed().connect(
-        sigc::mem_fun(this, &GameConnection::addToolbarItems)
-    );
-}
-
-void GameConnection::shutdownModule()
-{
-    disconnect(true);
-}
-
-void GameConnection::addToolbarItems()
-{
-    wxToolBar* camTB = GlobalMainFrame().getToolbar(IMainFrame::Toolbar::CAMERA);
-    if (camTB)
-    {
-        // Separate GameConnection tools from regular camera tools
-        camTB->AddSeparator();
-
-        // Add toggles for the camera sync functions
-        auto camSyncT = camTB->AddTool(
-            wxID_ANY, "L", wxutil::GetLocalBitmap("CameraSync.png"),
-            _("Enable game camera sync with DarkRadiant camera"),
-            wxITEM_CHECK
-        );
-        _camSyncToggle->connectToolItem(camSyncT);
-        auto camSyncBackT = camTB->AddTool(
-            wxID_ANY, "B", wxutil::GetLocalBitmap("CameraSyncBack.png"),
-            _("Move camera to current game position")
-        );
-        _camSyncBackButton->connectToolItem(camSyncBackT);
-
-        camTB->Realize();
-    }
-}
-
 std::string GameConnection::composeConExecRequest(std::string consoleLine) {
     //remove trailing spaces/EOLs
     while (!consoleLine.empty() && isspace(consoleLine.back()))
@@ -606,6 +496,118 @@ void GameConnection::doUpdateMap()
     std::string response = executeRequest(actionPreamble("reloadmap-diff") + "content:\n" + diff);
     if (response.find("HotReload: SUCCESS") != std::string::npos)
         _mapObserver.clear();
+}
+
+//-------------------------------------------------------------
+
+const std::string& GameConnection::getName() const
+{
+    static std::string _name("GameConnection");
+    return _name;
+}
+
+const StringSet& GameConnection::getDependencies() const
+{
+    static StringSet _dependencies {
+        MODULE_CAMERA_MANAGER, MODULE_COMMANDSYSTEM, MODULE_MAP,
+        MODULE_SCENEGRAPH, MODULE_SELECTIONSYSTEM, MODULE_EVENTMANAGER,
+        MODULE_MENUMANAGER, MODULE_MAINFRAME
+    };
+    return _dependencies;
+}
+
+void GameConnection::initialiseModule(const IApplicationContext& ctx)
+{
+    // Construct toggles
+    _camSyncToggle = GlobalEventManager().addAdvancedToggle(
+        "GameConnectionToggleCameraSync",
+        [this](bool v) { return setCameraSyncEnabled(v); }
+    );
+    GlobalEventManager().addAdvancedToggle(
+        "GameConnectionToggleAutoMapReload",
+        [this](bool v) { return setAutoReloadMapEnabled(v); }
+    );
+    GlobalEventManager().addAdvancedToggle(
+        "GameConnectionToggleHotReload",
+        [this](bool v) { return setMapHotReload(v); }
+    );
+
+    // Add one-shot commands and associated toolbar buttons
+    GlobalCommandSystem().addCommand("GameConnectionBackSyncCamera",
+        [this](const cmd::ArgumentList&) { backSyncCamera(); });
+    _camSyncBackButton = GlobalEventManager().addCommand(
+        "GameConnectionBackSyncCamera", "GameConnectionBackSyncCamera", false
+    );
+    GlobalCommandSystem().addCommand("GameConnectionReloadMap",
+        [this](const cmd::ArgumentList&) { reloadMap(); });
+    GlobalCommandSystem().addCommand("GameConnectionUpdateMap",
+        [this](const cmd::ArgumentList&) { doUpdateMap(); });
+    GlobalCommandSystem().addCommand("GameConnectionPauseGame",
+        [this](const cmd::ArgumentList&) { togglePauseGame(); });
+    GlobalCommandSystem().addCommand("GameConnectionRespawnSelected",
+        [this](const cmd::ArgumentList&) { respawnSelectedEntities(); });
+
+    // Add menu items
+    ui::menu::IMenuManager& mm = GlobalMenuManager();
+    mm.insert("main/help", "connection", ui::menu::ItemType::Folder, _("Connection"), "", "");
+
+    mm.add("main/connection", "cameraSyncEnable", ui::menu::ItemType::Item,
+           _("Game position follows DarkRadiant camera"), "", "GameConnectionToggleCameraSync");
+    mm.add("main/connection", "backSyncCamera", ui::menu::ItemType::Item,
+           _("Move camera to current game position"), "", "GameConnectionBackSyncCamera");
+    mm.add("main/connection", "postCameraSep", ui::menu::ItemType::Separator);
+
+    mm.add("main/connection", "reloadMapAutoEnable", ui::menu::ItemType::Item,
+           _("Game reloads .map file on save"), "", "GameConnectionToggleAutoMapReload");
+    mm.add("main/connection", "reloadMap", ui::menu::ItemType::Item,
+           _("Tell game to reload .map file now"), "", "GameConnectionReloadMap");
+    mm.add("main/connection", "postMapFileSep", ui::menu::ItemType::Separator);
+
+    mm.add("main/connection", "mapHotReload", ui::menu::ItemType::Item,
+           _("Update entities on every change"), "", "GameConnectionToggleHotReload");
+    mm.add("main/connection", "updateMap", ui::menu::ItemType::Item,
+           _("Update entities now"), "", "GameConnectionUpdateMap");
+    mm.add("main/connection", "postHotReloadSep", ui::menu::ItemType::Separator);
+
+    mm.add("main/connection", "pauseGame", ui::menu::ItemType::Item,
+           _("Pause game"), "", "GameConnectionPauseGame");
+    mm.add("main/connection", "respawnSelected", ui::menu::ItemType::Item,
+           _("Respawn selected entities"), "", "GameConnectionRespawnSelected");
+
+    // Toolbar button(s)
+    GlobalMainFrame().signal_MainFrameConstructed().connect(
+        sigc::mem_fun(this, &GameConnection::addToolbarItems)
+    );
+}
+
+void GameConnection::shutdownModule()
+{
+    disconnect(true);
+}
+
+void GameConnection::addToolbarItems()
+{
+    wxToolBar* camTB = GlobalMainFrame().getToolbar(IMainFrame::Toolbar::CAMERA);
+    if (camTB)
+    {
+        // Separate GameConnection tools from regular camera tools
+        camTB->AddSeparator();
+
+        // Add toggles for the camera sync functions
+        auto camSyncT = camTB->AddTool(
+            wxID_ANY, "L", wxutil::GetLocalBitmap("CameraSync.png"),
+            _("Enable game camera sync with DarkRadiant camera"),
+            wxITEM_CHECK
+        );
+        _camSyncToggle->connectToolItem(camSyncT);
+        auto camSyncBackT = camTB->AddTool(
+            wxID_ANY, "B", wxutil::GetLocalBitmap("CameraSyncBack.png"),
+            _("Move camera to current game position")
+        );
+        _camSyncBackButton->connectToolItem(camSyncBackT);
+
+        camTB->Realize();
+    }
 }
 
 }
