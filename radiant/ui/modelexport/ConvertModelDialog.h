14b912e6634ac8febf099176038f553a7faf698e
codereader <greebo@angua.at>
2021-08-22 10:11:07 +0200

    #5721: Add some stub files for the new dialog

---
+++ radiant/ui/modelexport/ConvertModelDialog.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "icommandsystem.h"
+
+#include "wxutil/dialog/DialogBase.h"
+#include "wxutil/XmlResourceBasedWidget.h"
+
+namespace ui
+{
+
+class ConvertModelDialog :
+	public wxutil::DialogBase,
+	private wxutil::XmlResourceBasedWidget
+{
+public:
+	// Constructor
+    ConvertModelDialog(wxWindow* parent = nullptr);
+
+	static void ShowDialog(const cmd::ArgumentList& args);
+
+protected:
+	bool _onDeleteEvent() override;
+
+private:
+	void populateWindow();
+
+	void onExport(wxCommandEvent& ev);
+	void onCancel(wxCommandEvent& ev);
+	void onFormatSelection(wxCommandEvent& ev);
+
+	void saveOptionsToRegistry();
+	void handleFormatSelectionChange();
+};
+
+}
