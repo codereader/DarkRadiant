02dba8db4979b0e989d0cc064c6d7efd45a16a81
codereader <greebo@angua.at>
2021-10-12 03:52:08 +0200

    #5778: Improve dependency script, it now checks the ID of the asset attached to the windeps release, to detect whether an updated dependency package can be acquired.

--- tools/scripts/download_windeps.ps1
+++ tools/scripts/download_windeps.ps1
@@ -8,32 +8,44 @@ try
 
     $latest = $releases | sort created_at -Descending | select -First 1
 
-    $windeps_package = $latest.assets | ? { $_.name -eq "windeps.7z" }
+    $windeps_package = $latest.assets | ? { $_.name -eq "windeps_update.7z" }
 
     if ($null -eq $windeps_package)
     {
         throw "Could not find the windows dependencies package URL"
     }
 
-    Write-Host ("Downloading dependencies package ({0:0.0} MB)" -f ($windeps_package.size / 1024 / 1024))
+    $package_filename = [System.IO.Path]::GetFileNameWithoutExtension($windeps_package.name) + `
+                        $windeps_package.id + [System.IO.Path]::GetExtension($windeps_package.name)
 
-    $package_path = Join-Path "..\..\" $windeps_package.name
+    $target_folder = Join-Path (Get-Location) "..\..\"
+    $package_path = Join-Path $target_folder $package_filename
+
+    Write-Host ("Found package {1} ({0:0.0} MB)" -f ($windeps_package.size / 1024 / 1024), $windeps_package.name)
 
     if ((Get-ChildItem $package_path -ErrorAction SilentlyContinue).Length -eq $windeps_package.size)
     {
         Write-Host "Package has already been downloaded to $package_path"
     }
+    else
+    {
+        Write-Host ("Downloading dependencies package ({0:0.0} MB)" -f ($windeps_package.size / 1024 / 1024))
+        Invoke-WebRequest $windeps_package.browser_download_url -OutFile $package_path
+    }
 
-    Invoke-WebRequest $windeps_package.browser_download_url -OutFile $package_path
+    Write-Host ("Cleaning up old dependencies...")
 
-    $target_folder = Join-Path (Get-Location) "..\..\"
+    Remove-Item (Join-Path $target_folder "w32deps") -Recurse -ErrorAction SilentlyContinue
+    Remove-Item (Join-Path $target_folder "w64deps") -Recurse -ErrorAction SilentlyContinue
+    Remove-Item (Join-Path $target_folder "windeps") -Recurse -ErrorAction SilentlyContinue
+    
     Write-Host ("Extracting dependencies package...")
 
     & ..\7z\7za.exe x $package_path -o"$target_folder"
 
-    if ((Get-ChildItem "..\..\w32deps" -ErrorAction SilentlyContinue) -eq $null)
+    if ((Get-ChildItem "..\..\windeps" -ErrorAction SilentlyContinue) -eq $null)
     {
-        throw "w32deps folder still not present after download... cannot continue"
+        throw "windeps folder still not present after download... cannot continue"
     }
 }
 catch
