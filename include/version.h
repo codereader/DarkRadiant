91f58de26e3052d540cecd5c3cb5392669933a4b
codereader <greebo@angua.at>
2021-11-27 09:29:09 +0100

    Adjust VC++ build after merge

--- include/version.h
+++ include/version.h
@@ -2,7 +2,7 @@
 #include <config.h>
 #define RADIANT_VERSION PACKAGE_VERSION
 #else
-#define RADIANT_VERSION "2.14.0"
+#define RADIANT_VERSION "2.15.0pre1"
 #endif
 
 #define RADIANT_APPNAME "DarkRadiant"
