fcadac255a3c3da86a419558a46a2b74374a2dc7
codereader <greebo@angua.at>
2022-03-02 06:05:49 +0100

    #5853: Add a .prt file precedence scenario to the test resources

---
+++ test/Particles.cpp
@@ -0,0 +1,29 @@
+#include "RadiantTest.h"
+
+#include "iparticles.h"
+#include "iparticlestage.h"
+
+namespace test
+{
+
+using ParticlesTest = RadiantTest;
+
+// #5853: Two files define the same particle def:
+// 1) in VFS: particles/z_precedence.prt
+// 2) in PK4: test_particles.pk4/particles/precedence.prt
+// Even though the filesystem folder has higher priority, the file
+// in the PK4 should be parsed first, since the lexicographical order
+// is considered when looking for .prt files. That's why the
+// declaration in the filesystem should not take effect.
+TEST_F(ParticlesTest, ParticleFilenamePrecedence)
+{
+    auto decl = GlobalParticlesManager().getDefByName("precedencecheck");
+
+    EXPECT_TRUE(decl) << "Could not find the particle 'precedencecheck'";
+
+    // The decl in the PK4 should be processed first, the one in the filesystem just produces a warning
+    EXPECT_EQ(decl->getStage(0).getMaterialName(), "textures/common/caulk")
+        << "The particle using the caulk texture should have taken precedence";
+}
+
+}
