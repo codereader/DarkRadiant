ca7270018ae0dca3b212dae6412da99828e5b0fd
Matthew Mott <orbweaver3d@gmail.com>
2022-06-29 20:08:19 +0100

    #410: add a simple test for adding and executing a Command
    This simple command just increments a counter when it is called, so we
    can confirm that the registration and callback mechanism is working.

--- test/CommandSystem.cpp
+++ test/CommandSystem.cpp
@@ -11,4 +11,21 @@ TEST_F(CommandSystemTest, GetCommandSystem)
     EXPECT_EQ(mod.getName(), "CommandSystem");
 }
 
+TEST_F(CommandSystemTest, AddAndRunCommand)
+{
+    int runCount = 0;
+
+    // Add a command which just logs the number of times it is called
+    EXPECT_FALSE(GlobalCommandSystem().commandExists("testRunCount"));
+    GlobalCommandSystem().addCommand("testRunCount",
+                                     [&](const cmd::ArgumentList&) { ++runCount; });
+    EXPECT_TRUE(GlobalCommandSystem().commandExists("testRunCount"));
+
+    // Ensure that the call happens when we run the command
+    GlobalCommandSystem().executeCommand("testRunCount");
+    EXPECT_EQ(runCount, 1);
+    GlobalCommandSystem().executeCommand("testRunCount");
+    EXPECT_EQ(runCount, 2);
+}
+
 }
