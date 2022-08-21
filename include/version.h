6c49cc66bae17348ddcfd5127513a55099592902
codereader <greebo@angua.at>
2022-08-21 08:39:27 +0200

    Bump version for release

--- include/version.h
+++ include/version.h
@@ -4,7 +4,7 @@
 #include <config.h>
 #define RADIANT_VERSION PACKAGE_VERSION
 #else
-#define RADIANT_VERSION "3.1.0pre2"
+#define RADIANT_VERSION "3.1.0"
 #endif
 
 #define RADIANT_APPNAME "DarkRadiant"
