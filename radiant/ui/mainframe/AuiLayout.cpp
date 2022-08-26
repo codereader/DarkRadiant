8485c987cb9158ec8392e820802e1affb92c12d5
codereader <greebo@angua.at>
2022-08-26 08:02:55 +0200

    #6075: Improve wxAuiManager shutdown

--- radiant/ui/mainframe/AuiLayout.cpp
+++ radiant/ui/mainframe/AuiLayout.cpp
@@ -164,12 +164,14 @@ void AuiLayout::deactivate()
 
     GlobalGroupDialog().removePage("textures"); // do this after destroyWindow()
 
-    // Dispose of the managed window
-    _auiMgr.GetManagedWindow()->Destroy();
+    // Get a reference to the managed window, it might be cleared by UnInit()
+    auto managedWindow = _auiMgr.GetManagedWindow();
 
     // Unregister the AuiMgr from the event handlers of the managed window
     // otherwise we run into crashes during shutdown (#5586)
     _auiMgr.UnInit();
+
+    managedWindow->Destroy();
 }
 
 void AuiLayout::restoreStateFromRegistry()
