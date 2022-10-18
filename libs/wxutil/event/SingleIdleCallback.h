0cc217d48b6c41e6222130eea4f6a6f013f09c70
codereader <greebo@angua.at>
2022-10-18 21:34:28 +0200

    #6131: Change wxutil::SingleIdleCallback to maintain a wxEvtHandler member instead of deriving from it
    This should solve ambiguous type compilation problems in gcc in classes that derive
    from both wxWindow and wxutil::SingleIdleCallback.

--- libs/wxutil/event/SingleIdleCallback.h
+++ libs/wxutil/event/SingleIdleCallback.h
@@ -1,8 +1,7 @@
 #pragma once
 
-#include <wx/wxprec.h>
+#include <wx/event.h>
 #include <wx/app.h>
-#include <iostream>
 
 namespace wxutil
 {
@@ -15,19 +14,35 @@ namespace wxutil
  * whenever it is ready to receive an idle callback. Subsequently, it will have
  * its onIdle() method invoked once during the idle period.
  */
-class SingleIdleCallback :
-	public wxEvtHandler
+class SingleIdleCallback
 {
 private:
 	bool _callbackPending;
 
+    // To avoid inheriting from wxEvtHandler, we use composition
+    // maintaining an inner wxEvtHandler instance to be invoked on idle
+    class InternalEventHandler :
+        public wxEvtHandler
+    {
+    private:
+        SingleIdleCallback& _owner;
+    public:
+        InternalEventHandler(SingleIdleCallback& owner) :
+            _owner(owner)
+        {}
+
+        void _onIdle(wxIdleEvent&)
+        {
+            // Call the virtual function
+            _owner.handleIdleCallback();
+        }
+    } _evtHandler;
+
 private:
 
-	// Actual wxIdelEvent callback method, which invokes the child class'
-	// implementing method
-	void _onIdle(wxIdleEvent& ev)
+	void handleIdleCallback()
 	{
-		wxTheApp->Disconnect(wxEVT_IDLE, wxIdleEventHandler(wxutil::SingleIdleCallback::_onIdle), NULL, this);
+        wxTheApp->Unbind(wxEVT_IDLE, &InternalEventHandler::_onIdle, &_evtHandler);
 		
 		// Call the virtual function
 		onIdle();
@@ -38,15 +53,12 @@ private:
 	// Remove the callback
 	void deregisterCallback()
 	{
-		if (_callbackPending)
-		{
-			if (wxTheApp)
-			{
-				wxTheApp->Disconnect(wxEVT_IDLE, wxIdleEventHandler(wxutil::SingleIdleCallback::_onIdle), NULL, this);
-			}
+        if (!_callbackPending) return;
 
-			_callbackPending = false;
-		}
+        if (wxTheApp)
+        {
+            wxTheApp->Unbind(wxEVT_IDLE, &InternalEventHandler::_onIdle, &_evtHandler);
+        }
 	}
 
 protected:
@@ -62,7 +74,7 @@ protected:
 		if (wxTheApp)
 		{
 			_callbackPending = true;
-			wxTheApp->Connect(wxEVT_IDLE, wxIdleEventHandler(wxutil::SingleIdleCallback::_onIdle), NULL, this);
+            wxTheApp->Bind(wxEVT_IDLE, &InternalEventHandler::_onIdle, &_evtHandler);
 		}
 	}
 
@@ -98,9 +110,10 @@ protected:
 
 public:
 
-	SingleIdleCallback() :
-		_callbackPending(0)
-	{}
+    SingleIdleCallback() :
+        _callbackPending(false),
+        _evtHandler(*this)
+    {}
 
 	/**
 	 * Destructor. De-registers the callback from wx, so that the method will
