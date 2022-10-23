019025e95bb2674caeb416c3f25fbd3afc7a2422
codereader <greebo@angua.at>
2022-10-23 09:34:29 +0200

    #6131: Remove all other layouts, the only remaining one is the AuiLayout

--- radiant/ui/mainframe/MainFrameLayoutManager.cpp
+++ radiant/ui/mainframe/MainFrameLayoutManager.cpp
@@ -1,58 +1,51 @@
 #include "MainFrameLayoutManager.h"
 
 #include "itextstream.h"
-#include "ui/imainframe.h"
 #include "ui/igroupdialog.h"
 #include "icommandsystem.h"
 
 #include "module/StaticModule.h"
 
 #include "AuiLayout.h"
-#include "SplitPaneLayout.h"
-#include "RegularLayout.h"
-#include "EmbeddedLayout.h"
 
-namespace ui {
+namespace ui
+{
 
-IMainFrameLayoutPtr MainFrameLayoutManager::getLayout(const std::string& name) {
+IMainFrameLayoutPtr MainFrameLayoutManager::getLayout(const std::string& name)
+{
 	// Try to lookup that layout
-	LayoutMap::const_iterator found = _layouts.find(name);
+	auto found = _layouts.find(name);
 
-	if (found == _layouts.end()) {
+	if (found == _layouts.end())
+    {
 		rError() << "MainFrameLayoutManager: Could not find " << name << std::endl;
 		return IMainFrameLayoutPtr();
 	}
 
-	// Call the creation function and retrieve the layout
-	IMainFrameLayoutPtr layout = found->second();
-	return layout;
+	// Call the creation function and return the layout
+	return found->second();
 }
 
-// Register a layout by passing a name and a function to create such a layout.
 void MainFrameLayoutManager::registerLayout(
 	const std::string& name, const CreateMainFrameLayoutFunc& func)
 {
-    auto result = _layouts.insert({name, func});
-
     // Check if the insertion was successful
-	if (!result.second) {
-		rError() << "MainFrameLayoutManager: Layout "
-			<< name << " already registered." << std::endl;
-		return;
-	}
+    if (!_layouts.emplace(name, func).second)
+    {
+        rError() << "MainFrameLayoutManager: Layout " << name << " already registered." << std::endl;
+    }
 }
 
 void MainFrameLayoutManager::registerCommands()
 {
-	// remove all commands beforehand
-	_commands.clear();
+    // remove all commands beforehand
+    _commands.clear();
 
-	for (LayoutMap::const_iterator i = _layouts.begin(); i != _layouts.end(); ++i) {
-		// add a new command for each layout
-		_commands.push_back(
-			LayoutCommandPtr(new LayoutCommand(i->first))
-		);
-	}
+    // Add a new command for each layout
+    for (auto i = _layouts.begin(); i != _layouts.end(); ++i)
+    {
+        _commands.emplace_back(std::make_shared<LayoutCommand>(i->first));
+    }
 }
 
 // RegisterableModule implementation
@@ -64,28 +57,18 @@ const std::string& MainFrameLayoutManager::getName() const
 
 const StringSet& MainFrameLayoutManager::getDependencies() const
 {
-    static StringSet _dependencies
-    {
-        MODULE_GROUPDIALOG,
-        MODULE_COMMANDSYSTEM
-    };
-
+    static StringSet _dependencies { MODULE_COMMANDSYSTEM };
 	return _dependencies;
 }
 
 void MainFrameLayoutManager::initialiseModule(const IApplicationContext& ctx)
 {
-	// Register the default layouts
-	registerLayout(EMBEDDED_LAYOUT_NAME, EmbeddedLayout::CreateInstance);
+	// Register the default layout
 	registerLayout(AUI_LAYOUT_NAME, AuiLayout::CreateInstance);
-	registerLayout(SPLITPANE_LAYOUT_NAME, SplitPaneLayout::CreateInstance);
-	registerLayout(REGULAR_LAYOUT_NAME, RegularLayout::CreateRegularInstance);
-	registerLayout(REGULAR_LEFT_LAYOUT_NAME, RegularLayout::CreateRegularLeftInstance);
 }
 
-void MainFrameLayoutManager::shutdownModule() {
-	rMessage() << "MainFrameLayoutManager::shutdownModule called.\n";
-
+void MainFrameLayoutManager::shutdownModule()
+{
 	_commands.clear();
 	_layouts.clear();
 }
--- radiant/ui/mainframe/MainFrameLayoutManager.h
+++ radiant/ui/mainframe/MainFrameLayoutManager.h
@@ -7,33 +7,33 @@
 
 #include "LayoutCommand.h"
 
-namespace ui {
+namespace ui
+{
 
 class MainFrameLayoutManager :
 	public IMainFrameLayoutManager
 {
 	// A map associating names with creation functions
-	typedef std::map<std::string, CreateMainFrameLayoutFunc> LayoutMap;
-	LayoutMap _layouts;
+    std::map<std::string, CreateMainFrameLayoutFunc> _layouts;
 
 	// The container for the LayoutCommand objects.
 	std::vector<LayoutCommandPtr> _commands;
 
 public:
 	// Retrieves a layout with the given name. Returns NULL if not found.
-	IMainFrameLayoutPtr getLayout(const std::string& name);
+	IMainFrameLayoutPtr getLayout(const std::string& name) override;
 
 	// Register a layout by passing a name and a function to create such a layout.
-	void registerLayout(const std::string& name, const CreateMainFrameLayoutFunc& func);
+	void registerLayout(const std::string& name, const CreateMainFrameLayoutFunc& func) override;
 
 	// Adds all commands for all layouts registered so far
-	void registerCommands();
+	void registerCommands() override;
 
 	// RegisterableModule implementation
-	const std::string& getName() const;
-	const StringSet& getDependencies() const;
-	void initialiseModule(const IApplicationContext& ctx);
-	void shutdownModule();
+	const std::string& getName() const override;
+	const StringSet& getDependencies() const override;
+	void initialiseModule(const IApplicationContext& ctx) override;
+	void shutdownModule() override;
 };
 
 } // namespace ui
