bfd015bb6b5ffe183bc11941494a6a0fad87f137
Matthew Mott <orbweaver3d@gmail.com>
2022-10-25 20:00:37 +0100

    #5827: fix inconsistent tabs/spaces in EntityInspector.h

--- radiant/ui/einspector/EntityInspector.h
+++ radiant/ui/einspector/EntityInspector.h
@@ -50,32 +50,32 @@ class PropertyEditorFactory;
  * contains a method to return the current instance.
  */
 class EntityInspector final :
-	public IEntityInspector,
+    public IEntityInspector,
     public selection::SelectionSystem::Observer,
     public wxutil::SingleIdleCallback
 {
 public:
-	struct TreeColumns :
-		public wxutil::TreeModel::ColumnRecord
-	{
-		TreeColumns() :
-			name(add(wxutil::TreeModel::Column::IconText)),
-			value(add(wxutil::TreeModel::Column::String)),
-			isInherited(add(wxutil::TreeModel::Column::Boolean)),
-			isMultiValue(add(wxutil::TreeModel::Column::Boolean)),
-			booleanValue(add(wxutil::TreeModel::Column::Boolean)),
+    struct TreeColumns :
+        public wxutil::TreeModel::ColumnRecord
+    {
+        TreeColumns() :
+            name(add(wxutil::TreeModel::Column::IconText)),
+            value(add(wxutil::TreeModel::Column::String)),
+            isInherited(add(wxutil::TreeModel::Column::Boolean)),
+            isMultiValue(add(wxutil::TreeModel::Column::Boolean)),
+            booleanValue(add(wxutil::TreeModel::Column::Boolean)),
             oldValue(add(wxutil::TreeModel::Column::String)),
             newValue(add(wxutil::TreeModel::Column::String))
-		{}
+        {}
 
-		wxutil::TreeModel::Column name;
-		wxutil::TreeModel::Column value;
-		wxutil::TreeModel::Column isInherited;
-		wxutil::TreeModel::Column isMultiValue; // non-unique value (when multiple entities are selected)
-		wxutil::TreeModel::Column booleanValue;
-		wxutil::TreeModel::Column oldValue; // when displaying merge changes
-		wxutil::TreeModel::Column newValue; // when displaying merge changes
-	};
+        wxutil::TreeModel::Column name;
+        wxutil::TreeModel::Column value;
+        wxutil::TreeModel::Column isInherited;
+        wxutil::TreeModel::Column isMultiValue; // non-unique value (when multiple entities are selected)
+        wxutil::TreeModel::Column booleanValue;
+        wxutil::TreeModel::Column oldValue; // when displaying merge changes
+        wxutil::TreeModel::Column newValue; // when displaying merge changes
+    };
 
 private:
     std::unique_ptr<PropertyEditorFactory> _propertyEditorFactory;
@@ -84,23 +84,23 @@ private:
     std::unique_ptr<selection::CollectiveSpawnargs> _spawnargs;
     std::unique_ptr<selection::EntitySelection> _entitySelection;
 
-	// Main EntityInspector widget
-	wxPanel* _mainWidget = nullptr;
+    // Main EntityInspector widget
+    wxPanel* _mainWidget = nullptr;
 
-	// Frame to contain the Property Editor
-	wxPanel* _editorFrame = nullptr;
+    // Frame to contain the Property Editor
+    wxPanel* _editorFrame = nullptr;
 
-	// The checkbox for showing the eclass properties
-	wxCheckBox* _showInheritedCheckbox = nullptr;
-	wxCheckBox* _showHelpColumnCheckbox = nullptr;
+    // The checkbox for showing the eclass properties
+    wxCheckBox* _showInheritedCheckbox = nullptr;
+    wxCheckBox* _showHelpColumnCheckbox = nullptr;
 
-	// A label showing the primitive number
-	wxStaticText* _primitiveNumLabel = nullptr;
+    // A label showing the primitive number
+    wxStaticText* _primitiveNumLabel = nullptr;
 
     // View and model for the keyvalue list
-	wxutil::TreeView* _keyValueTreeView;
-	TreeColumns _columns;
-	wxutil::TreeModel::Ptr _kvStore;
+    wxutil::TreeView* _keyValueTreeView;
+    TreeColumns _columns;
+    wxutil::TreeModel::Ptr _kvStore;
     wxDataViewColumn* _booleanColumn = nullptr;
     wxDataViewColumn* _valueColumn = nullptr;
     wxDataViewColumn* _oldValueColumn = nullptr;
@@ -109,44 +109,44 @@ private:
     wxutil::Icon _emptyIcon;
 
     // Cache of wxDataViewItems pointing to keyvalue rows,
-	// so we can quickly find existing keys to change their values
-	typedef std::map<std::string, wxDataViewItem, string::ILess> TreeIterMap;
+    // so we can quickly find existing keys to change their values
+    typedef std::map<std::string, wxDataViewItem, string::ILess> TreeIterMap;
     TreeIterMap _keyValueIterMap;
 
-	// Key and value edit boxes. These remain available even for multiple entity
+    // Key and value edit boxes. These remain available even for multiple entity
     // selections.
-	wxTextCtrl* _keyEntry = nullptr;
-	wxTextCtrl* _valEntry = nullptr;
+    wxTextCtrl* _keyEntry = nullptr;
+    wxTextCtrl* _valEntry = nullptr;
     wxBitmapButton* _setButton = nullptr;
 
-	wxTextCtrl* _helpText;
+    wxTextCtrl* _helpText;
 
-	// The pane dividing the treeview and the property editors
-	wxSplitterWindow* _paned;
+    // The pane dividing the treeview and the property editors
+    wxSplitterWindow* _paned;
 
-	// An object tracking the divider position of the paned view
-	wxutil::PanedPosition _panedPosition;
+    // An object tracking the divider position of the paned view
+    wxutil::PanedPosition _panedPosition;
 
-	// Context menu
-	wxutil::PopupMenuPtr _contextMenu;
+    // Context menu
+    wxutil::PopupMenuPtr _contextMenu;
 
-	// Currently displayed PropertyEditor
-	IPropertyEditor::Ptr _currentPropertyEditor;
+    // Currently displayed PropertyEditor
+    IPropertyEditor::Ptr _currentPropertyEditor;
 
-	// The clipboard for spawnargs
-	typedef std::pair<std::string, std::string> KeyValuePair;
-	typedef std::vector<KeyValuePair> ClipBoard;
-	ClipBoard _clipboard;
+    // The clipboard for spawnargs
+    typedef std::pair<std::string, std::string> KeyValuePair;
+    typedef std::vector<KeyValuePair> ClipBoard;
+    ClipBoard _clipboard;
 
-	// Map of property names to type, mapped like this: regex => type
+    // Map of property names to type, mapped like this: regex => type
     std::map<std::string, std::string> _propertyTypes;
 
-	sigc::connection _defsReloadedHandler;
-	sigc::connection _mapEditModeChangedHandler;
+    sigc::connection _defsReloadedHandler;
+    sigc::connection _mapEditModeChangedHandler;
 
-	sigc::connection _keyValueAddedHandler;
-	sigc::connection _keyValueRemovedHandler;
-	sigc::connection _keyValueSetChangedHandler;
+    sigc::connection _keyValueAddedHandler;
+    sigc::connection _keyValueRemovedHandler;
+    sigc::connection _keyValueSetChangedHandler;
 
     // Maps the key names to a possible merge action that should be displayed
     std::map<std::string, scene::merge::IEntityKeyValueMergeAction::Ptr> _mergeActions;
@@ -160,48 +160,48 @@ private:
     bool canUpdateEntity();
 
     // Utility functions to construct the Gtk components
-	void construct();
+    void construct();
 
-	wxWindow* createPropertyEditorPane(wxWindow* parent); // bottom widget pane
+    wxWindow* createPropertyEditorPane(wxWindow* parent); // bottom widget pane
     wxWindow* createTreeViewPane(wxWindow* parent); // tree view for selecting attributes
     void createContextMenu();
 
-	// Utility function to retrieve the string selection from the given column in the
-	// list store
-	// getSelectedKey() returns an empty string if nothing is selected
-	std::string getSelectedKey();
-	std::string getListSelection(const wxutil::TreeModel::Column& col);
-	bool getListSelectionBool(const wxutil::TreeModel::Column& col);
+    // Utility function to retrieve the string selection from the given column in the
+    // list store
+    // getSelectedKey() returns an empty string if nothing is selected
+    std::string getSelectedKey();
+    std::string getListSelection(const wxutil::TreeModel::Column& col);
+    bool getListSelectionBool(const wxutil::TreeModel::Column& col);
 
-	// wxutil::PopupMenu callbacks
-	void _onAddKey();
-	void _onDeleteKey();
-	void _onCopyKey();
-	void _onCutKey();
-	void _onPasteKey();
-	void _onAcceptMergeAction();
-	void _onRejectMergeAction();
+    // wxutil::PopupMenu callbacks
+    void _onAddKey();
+    void _onDeleteKey();
+    void _onCopyKey();
+    void _onCutKey();
+    void _onPasteKey();
+    void _onAcceptMergeAction();
+    void _onRejectMergeAction();
 
-	bool _testAddKey();
-	bool _testDeleteKey();
-	bool _testCopyKey();
-	bool _testCutKey();
-	bool _testPasteKey();
-	bool _testAcceptMergeAction();
-	bool _testRejectMergeAction();
+    bool _testAddKey();
+    bool _testDeleteKey();
+    bool _testCopyKey();
+    bool _testCutKey();
+    bool _testPasteKey();
+    bool _testAcceptMergeAction();
+    bool _testRejectMergeAction();
 
-	// Shared by cut and delete keys
-	bool _testNonEmptyAndDeletableSelection();
-	bool isItemDeletable(const wxutil::TreeModel::Row& row);
-	bool isItemAffecedByMergeConflict(const wxutil::TreeModel::Row& row);
-	bool isItemAffecedByMergeOperation(const wxutil::TreeModel::Row& row);
+    // Shared by cut and delete keys
+    bool _testNonEmptyAndDeletableSelection();
+    bool isItemDeletable(const wxutil::TreeModel::Row& row);
+    bool isItemAffecedByMergeConflict(const wxutil::TreeModel::Row& row);
+    bool isItemAffecedByMergeOperation(const wxutil::TreeModel::Row& row);
 
     // callbacks
-	void _onEntryActivate(wxCommandEvent& ev);
-	void _onSetProperty(wxCommandEvent& ev);
-	void _onTreeViewSelectionChanged(wxDataViewEvent& ev);
-	void _onContextMenu(wxDataViewEvent& ev);
-	void _onDataViewItemChanged(wxDataViewEvent& ev);
+    void _onEntryActivate(wxCommandEvent& ev);
+    void _onSetProperty(wxCommandEvent& ev);
+    void _onTreeViewSelectionChanged(wxDataViewEvent& ev);
+    void _onContextMenu(wxDataViewEvent& ev);
+    void _onDataViewItemChanged(wxDataViewEvent& ev);
 
     void onKeyAdded(const std::string& key, const std::string& value);
     void onKeyRemoved(const std::string& key);
@@ -210,10 +210,10 @@ private:
     // Routines shared by onKeyAdded, onKeyRemoved and onKeyValueSetChanged
     void onKeyUpdatedCommon(const std::string& key);
 
-	void handleShowInheritedChanged();
-	void updateHelpTextPanel();
-	void updateHelpText(const wxutil::TreeModel::Row& row);
-	void setHelpText(const std::string& newText);
+    void handleShowInheritedChanged();
+    void updateHelpTextPanel();
+    void updateHelpText(const wxutil::TreeModel::Row& row);
+    void setHelpText(const std::string& newText);
     void updatePrimitiveNumber();
     static std::string  cleanInputString( const std::string& );
 
@@ -229,20 +229,20 @@ private:
     void handleKeyValueMergeAction(const scene::merge::IEntityKeyValueMergeAction::Ptr& mergeAction);
 
     // Set the keyval on all selected entities from the key and value textboxes
-	void setPropertyFromEntries();
+    void setPropertyFromEntries();
 
     // Applies the given key/value pair to the selection (works with multiple
     // selected entities)
     void applyKeyValueToSelection(const std::string& key,
                                   const std::string& value);
 
-	// Initialise the property lookup tables
-	void loadPropertyMap();
+    // Initialise the property lookup tables
+    void loadPropertyMap();
 
-	// Returns property type and option for the given entity key
-	std::string getPropertyTypeFromGame(const std::string& key);
-	std::string getPropertyTypeForKey(const std::string& key);
-	std::string getPropertyTypeForAttachmentKey(const std::string& key);
+    // Returns property type and option for the given entity key
+    std::string getPropertyTypeFromGame(const std::string& key);
+    std::string getPropertyTypeForKey(const std::string& key);
+    std::string getPropertyTypeForAttachmentKey(const std::string& key);
     void updateListedKeyTypes();
     void updateKeyType(wxutil::TreeModel::Row& row);
 
@@ -255,19 +255,19 @@ private:
     void refresh();
 
     void onMapEditModeChanged(IMap::EditMode mode);
-	void onDefsReloaded();
+    void onDefsReloaded();
 
 protected:
     // Called when the app is idle
     void onIdle() override;
 
 public:
-	// Get the main widget for packing
-	wxPanel* getWidget() override;
+    // Get the main widget for packing
+    wxPanel* getWidget() override;
 
-	/** greebo: Gets called by the RadiantSelectionSystem upon selection change.
-	 */
-	void selectionChanged(const scene::INodePtr& node, bool isComponent) override;
+    /** greebo: Gets called by the RadiantSelectionSystem upon selection change.
+     */
+    void selectionChanged(const scene::INodePtr& node, bool isComponent) override;
 
     void registerPropertyEditor(const std::string& key,
                                 const IPropertyEditor::CreationFunc& creator) override;
@@ -277,18 +277,18 @@ public:
     IPropertyEditorDialog::Ptr createDialog(const std::string& key) override;
     void unregisterPropertyEditorDialog(const std::string& key) override;
 
-	void onMainFrameConstructed();
-	void onMainFrameShuttingDown();
+    void onMainFrameConstructed();
+    void onMainFrameShuttingDown();
 
     void onKeyChange(const std::string& key, const std::string& value, bool isMultiValue = false);
 
-	// greebo: Tells the inspector to reload the window settings from the registry.
-	void restoreSettings() override;
+    // greebo: Tells the inspector to reload the window settings from the registry.
+    void restoreSettings() override;
 
-	/**
-	 * greebo: Static command target for toggling the Entity Inspector in the GroupDialog.
-	 */
-	static void toggle(const cmd::ArgumentList& args);
+    /**
+     * greebo: Static command target for toggling the Entity Inspector in the GroupDialog.
+     */
+    static void toggle(const cmd::ArgumentList& args);
 
     // RegisterableModule implementation
     const std::string& getName() const override;
