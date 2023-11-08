d54ed353ab8823b56701ff1b89a3e8229335b099
Matthew Mott <orbweaver3d@gmail.com>
2023-11-08 19:35:58 +0000

    Use modern event binding API for OrthoBackgroundPanel spinboxes

--- radiant/ui/overlay/OrthoBackgroundPanel.h
+++ radiant/ui/overlay/OrthoBackgroundPanel.h
@@ -11,37 +11,34 @@ class wxSpinCtrlDouble;
 namespace ui
 {
 
-/**
- * Panel to configure the background image overlay options for the ortho view.
- */
+/// Panel to configure the background image overlay options for the ortho view.
 class OrthoBackgroundPanel :
-	public wxutil::DockablePanel,
-	private wxutil::XmlResourceBasedWidget
+    public wxutil::DockablePanel,
+    private wxutil::XmlResourceBasedWidget
 {
-private:
-	wxSpinCtrlDouble* _spinScale;
-	wxSpinCtrlDouble* _spinHorizOffset;
-	wxSpinCtrlDouble* _spinVertOffset;
+    wxSpinCtrlDouble* _spinScale = nullptr;
+    wxSpinCtrlDouble* _spinHorizOffset = nullptr;
+    wxSpinCtrlDouble* _spinVertOffset = nullptr;
 
-	// TRUE, if a widget update is in progress (to avoid callback loops)
-	bool _callbackActive;
+    // TRUE, if a widget update is in progress (to avoid callback loops)
+    bool _callbackActive = false;
 
 public:
-	OrthoBackgroundPanel(wxWindow* parent);
+    OrthoBackgroundPanel(wxWindow* parent);
 
 private:
-	// Widget construction helpers
-	void setupDialog();
+    // Widget construction helpers
+    void setupDialog();
 
-	void initialiseWidgets();
-	void updateSensitivity();
+    void initialiseWidgets();
+    void updateSensitivity();
 
-	// callbacks
-	void _onFileSelection(wxFileDirPickerEvent& ev);
-	void _onToggleUseImage(wxCommandEvent& ev);
-	void _onOptionToggled(wxCommandEvent& ev);
-	void _onScrollChange(wxScrollEvent& ev);
-	void _onSpinChange(wxSpinDoubleEvent& ev);
+    // callbacks
+    void onFileSelection(wxFileDirPickerEvent& ev);
+    void onToggleUseImage(wxCommandEvent& ev);
+    void onOptionToggled(wxCommandEvent& ev);
+    void onScrollChange(wxScrollEvent& ev);
+    void onSpinChange(wxSpinDoubleEvent& ev);
 };
 
 }
