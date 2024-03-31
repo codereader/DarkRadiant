c5a8f73d41f65a5f68e584164f7ee713b0b13358
Alberto Salvia Novella <es20490446e@gmail.com>
2024-03-31 17:46:36 +0200

    Compatibility with libgit2 1.8+

--- plugins/vcs/Commit.h
+++ plugins/vcs/Commit.h
@@ -8,56 +8,60 @@
 namespace vcs
 {
 
-namespace git
-{
+	namespace git
+	{
 
-class Commit final
-{
-private:
-    git_commit* _commit;
+		class Commit final
+		{
+		private:
+			git_commit* _commit;
 
-public:
-    using Ptr = std::shared_ptr<Commit>;
+		public:
+			using Ptr = std::shared_ptr<Commit>;
 
-    Commit(git_commit* commit) :
-        _commit(commit)
-    {}
+			Commit(git_commit* commit) :
+			_commit(commit)
+			{}
 
-    ~Commit()
-    {
-        git_commit_free(_commit);
-    }
+			~Commit()
+			{
+				git_commit_free(_commit);
+			}
 
-    const git_oid* getOid() const
-    {
-        return git_commit_id(_commit);
-    }
+			const git_oid* getOid() const
+			{
+				return git_commit_id(_commit);
+			}
 
-    std::shared_ptr<Tree> getTree()
-    {
-        git_tree* tree;
-        auto error = git_commit_tree(&tree, _commit);
-        GitException::ThrowOnError(error);
+			std::shared_ptr<Tree> getTree()
+			{
+				git_tree* tree;
+				auto error = git_commit_tree(&tree, _commit);
+				GitException::ThrowOnError(error);
 
-        return std::make_shared<Tree>(tree);
-    }
+				return std::make_shared<Tree>(tree);
+			}
 
-    static Ptr LookupFromOid(git_repository* repository, git_oid* oid)
-    {
-        git_commit* commit;
+			static Ptr LookupFromOid(git_repository* repository, git_oid* oid)
+			{
+				git_commit* commit;
 
-        auto error = git_commit_lookup(&commit, repository, oid);
-        GitException::ThrowOnError(error);
+				auto error = git_commit_lookup(&commit, repository, oid);
+				GitException::ThrowOnError(error);
 
-        return std::make_shared<Commit>(commit);
-    }
+				return std::make_shared<Commit>(commit);
+			}
 
-    const git_commit* _get()
-    {
-        return _commit;
-    }
-};
+			#if ( LIBGIT2_VER_MAJOR > 1 ) || ( LIBGIT2_VER_MAJOR == 1 && LIBGIT2_VER_MINOR >= 8 )
+				git_commit* _get()
+			#else
+				const git_commit* _get()
+			#endif
+				{
+					return _commit;
+				}
+		};
 
-}
+	}
 
 }
