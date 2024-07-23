41763df9b3c60b151353344b9919c606a2b0349a
Matthew Mott <orbweaver3d@gmail.com>
2024-07-23 19:41:48 +0100

    #6514: remove obsolete code from PrefDialog
    Remove code relating to expanding nodes and adding sub-pages, since all
    pages are now at the same level. Remove function to show Game dialog,
    which is never called and would fail since the Game page is no longer
    part of this dialog. Switch from wxTreebook to wxListbook, which does
    not waste space for non-existent tree expansion controls (although it
    does center the page labels, which may not be desirable).

--- radiant/ui/prefdialog/PrefDialog.h
+++ radiant/ui/prefdialog/PrefDialog.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <map>
+#include <wx/bookctrl.h>
 #include "iradiant.h"
 #include "icommandsystem.h"
 
@@ -12,42 +13,34 @@ class wxTreebook;
 namespace ui
 {
 
-class PrefDialog :
-	public wxutil::DialogBase
+/// Dialog for editing user preferences
+class PrefDialog: public wxutil::DialogBase
 {
-	wxTreebook* _notebook = nullptr;
+    wxBookCtrlBase* _notebook = nullptr;
 
-	// Each notebook page is created and maintained by a PrefPage class
-	// Map the page path to its widget
-	typedef std::map<std::string, PrefPage*> PageMap;
-	PageMap _pages;
+    // Each notebook page is created and maintained by a PrefPage class
+    // Map the page path to its widget
+    typedef std::map<std::string, PrefPage*> PageMap;
+    PageMap _pages;
 
-	PrefDialog(wxWindow* parent);
+    PrefDialog(wxWindow* parent);
 
 public:
-	/** greebo: Runs the modal dialog
-	 */
-	static void ShowPrefDialog(const cmd::ArgumentList& args);
+    /** greebo: Makes sure that the dialog is visible.
+     *          (does nothing if the dialog is already on screen)
+     */
+    static void ShowDialog(const std::string& path = "");
 
-	/** greebo: Makes sure that the dialog is visible.
-	 * 			(does nothing if the dialog is already on screen)
-	 */
-	static void ShowDialog(const std::string& path = "");
-
-	/** greebo: The command target to show the Game settings preferences.
-	 */
-	static void ShowProjectSettings(const cmd::ArgumentList& args);
-
-	/** greebo: Displays the page with the specified path.
-	 *
-	 * @path: a string like "Settings/Patches"
-	 */
-	void showPage(const std::string& path);
+    /** greebo: Displays the page with the specified path.
+     *
+     * @path: a string like "Settings/Patches"
+     */
+    void showPage(const std::string& path);
 
 private:
-	void showModal(const std::string& requestedPage);
+    void showModal(const std::string& requestedPage);
 
-	void createPages();
+    void createPages();
 };
 
 } // namespace ui
